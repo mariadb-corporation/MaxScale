2359b2bb193baad6e6bcb8679e798b2330fad888
Massimiliano Pinto <massimiliano.pinto@skysql.com>
2013-04-15 18:16:02 +0200

    GPL Header added

--- protocol_1.0/skysql_client.h
+++ protocol_1.0/skysql_client.h
@@ -1,7 +1,25 @@
+/*
+This file is distributed as part of the SkySQL Gateway. It is free
+software: you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation,
+version 2.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51
+Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Copyright SkySQL Ab
+
+*/
+
 ////////////////////////////////////////
-// SKYSQL header file
+// SKYSQL mysql protocol header file
 // By Massimiliano Pinto 2012/2013
-// SkySQL AB
 ////////////////////////////////////////
 
 #include "ap_config.h"
