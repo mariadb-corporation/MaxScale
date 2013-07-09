e6b72a19009284f9ef7921b617e3b8f1ee993346
vraatikka <vilho.raatikka@skysql.com>
2013-07-09 12:33:25 +0300

    Added - rm *.so to clean to avoid accidentally use old libraries that were just lying around.

--- modules/monitor/depend.mk
+++ modules/monitor/depend.mk
@@ -1,16 +1,25 @@
 mysql_mon.o: mysql_mon.c /usr/include/stdio.h /usr/include/features.h \
- /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
- /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
- /usr/lib/gcc/x86_64-redhat-linux/4.4.6/include/stddef.h \
+ /usr/include/stdc-predef.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib64/gcc/x86_64-suse-linux/4.7/include/stddef.h \
  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
  /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
- /usr/lib/gcc/x86_64-redhat-linux/4.4.6/include/stdarg.h \
+ /usr/lib64/gcc/x86_64-suse-linux/4.7/include/stdarg.h \
  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
- ../../include/monitor.h mysqlmon.h ../../include/server.h \
- ../../include/dcb.h ../../include/spinlock.h ../../include/thread.h \
- /usr/include/pthread.h /usr/include/endian.h /usr/include/bits/endian.h \
- /usr/include/bits/byteswap.h /usr/include/sched.h /usr/include/time.h \
- /usr/include/bits/sched.h /usr/include/bits/time.h \
- /usr/include/xlocale.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/byteswap-16.h /usr/include/sys/types.h \
+ /usr/include/time.h /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/bits/time.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ ../../include/monitor.h ../../include/server.h ../../include/dcb.h \
+ ../../include/spinlock.h ../../include/thread.h /usr/include/pthread.h \
+ /usr/include/sched.h /usr/include/bits/sched.h /usr/include/xlocale.h \
  /usr/include/bits/setjmp.h ../../include/buffer.h \
- ../../include/gwbitmask.h
+ ../../include/gwbitmask.h mysqlmon.h /usr/include/mysql/mysql.h \
+ /usr/include/mysql/mysql_version.h /usr/include/mysql/mysql_com.h \
+ /usr/include/mysql/mysql_time.h /usr/include/mysql/my_list.h \
+ /usr/include/mysql/typelib.h /usr/include/mysql/my_alloc.h
---
+++ modules/routing/readwritesplit/depend.mk
@@ -0,0 +1,44 @@
+router.o: router.c /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/stdc-predef.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib64/gcc/x86_64-suse-linux/4.7/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib64/gcc/x86_64-suse-linux/4.7/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ ../../../include/router.h ../../../include/service.h /usr/include/time.h \
+ /usr/include/bits/time.h /usr/include/xlocale.h \
+ ../../../include/spinlock.h ../../../include/thread.h \
+ /usr/include/pthread.h /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/setjmp.h \
+ ../../../include/dcb.h ../../../include/buffer.h \
+ ../../../include/gwbitmask.h ../../../include/server.h \
+ ../../../include/session.h /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/sys/types.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/sigset.h \
+ /usr/include/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/mysql.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/mysql_version.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/mysql_com.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/mysql_time.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/my_list.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/typelib.h \
+ /home/raatikka/src/bazaar/shared/maria/5.5/include/my_alloc.h \
+ /home/raatikka/src/bazaar/shared/skygateway/utils/skygw_utils.h \
+ /home/raatikka/src/bazaar/shared/skygateway/utils/skygw_types.h \
+ /usr/include/math.h /usr/include/bits/huge_val.h \
+ /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+ /usr/include/bits/inf.h /usr/include/bits/nan.h \
+ /usr/include/bits/mathdef.h /usr/include/bits/mathcalls.h \
+ /home/raatikka/src/bazaar/shared/skygateway/utils/skygw_debug.h \
+ /usr/include/assert.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/getopt.h \
+ /home/raatikka/src/bazaar/shared/skygateway/log_manager/log_manager.h \
+ /home/raatikka/src/bazaar/shared/skygateway/query_classifier/query_classifier.h \
+ /home/raatikka/src/bazaar/shared/skygateway/query_classifier/../utils/skygw_utils.h
