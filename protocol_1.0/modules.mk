d2432ab3078c3c6501123394ab992583e53f64e1
Massimiliano Pinto <massimiliano.pinto@skysql.com>
2013-07-29 10:08:53 +0200

    Removed protocol_1.0, the Apache APR prototype

--- protocol_1.0/modules.mk
+++
@@ -1,4 +0,0 @@
-mod_skysql.la: mod_skysql.slo skysql_utils.slo skysql_backend.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_skysql.lo skysql_utils.lo skysql_backend.lo
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_skysql.la
