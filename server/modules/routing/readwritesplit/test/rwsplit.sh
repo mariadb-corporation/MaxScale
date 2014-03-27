9b1035751a2ccddaa4a1d967ba6847e01454341f
VilhoRaatikka <vilho.raatikka@skysql.com>
2014-03-27 17:17:01 +0200

    Made rwsplit test select_for_var_set.sql generic so that master server's id is filled in test.inc.

--- server/modules/routing/readwritesplit/test/rwsplit.sh
+++ server/modules/routing/readwritesplit/test/rwsplit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-NARGS=5
+NARGS=6
 TLOG=$1
 THOST=$2
 TPORT=$3
-TUSER=$4
-TPWD=$5
+TMASTER_ID=$4
+TUSER=$5
+TPWD=$6
 
 if [ $# != $NARGS ] ;
 then
@@ -12,7 +13,7 @@ echo""
 echo "Wrong number of arguments, gave "$#" but "$NARGS" is required"
 echo "" 
 echo "Usage :" 
-echo "        rwsplit.sh <log filename> <host> <port> <user> <password>"
+echo "        rwsplit.sh <log filename> <host> <port> <master id> <user> <password>"
 echo ""
 exit 1
 fi
@@ -40,7 +41,7 @@ fi
 
 # set a var via SELECT INTO @, get data from master, returning server-id: put master server-id value in TRETVAL
 TINPUT=select_for_var_set.sql
-TRETVAL=2
+TRETVAL=$TMASTER_ID
 
 a=`$RUNCMD < ./$TINPUT`
 if [ "$a" != "$TRETVAL" ]; then 
