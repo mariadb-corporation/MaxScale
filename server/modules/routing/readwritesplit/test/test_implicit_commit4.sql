d0aeeb13cce876627f4a16ff59b5a901f2686bc8
Hartmut Holzgraefe <hartmut@php.net>
2014-05-21 18:46:56 +0200

    SQL comments require whitespace between "--" and comment text

--- server/modules/routing/readwritesplit/test/test_implicit_commit4.sql
+++ server/modules/routing/readwritesplit/test/test_implicit_commit4.sql
@@ -4,6 +4,6 @@ SET autocommit=0;
 BEGIN;
 CREATE TEMPORARY TABLE T1 (id integer); -- NO implicit commit
 SELECT (@@server_id) INTO @a;
-SELECT @a; --should read from master
+SELECT @a; -- should read from master
 DROP TABLE IF EXISTS T1;
 COMMIT;
