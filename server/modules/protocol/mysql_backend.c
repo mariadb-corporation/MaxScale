0cb1eb842a398e7ef3b7ddabca7f67b7b36bcf66
VilhoRaatikka <vilho.raatikka@skysql.com>
2014-05-28 14:03:59 +0300

    Bug #438, http://bugs.skysql.com/show_bug.cgi?id=438 try to complete the fix. dcb->authlock was double-freed.

--- server/modules/protocol/mysql_backend.c
+++ server/modules/protocol/mysql_backend.c
@@ -324,7 +324,7 @@ static int gw_read_backend_event(DCB *dcb) {
                                         if (session->client->session == NULL)
                                         {
                                                 rc = 1;
-                                                goto return_with_lock;
+                                                goto return_rc;
                                         }
                                         usleep(1);
                                 }
