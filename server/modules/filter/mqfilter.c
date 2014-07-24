4ff501fa4182c4b3a7d7473462f57d7cd5964fae
Markus Makela <markus.makela@skysql.com>
2014-07-24 12:49:33 +0300

    queries and backend replies and now matched by unique identifiers

--- server/modules/filter/mqfilter.c
+++ server/modules/filter/mqfilter.c
@@ -50,6 +50,7 @@
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
+#include <atomic.h>
 #include <amqp.h>
 #include <amqp_framing.h>
 #include <amqp_tcp_socket.h>
@@ -64,6 +65,7 @@ MODULE_INFO 	info = {
 };
 
 static char *version_str = "V1.0.0";
+static int uid_gen;
 
 /*
  * The filter entry points
@@ -85,9 +87,9 @@ static FILTER_OBJECT MyObject = {
   closeSession,
   freeSession,
   setDownstream,
-  setUpstream, /**Upstream not implemented*/
+  setUpstream, 
   routeQuery,
-  clientReply, /**Client reply not implemented*/
+  clientReply,
   diagnostic,
 };
 
@@ -132,6 +134,7 @@ typedef struct {
  *
  */
 typedef struct {
+  char*		uid;
   DOWNSTREAM	down;
   UPSTREAM	up;
   amqp_connection_state_t conn; /**The connection object*/
@@ -191,7 +194,7 @@ createInstance(char **options, FILTER_PARAMETER **params)
       (my_instance->rconn_lock = malloc(sizeof(SPINLOCK))))
     {
       spinlock_init(my_instance->rconn_lock);
-
+      uid_gen = 0;
       my_instance->channel = 1;
       my_instance->last_rconn = time(NULL);
       my_instance->conn_stat = AMQP_STATUS_OK;
@@ -380,6 +383,50 @@ init_conn(MQ_INSTANCE *my_instance, MQ_SESSION *my_session)
 }
 
 /**
+ * Declares a persistent, non-exclusive and non-passive queue that
+ * auto-deletes after all the messages have been consumed.
+ * @param my_session MQ_SESSION instance used to declare the queue
+ * @param qname Name of the queue to be declared
+ * @return Returns 0 if an error occurred, 1 if successful
+ */
+int declareQueue(MQ_INSTANCE	*my_instance, MQ_SESSION* my_session, char* qname)
+{
+  int success = 1;
+  amqp_rpc_reply_t reply;
+
+  spinlock_acquire(my_instance->rconn_lock);
+
+  amqp_queue_declare(my_session->conn,my_session->channel,
+		     amqp_cstring_bytes(qname),
+		     0, 1, 0, 1,
+		     amqp_empty_table);
+  reply = amqp_get_rpc_reply(my_session->conn);  
+  if(reply.reply_type != AMQP_RESPONSE_NORMAL){
+    success = 0;
+    skygw_log_write(LOGFILE_ERROR,
+		    "Error : Queue declaration failed.");
+   
+  }
+
+ 
+  amqp_queue_bind(my_session->conn,my_session->channel,
+		  amqp_cstring_bytes(qname),
+		  amqp_cstring_bytes(my_instance->exchange),
+		  amqp_cstring_bytes(my_session->uid),
+		  amqp_empty_table);
+  reply = amqp_get_rpc_reply(my_session->conn);  
+  if(reply.reply_type != AMQP_RESPONSE_NORMAL){
+    success = 0;
+    skygw_log_write(LOGFILE_ERROR,
+		    "Error : Failed to bind queue to exchange.");
+   
+  }
+    spinlock_release(my_instance->rconn_lock);        
+    return success;
+}
+
+
+/**
  * Associate a new session with this instance of the filter and opens
  * a connection to the server and prepares the exchange and the queue for use.
  *
@@ -403,6 +450,7 @@ newSession(FILTER *instance, SESSION *session)
     spinlock_acquire(my_instance->rconn_lock);  
     init_conn(my_instance,my_session);
     my_session->was_query = 0;
+    my_session->uid = NULL;
     spinlock_release(my_instance->rconn_lock);      
   }
 
@@ -438,6 +486,7 @@ static void
 freeSession(FILTER *instance, void *session)
 {
   MQ_SESSION	*my_session = (MQ_SESSION *)session;
+  free(my_session->uid);
   free(my_session);
   return;
 }
@@ -462,6 +511,32 @@ static	void	setUpstream(FILTER *instance, void *session, UPSTREAM *upstream)
   MQ_SESSION	*my_session = (MQ_SESSION *)session;
   my_session->up = *upstream;
 }
+
+
+/**
+ * Generates a unique 8-character key using an unique unsigned long
+ * split into 4-bit parts. The caller is responsible for the allocated memory.
+ * @return A newly allocated unique null-terminated character string.
+ */
+char* genkey()
+{
+  int i;
+  unsigned long uid = 0;
+  char hash[9], ch;
+
+  uid |= atomic_add(&uid_gen,1);
+  uid |= (atomic_add(&uid_gen,1) << 16);
+
+  for(i = 0;i<8;i++){
+    ch = 'a';
+    ch += (0x0f & (uid >> 4*i));
+    hash[i] = ch;
+  }
+  hash[8] = '\0';
+  return strdup(hash);
+}
+
+
 
 /**
  * The routeQuery entry point. This is passed the query buffer
@@ -507,14 +582,29 @@ routeQuery(FILTER *instance, void *session, GWBUF *queue)
   }
   spinlock_release(my_instance->rconn_lock);
 
+  if(modutil_is_SQL(queue)){
+
+    if(my_session->uid == NULL){
+      my_session->uid = genkey();
+    }
+
+    if(!my_session->uid){
+      skygw_log_write(LOGFILE_ERROR,"Error : Out of memory.");
+    }
+  }
+
   if (err_code == AMQP_STATUS_OK){
 
     if(modutil_extract_SQL(queue, &ptr, &length)){
+
       my_session->was_query = 1;
 
-      prop._flags = AMQP_BASIC_CONTENT_TYPE_FLAG|AMQP_BASIC_DELIVERY_MODE_FLAG;
+      prop._flags = AMQP_BASIC_CONTENT_TYPE_FLAG|
+	AMQP_BASIC_DELIVERY_MODE_FLAG|
+	AMQP_BASIC_CORRELATION_ID_FLAG;
       prop.content_type = amqp_cstring_bytes("text/plain");
       prop.delivery_mode = AMQP_DELIVERY_PERSISTENT;
+      prop.correlation_id = amqp_cstring_bytes(my_session->uid);
       
       gettimeofday(&tv, NULL);
       localtime_r(&tv.tv_sec, &t);
@@ -529,7 +619,7 @@ routeQuery(FILTER *instance, void *session, GWBUF *queue)
       }
       strcpy(combined,t_buf);
       strncat(combined,ptr,length);
-
+      
       if((err_code = amqp_basic_publish(my_session->conn,my_session->channel,
 					amqp_cstring_bytes(my_instance->exchange),
 					amqp_cstring_bytes(my_instance->key),
@@ -553,6 +643,11 @@ routeQuery(FILTER *instance, void *session, GWBUF *queue)
 				     my_session->down.session, queue);
 }
 
+/**
+ * Calculated the length of the SQL packet.
+ * @param c Pointer to the first byte of a packet
+ * @return The length of the packet
+ */
 unsigned int pktlen(void* c)
 {
   unsigned char* ptr = (unsigned char*)c;
@@ -562,9 +657,15 @@ unsigned int pktlen(void* c)
   return plen;
 }
 
-unsigned int leitoi(void* c)
+/**
+ * Converts a length-encoded integer to an unsigned integer as defined by the
+ * MySQL manual.
+ * @param c Pointer to the first byte of the length-encoded integer
+ * @return The value converted to a standard unsigned integer
+ */
+unsigned int leitoi(unsigned char* c)
 {
-  unsigned char* ptr = (unsigned char*)c;
+  unsigned char* ptr = c;
   unsigned int sz = *ptr;
   if(*ptr  < 0xfb) return sz;
   if(*ptr == 0xfc){
@@ -588,11 +689,35 @@ unsigned int leitoi(void* c)
   return sz;
 }
 
+/**
+ * Converts a length-encoded integer into a standard unsigned integer 
+ * and advances the pointer to the next unrelated byte
+ */
+unsigned int consume_leitoi(unsigned char** c)
+{
+  unsigned int rval = leitoi(*c);
+  if(**c == 0xfc){
+    *c += 3;
+  }else if(**c == 0xfd){
+    *c += 4;
+  }else if(**c == 0xfe){
+    *c += 9;
+  }else{
+    *c += 1;
+  }
+  return rval;
+}
+/**
+ *Checks whether a packet in a GWBUF is an EOF packet.
+ * @param p Pointer to the first byte in the buffer
+ * @return 1 if the packet is an EOF packet and 0 if it is not
+ */
 unsigned int is_eof(void* p)
 {
   unsigned char* ptr = (unsigned char*) p;
   return *(ptr) == 0x05 && *(ptr + 1) == 0x00 &&  *(ptr + 2) == 0x00 &&  *(ptr + 4) == 0xfe;
 }
+
 static int clientReply(FILTER* instance, void *session, GWBUF *reply)
 {
   MQ_SESSION		*my_session = (MQ_SESSION *)session;
@@ -626,13 +751,15 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
   spinlock_release(my_instance->rconn_lock);
 
   if (err_code == AMQP_STATUS_OK && my_session->was_query){
-    int packet_ok = 0;
+    int packet_ok = 0, was_last = 0;
     my_session->was_query = 0;
 
     if(pkt_len > 0){
-      prop._flags = AMQP_BASIC_CONTENT_TYPE_FLAG|AMQP_BASIC_DELIVERY_MODE_FLAG;
+      prop._flags = AMQP_BASIC_CONTENT_TYPE_FLAG|
+	AMQP_BASIC_DELIVERY_MODE_FLAG|AMQP_BASIC_CORRELATION_ID_FLAG;
       prop.content_type = amqp_cstring_bytes("text/plain");
       prop.delivery_mode = AMQP_DELIVERY_PERSISTENT;
+      prop.correlation_id = amqp_cstring_bytes(my_session->uid);
       
       if(!(combined = calloc(GWBUF_LENGTH(reply) + 256,sizeof(char)))){
 	skygw_log_write_flush(LOGFILE_ERROR,
@@ -641,6 +768,7 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
 
       gettimeofday(&tv, NULL);
       localtime_r(&tv.tv_sec, &t);
+      memset(t_buf,0,40);
       sprintf(t_buf, "%02d:%02d:%02d.%-3d %d/%02d/%d, ",
 	      t.tm_hour, t.tm_min, t.tm_sec, (int)(tv.tv_usec / 1000),
 	      t.tm_mday, t.tm_mon + 1, 1900 + t.tm_year);
@@ -651,21 +779,33 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
       offset += strnlen(t_buf,40);
 
       if(*(reply->sbuf->data + 4) == 0x00){ /**OK packet*/
-
-	sprintf(combined + offset,"OK - affected_rows: %d "
-		" last_insert_id: %d "
-		" status_flags: %x %x "
-		" warnings: %x %x ",
-		leitoi(reply->sbuf->data + 5),leitoi(reply->sbuf->data + 6),
-		reply->sbuf->data[7],reply->sbuf->data[8],
-		reply->sbuf->data[9],reply->sbuf->data[10]);
+	unsigned int aff_rows, l_id, s_flg = 0, wrn = 0, st_inf;
+	unsigned char *ptr = (unsigned char*)(reply->sbuf->data + 5);
+	pkt_len = pktlen(reply->sbuf->data);
+	aff_rows = consume_leitoi(&ptr);
+	l_id = consume_leitoi(&ptr);
+	s_flg |= *ptr++;
+	s_flg |= (*ptr++ << 8);
+	wrn |= *ptr++;
+	wrn |= (*ptr++ << 8);
+	st_inf = consume_leitoi(&ptr);
+	pkt_len -= (ptr - (reply->sbuf->data + 4));
+	sprintf(combined + offset,"OK - affected_rows: %d\n"
+		" last_insert_id: %d\n"
+		" status_flags: %4.x\n"
+		" warnings: %4.x\n"
+		" state_info: %d\n"
+		" message: %*s",
+		aff_rows,l_id,s_flg,wrn,st_inf,pkt_len,ptr);
 	packet_ok = 1;
+	was_last = 1;
 
       }else if(*(reply->sbuf->data + 4) == 0xff){ /**ERR packet*/
 
 	sprintf(combined + offset,"ERROR - message: %s",
 		reply->sbuf->data + 13);
 	packet_ok = 1;
+	was_last = 1;
     
       }else if(*(reply->sbuf->data + 4) == 0xfb){ /**LOCAL_INFILE request packet*/
       
@@ -673,6 +813,7 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
 	strcpy(combined + offset,"LOCAL_INFILE: ");
 	strncat(combined + offset,(const char*)rset+5,pktlen(rset));
 	packet_ok = 1;
+	was_last = 1;
       
       }else{ /**Result set*/
       
@@ -680,11 +821,12 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
 	char		*tmp;
 	unsigned int	col_cnt = (unsigned int)*(rset+4);
 
-	tmp = malloc(sizeof(char)*256);
+	tmp = calloc(256,sizeof(char));
 	sprintf(tmp,"Columns: %d",col_cnt);
-	memcpy(combined + offset,tmp,strnlen(tmp,256));
+	memcpy(combined + offset,tmp,strnlen(tmp,256) + 1);
 	free(tmp);
 	packet_ok = 1;
+	was_last = 1;
       }
       if(packet_ok){
 	if((err_code = amqp_basic_publish(my_session->conn,my_session->channel,
@@ -700,6 +842,13 @@ static int clientReply(FILTER* instance, void *session, GWBUF *reply)
 				"Error : Failed to publish message to MQRabbit server: "
 				"%s",amqp_error_string2(err_code));
 	
+	}else if(was_last){
+
+	  /**Successful reply received and sent, releasing uid*/
+	  
+	  free(my_session->uid);
+	  my_session->uid = NULL;
+
 	} 
       }
       free(combined);
