f0e75683c93608a90effb118c101e5c96de425e7
Markus Makela <markus.makela@skysql.com>
2014-08-05 20:35:34 +0300

    Split the tests into separate files

--- server/modules/routing/readwritesplit/test/test_hints/rwsplit_hints.sh
+++ server/modules/routing/readwritesplit/test/test_hints/rwsplit_hints.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-NARGS=6
+NARGS=7
 TLOG=$1
 THOST=$2
 TPORT=$3
 TMASTER_ID=$4
 TUSER=$5
 TPWD=$6
+TESTINPUT=$7
 
 if [ $# != $NARGS ] ;
 then
@@ -13,24 +14,34 @@ echo""
 echo "Wrong number of arguments, gave "$#" but "$NARGS" is required"
 echo "" 
 echo "Usage :" 
-echo "        rwsplit.sh <log filename> <host> <port> <master id> <user> <password>"
+echo "        rwsplit.sh <log filename> <host> <port> <master id> <user> <password> <test file>"
 echo ""
 exit 1
 fi
 
-TESTINPUT=hints.txt
-QUERY="select @@server_id;"
-RUNCMD=mysql\ --host=$THOST\ -P$TPORT\ -u$TUSER\ -p$TPWD\ --unbuffered=true\ --disable-reconnect\ --silent\
+
+RUNCMD=mysql\ --host=$THOST\ -P$TPORT\ -u$TUSER\ -p$TPWD\ --unbuffered=true\ --disable-reconnect\ --silent\ --comment
+i=0
 
 while read -r LINE
 do
-TINPUT=`echo "$LINE"|awk '{split($0,a,":");print a[1]}'`
-TRETVAL=`echo "$LINE"|awk '{split($0,a,":");print a[2]}'`
-a=`$RUNCMD -e"$QUERY$TINPUT"`
-if [ "$a" != "$TRETVAL" ]; then 
-        echo "$TINPUT FAILED, return value $a when $TRETVAL was expected">>$TLOG; 
-else 
-        echo "$TINPUT PASSED">>$TLOG ; 
-fi
-
+TINPUT[$i]=`echo "$LINE"|awk '{split($0,a,":");print a[1]}'`
+TRETVAL[$i]=`echo "$LINE"|awk '{split($0,a,":");print a[2]}'`
+echo "${TINPUT[i]}" >> $TESTINPUT.sql
+i=$((i+1))
 done < $TESTINPUT
+
+`$RUNCMD < $TESTINPUT.sql > $TESTINPUT.output`
+
+x=0
+
+while read -r TOUTPUT
+do
+if [ "$TOUTPUT" != "${TRETVAL[x]}" ]; then 
+        echo "${TINPUT[x]} FAILED, return value $TOUTPUT when ${TRETVAL[x]} was expected">>$TLOG; 
+else 
+        echo "${TINPUT[x]} PASSED">>$TLOG ; 
+fi
+x=$((x+1))
+done < $TESTINPUT.output
+
