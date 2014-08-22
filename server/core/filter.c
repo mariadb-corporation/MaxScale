3476558f526d2e788b42445169ba8d747241838a
Mark Riddoch <Mark.Riddoch@skysql.com>
2014-08-22 14:25:27 +0100

    Fixed soem errors from a cppcheck run
    mbrampton@martin-office:~/Dropbox/development/skygit/MaxScale/server$ cppcheck -q core/*.c
    [core/adminusers.c:302]: (error) Resource leak: fp_tmp
    [core/filter.c:382]: (error) Uninitialized variable: me
    [core/service.c:1071]: (error) Uninitialized variable: succp

--- server/core/filter.c
+++ server/core/filter.c
@@ -359,7 +359,7 @@ DOWNSTREAM	*me;
 UPSTREAM *
 filterUpstream(FILTER_DEF *filter, void *fsession, UPSTREAM *upstream)
 {
-UPSTREAM	*me;
+UPSTREAM	*me = NULL;
 
 	/*
 	 * The the filter has no setUpstream entry point then is does
