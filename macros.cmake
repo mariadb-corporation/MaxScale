5d26cf6fd592af1fd5b3ad490d59b779fd1fb054
Markus Makela <markus.makela@skysql.com>
2014-09-14 07:30:41 +0300

    Added optional RabbitMQ component configuration Changed all the configuration variables to cached Fixed some errors in tests

--- macros.cmake
+++ macros.cmake
@@ -8,41 +8,38 @@ macro(set_maxscale_version)
 
 endmacro()
 
-macro(set_testing_variables)
+macro(set_variables)
 
   # hostname or IP address of MaxScale's host
-  if(NOT TEST_HOST)
-    set(TEST_HOST "127.0.0.1")
-  endif()
+  set(TEST_HOST "127.0.0.1" CACHE STRING "hostname or IP address of MaxScale's host")
 
   # port of read connection router module
-  if(NOT TEST_PORT_RW)
-    set(TEST_PORT_RW "4008")
-  endif()
+  set(TEST_PORT_RW "4008" CACHE STRING "port of read connection router module")
 
   # port of read/write split router module
-  if(NOT TEST_PORT_RW)
-    set(TEST_PORT_RW "4006")
-  endif()
+  set(TEST_PORT_RW "4006" CACHE STRING "port of read/write split router module")
 
   # port of read/write split router module with hints
-  if(NOT TEST_PORT_RW_HINT)
-    set(TEST_PORT_RW_HINT "4006")
-  endif()
+  set(TEST_PORT_RW_HINT "4006" CACHE STRING "port of read/write split router module with hints")
 
-  # master's server_id
-  if(NOT TEST_MASTER_ID)
-    set(TEST_MASTER_ID "3000")
-  endif()
+  # master test server server_id
+  set(TEST_MASTER_ID "3000" CACHE STRING "master test server server_id")
 
   # username of MaxScale user
-  if(NOT TEST_USER)
-    set(TEST_USER "maxuser")
-  endif()
+  set(TEST_USER "maxuser" CACHE STRING "username of MaxScale user")
 
   # password of MaxScale user
-  if(NOT TEST_PASSWORD)
-    set(TEST_PASSWORD "maxpwd")
-  endif()
+  set(TEST_PASSWORD "maxpwd" CACHE STRING "password of MaxScale user")
+  
+  # Use static version of libmysqld
+  set(STATIC_EMBEDDED FALSE CACHE BOOL "Use static version of libmysqld")
+  
+  # Build RabbitMQ components
+  set(BUILD_RABBITMQ FALSE CACHE BOOL "Build RabbitMQ components")
+  
+  # Use gcov build flags
+  set(GCOV FALSE CACHE BOOL "Use gcov build flags")
 
+  # Install init.d scripts and ldconf configuration files
+  set(INSTALL_SYSTEM_FILES TRUE CACHE BOOL "Install init.d scripts and ldconf configuration files")
 endmacro()
