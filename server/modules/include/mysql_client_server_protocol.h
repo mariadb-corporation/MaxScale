f3d32087d83b46fa6b5e19b6dfc4c9c1baf4d9b0
Mark Riddoch <Mark.Riddoch@skysql.com>
2014-09-16 12:37:57 +0100

    Performance improvement or streaming large result sets.
    -bash-4.1$ time mysql -h 127.0.0.1 -P4007 -umassi -pmassi information_schema -q -e "select * from engines a, engines b, engines c, engines d, engines e;" > /dev/null
    
    real	1m16.137s
    user	0m0.660s
    sys	0m0.392s
    -bash-4.1$ time mysql -h 127.0.0.1 -P4007 -umassi -pmassi information_schema -q -e "select * from engines a, engines b, engines c, engines d, engines e;" > /dev/null
    
    real	0m0.980s
    user	0m0.944s
    sys	0m0.027s

--- server/modules/include/mysql_client_server_protocol.h
+++ server/modules/include/mysql_client_server_protocol.h
@@ -82,10 +82,6 @@
 #endif
 
 #define GW_NOINTR_CALL(A)       do { errno = 0; A; } while (errno == EINTR)
-// network buffer is 32K
-#define MAX_BUFFER_SIZE 32768
-// socket send buffer for backend
-#define GW_BACKEND_SO_SNDBUF 1024
 #define SMALL_CHUNK 1024
 #define MAX_CHUNK SMALL_CHUNK * 8 * 4
 #define ToHex(Y) (Y>='0'&&Y<='9'?Y-'0':Y-'A'+10)
