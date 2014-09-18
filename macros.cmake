942174e94fb2760adbad4d4060d08b4024a01042
Markus Makela <markus.makela@skysql.com>
2014-09-18 15:50:58 +0300

    removed manual UBUNTU flag and renamed it to DEB_LIKE and made it automated

--- macros.cmake
+++ macros.cmake
@@ -65,7 +65,7 @@ macro(check_deps)
       set(DEPS_ERROR TRUE)
       set(FAILED_DEPS "${FAILED_DEPS} lib${lib}")
 	elseif(DEBUG_OUTPUT)
-	message(STATUS "Library was found at: ${lib${lib}}")
+	  message(STATUS "Library was found at: ${lib${lib}}")
     endif()
   endforeach()
 
@@ -76,49 +76,64 @@ macro(check_deps)
 endmacro()
 
 macro(check_dirs)
-# Find the MySQL headers if they were not defined
-if(NOT ( DEFINED MYSQL_DIR ) )
-  find_path(MYSQL_DIR mysql.h PATH_SUFFIXES mysql mariadb)
-  if(${MYSQL_DIR} STREQUAL "MYSQL_DIR-NOTFOUND")
-    message(FATAL_ERROR "Fatal Error: MySQL headers were not found.")
-  elseif(DEBUG_OUTPUT)
-	message(STATUS "Using MySQL headers found at: ${MYSQL_DIR}")
+  # Find the MySQL headers if they were not defined
+  if(NOT ( DEFINED MYSQL_DIR ) )
+	find_path(MYSQL_DIR mysql.h PATH_SUFFIXES mysql mariadb)
+	if(${MYSQL_DIR} STREQUAL "MYSQL_DIR-NOTFOUND")
+      message(FATAL_ERROR "Fatal Error: MySQL headers were not found.")
+	elseif(DEBUG_OUTPUT)
+	  message(STATUS "Using MySQL headers found at: ${MYSQL_DIR}")
+	endif()
   endif()
-endif()
 
-# Find the errmsg.sys file if it was not defied
-if( NOT ( DEFINED ERRMSG ) )
-  find_file(ERRMSG errmsg.sys PATHS /usr/share/mysql /usr/local/share/mysql ${CUSTOM_ERRMSG} PATH_SUFFIXES english)
-  if(${ERRMSG} STREQUAL "ERRMSG-NOTFOUND")
-    message(FATAL_ERROR "Fatal Error: The errmsg.sys file was not found.")
-  elseif(DEBUG_OUTPUT)
-	message(STATUS "Using errmsg.sys found at: ${ERRMSG}")
+  # Find the errmsg.sys file if it was not defied
+  if( NOT ( DEFINED ERRMSG ) )
+	find_file(ERRMSG errmsg.sys PATHS /usr/share/mysql /usr/local/share/mysql ${CUSTOM_ERRMSG} PATH_SUFFIXES english)
+	if(${ERRMSG} STREQUAL "ERRMSG-NOTFOUND")
+      message(FATAL_ERROR "Fatal Error: The errmsg.sys file was not found.")
+	elseif(DEBUG_OUTPUT)
+	  message(STATUS "Using errmsg.sys found at: ${ERRMSG}")
+	endif()
   endif()
-endif()
 
-# Find the embedded mysql library
-if(STATIC_EMBEDDED)
-  set(OLD_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-  find_library(EMBEDDED_LIB_STATIC libmysqld.a PATH_SUFFIXES mysql mariadb)      
-  set(EMBEDDED_LIB ${EMBEDDED_LIB_STATIC})      
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ${OLD_SUFFIXES})
-else()      
+  # Find the embedded mysql library
+  if(STATIC_EMBEDDED)
+	set(OLD_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+	set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+	find_library(EMBEDDED_LIB_STATIC libmysqld.a PATH_SUFFIXES mysql mariadb)      
+	set(EMBEDDED_LIB ${EMBEDDED_LIB_STATIC})      
+	set(CMAKE_FIND_LIBRARY_SUFFIXES ${OLD_SUFFIXES})
+  else()      
+	
+	find_library(EMBEDDED_LIB_DYNAMIC mysqld PATH_SUFFIXES mysql mariadb)            
+	set(EMBEDDED_LIB ${EMBEDDED_LIB_DYNAMIC})      
+
+  endif()
+
+
+  # Inform the user about the embedded library
+  if( (${EMBEDDED_LIB} STREQUAL "EMBEDDED_LIB_STATIC-NOTFOUND") OR (${EMBEDDED_LIB} STREQUAL "EMBEDDED_LIB_DYNAMIC-NOTFOUND"))
+	message(FATAL_ERROR "Library not found: libmysqld. If your install of MySQL is in a non-default location, please provide the location with -DEMBEDDED_LIB=<path to library>")
+  else()
+	get_filename_component(EMBEDDED_LIB ${EMBEDDED_LIB} REALPATH)
+	message(STATUS "Using embedded library: ${EMBEDDED_LIB}")
+  endif()
+
+
+  # Check which init.d script to install
+  find_file(RPM_FNC functions PATHS /etc/rc.d/init.d)
+  if(${RPM_FNC} MATCHES "RPM_FNC-NOTFOUND")
+	find_file(DEB_FNC init-functions PATHS /lib/lsb)
+	if(${DEB_FNC MATCHES} "DEB_FNC-NOTFOUND")
+	  message(FATAL_ERROR "Cannot find required init-functions in /lib/lsb/ or /etc/rc.d/init.d/, please confirm that your system files are OK.")
+	else()
+	  set(DEB_BASED TRUE CACHE BOOL "If init.d script uses /lib/lsb/init-functions instead of /etc/rc.d/init.d/functions.")
+	endif()
+  else()
+	set(DEB_BASED FALSE CACHE BOOL "If init.d script uses /lib/lsb/init-functions instead of /etc/rc.d/init.d/functions.")
+  endif()
   
-  find_library(EMBEDDED_LIB_DYNAMIC mysqld PATH_SUFFIXES mysql mariadb)            
-  set(EMBEDDED_LIB ${EMBEDDED_LIB_DYNAMIC})      
 
-endif()
-
-
-# Inform the user about the embedded library
-if( (${EMBEDDED_LIB} STREQUAL "EMBEDDED_LIB_STATIC-NOTFOUND") OR (${EMBEDDED_LIB} STREQUAL "EMBEDDED_LIB_DYNAMIC-NOTFOUND"))
-  message(FATAL_ERROR "Library not found: libmysqld. If your install of MySQL is in a non-default location, please provide the location with -DEMBEDDED_LIB=<path to library>")
-else()
-  get_filename_component(EMBEDDED_LIB ${EMBEDDED_LIB} REALPATH)
-  message(STATUS "Using embedded library: ${EMBEDDED_LIB}")
-endif()
-
-set(DEPS_OK TRUE CACHE BOOL "If all the dependencies were found.")
+  set(DEPS_OK TRUE CACHE BOOL "If all the dependencies were found.")
 
 endmacro()
