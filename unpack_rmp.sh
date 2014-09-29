9ee82ad2dafe094ae9d00af46e7531b03182ca6f
Markus Makela <markus.makela@skysql.com>
2014-09-29 14:27:29 +0300

    Script now unpacks all MariaDB RPMs in the folder.

--- unpack_rmp.sh
+++ unpack_rmp.sh
@@ -2,10 +2,31 @@
 
 #This script unpacks the RPM to the provided directory
 
+unpack_to(){
+	cd $2 && rpm2cpio $1 | cpio -id;
+}
+
+
 if [[ $# -lt 2 ]]
 then
-	echo "Usage: unpack_rpm.sh <path to RPM package> <installation directory>"
+	echo "Usage: unpack_rpm.sh <path to MariaDB RPMs> <installation directory>"
 	exit 0
 fi
-mkdir -p $2
-cd $2 && rpm2cpio $1 | cpio -id
+
+SOURCE=$1
+DEST=$2
+FILES=$(ls $SOURCE |grep -i mariadb.*`uname -m`.*.rpm)
+
+if [[ ! -d $DEST ]]
+then
+	mkdir -p $DEST
+fi
+
+echo "Unpacking RPMs to: $DEST"
+
+for rpm in $FILES
+do
+	echo "Unpacking $rpm..."
+	unpack_to $SOURCE/$rpm $DEST 
+done
+
