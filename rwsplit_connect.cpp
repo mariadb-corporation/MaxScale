509219580b9f1f949e236194c2e005b076b6bce6
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-09 22:14:54 +0300

    better output for rwsplit_connect

--- rwsplit_connect.cpp
+++ rwsplit_connect.cpp
@@ -13,13 +13,20 @@ int main()
 
     unsigned int conn_num;
     unsigned int all_conn=0;
+    printf("Checking number of connections ot backend servers\n");
     for (int i = 0; i < Test->repl->N; i++) {
         conn_num = get_conn_num(Test->repl->nodes[i], Test->Maxscale_IP, (char *) "test");
         printf("connections: %u\n", conn_num);
-        if ((i == 0) && (conn_num != 1)) {global_result=1;}
+        if ((i == 0) && (conn_num != 1)) {
+            printf("FAILED: Master should have only 1 connection, but it has %d connection(s)\n", conn_num);
+            global_result=1;
+        }
         all_conn += conn_num;
     }
-    if (all_conn != 2) {global_result=1;}
+    if (all_conn != 2) {
+        global_result=1;
+        printf("FAILED: there should be two connections in total: one to master and one to one of slaves, but number of connections is %d\n", all_conn);
+    }
 
     Test->CloseRWSplit();
     Test->repl->CloseConn();
