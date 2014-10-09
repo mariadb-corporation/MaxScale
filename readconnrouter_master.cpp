55a95a1b36110db8c332c16e15a0f240bb575e57
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-09 12:56:12 +0300

    add readconn_master.cpp

---
+++ readconnrouter_master.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "testconnections.h"
+
+using namespace std;
+
+int CheckConnnctionsOnlyToMaster(TestConnections * Test, int master)
+{
+    int res = 0;
+    int conn_num;
+    printf("Checking number of connections to each node\n");
+    for (int i = 0; i < Test->repl->N; i++) {
+        conn_num = get_conn_num(Test->repl->nodes[i], Test->Maxscale_IP, (char *) "test");
+        printf("Connections to node %d (%s):\t%d\n", i, Test->repl->IP[i], conn_num);
+        if (((i == master) && (conn_num != 1)) || ((i != master) && (conn_num != 0))) {
+            res++;
+            printf("FAILED: number of connections to node %d is wrong\n", i);
+        }
+    }
+}
+
+int main()
+{
+    MYSQL * conn_read;
+    int res = 0;
+
+    TestConnections * Test = new TestConnections();
+    Test->ReadEnv();
+    Test->PrintIP();
+    Test->repl->Connect();
+
+    printf("Connecting to ReadConnnRouter in 'master' mode\n");
+    conn_read = Test->ConnectReadMaster();
+    printf("Sleeping 5 seconds\n");
+    sleep(5);
+    res += CheckConnnctionsOnlyToMaster(Test, 0);
+    mysql_close(conn_read);
+
+    printf("Changing master to node 1\n");
+    Test->repl->ChangeMaster(1, 0);
+
+    printf("Connecting to ReadConnnRouter in 'master' mode\n");
+    conn_read = Test->ConnectReadMaster();
+    printf("Sleeping 5 seconds\n");
+    sleep(5);
+    res += CheckConnnctionsOnlyToMaster(Test, 1);
+    mysql_close(conn_read);
+
+    printf("Changing master back to node 0\n");
+    Test->repl->ChangeMaster(0, 1);
+
+    return(res);
+}
+
