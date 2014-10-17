ca891f6d7c17dd63cc80465854c23226aed4ac45
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-17 11:43:20 +0300

    add change_master_during_session

---
+++ change_master_during_session.cpp
@@ -0,0 +1,37 @@
+#include <my_config.h>
+#include <iostream>
+#include "testconnections.h"
+#include "sql_t1.h"
+
+using namespace std;
+
+
+int main()
+{
+    int global_result = 0;
+
+    TestConnections * Test = new TestConnections();
+    Test->ReadEnv();
+    Test->PrintIP();
+
+    printf("Connecting to RWsplit\n");
+    Test->ConnectRWSplit();
+
+    global_result += create_t1(Test->conn_rwsplit);
+
+    global_result += execute_query(Test->conn_rwsplit, (char *) "INSERT INTO t1 (x1, fl) VALUES(0, 1);");
+    printf("Changing master to node 1\n");
+    Test->repl->ChangeMaster(1, 0);
+    printf("executing 3 INSERTs\n");
+    execute_query(Test->conn_rwsplit, (char *) "INSERT INTO t1 (x1, fl) VALUES(0, 2);");
+    execute_query(Test->conn_rwsplit, (char *) "INSERT INTO t1 (x1, fl) VALUES(1, 2);");
+    execute_query(Test->conn_rwsplit, (char *) "INSERT INTO t1 (x1, fl) VALUES(2, 2);");
+    printf("executing SELECT\n");
+    execute_query(Test->conn_rwsplit, (char *) "SELECT * FROM t1;");
+
+
+    printf("Changing master back to node 0\n");
+    Test->repl->ChangeMaster(0, 1);
+
+    return(global_result);
+}
