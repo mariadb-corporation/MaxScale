c5610457ed1cfc119cce2b899c2644dc24bef2a7
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-22 13:15:53 +0300

    staring slave immediatelly after configuring it

--- sql_const.h
+++ sql_const.h
@@ -1,15 +1,16 @@
 #ifndef SQL_CONST_H
 #define SQL_CONST_H
 
-/*const char * create_repl_user =
+const char * create_repl_user =
         "grant replication slave on *.* to repl@'%' identified by 'repl';\
-        FLUSH PRIVILEGES";*/
+        FLUSH PRIVILEGES";
 const char * setup_slave =
         "change master to MASTER_HOST='%s';\
         change master to MASTER_USER='repl';\
         change master to MASTER_PASSWORD='repl';\
         change master to MASTER_LOG_FILE='%s';\
         change master to MASTER_LOG_POS=%s;\
-        change master to MASTER_PORT=%d";
+        change master to MASTER_PORT=%d;\
+        start slave;";
 
 #endif // SQL_CONST_H
