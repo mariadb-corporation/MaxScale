7a1b35dc88e871bdc11d84d0809dd615e19194ac
VilhoRaatikka <vilho.raatikka@skysql.com>
2014-10-23 18:56:27 +0300

    Fix to bug #590, http://bugs.mariadb.com/show_bug.cgi?id=590 readwritesplit.c:execute_sescmd_in_backend didn't set GWBUF_TYPE_SESCMD type to GWBUF the command is in. Thus reply wasn't processed properly. Instead all replies were sent to client.

--- server/modules/routing/readwritesplit/readwritesplit.c
+++ server/modules/routing/readwritesplit/readwritesplit.c
@@ -3515,7 +3515,9 @@ static bool execute_sescmd_in_backend(
 #endif /*< SS_DEBUG */
         switch (scur->scmd_cur_cmd->my_sescmd_packet_type) {
                 case MYSQL_COM_CHANGE_USER:
-                        rc = dcb->func.auth(
+			/** This makes it possible to handle replies correctly */
+			gwbuf_set_type(scur->scmd_cur_cmd->my_sescmd_buf, GWBUF_TYPE_SESCMD);
+			rc = dcb->func.auth(
                                 dcb, 
                                 NULL, 
                                 dcb->session, 
