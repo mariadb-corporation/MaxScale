e39a6c11a0e86d90a53c7daa0eb3122348cd09f4
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-26 21:30:45 +0200

    add queries from bug470 to the bug473 case

--- bug473.cpp
+++ bug473.cpp
@@ -1,4 +1,4 @@
-//also reletes to bug472
+//also reletes to bug472 and bug470
 
 #include <my_config.h>
 #include <iostream>
@@ -16,6 +16,9 @@ int main()
     Test->PrintIP();
     Test->ConnectMaxscale();
 
+
+    printf("Trying queries that caused crashes before fix: bug473\n"); fflush(stdout);
+
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server =(");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server =)");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server =:");
@@ -23,12 +26,22 @@ int main()
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server = a");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server = кириллица åäö");
 
+    // bug472
+    printf("Trying queries that caused crashes before fix: bug472\n"); fflush(stdout);
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale s1 begin route to server server3");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale end");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale s1 begin");
 
+    // bug470
+    printf("Trying queries that caused crashes before fix: bug470\n"); fflush(stdout);
+    global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale named begin route to master");
+    global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id;");
+    global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale named begin route to master; select @@server_id;");
+
+
     Test->CloseMaxscaleConn();
 
+    printf("Checking if Maxscale is alive\n"); fflush(stdout);
     global_result += CheckMaxscaleAlive();
 
     return(global_result);
