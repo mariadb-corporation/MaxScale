ed4094cbe06d8d8e7d99b9c5a32c15912bc4c60f
Timofey Turenko <timofey.turenko@skysql.com>
2014-11-03 15:30:42 +0200

    fix 526 test

--- bug526.cpp
+++ bug526.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 int main()
 {
-    int global_result = CheckLogErr((char *) "Unable to find filter 'testfilter'", TRUE);
+    int global_result = CheckLogErr((char *) "Error : Unable to find library for module: foobar", TRUE);
+    global_result += CheckLogErr((char *) "Failed to create filter 'testfilter' for service", TRUE);
+    global_result += CheckLogErr((char *) "Error : Failed to create RW Split Router session", TRUE);
     global_result += CheckMaxscaleAlive();
     return(global_result);
 }
