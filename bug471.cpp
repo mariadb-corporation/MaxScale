3cd428dbdda00bcbd0f70989c681fa1c7992eaba
Timofey Turenko <timofey.turenko@skysql.com>
2014-11-03 17:21:25 +0200

    add bug471 code

---
+++ bug471.cpp
@@ -0,0 +1,53 @@
+#include <my_config.h>
+#include <iostream>
+#include <unistd.h>
+#include "testconnections.h"
+
+using namespace std;
+
+int main()
+{
+    TestConnections * Test = new TestConnections();
+    int global_result = 0;
+
+    Test->ReadEnv();
+    Test->PrintIP();
+    Test->repl->Connect();
+    Test->ConnectMaxscale();
+
+    char server_id[256];
+    char server_id_d[256];
+
+    char hint_sql[64];
+
+    for (int i = 1; i < 25; i++) {
+        for (int j = 0; j < Test->repl->N; j++) {
+            if (j !=1 ) {
+
+                sprintf(hint_sql, "select @@server_id; -- maxscale route to server server%d", j+1);
+
+                find_status_field(Test->conn_rwsplit, hint_sql, (char *) "@@server_id", &server_id[0]);
+                find_status_field(Test->repl->nodes[j], (char *) "select @@server_id;", (char *) "@@server_id", &server_id_d[0]);
+
+                printf("server%d ID from Maxscale: \t%s\n", j+1, server_id);
+                printf("server%d ID directly from node: \t%s\n", j+1, server_id_d);
+
+                if (strcmp(server_id, server_id_d) !=0 )  {
+                    global_result = 1;
+                    printf("Hints does not work!\n");
+                }
+            }
+        }
+    }
+
+
+    Test->CloseMaxscaleConn();
+    Test->repl->CloseConn();
+
+    global_result += CheckMaxscaleAlive();
+
+    return(global_result);
+}
+
+
+
