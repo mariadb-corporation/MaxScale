89525123e52bd7fad9342810ca6a3862723ef236
Timofey Turenko <timofey.turenko@skysql.com>
2014-11-11 00:53:51 +0200

    add more printinf bug473

--- bug473.cpp
+++ bug473.cpp
@@ -26,11 +26,19 @@ int main()
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server = a");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale route to server = кириллица åäö");
 
+    if (global_result == 0) {
+        printf("bug473 ok\n");
+    }
+
     // bug472
     printf("Trying queries that caused crashes before fix: bug472\n"); fflush(stdout);
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale s1 begin route to server server3");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale end");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale s1 begin");
+
+    if (global_result == 0) {
+        printf("bug472 ok\n");
+    }
 
     // bug470
     printf("Trying queries that caused crashes before fix: bug470\n"); fflush(stdout);
@@ -38,6 +46,9 @@ int main()
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id;");
     global_result += execute_query(Test->conn_rwsplit, (char *) "select @@server_id; -- maxscale named begin route to master; select @@server_id;");
 
+    if (global_result == 0) {
+        printf("bug470 ok\n");
+    }
 
     global_result += Test->CloseMaxscaleConn();
 
