f019a136e74e781b3adecbd22c82ad0cd512c58a
Timofey Turenko <timofey.turenko@skysql.com>
2014-12-10 18:49:06 +0200

    add bug634

---
+++ bug634.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file bug634.cpp  regression case for bug 634 ("SHOW SLAVE STATUS in RW SPLITTER is send to master")
+ *
+ * - execute SHOW SLAVE STATUS and check resut
+ */
+
+#include <my_config.h>
+#include <iostream>
+#include "testconnections.h"
+
+int main()
+{
+    TestConnections * Test = new TestConnections();
+    int global_result = 0;
+    char master_ip[100];
+
+    Test->ReadEnv();
+    Test->PrintIP();
+
+    Test->ConnectMaxscale();
+
+    for (int i = 0; i < 100; i++) {
+        if (find_status_field(Test->conn_rwsplit, (char *) "SHOW SLAVE STATUS", (char *) "Master_Host", master_ip) == 0) {
+            printf("Master_host files is not found in the SHOW SLAVE STATUS reply, probably query went to master\n");
+            global_result++;
+        }
+        if (strcmp(master_ip, Test->repl->IP[0])) {
+            printf("Master IP is wrong\n");
+            global_result++;
+        }
+    }
+
+    Test->CloseMaxscaleConn();
+
+    return(global_result);
+}
