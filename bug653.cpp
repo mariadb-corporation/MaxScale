194036f83c8b4f73723d7b61a377c1d2bc891ff1
Timofey Turenko <timofey.turenko@skysql.com>
2014-12-22 17:27:39 +0200

    fix user name in  bug653

--- bug653.cpp
+++ bug653.cpp
@@ -22,11 +22,11 @@ int main()
     Test->ConnectMaxscale();
 
     printf("Creating user with old style password\n");
-    global_result += execute_query(Test->conn_rwsplit, (char *) "CREATE USER 'user_long_hoste'@'very_long_hostname_that_probably_caused_crashhh.com.net.org' IDENTIFIED BY 'old'; GRANT ALL PRIVILEGES ON *.* TO 'user_long_hoste'@'very_long_hostname_that_probably_caused_crashhh.com.net.org' WITH GRANT OPTION;");
+    global_result += execute_query(Test->conn_rwsplit, (char *) "CREATE USER 'user_long_host11'@'very_long_hostname_that_probably_caused_crashhh.com.net.org' IDENTIFIED BY 'old'; GRANT ALL PRIVILEGES ON *.* TO 'user_long_hoste'@'very_long_hostname_that_probably_caused_crashhh.com.net.org' WITH GRANT OPTION;");
     sleep(10);
 
     printf("Trying to connect using user with old style password\n");
-    MYSQL * conn = open_conn(Test->rwsplit_port, Test->Maxscale_IP, (char *) "user_long_host", (char *)  "old");
+    MYSQL * conn = open_conn(Test->rwsplit_port, Test->Maxscale_IP, (char *) "user_long_host11", (char *)  "old");
 
     if ( conn == NULL) {
         printf("Connections is not open as expected\n");
@@ -36,7 +36,7 @@ int main()
         mysql_close(conn);
     }
 
-    global_result += execute_query(Test->conn_rwsplit, (char *) "DROP USER 'user_long_host'@'very_long_hostname_that_probably_caused_crashhh.com.net.org'");
+    global_result += execute_query(Test->conn_rwsplit, (char *) "DROP USER 'user_long_host11'@'very_long_hostname_that_probably_caused_crashhh.com.net.org'");
     Test->CloseMaxscaleConn();
 
 
