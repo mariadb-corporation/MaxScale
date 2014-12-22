0b564a5f37cb2f781c630d9de95dbe41790e386d
Timofey Turenko <timofey.turenko@skysql.com>
2014-12-22 16:54:53 +0200

    add bug653

---
+++ bug653.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file bug653.cpp  regression case for bug 653 ("Memory corruption when users with long hostnames that can no the resolved are loaded into MaxScale")
+ *
+ * - CREATE USER 'user_with_very_long_hostname'@'very_long_hostname_that_can_not_be_resolved_and_it_probably_caused_crash.com.net.org' IDENTIFIED BY 'old';
+ * - try to connect using user 'user_with_very_long_hostname'
+ * - DROP USER 'user_with_very_long_hostname'@'very_long_hostname_that_can_not_be_resolved_and_it_probably_caused_crash.com.net.org'
+ * - check MaxScale is alive
+ */
+
+#include <my_config.h>
+#include <iostream>
+#include "testconnections.h"
+
+int main()
+{
+    TestConnections * Test = new TestConnections();
+    int global_result = 0;
+
+    Test->ReadEnv();
+    Test->PrintIP();
+
+    Test->ConnectMaxscale();
+
+    printf("Creating user with old style password\n");
+    global_result += execute_query(Test->conn_rwsplit, (char *) "CREATE USER 'user_with_very_long_hostname'@'very_long_hostname_that_can_not_be_resolved_and_it_probably_caused_crash.com.net.org' IDENTIFIED BY 'old';");
+    sleep(10);
+
+    printf("Trying to connect using user with old style password\n");
+    MYSQL * conn = open_conn(Test->rwsplit_port, Test->Maxscale_IP, (char *) "user_with_very_long_hostname", (char *)  "old");
+
+    if ( conn == NULL) {
+        printf("Connections is not open as expected\n");
+    } else {
+        printf("Connections is open for the user with bad host. FAILED!\n");
+        global_result++;
+        mysql_close(conn);
+    }
+
+    global_result += execute_query(Test->conn_rwsplit, (char *) "DROP USER 'user_with_very_long_hostname'@'very_long_hostname_that_can_not_be_resolved_and_it_probably_caused_crash.com.net.org'");
+    Test->CloseMaxscaleConn();
+
+
+    global_result += CheckMaxscaleAlive();
+
+    return(global_result);
+}
