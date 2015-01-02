503c274ddfc57890cb7d0963b02c01ee1f3ec3c5
Timofey Turenko <timofey.turenko@skysql.com>
2015-01-02 16:28:29 +0200

    more sleep after blocking all backends in bug658

--- bug658.cpp
+++ bug658.cpp
@@ -56,6 +56,8 @@ int main()
     global_result += CheckMaxscaleAlive(); fflush(stdout);
     if (global_result !=0) {
         printf("MaxScale is not alive\n");
+    } else {
+        printf("MaxScale is still alive\n");
     }
 
     Test->CloseMaxscaleConn(); fflush(stdout);
@@ -86,21 +88,30 @@ int main()
         printf("%s\n", sys1);  fflush(stdout);
         system(sys1); fflush(stdout);
     }
-    sleep(10);
+
+    printf("Sleeping 60 seconds\n");
+    sleep(60);
 
     printf("Checking Maxscale is alive\n"); fflush(stdout);
     global_result += CheckMaxscaleAlive(); fflush(stdout);
     if (global_result !=0) {
         printf("MaxScale is not alive\n");
+    } else {
+        printf("MaxScale is still alive\n");
     }
+
 
     Test->CloseMaxscaleConn(); fflush(stdout);
 
 
     printf("Reconnecting and trying query to RWSplit\n"); fflush(stdout);
-    Test->ConnectRWSplit();
+    Test->ConnectMaxscale();
     global_result += execute_query(Test->conn_rwsplit, (char *) "show processlist;");
-    Test->CloseRWSplit();
+    printf("Trying query to ReadConn master\n"); fflush(stdout);
+    global_result += execute_query(Test->conn_master, (char *) "show processlist;");
+    printf("Trying query to ReadConn slave\n"); fflush(stdout);
+    global_result += execute_query(Test->conn_slave, (char *) "show processlist;");
+    Test->CloseMaxscaleConn();
 
     exit(global_result);
 }
