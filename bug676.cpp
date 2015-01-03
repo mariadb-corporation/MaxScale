1866a0860f7f7216ceb532166cd82cfe4dc62a0e
Timofey Turenko <timofey.turenko@skysql.com>
2015-01-03 15:10:39 +0200

    tune bug676

--- bug676.cpp
+++ bug676.cpp
@@ -14,11 +14,35 @@ int main()
 {
     TestConnections * Test = new TestConnections();
     int global_result = 0;
+    int i;
+    char sys1[4096];
 
     Test->ReadEnv();
     Test->PrintIP();
 
-    //Test->ConnectMaxscale();
+    printf("Stopping all Galera nodes\n");  fflush(stdout);
+    for (i = 0; i < Test->repl->N; i++) {
+        printf("Stopping %d\n", i); fflush(stdout);
+        sprintf(&sys1[0], "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s '/etc/init.d/mysql stop'", Test->repl->sshkey[i], Test->repl->IP[i]);
+        printf("%s\n", sys1);  fflush(stdout);
+        system(sys1); fflush(stdout);
+
+        sprintf(&sys1[0], "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s 'sed -i \"s/wsrep_sst_method=rsync/wsrep_sst_method=xtrabackup-v2/\" /etc/my.cnf.d/skysql-galera.cnf'", Test->repl->sshkey[i], Test->repl->IP[i]);
+        printf("%s\n", sys1);  fflush(stdout);
+    }
+
+    printf("Starting back all Galera nodes\n");  fflush(stdout);
+    printf("Starting node %d\n", Test->repl->N-1); fflush(stdout);
+    sprintf(&sys1[0], "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s '/etc/init.d/mysql start'", Test->repl->sshkey[Test->repl->N-1], Test->repl->IP[Test->repl->N-1]);
+    printf("%s\n", sys1);  fflush(stdout);
+    system(sys1); fflush(stdout);
+
+    for (i = 0; i < Test->repl->N-2; i++) {
+        printf("Starting node %d\n", i); fflush(stdout);
+        sprintf(&sys1[0], "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s '/etc/init.d/mysql start --wsrep-cluster-address=gcomm://%s'", Test->repl->sshkey[i], Test->repl->IP[i], Test->repl->IP[Test->repl->N-1]);
+        printf("%s\n", sys1);  fflush(stdout);
+        system(sys1); fflush(stdout);
+    }
 
 
     MYSQL * conn = open_conn_no_db(Test->rwsplit_port, Test->Maxscale_IP, Test->Maxscale_User, Test->Maxscale_Password);
@@ -26,11 +50,12 @@ int main()
     printf("selecting DB 'test' for rwsplit\n"); fflush(stdout);
     global_result += execute_query(conn, "USE test");
 
-    //Test->CloseMaxscaleConn();
     printf("Closing connection\n"); fflush(stdout);
     mysql_close(conn);
 
-    global_result += CheckMaxscaleAlive();
+    Test->ConnectMaxscale();
+    global_result += execute_query(Test->conn_rwsplit, "show processlist;");
+    Test->CloseMaxscaleConn();
 
     return(global_result);
 }
