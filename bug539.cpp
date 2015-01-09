127f93b50f1eb8141d2de9d9893943f6c6fd846b
Timofey Turenko <timofey.turenko@skysql.com>
2015-01-09 18:55:05 +0200

    add bug539 c implementation

---
+++ bug539.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file bug539.cpp  regression case for bug539 ("MaxScale crashes in session_setup_filters")
+ *
+ */
+
+#include <my_config.h>
+#include "testconnections.h"
+#include "maxadmin_operations.h"
+
+int main()
+{
+    TestConnections * Test = new TestConnections();
+    int global_result = 0;
+    int i, j;
+    char result[1024];
+    MYSQL * conn;
+
+    int N_cmd=2;
+    char * fail_cmd[N_cmd-1];
+
+    int N_ports = 3;
+    int ports[N_ports];
+
+    fail_cmd[0] = "fail backendfd";
+    fail_cmd[1] = "fail clientfd";
+
+    ports[0] = Test->rwsplit_port;
+    ports[1] = Test->readconn_master_port;
+    ports[2] = Test->readconn_slave_port;
+
+
+    Test->ReadEnv();
+    Test->PrintIP();
+
+    for (i = 0; i < N_cmd; i++) {
+        for (j = 0; j < N_ports; j++) {
+            printf("Executing MaxAdmin command '%s'\n", fail_cmd[i]); fflush(stdout);
+            if (getMaxadminParam(Test->Maxscale_IP, (char *) "admin", (char *) "skysql", fail_cmd[i], (char *) "", result) != 0) {
+                printf("MaxAdmin command failed\n"); fflush(stdout);
+                global_result++;
+            } else {
+                printf("Trying query against %d\n", ports[j]);
+                conn = open_conn(ports[j], Test->Maxscale_IP, Test->Maxscale_User, Test->Maxscale_User);
+                global_result += execute_query(conn, (char *) "show proceslist;");
+            }
+        }
+    }
+
+    exit(global_result);
+}
