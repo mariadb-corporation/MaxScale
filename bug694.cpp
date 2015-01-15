a5c9c695bb7d3f4d261e274b129c59d108c80513
Timofey Turenko <timofey.turenko@skysql.com>
2015-01-16 00:30:05 +0200

    finalize bug694

--- bug694.cpp
+++ bug694.cpp
@@ -28,13 +28,19 @@ int main()
 
     printf("Trying SELECT @a:=@a+1 as a, test.b FROM test\n"); fflush(stdout);
     global_result += execute_query(Test->conn_rwsplit, "CREATE TABLE test (b integer);");
-    global_result += execute_query(Test->conn_rwsplit, "SELECT @a:=@a+1 as a, test.b FROM test;");
-    printf("Trying USE test"); fflush(stdout);
+    if (execute_query(Test->conn_rwsplit, "SELECT @a:=@a+1 as a, test.b FROM test;") == 0) {
+        printf("Query succeded, bu expected to fail. Test FAILED!\n"); fflush(stdout);
+        global_result++;
+    }
+    printf("Trying USE test\n"); fflush(stdout);
     global_result += execute_query(Test->conn_rwsplit, "USE test");
 
+    printf("Checking if MaxScale alive\n"); fflush(stdout);
     Test->CloseMaxscaleConn();
 
-    //global_result    += CheckLogErr((char *) "Error : Unable to start RW Split Router service. There are too few backend servers configured in MaxScale.cnf. Found 10% when at least 33% would be required", TRUE);
+    printf("Checking logs\n"); fflush(stdout);
+    global_result    += CheckLogErr((char *) "Warning : The query can't be routed to all backend servers because it includes SELECT and SQL variable modifications which is not supported", TRUE);
+    global_result    += CheckLogErr((char *) "SELECT with session data modification is not supported if configuration parameter use_sql_variables_in=all", TRUE);
 
     global_result += CheckMaxscaleAlive();
 
