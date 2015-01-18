d4d9304dab8b37b06afd23c186667e6590a52467
Markus Makela <markus.makela@mariadb.com>
2015-01-18 06:08:38 +0200

    Removed unused values.

--- server/core/gateway.c
+++ server/core/gateway.c
@@ -1096,7 +1096,7 @@ int main(int argc, char **argv)
 				 long_options, &option_index)) != -1)
         {
                 bool succp = true;
-                struct option* optr = long_options;
+
                 switch (opt) {
                 case 'd':
                         /*< Debug mode, maxscale runs in this same process */
