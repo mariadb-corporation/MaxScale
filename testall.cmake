9b9c968ca1cc88717c9da410e469ac828656e28a
Markus Makela <markus.makela@mariadb.com>
2015-01-26 09:07:33 +0200

    Fix to bug 704: http://bugs.mariadb.com/show_bug.cgi?id=704 The testall target now exits with the correct value.

---
+++ testall.cmake
@@ -0,0 +1,8 @@
+execute_process(COMMAND /bin/sh -c "${CMAKE_BINARY_DIR}/bin/maxscale -c ${CMAKE_BINARY_DIR} &>/dev/null")
+execute_process(COMMAND make test RESULT_VARIABLE RVAL)
+execute_process(COMMAND killall maxscale)
+if(NOT RVAL EQUAL 0)
+  message(FATAL_ERROR "Test suite failed with status: ${RVAL}")
+else()
+  message(STATUS "Test exited with status: ${RVAL}")
+endif()
