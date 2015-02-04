16b309c995ede74238c79dd11be3b45f69a176f7
Timofey Turenko <timofey.turenko@skysql.com>
2015-02-04 12:49:42 +0200

    add crash check to setup_binlog

--- setup_binlog.cpp
+++ setup_binlog.cpp
@@ -6,17 +6,21 @@
 #include <my_config.h>
 #include <iostream>
 #include "testconnections.h"
+#include "maxadmin_operations.h"
 
 int main()
 {
     TestConnections * Test = new TestConnections();
-    //global_result = 0;
+    int global_result = 0;
 
     Test->ReadEnv();
     Test->PrintIP();
 
     Test->repl->StartBinlog(Test->Maxscale_IP, 5306);
 
+    global_result += executeMaxadminCommand(Test->Maxscale_IP, (char *) "admin", (char *) "skysql", (char *) "show servers");
+
+    return(global_result);
 }
 
 
