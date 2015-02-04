add315d795ffc2b36b828fc5f65aaaad04169695
Mark Riddoch <Mark.Riddoch@skysql.com>
2015-02-04 11:17:29 +0000

    Updated to reflect the shange to the way servers are stored within services

--- server/modules/routing/binlog/blr_slave.c
+++ server/modules/routing/binlog/blr_slave.c
@@ -706,7 +706,7 @@ int	len, actual_len, col_len, seqno, ncols, i;
 	strncpy((char *)ptr, column, col_len);		// Result string
 	ptr += col_len;
 
-	sprintf(column, "%d", router->service->databases->port);
+	sprintf(column, "%d", router->service->dbref->server->port);
 	col_len = strlen(column);
 	*ptr++ = col_len;					// Length of result string
 	strncpy((char *)ptr, column, col_len);		// Result string
