3ecf9260923848d74293e850ae8cf03da7e126d1
Hartmut Holzgraefe <hartmut@php.net>
2015-02-14 15:34:11 +0100

    prevent signed/unsigned conflicts

--- server/core/modutil.c
+++ server/core/modutil.c
@@ -237,7 +237,7 @@ char *
 modutil_get_SQL(GWBUF *buf)
 {
 unsigned int	len, length;
-unsigned char	*ptr, *dptr, *rval = NULL;
+char	*ptr, *dptr, *rval = NULL;
 
 	if (!modutil_is_SQL(buf))
 		return rval;
