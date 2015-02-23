ce7537505aa20dbccc76e0d48b5fd2a0c81294ec
Markus Makela <markus.makela@mariadb.com>
2015-02-23 19:37:12 +0200

    Changed the RabbitMQ to be disabled by default.

--- macros.cmake
+++ macros.cmake
@@ -44,13 +44,13 @@ macro(set_variables)
 
   # password of MaxScale user
   set(TEST_PASSWORD "maxpwd" CACHE STRING "password of MaxScale user")
-  
+
   # Use static version of libmysqld
   set(STATIC_EMBEDDED TRUE CACHE BOOL "Use static version of libmysqld")
-  
+
   # Build RabbitMQ components
-  set(BUILD_RABBITMQ TRUE CACHE BOOL "Build RabbitMQ components")
-  
+  set(BUILD_RABBITMQ FALSE CACHE BOOL "Build RabbitMQ components")
+
   # Build the binlog router
   set(BUILD_BINLOG TRUE CACHE BOOL "Build binlog router")
 
