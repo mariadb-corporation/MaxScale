53181966ad437e75f2b81653c88ab54cd4dc3bf0
Timofey Turenko <timofey.turenko@skysql.com>
2015-02-28 12:49:44 +0200

    add more sleep to 519

--- bug519.cpp
+++ bug519.cpp
@@ -60,14 +60,14 @@ int main(int argc, char *argv[])
         printf("Dropping t1 \n");fflush(stdout);
         global_result += execute_query(Test->conn_rwsplit, (char *) "DROP TABLE t1;");
         printf("Sleeping to let replication happen\n");fflush(stdout);
-        sleep(30);
+        sleep(100);
         printf("Create t1\n"); fflush(stdout);
         create_t1(Test->conn_rwsplit);
         printf("Loading data to t1 from file\n");fflush(stdout);
         global_result += execute_query(srv[i], (char *) "LOAD DATA LOCAL INFILE 't1.csv' INTO TABLE t1;");
 
         printf("Sleeping to let replication happen\n");fflush(stdout);
-        sleep(30);
+        sleep(100);
         printf("SELECT: rwsplitter\n");fflush(stdout);
         global_result += select_from_t1(Test->conn_rwsplit, N);
         printf("SELECT: master\n");fflush(stdout);
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
         printf("SELECT: slave\n");fflush(stdout);
         global_result += select_from_t1(Test->conn_slave, N);
         printf("Sleeping to let replication happen\n");fflush(stdout);
-        sleep(30);
+        sleep(100);
         for (int i=0; i<Test->repl->N; i++) {
             printf("SELECT: directly from node %d\n", i);fflush(stdout);
             global_result += select_from_t1(Test->repl->nodes[i], N);
