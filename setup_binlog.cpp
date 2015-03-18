d324aa98d872c2b1683a3751df1eac471bbd0a07
Timofey Turenko <timofey.turenko@skysql.com>
2015-03-18 09:33:20 +0200

    fix cleanup binlog

--- setup_binlog.cpp
+++ setup_binlog.cpp
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
     Test->read_env();
     Test->print_env();
 
+    char sys1[1024];
     sprintf(&sys1[0], "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s 'rm -rf /usr/local/skysql/maxscale/Binlog_Service/*'", Test->maxscale_IP, Test->maxscale_sshkey);
     printf("%s\n", sys1);  fflush(stdout);
     global_result +=  system(sys1); fflush(stdout);
