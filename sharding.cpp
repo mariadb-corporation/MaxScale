347c94544975b57246c23fe3ac4eef35a6441ba4
Timofey Turenko <timofey.turenko@skysql.com>
2015-03-25 15:36:54 +0200

    add sharding

--- sharding.cpp
+++ sharding.cpp
@@ -15,6 +15,10 @@ int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
     int global_result = 0;
+    int i;
+    char str[256];
+    char user_str[256];
+    char pass_str[256];
 
     Test->repl->stop_slaves();
 
@@ -22,14 +26,29 @@ int main(int argc, char *argv[])
 
     Test->repl->connect();
 
-    for (int i = 0; i < Test->repl->N; i++) {
-        execute_query(Test->repl->nodes, "CREATE USER 'user%d'@'%%' IDENTIFIED BY 'pass%d';", i, i);
-        execute_query(Test->repl->nodes, "CREATE DATABASE 'db%d;", i);
-        execute_query(Test->repl->nodes, "GRANT SELECT,USAGE ON db%d.* TO 'user%d'@'%%'", i, i);
-
+    for (i = 0; i < Test->repl->N; i++) {
+        sprintf(str, "CREATE USER 'user%d'@'%%' IDENTIFIED BY 'pass%d';", i, i);
+        execute_query(Test->repl->nodes, str);
+        sprintf(str, "CREATE DATABASE 'db%d;", i);
+        execute_query(Test->repl->nodes, str);
+        sprintf(str, "GRANT SELECT,USAGE ON db%d.* TO 'user%d'@'%%'", i, i);
+        execute_query(Test->repl->nodes, str);
     }
 
-    global_result += check_maxscale_alive();
+    MYSQL conn[Test->repl->N];
+    for (i = 0; i < Test->repl->N; i++) {
+        sprintf(user_str, "user%d", i);
+        sprintf(pass_str, "pass%d", i);
+        conn[i] = open_conn(Test->rwsplit_port, Test->maxscale_IP, user_str, pass_str);
+
+        sprintf(str, "DROP TABLE IF EXISTS table%d", i);
+        execute_query(Test->repl->nodes, str);
+        sprintf(str, "CREATE TABLE table%d (x1 int, fl int);", i);
+        execute_query(Test->repl->nodes, str);
+    }
+
+
+    //global_result += check_maxscale_alive();
 
     Test->copy_all_logs(); return(global_result);
 }
