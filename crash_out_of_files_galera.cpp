21feb6e28d63b53495c5d66ad20530923be8c13f
Timofey Turenko <timofey.turenko@skysql.com>
2015-04-07 19:27:26 +0300

    fix repl in crash_out_ouf_files_galera

--- crash_out_of_files_galera.cpp
+++ crash_out_of_files_galera.cpp
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
     Test->read_env();
     Test->print_env();
 
-    Test->repl->connect();
+    Test->galera->connect();
     for (int i = 0; i < Test->galera->N; i++) {
         execute_query(Test->galera->nodes[i], (char *) "set global max_connections = 20;");
     }
-    Test->repl->close_connections();
+    Test->galera->close_connections();
 
 
     load(&new_inserts[0], &new_selects[0], &selects[0], &inserts[0], 100, Test, &i1, &i2, 0, true);
