327f22a0dcb5996c894b208649b167bdcb2b2344
Markus Makela <markus.makela@mariadb.com>
2015-04-24 06:04:46 +0300

    Added the release number into the maxscale version macro in macros.cmake and used it in CPack configuration.

--- macros.cmake
+++ macros.cmake
@@ -7,13 +7,15 @@ endfunction()
 
 macro(set_maxscale_version)
 
-  #MaxScale version number
+  # MaxScale version number
   set(MAXSCALE_VERSION_MAJOR "1")
   set(MAXSCALE_VERSION_MINOR "1")
   set(MAXSCALE_VERSION_PATCH "0")
   set(MAXSCALE_VERSION_NUMERIC "${MAXSCALE_VERSION_MAJOR}.${MAXSCALE_VERSION_MINOR}.${MAXSCALE_VERSION_PATCH}")
   set(MAXSCALE_VERSION "${MAXSCALE_VERSION_MAJOR}.${MAXSCALE_VERSION_MINOR}.${MAXSCALE_VERSION_PATCH}")
 
+  # This should be incremented each time a package is rebuilt
+  set(MAXSCALE_BUILD_NUMBER 2)
 endmacro()
 
 macro(set_variables)
