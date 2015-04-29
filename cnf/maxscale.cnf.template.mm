629b6b09a07b06e88d1057ed3555b61ffe52bc0a
Timofey Turenko <timofey.turenko@skysql.com>
2015-04-29 13:51:34 +0300

    fix  mm test

--- cnf/maxscale.cnf.template.mm
+++ cnf/maxscale.cnf.template.mm
@@ -141,17 +141,17 @@ address=###repl_server_IP_2###
 port=###repl_server_port_3###
 protocol=MySQLBackend
 
-[server3]
-type=server
-address=###repl_server_IP_3###
-port=###repl_server_port_3###
-protocol=MySQLBackend
+#[server3]
+#type=server
+#address=###repl_server_IP_3###
+#port=###repl_server_port_3###
+#protocol=MySQLBackend
 
-[server4]
-type=server
-address=###repl_server_IP_4###
-port=###repl_server_port_4###
-protocol=MySQLBackend
+#[server4]
+#type=server
+#address=###repl_server_IP_4###
+#port=###repl_server_port_4###
+#protocol=MySQLBackend
 
 [QLA]
 type=filter
