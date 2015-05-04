463cd4a97e2342ffd9c5e3895cd4ebdb38b0f095
Markus Makela <markus.makela@mariadb.com>
2015-05-04 07:04:31 +0300

    Moved common sharding functions to a separate file.

---
+++ server/modules/include/sharding_common.h
@@ -0,0 +1,18 @@
+#ifndef _SHARDING_COMMON_HG
+#define _SHARDING_COMMON_HG
+
+#include <poll.h>
+#include <buffer.h>
+#include <modutil.h>
+#include <mysql_client_server_protocol.h>
+#include <hashtable.h>
+#include <log_manager.h>
+#include <query_classifier.h>
+
+bool extract_database(GWBUF* buf, char* str);
+void create_error_reply(char* fail_str,DCB* dcb);
+bool change_current_db(MYSQL_session* mysql_session,
+			      HASHTABLE* dbhash,
+			      GWBUF* buf);
+
+#endif
