d7fcf37cabe2e139858c9ffda1909cb8c61ec906
Timofey Turenko <timofey.turenko@skysql.com>
2015-05-07 13:49:18 +0300

    add check checksum in binlog test

--- setup_binlog.cpp
+++ setup_binlog.cpp
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
     FILE *ls;
 
     char buf[1024];
+    char buf_max[1024];
 
     Test->read_env();
     Test->print_env();
@@ -44,25 +45,26 @@ int main(int argc, char *argv[])
 
         Test->repl->close_connections();
 
+        sprintf(sys, "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s 'sha1sum %s/Binlog_Service/mar-bin.000001'", Test->maxscale_sshkey, Test->maxscale_IP, Test->maxdir);
+        ls = popen(sys, "r");
+        fgets(buf_max, sizeof(buf), ls);
+        pclose(ls);
+        x = strchr(buf_max, ' '); x[0] = 0;
+        printf("Binlog checksum from Maxscale %s\n", buf_max);
+
         for (i = 0; i < Test->repl->N; i++) {
             sprintf(sys, "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s 'sha1sum /var/lib/mysql/mar-bin.000001'", Test->repl->sshkey[i], Test->repl->IP[i]);
-            //system(sys);
             ls = popen(sys, "r");
             fgets(buf, sizeof(buf), ls);
             pclose(ls);
-            printf("%s\n", buf);
             x = strchr(buf, ' '); x[0] = 0;
-            printf("%s\n", buf);
+            printf("Binlog checksum from node %d %s\n", i, buf);
+            if (strcmp(buf_max, buf) != 0) {
+                printf("Binlog from node %d checksum is not eqiual to binlog checksum from Maxscale node", i);
+            }
         }
     }
-    sprintf(sys, "ssh -i %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@%s 'sha1sum %s/Binlog_Service/mar-bin.000001'", Test->maxscale_sshkey, Test->maxscale_IP, Test->maxdir);
-    //system(sys);
-    ls = popen(sys, "r");
-    fgets(buf, sizeof(buf), ls);
-    pclose(ls);
-    printf("%s\n", buf);
-    x = strchr(buf, ' '); x[0] = 0;
-    printf("%s\n", buf);
+
 
     Test->copy_all_logs(); return(global_result);
 }
