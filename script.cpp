3c8ec34d7c908d4c3555d31c7211cefec02d44bc
Timofey Turenko <timofey.turenko@skysql.com>
2015-05-21 15:30:22 +0300

    fix type in script test

--- script.cpp
+++ script.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     Test->print_env();
 
     printf("Creating script on Maxscale machine\n"); fflush(stdout);
-    sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s '; rm /home/ec2-user/script_output; echo \"echo \\$* >> /home/ec2-user/script_output\" > /home/ec2-user/script.sh; chmod a+x /home/ec2-user/script.sh'", Test->maxscale_sshkey, Test->maxscale_IP);
+    sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'rm /home/ec2-user/script_output; echo \"echo \\$* >> /home/ec2-user/script_output\" > /home/ec2-user/script.sh; chmod a+x /home/ec2-user/script.sh'", Test->maxscale_sshkey, Test->maxscale_IP);
     system(str);
 
     printf("Block master node\n"); fflush(stdout);
