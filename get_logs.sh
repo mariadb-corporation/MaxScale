e563b6b33b51876a7b98ad58bb29da32e7f73a45
Timofey Turenko <timofey.turenko@skysql.com>
2015-05-22 00:10:36 +0300

    fix get_logs

--- get_logs.sh
+++ get_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-scp -i $maxscale_sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$maxscale_IP:$maxscale_log_dir/log/* .
+scp -i $maxscale_sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$maxscale_IP:$maxscale_log_dir/* .
 scp -i $maxscale_sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$maxscale_IP:$maxscale_cnf .
