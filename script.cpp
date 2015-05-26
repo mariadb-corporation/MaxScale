c28a646adf9e7b96f0760e674c2a2fe8e27446ac
Timofey Turenko <timofey.turenko@skysql.com>
2015-05-26 13:18:06 +0300

    add galera to script test

--- script.cpp
+++ script.cpp
@@ -1,17 +1,14 @@
 /**
  * @file script.cpp - test for running external script feature (MXS-121)
- * - setup Maxscale to execute script on folowing events: master_down,master_up, slave_down,   server_down  ,server_up,lost_master,lost_slave,new_master,new_slave
+ * - setup Maxscale to execute script on folowing events: master_down,master_up, slave_up,   server_down  ,server_up,lost_master,lost_slave,new_master,new_slave
  * - block master, unblock master, block node1, unblock node1
  * - expect following as a script output:
  * @verbatim
---event=new_master --initiator=server1 --nodelist=server1,server2,server3,server4
---event=new_slave --initiator=server2 --nodelist=server1,server2,server3,server4
---event=new_slave --initiator=server3 --nodelist=server1,server2,server3,server4
---event=new_slave --initiator=server4 --nodelist=server1,server2,server3,server4
 --event=master_down --initiator=server1 --nodelist=server1,server2,server3,server4
 --event=master_up --initiator=server1 --nodelist=server1,server2,server3,server4
---event=slave_down --initiator=server2 --nodelist=server1,server2,server3,server4
+--event=slave_up --initiator=server2 --nodelist=server1,server2,server3,server4
 @endverbatim
+ * - repeat test for Galera monitor
  * - make script non-executable
  * - block and unblocm node1
  * - check error log for 'Error: The file cannot be executed: /home/ec2-user/script.sh' error
@@ -23,24 +20,16 @@
 #include <iostream>
 #include "testconnections.h"
 
-int main(int argc, char *argv[])
+int test_script_monitor(TestConnections* Test, Mariadb_nodes* nodes)
 {
-    TestConnections * Test = new TestConnections(argc, argv);
     int global_result = 0;
-    int i;
     char str[256];
 
-    Test->read_env();
-    Test->print_env();
-
-    printf("Creating script on Maxscale machine\n"); fflush(stdout);
     sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'rm /home/ec2-user/script_output'", Test->maxscale_sshkey, Test->maxscale_IP);
-    system(str);
-    sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'echo \"echo \\$* >> /home/ec2-user/script_output\" > /home/ec2-user/script.sh; chmod a+x /home/ec2-user/script.sh'", Test->maxscale_sshkey, Test->maxscale_IP);
     system(str);
 
     printf("Block master node\n"); fflush(stdout);
-    Test->repl->block_node(0);
+    nodes->block_node(0);
 
     printf("Sleeping\n"); fflush(stdout);
     sleep(30);
@@ -52,13 +41,13 @@ int main(int argc, char *argv[])
     sleep(30);
 
     printf("Block node1\n"); fflush(stdout);
-    Test->repl->block_node(1);
+    nodes->block_node(1);
 
     printf("Sleeping\n"); fflush(stdout);
     sleep(30);
 
     printf("Unblock node1\n"); fflush(stdout);
-    Test->repl->unblock_node(1);
+    nodes->unblock_node(1);
 
     printf("Sleeping\n"); fflush(stdout);
     sleep(30);
@@ -79,6 +68,25 @@ int main(int argc, char *argv[])
     } else {
         printf("Script output is OK!\n");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    TestConnections * Test = new TestConnections(argc, argv);
+    int global_result = 0;
+    int i;
+    char str[256];
+
+    Test->read_env();
+    Test->print_env();
+
+    printf("Creating script on Maxscale machine\n"); fflush(stdout);
+
+    sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'echo \"echo \\$* >> /home/ec2-user/script_output\" > /home/ec2-user/script.sh; chmod a+x /home/ec2-user/script.sh'", Test->maxscale_sshkey, Test->maxscale_IP);
+    system(str);
+
+    global_result += test_script_monitor(Test, Test->repl);
+    global_result += test_script_monitor(Test, Test->galera);
 
     printf("Making script non-executable\n"); fflush(stdout);
     sprintf(str, "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'chmod a-x /home/ec2-user/script.sh'", Test->maxscale_sshkey, Test->maxscale_IP);
