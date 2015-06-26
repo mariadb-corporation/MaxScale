057dbf414a2c2b098c500f6b98ee0a1b82295c1e
Timofey Turenko <timofey.turenko@skysql.com>
2015-06-26 08:27:07 +0300

    remove start_without_root

--- start_without_root.sh
+++ start_without_root.sh
@@ -7,15 +7,16 @@ errmsg="MaxScale doesn't have write permission to MAXSCALE_HOME. Exiting"
 ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "$access_sudo service maxscale stop" &
 sleep 5
 
-ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bindir; ./maxscale -d -c ." 2>&1 | grep "$errmsg"
+conf_dir=$(dirname "${maxscale_cnf}")
+ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bin; ./maxscale -d -c $conf_dir" 2>&1 | grep "$errmsg"
 res=$?
-ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bindir; ./maxscale -d -c ." 2>&1 | grep "$errmsg"
+ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bin; ./maxscale -d -c $conf_dir" 2>&1 | grep "$errmsg"
 res1=$?
 
 if [[ $res != 0  || $res1 != 0 ]] ; then
 	echo "FAILED: no proper error message"
-	ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bindir; ./maxscale -d -c ." 
-	ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bindir; ./maxscale -c ." 
+	ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bin; ./maxscale -d -c $conf_dir" 
+	ssh -i $maxscale_sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $access_user@$maxscale_IP "cd $maxdir_bin; ./maxscale -c $cond_dir" 
 	$test_dir/copy_logs.sh start_without_root
 	exit 1
 fi
