01f6d695066d6e50064170fde04770976294b72f
Timofey Turenko <timofey.turenko@skysql.com>
2015-07-08 09:42:02 +0300

    add

--- configure_maxscale.sh
+++ configure_maxscale.sh
@@ -31,7 +31,7 @@ fi
 sed -i "s/###threads###/$threads/"  MaxScale.cnf
 
 if [ "x$mysql51_only" == "xyes" ] ; then
-	sed -i "s/module=mysqlmon/module=mysqlmon\nmysql51_only=true/"  MaxScale.cnf
+	sed -i "s/module=mysqlmon/module=mysqlmon\nmysql51_replication=true/"  MaxScale.cnf
 fi
 
 for prefix in "repl" "galera"
