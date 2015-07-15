260f829046409a77faa7aac7b6911d2311a3b23a
counterpoint <counterpoint@aliro.org>
2015-07-15 08:34:00 +0100

    Add blank line to start of Document Contents (link otherwise not rendered correctly by web site); move upgrade documents into their own directory; modify poll.c to fix problem relating to cloned DCBs.

--- server/core/poll.c
+++ server/core/poll.c
@@ -370,24 +370,25 @@ poll_remove_dcb(DCB *dcb)
 
         /**
          * Only positive fds can be removed from epoll set.
-         * Cloned DCBs are in the epoll set but do not have a valid file descriptor.
-         */
-        if (dcb->fd > 0) {
-	    rc = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, dcb->fd, &ev);
-	    /**
-	     * The poll_resolve_error function will always
-	     * return 0 or crash.  So if it returns non-zero result,
-	     * things have gone wrong and we crash.
-	     */
-	    if (rc) rc = poll_resolve_error(dcb, errno, false);
-	    if (rc) raise(SIGABRT);
+         * Cloned DCBs can have a state of DCB_STATE_POLLING but are not in 
+         * the epoll set and do not have a valid file descriptor.  Hence the
+         * only action for them is already done - the change of state to 
+         * DCB_STATE_NOPOLLING.
+         */		 
+        spinlock_release(&dcb->dcb_initlock);
+        if (dcb->fd > 0) 
+        {
+            rc = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, dcb->fd, &ev);
+            /**
+             * The poll_resolve_error function will always
+             * return 0 or crash.  So if it returns non-zero result, 
+             * things have gone wrong and we crash.
+             */
+            if (rc) rc = poll_resolve_error(dcb, errno, false);
+            if (rc) raise(SIGABRT);
+            /*< Set bit for each maxscale thread */
+            bitmask_copy(&dcb->memdata.bitmask, poll_bitmask());
         }
-	else
-	{
-	    rc = 0;
-	}
-	/*< Set bit for each maxscale thread */
-        bitmask_copy(&dcb->memdata.bitmask, poll_bitmask());
         return rc;
 }
 
