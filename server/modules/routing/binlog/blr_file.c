12ddd8e830f42aa4ca20e72194c315030b26a788
MassimilianoPinto <massimiliano.pinto@mariadb.com>
2015-08-09 20:34:45 +0200

    Added mariadb10 cache data
    Added mariadb10 cache data

--- server/modules/routing/binlog/blr_file.c
+++ server/modules/routing/binlog/blr_file.c
@@ -768,6 +768,7 @@ blr_cache_read_master_data(ROUTER_INSTANCE *router)
 	router->saved_master.selectvercom = blr_cache_read_response(router, "selectvercom");
 	router->saved_master.selecthostname = blr_cache_read_response(router, "selecthostname");
 	router->saved_master.map = blr_cache_read_response(router, "map");
+	router->saved_master.mariadb10 = blr_cache_read_response(router, "mariadb10");
 }
 
 
