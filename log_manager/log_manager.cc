2f9ae48f6a125337392735dafbb95b30f6db6984
Markus Makela <markus.makela@mariadb.com>
2015-08-26 12:49:00 +0300

    Fixed debug logs not working when logging and flushing.

--- log_manager/log_manager.cc
+++ log_manager/log_manager.cc
@@ -1395,7 +1395,7 @@ int skygw_log_write_flush(
         /**
          * Write log string to buffer and add to file write list.
          */
-        for (i = LOGFILE_FIRST; i<LOGFILE_LAST ;i <<=1)
+        for (i = LOGFILE_FIRST; i<=LOGFILE_LAST ;i <<=1)
         {
             /**
              * If particular log is disabled in general and it is not enabled for
