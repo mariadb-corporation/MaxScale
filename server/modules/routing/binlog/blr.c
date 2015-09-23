f7946a05fa74044e2857ed28d906ec3d7257d23b
MassimilianoPinto <massimiliano.pinto@mariadb.com>
2015-09-23 14:05:02 +0200

    Fix in extract_message
    Fix in extract_message

--- server/modules/routing/binlog/blr.c
+++ server/modules/routing/binlog/blr.c
@@ -1057,7 +1057,8 @@ int	len;
 		return NULL;
 	memcpy(rval, (char *)(errpkt->start) + 7, 6);
 	rval[6] = ' ';
-	memcpy(&rval[7], (char *)(errpkt->start) + 13, len - 8);
+	/* message size is len - (1 byte field count + 2 bytes errno + 6 bytes status) */
+	memcpy(&rval[7], (char *)(errpkt->start) + 13, len - 9);
 	rval[len-2] = 0;
 	return rval;
 }
