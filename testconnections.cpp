ca6d5ad5a971ac51e8773ac96678588560c1a5db
Timofey Turenko <timofey.turenko@skysql.com>
2015-11-04 15:20:11 +0200

    even more printing to pers_*

--- testconnections.cpp
+++ testconnections.cpp
@@ -618,26 +618,31 @@ int TestConnections::create_connections(int conn_N)
     tprintf("Opening %d connections to each router\n", conn_N);
     for (i = 0; i < conn_N; i++) {
         set_timeout(10);
-        tprintf("opening %d-connection: ", i);
-        printf("RWSplit ");
+        tprintf("opening %d-connection: ", i+1);
+        printf("RWSplit \t");
         rwsplit_conn[i] = open_rwsplit_connection();
         if (mysql_errno(rwsplit_conn[i]) != 0) {
             tprintf("%s\n", mysql_error(rwsplit_conn[i]));
         }
-        printf("ReadConn master ");
+        printf("ReadConn master \t");
         master_conn[i] = open_readconn_master_connection();
-        printf("ReadConn slave ");
+        printf("ReadConn slave \t");
         slave_conn[i] = open_readconn_slave_connection();
-        printf("galera ");
+        printf("galera \n");
         galera_conn[i] = open_conn(4016, maxscale_IP, maxscale_user, maxscale_password, ssl);
     }
     for (i = 0; i < conn_N; i++) {
         set_timeout(10);
+        tprintf("Trying query against %d-connection: ", i+1);
+        printf("RWSplit \t");
         if (execute_query(rwsplit_conn[i], "select 1;") != 0) {
             tprintf("Query failed!\n");
         }
+        printf("ReadConn master \t");
         execute_query(master_conn[i], "select 1;");
+        printf("ReadConn slave \t");
         execute_query(slave_conn[i], "select 1;");
+        printf("galera \n");
         execute_query(galera_conn[i], "select 1;");
     }
 
