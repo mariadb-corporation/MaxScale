126b4c1d79a9ef5af81801b4927300bfe6d7bc71
Johan Wikman <johan.wikman@mariadb.com>
2015-11-04 12:14:43 +0200

    Whitespace-fixes of maxkeys and maxpasswd.
    Whitespace and indentation fixes of maxkeys.c and maxpasswd.c.
    No other changes.

--- server/core/maxpasswd.c
+++ server/core/maxpasswd.c
@@ -22,74 +22,79 @@
  * @verbatim
  * Revision History
  *
- * Date		Who		Description
- * 24/07/13	Mark Riddoch	Initial implementation
+ * Date         Who             Description
+ * 24/07/13     Mark Riddoch    Initial implementation
  *
  * @endverbatim
  */
-#include	<stdio.h>
-#include	<secrets.h>
+#include <stdio.h>
+#include <secrets.h>
 #include <skygw_utils.h>
 #include <log_manager.h>
 /**
  * Encrypt a password for storing in the MaxScale.cnf file
  *
- * @param argc	Argument count
- * @param argv	Argument vector
+ * @param argc  Argument count
+ * @param argv  Argument vector
  */
 int
 main(int argc, char **argv)
 {
-	char	*enc, *pw;
-	int	arg_count = 6;
-	char	*home;
+    char  *enc;
+    char  *pw;
+    int    arg_count = 6;
+    char  *home;
     char** arg_vector;
-    int rval = 0;
+    int    rval = 0;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: %s <file> <password>\n", argv[0]);
-		return 1;
-	}
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <file> <password>\n", argv[0]);
+        return 1;
+    }
 
-    arg_vector = malloc(sizeof(char*)*(arg_count + 1));
+    arg_vector = malloc(sizeof(char*) * (arg_count + 1));
 
-	if(arg_vector == NULL)
-	{
-	    fprintf(stderr,"Error: Memory allocation failed.\n");
-	    return 1;
-	}
+    if (arg_vector == NULL)
+    {
+        fprintf(stderr,"Error: Memory allocation failed.\n");
+        return 1;
+    }
 
-	arg_vector[0] = strdup("logmanager");
-	arg_vector[1] = strdup("-j");
-	arg_vector[2] = strdup("/var/log/maxscale");
+    arg_vector[0] = strdup("logmanager");
+    arg_vector[1] = strdup("-j");
+    arg_vector[2] = strdup("/var/log/maxscale");
 
-	arg_vector[3] = "-o";
-	arg_vector[4] = "-l";
-	arg_vector[5] = "LOGFILE_ERROR";
-	arg_vector[6] = NULL;
-	skygw_logmanager_init(arg_count,arg_vector);
-	free(arg_vector[2]);
-	free(arg_vector);
-	
-	pw = calloc(81,sizeof(char));
+    arg_vector[3] = "-o";
+    arg_vector[4] = "-l";
+    arg_vector[5] = "LOGFILE_ERROR";
+    arg_vector[6] = NULL;
+    skygw_logmanager_init(arg_count, arg_vector);
+    free(arg_vector[2]);
+    free(arg_vector);
 
-	if(pw == NULL){
-		fprintf(stderr, "Error: cannot allocate enough memory.");
-		return 1;
-	}
+    pw = calloc(81, sizeof(char));
 
-	strncpy(pw,argv[2],80);
+    if (pw == NULL)
+    {
+        fprintf(stderr, "Error: cannot allocate enough memory.");
+        return 1;
+    }
 
-	if ((enc = encryptPassword(argv[1],pw)) != NULL){
-		printf("%s\n", enc);
-	}else{
-		fprintf(stderr, "Failed to encode the password\n");
-		rval = 1;
-	}
+    strncpy(pw, argv[2], 80);
 
-	free(pw);
-	skygw_log_sync_all();
-	skygw_logmanager_done();
-	return rval;
+    if ((enc = encryptPassword(argv[1], pw)) != NULL)
+    {
+        printf("%s\n", enc);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to encode the password\n");
+        rval = 1;
+    }
+
+    free(pw);
+    skygw_log_sync_all();
+    skygw_logmanager_done();
+    return rval;
 }
