0954949685b42d3f10d84b1c433b307bd40c4ea7
Timofey Turenko <timofey.turenko@skysql.com>
2015-11-05 16:49:02 +0200

    add sleep before checking log in schemarouter_duplicate_db

--- schemarouter_duplicate_db.cpp
+++ schemarouter_duplicate_db.cpp
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
 
+    Test->set_timeout(30);
     Test->connect_maxscale();
 
     /** Create a database on all nodes */
@@ -22,6 +23,8 @@ int main(int argc, char *argv[])
     execute_query(Test->conn_master, "CREATE DATABASE duplicate;");
 
     Test->add_result(execute_query(Test->conn_rwsplit, "SELECT 1") == 0, "Query should fail when duplicate database is found.");
+    Test->stop_timeout();
+    sleep(10);
     Test->check_log_err((char *) "Duplicate databases found", true);
 
     Test->copy_all_logs();
