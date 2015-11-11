2183d5d3c5f582ea82b5279ad1a127b879972edb
counterpoint <counterpoint@aliro.org>
2015-11-11 11:31:07 +0000

    Fix mistakes in merging.

--- server/core/buffer.c
+++ server/core/buffer.c
@@ -56,11 +56,6 @@
 static HASHTABLE *buffer_hashtable = NULL;
 #endif
 
-/** Defined in log_manager.cc */
-extern int            lm_enabled_logfiles_bitmask;
-extern size_t         log_ses_count[];
-extern __thread log_info_t tls_log_info;
-
 static buffer_object_t* gwbuf_remove_buffer_object(
         GWBUF*           buf,
         buffer_object_t* bufobj);
