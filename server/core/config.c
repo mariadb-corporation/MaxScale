37f8374a10817b8ae1d434099f58404aa8e955ca
Markus Makela <markus.makela@mariadb.com>
2015-11-21 15:21:23 +0200

    Fixed multiline configuration processing
    The regular expression used when cleaning multiline configuration parameters
    didn't match trailing backslash characters in pathnames. This caused them to be
    added to the next line causing a possible error.

--- server/core/config.c
+++ server/core/config.c
@@ -146,7 +146,7 @@ char* config_clean_string_list(char* str)
         int re_err;
         size_t err_offset;
 
-        if ((re = pcre2_compile((PCRE2_SPTR) "[[:space:],]*([^,]+)\\b[[:space:],]*", PCRE2_ZERO_TERMINATED, 0,
+        if ((re = pcre2_compile((PCRE2_SPTR) "[[:space:],]*([^,]*[^[:space:],])[[:space:],]*", PCRE2_ZERO_TERMINATED, 0,
                                 &re_err, &err_offset, NULL)) == NULL ||
             (data = pcre2_match_data_create_from_pattern(re, NULL)) == NULL)
         {
