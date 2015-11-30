d2aebe2bf05384ec1ea25ab3ad6806378381ef37
Johan Wikman <johan.wikman@mariadb.com>
2015-11-30 13:16:47 +0200

    Reindented server/core/gwdirs.c

--- server/core/gwdirs.c
+++ server/core/gwdirs.c
@@ -1,3 +1,21 @@
+/*
+ * This file is distributed as part of the MariaDB Corporation MaxScale.  It is free
+ * software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright MariaDB Corporation Ab 2013-2014
+ */
+
 #include <gwdirs.h>
 
 void set_configdir(char* str)
@@ -5,16 +23,19 @@ void set_configdir(char* str)
     free(configdir);
     configdir = str;
 }
+
 void set_logdir(char* str)
 {
     free(logdir);
     logdir = str;
 }
+
 void set_langdir(char* str)
 {
     free(langdir);
     langdir = str;
 }
+
 void set_piddir(char* str)
 {
     free(piddir);
@@ -27,13 +48,16 @@ void set_piddir(char* str)
  */
 char* get_libdir()
 {
-    return libdir?libdir:(char*)default_libdir;
+    return libdir ? libdir : (char*)default_libdir;
 }
 
 void set_libdir(char* param)
 {
-    if(libdir)
-	free(libdir);
+    if (libdir)
+    {
+        free(libdir);
+    }
+
     libdir = param;
 }
 /**
@@ -42,13 +66,16 @@ void set_libdir(char* param)
  */
 char* get_cachedir()
 {
-    return cachedir?cachedir:(char*)default_cachedir;
+    return cachedir ? cachedir : (char*)default_cachedir;
 }
 
 void set_cachedir(char* param)
 {
-    if(cachedir)
-	free(cachedir);
+    if (cachedir)
+    {
+        free(cachedir);
+    }
+
     cachedir = param;
 }
 
@@ -58,32 +85,35 @@ void set_cachedir(char* param)
  */
 char* get_datadir()
 {
-    return maxscaledatadir?maxscaledatadir:(char*)default_datadir;
+    return maxscaledatadir ? maxscaledatadir : (char*)default_datadir;
 }
 
 void set_datadir(char* param)
 {
-    if(maxscaledatadir)
-	free(maxscaledatadir);
+    if (maxscaledatadir)
+    {
+        free(maxscaledatadir);
+    }
+
     maxscaledatadir = param;
 }
 
 char* get_configdir()
 {
-    return configdir?configdir:(char*)default_configdir;
+    return configdir ? configdir : (char*)default_configdir;
 }
 
 char* get_piddir()
 {
-    return piddir?piddir:(char*)default_piddir;
+    return piddir ? piddir : (char*)default_piddir;
 }
 
 char* get_logdir()
 {
-    return logdir?logdir:(char*)default_logdir;
+    return logdir ? logdir : (char*)default_logdir;
 }
 
 char* get_langdir()
 {
-    return langdir?langdir:(char*)default_langdir;
+    return langdir ? langdir : (char*)default_langdir;
 }
