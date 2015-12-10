bf62fc51a26a3da62f6ab8fcd33ea325782b4f39
Timofey Turenko <timofey.turenko@skysql.com>
2015-12-10 16:21:52 +0200

    add load_balancing galera

---
+++ load_balancing_galera.cpp
@@ -0,0 +1,75 @@
+/**
+ * @file load_balancing_galera.cpp Checks how Maxscale balances load
+ *
+ * - start two groups of threads: each group consists of 25 threads, each thread creates connections to RWSplit,
+ * threads from first group try to execute as many SELECTs as possible, from second group - one query per second
+ * - after 100 seconds all threads are stopped
+ * - check number of connections to every slave: test PASSED if COM_SELECT difference between slaves is not greater then 3 times and no
+ * more then 10% of quesries went to Master
+ */
+
+
+#include <my_config.h>
+#include "testconnections.h"
+#include "sql_t1.h"
+#include "get_com_select_insert.h"
+
+#include "big_load.h"
+
+int main(int argc, char *argv[])
+{
+
+    TestConnections * Test = new TestConnections(argc, argv);
+    Test->set_timeout(20);
+    long int q;
+    int threads_num = 25;
+
+    long int selects[256];
+    long int inserts[256];
+    long int new_selects[256];
+    long int new_inserts[256];
+    long int i1, i2;
+
+    if (Test->smoke) {threads_num = 15;}
+    Test->galera->connect();
+    for (int i = 0; i < Test->galera->N; i++) {
+        execute_query(Test->galera->nodes[i], (char *) "set global max_connections = 300;");
+        execute_query(Test->galera->nodes[i], (char *) "set global max_connect_errors = 100000;");
+    }
+    Test->galera->close_connections();
+
+    Test->set_timeout(1200);
+    load(&new_inserts[0], &new_selects[0], &selects[0], &inserts[0], threads_num, Test, &i1, &i2, 1, TRUE, TRUE);
+
+    long int avr = (i1 + i2 ) / (Test->galera->N);
+    Test->tprintf("average number of quries per node %ld\n", avr);
+    long int min_q = avr / 3;
+    long int max_q = avr * 3;
+    Test->tprintf("Acceplable value for every node from %ld until %ld\n", min_q, max_q);
+
+    for (int i = 1; i < Test->galera->N; i++) {
+        q = new_selects[i] - selects[i];
+        if ((q > max_q) || (q < min_q)) {
+            Test->add_result(1, "number of queries for node %d is %ld\n", i+1, q);
+        }
+    }
+
+    if ((new_selects[0] - selects[0]) > avr / 3 ) {
+        Test->add_result(1, "number of queries for master greater then 30%% of averange number of queries per node\n");
+    }
+
+    Test->tprintf("Restoring nodes\n");
+    Test->galera->connect();
+    for (int i = 0; i < Test->galera->N; i++) {
+        execute_query(Test->galera->nodes[i], (char *) "flush hosts;");
+        execute_query(Test->galera->nodes[i], (char *) "set global max_connections = 151;");
+    }
+    Test->galera->close_connections();
+
+
+    Test->check_maxscale_alive();
+
+    Test->galera->start_replication();
+
+    Test->copy_all_logs(); return(Test->global_result);
+}
