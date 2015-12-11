1428571ad6f0c359e646f301cf664548337c8b22
Timofey Turenko <timofey.turenko@skysql.com>
2015-12-11 11:43:24 +0200

    add tread_id to open_close

--- open_close_connections.cpp
+++ open_close_connections.cpp
@@ -5,6 +5,7 @@
 
 typedef struct  {
     int exit_flag;
+    int thread_id;
     long i;
     int rwsplit_only;
     TestConnections * Test;
@@ -14,9 +15,10 @@ int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
     Test->set_timeout(20);
-    openclose_thread_data data;
 
     int threads_num = 20;
+    openclose_thread_data data[threads_num];
+    int i;
 
     int run_time=300;
 
@@ -24,29 +26,33 @@ int main(int argc, char *argv[])
         run_time=10;
     }
 
-    data.i = 0;
-    data.exit_flag = 0;
-    data.Test = Test;
-    data.rwsplit_only = 1;
+    for (i = 0; i < threads_num; i++) {
+        data[i].i = 0;
+        data[i].exit_flag = 0;
+        data[i].Test = Test;
+        data[i].rwsplit_only = 1;
+        data[i].thread_id = i;
+    }
 
     pthread_t thread1[threads_num];
 
     int  iret1[threads_num];
 
-    data.exit_flag=0;
     /* Create independent threads each of them will execute function */
-    for (int i = 0; i < threads_num; i++) {
-        iret1[i] = pthread_create( &thread1[i], NULL, query_thread1, &data);
+    for (i = 0; i < threads_num; i++) {
+        iret1[i] = pthread_create( &thread1[i], NULL, query_thread1, &data[i]);
     }
     Test->tprintf("Threads are running %d seconds \n", run_time);
+    for (i = 0; i < threads_num; i++) { data[i].rwsplit_only = 1;}
     Test->set_timeout(run_time + 20);
     sleep(run_time);
 
     Test->tprintf("all routers are involved, threads are running %d seconds more\n", run_time);
     Test->set_timeout(run_time + 20);
-    data.rwsplit_only = 0;
+
+    for (i = 0; i < threads_num; i++) { data[i].rwsplit_only = 0;}
     sleep(run_time);
-    data.exit_flag = 1;
+    for (i = 0; i < threads_num; i++) { data[i].exit_flag = 1;}
     sleep(5);
 
     Test->stop_timeout();
@@ -64,12 +70,12 @@ void *query_thread1( void *ptr )
 
     while (data->exit_flag == 0) {
         conn1 = data->Test->open_rwsplit_connection();
-        data->Test->add_result(mysql_errno(conn1), "Error opening RWsplit conn, iteration %d, error is %s\n", data->i, mysql_error(conn1));
+        data->Test->add_result(mysql_errno(conn1), "Error opening RWsplit conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn1));
         if (rw_o == 0) {
             conn2 = data->Test->open_readconn_master_connection();
-            data->Test->add_result(mysql_errno(conn2), "Error opening ReadConn master conn, iteration %d, error is %s\n", data->i, mysql_error(conn2));
+            data->Test->add_result(mysql_errno(conn2), "Error opening ReadConn master conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn2));
             conn3 = data->Test->open_readconn_slave_connection();
-            data->Test->add_result(mysql_errno(conn3), "Error opening ReadConn master conn, iteration %d, error is %s\n", data->i, mysql_error(conn3));
+            data->Test->add_result(mysql_errno(conn3), "Error opening ReadConn master conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn3));
         }
         if (conn1 != NULL) {mysql_close(conn1);}
         if (rw_o == 0) {
