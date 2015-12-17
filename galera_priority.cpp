4d862121aeee723419242e1ab4480b12d3fe3f65
Markus Makela <markus.makela@mariadb.com>
2015-12-17 05:49:53 +0200

    Fixed galera_priority test
    Fixed use of printf instead of sprintf.
    Rwsplit connections are now opened and closed before each set of queries.
    cleaned up code and added some error handling.

--- galera_priority.cpp
+++ galera_priority.cpp
@@ -7,7 +7,8 @@
  * The test executes a SELECT @@server_id to get the server id of each
  * node. The same query is executed in a transaction through MaxScale
  * and the server id should match the expected output depending on which
- * of the nodes are available.
+ * of the nodes are available. The simple test blocks nodes from highest priority
+ * to lowest priority.
  */
 
 #include <my_config.h>
@@ -19,72 +20,98 @@ using namespace std;
 int check_server_id(TestConnections* test, char *node_id)
 {
     char str[1024];
-
-    if (execute_query_silent(test->conn_rwsplit, "BEGIN") ||
+    int rval = 0;
+    if (execute_query(test->conn_rwsplit, "BEGIN") ||
         find_field(test->conn_rwsplit, "SELECT @@server_id", "@@server_id", str) ||
-        execute_query_silent(test->conn_rwsplit, "COMMIT"))
+        execute_query(test->conn_rwsplit, "COMMIT"))
     {
-        printf("Failed to query server_id through MaxScale.\n");
-        return 1;
+        printf("Failed to compare @@server_id.\n");
+        rval = 1;
     }
     else if (strcmp(node_id, str))
     {
-        printf("Node ID is %s instead of %s\n", node_id, str);
-        return 1;
+        printf("@@server_id is %s instead of %s\n", node_id, str);
+        rval = 1;
     }
-    return 0;
+    return rval;
 }
 
-int main(int argc, char **argv)
+int simple_failover(TestConnections* test)
 {
-    TestConnections *test = new TestConnections(argc, argv);
     test->galera->connect();
-
+    int rval = 0;
     char server_id[test->galera->N][1024];
 
     /** Get server_id for each node */
     for (int i = 0; i < test->galera->N; i++)
     {
-        printf(server_id[i], "%d", test->galera->get_server_id(i));
+        sprintf(server_id[i], "%d", test->galera->get_server_id(i));
     }
 
-    /** Node 3 should be master */
-    if (check_server_id(test, server_id[2]))
+    try
     {
-        return 1;
-    }
-    test->galera->block_node(2);
-    sleep(15);
+        /** Node 3 should be master */
+        if (test->connect_rwsplit() || check_server_id(test, server_id[2]))
+        {
+            throw;
+        }
+        test->close_rwsplit();
+        test->galera->block_node(2);
+        sleep(15);
 
-    /** Block node 3 and node 1 should be master */
-    if (check_server_id(test, server_id[0]))
-    {
-        return 1;
-    }
-    test->galera->block_node(0);
-    sleep(15);
+        /** Block node 3 and node 1 should be master */
+        if (test->connect_rwsplit() || check_server_id(test, server_id[0]))
+        {
+            throw;
+        }
+        test->close_rwsplit();
+        test->galera->block_node(0);
+        sleep(15);
 
-    /** Block node 1 and node 4 should be master */
-    if (check_server_id(test, server_id[3]))
-    {
-        return 1;
-    }
-    test->galera->block_node(3);
-    sleep(15);
+        /** Block node 1 and node 4 should be master */
+        if (test->connect_rwsplit() || check_server_id(test, server_id[3]))
+        {
+            throw;
+        }
+        test->close_rwsplit();
+        test->galera->block_node(3);
+        sleep(15);
 
-    /** Block node 4 and node 2 should be master */
-    if (check_server_id(test, server_id[1]))
-    {
-        return 1;
-    }
-    test->galera->block_node(1);
-    sleep(15);
+        /** Block node 4 and node 2 should be master */
+        if (test->connect_rwsplit() || check_server_id(test, server_id[1]))
+        {
+            throw;
+        }
+        test->close_rwsplit();
+        test->galera->block_node(1);
+        sleep(15);
 
-    /** All nodes blocked, expect failure */
-    if (execute_query(test->conn_rwsplit, "SELECT @@server_id") == 0)
-    {
-        printf("SELECT @@server_id was expected to fail but the query was successful.\n");
-        exit(1);
+        /** All nodes blocked, expect failure */
+        if (test->connect_rwsplit() || execute_query(test->conn_rwsplit, "SELECT @@server_id") == 0)
+        {
+            printf("SELECT @@server_id was expected to fail but the query was successful.\n");
+            throw;
+        }
+        test->close_rwsplit();
     }
-    return 0;
+    catch (...)
+    {
+        rval = 1;
+        test->close_rwsplit();
+    }
+
+    for (int i = 0; i < test->galera->N; i++)
+    {
+        test->galera->unblock_node(i);
+    }
+
+    return rval;
+}
+
+int main(int argc, char **argv)
+{
+    TestConnections *test = new TestConnections(argc, argv);
+    int rval = 0;
+    rval += simple_failover(test);
+    return rval;
 }
