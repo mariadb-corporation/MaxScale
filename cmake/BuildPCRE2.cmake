080a9ebc9db61085372a1177bcb27247651aaf06
Markus Makela <markus.makela@mariadb.com>
2015-12-17 23:03:22 +0200

    PCRE2 is now statically linked
    Since the PCRE2 library was always going to be a part of MaxScale, there was
    no real reason to have it as a shared library apart from smaller binaries.

--- cmake/BuildPCRE2.cmake
+++ cmake/BuildPCRE2.cmake
@@ -1,23 +1,18 @@
 # Build the PCRE2 library from source
 #
-# This will add a 'pcre2' target to CMake which will generate the libpcre2-8.so
-# dynamic library and the pcre2.h header. If your target requires PCRE2 you
+# This will add a 'pcre2' target to CMake which will generate the libpcre2-8.a
+# static library and the pcre2.h header. If your target requires PCRE2 you
 # need to add a dependeny on the 'pcre2' target by adding add_dependencies(<target> pcre2)
-# to the CMakeLists.txt
+# to the CMakeLists.txt. You don't need to link against the pcre2 library
+# because the static symbols will be in MaxScale.
 
 include(ExternalProject)
 
-set(PCRE2_ROOT_DIR ${CMAKE_SOURCE_DIR}/pcre2/)
-set(PCRE2_BUILD_DIR ${CMAKE_BINARY_DIR}/pcre2/)
-set(PCRE2_LIBRARIES ${CMAKE_BINARY_DIR}/pcre2/libpcre2-8.so
-  ${CMAKE_BINARY_DIR}/pcre2/libpcre2-8.so.1.0.0
-  CACHE STRING "PCRE2 dynamic libraries" FORCE)
-
-ExternalProject_Add(pcre2 SOURCE_DIR ${PCRE2_ROOT_DIR}
-  CMAKE_ARGS -DBUILD_SHARED_LIBS=Y -DPCRE2_BUILD_PCRE2GREP=N -DPCRE2_BUILD_TESTS=N
-  BINARY_DIR ${PCRE2_BUILD_DIR}
+ExternalProject_Add(pcre2 SOURCE_DIR ${CMAKE_SOURCE_DIR}/pcre2/
+  CMAKE_ARGS -DBUILD_SHARED_LIBS=N -DPCRE2_BUILD_PCRE2GREP=N  -DPCRE2_BUILD_TESTS=N
+  BINARY_DIR ${CMAKE_BINARY_DIR}/pcre2/
   BUILD_COMMAND make
   INSTALL_COMMAND "")
 
 include_directories(${CMAKE_BINARY_DIR}/pcre2/)
-install(PROGRAMS ${PCRE2_LIBRARIES} DESTINATION ${MAXSCALE_LIBDIR})
+set(PCRE2_LIBRARIES ${CMAKE_BINARY_DIR}/pcre2/libpcre2-8.a CACHE INTERNAL "")
