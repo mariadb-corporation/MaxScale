822d4410c8ac00f05e772a3c16638c608aef007d
Timofey Turenko <timofey.turenko@skysql.com>
2015-12-31 12:46:47 +0200

    extract test into fuction in longblob

--- longblob.cpp
+++ longblob.cpp
@@ -5,10 +5,8 @@
 #include <my_config.h>
 #include "testconnections.h"
 
-int main(int argc, char *argv[])
+int test_longblob(TestConnections* Test, MYSQL * conn)
 {
-    TestConnections * Test = new TestConnections(argc, argv);
-    Test->set_timeout(60);
     unsigned long size = 1000000;
     unsigned long * data;
     unsigned long i;
@@ -16,16 +14,16 @@ int main(int argc, char *argv[])
 
     char *insert_stmt = (char *) "INSERT INTO long_blob_table(x, b) VALUES(1, ?)";
 
-    Test->connect_maxscale();
+
 
     Test->tprintf("Creating table with LONGBLOB\n");
-    Test->try_query(Test->conn_rwsplit, (char *) "DROP TABLE IF EXISTS long_blob_table");
-    Test->try_query(Test->conn_rwsplit, (char *) "CREATE TABLE long_blob_table(x INT, b LONGBLOB)");
+    Test->try_query(conn, (char *) "DROP TABLE IF EXISTS long_blob_table");
+    Test->try_query(conn, (char *) "CREATE TABLE long_blob_table(x INT, b LONGBLOB)");
     Test->tprintf("set global max_allowed_packet=10000000\n");
-    Test->try_query(Test->conn_rwsplit, (char *) "set global max_allowed_packet=10000000");
+    Test->try_query(conn, (char *) "set global max_allowed_packet=10000000");
 
     Test->tprintf("Preparintg INSERT stmt\n");
-    MYSQL_STMT * stmt = mysql_stmt_init(Test->conn_rwsplit);
+    MYSQL_STMT * stmt = mysql_stmt_init(conn);
 
     Test->add_result(mysql_stmt_prepare(stmt, insert_stmt, strlen(insert_stmt)), "Error preparing stmt: %s\n", mysql_stmt_error(stmt));
 
@@ -54,6 +52,19 @@ int main(int argc, char *argv[])
     }
 
     Test->add_result(mysql_stmt_execute(stmt), mysql_stmt_error(stmt));
+    Test->add_result(mysql_stmt_close(stmt), "Error closing stmt\n");
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    TestConnections * Test = new TestConnections(argc, argv);
+    Test->set_timeout(60);
+    Test->connect_maxscale();
+
+    test_longblob(Test, Test->conn_rwsplit);
+
 
     Test->copy_all_logs(); return(Test->global_result);
 }
