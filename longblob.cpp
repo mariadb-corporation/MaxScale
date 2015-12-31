2872c1117ac755ee97416bb8654e796095b35ad5
Timofey Turenko <timofey.turenko@skysql.com>
2015-12-31 10:18:42 +0200

    add printing to longblob test

--- longblob.cpp
+++ longblob.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
     Test->set_timeout(60);
-    long int size = 100000;
+    long int size = 10000000;
     long int * data;
     long int i;
     MYSQL_BIND param[1];
@@ -18,17 +18,20 @@ int main(int argc, char *argv[])
 
     Test->connect_maxscale();
 
+    Test->tprintf("Creating table with LONGBLOB\n");
     execute_query(Test->conn_rwsplit, (char *) "DROP TABLE IF EXISTS long_blob_table");
-    execute_query(Test->conn_rwsplit, (char *) "CREATE TABLE long_blob_table(x INT, b LONGBLOB");
+    execute_query(Test->conn_rwsplit, (char *) "CREATE TABLE long_blob_table(x INT, b LONGBLOB)");
 
+    Test->tprintf("Preparintg INSERT stmt\n");
     MYSQL_STMT * stmt = mysql_stmt_init(Test->conn_rwsplit);
 
-    mysql_stmt_prepare(stmt, insert_stmt, strlen(insert_stmt));
+    Test->add_result(mysql_stmt_prepare(stmt, insert_stmt, strlen(insert_stmt)), "Error preparing stmt: %s\n", mysql_stmt_error(stmt));
 
     param[0].buffer_type = MYSQL_TYPE_STRING;
     param[0].is_null = 0;
 
-    mysql_stmt_bind_param(stmt, param);
+    Test->tprintf("Binding parameter\n");
+    Test->add_result(mysql_stmt_bind_param(stmt, param), "Error parameter binding: %s\n", mysql_stmt_error(stmt));
 
     data = (long int *) malloc(size * sizeof(long int));
 
@@ -37,9 +40,9 @@ int main(int argc, char *argv[])
         data[i] = i;
     }
 
-
+    Test->tprintf("Sending data in %d chancks\n", sizeof(data));
     for (i = 0; i < 1000; i++) {
-        mysql_stmt_send_long_data(stmt, 0, (char *) data, sizeof(data));
+        Test->add_result(mysql_stmt_send_long_data(stmt, 0, (char *) data, sizeof(data)), "Error inserting data, iteration %d, error %s\n", i, mysql_stmt_error(stmt));
     }
 
     Test->add_result(mysql_stmt_execute(stmt), mysql_stmt_error(stmt));
