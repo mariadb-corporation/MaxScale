9f72ffd94d2a116694ce69e0e518a18e9bdeb537
Markus Makela <markus.makela@mariadb.com>
2016-02-15 19:17:53 +0200

    Added simple configuration tested
    This allows configuration errors to be checked in a very simple way.

---
+++ config_check.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file config_check.cpp Simple configuration checking class
+ */
+
+
+#include <my_config.h>
+#include <iostream>
+#include <unistd.h>
+#include "testconnections.h"
+
+using namespace std;
+
+bool test_config_works(const char* config, const char **expected_output = NULL)
+{
+    const char *argv[] =
+        {
+            config,
+            NULL
+        };
+
+    TestConnections * Test = new TestConnections(1, (char**)argv);
+    Test->set_timeout(10);
+    if (expected_output)
+    {
+        Test->check_log_err((char *) "Unsupported router option \"slave\"", TRUE);
+        Test->check_log_err((char *) "Couldn't find suitable Master", FALSE);
+    }
+    Test->check_maxscale_alive();
+    Test->copy_all_logs();
+    return Test->global_result == 0;
+}
---
+++ config_check.h
@@ -0,0 +1,4 @@
+#ifndef _CHECK_CONFIG
+#define _CHECK_CONFIG
+bool test_config_works(const char* config, const char **expected_output = NULL);
+#endif
