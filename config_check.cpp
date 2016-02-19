d9507e874ed629874179bbc46ba9317b568cf3e5
Markus Makela <markus.makela@mariadb.com>
2016-02-19 12:07:01 +0200

    Configuration checking function now forks a subprocess to test the configuration
    This will allow the output to be cleanly suppressed which keeps the test output cleaner.

--- config_check.cpp
+++ config_check.cpp
@@ -1,5 +1,5 @@
 /**
- * @file config_check.cpp Simple configuration checking class
+ * @file config_check.cpp Simple configuration checking
  */
 
 
@@ -7,26 +7,54 @@
 #include <iostream>
 #include <unistd.h>
 #include "testconnections.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
+/**
+* @brief Check if MaxScale starts with the provided configuration template
+*
+* @param config Configuration template name
+* @param expected_output If the parameter is not NULL, it is expected to contain
+* an array of null-terminated strings with the last element set to NULL
+* @return True if MaxScale is running and all strings in @p expected_output are
+* found from the log otherwise false
+*/
 bool test_config_works(const char* config, const char **expected_output = NULL)
 {
     const char *argv[] =
         {
             config,
-            "--quiet",
             NULL
         };
+    pid_t pid = fork();
 
-    TestConnections * Test = new TestConnections(2, (char**)argv);
-    Test->set_timeout(10);
-    if (expected_output)
+    if (pid == 0)
     {
-        Test->check_log_err((char *) "Unsupported router option \"slave\"", TRUE);
-        Test->check_log_err((char *) "Couldn't find suitable Master", FALSE);
+        char testname[1024];
+        sprintf(testname, "%s-%d-%u.log", config, getpid(), time(NULL));
+        freopen(testname, "w", stdout);
+        freopen(testname, "w", stderr);
+        TestConnections * Test = new TestConnections(1, (char**)argv);
+        Test->set_timeout(10);
+        for (int i = 0; expected_output[i]; i++)
+        {
+            Test->check_log_err((char *)expected_output[i], TRUE);
+        }
+        Test->check_maxscale_alive();
+        Test->copy_all_logs();
+        _exit(Test->global_result);
+        return false;
     }
-    Test->check_maxscale_alive();
-    Test->copy_all_logs();
-    return Test->global_result == 0;
+    else if (pid > 0)
+    {
+        int rc = 0;
+        wait(&rc);
+        return rc == 0;
+    }
+    else
+    {
+        return false;
+    }
 }
