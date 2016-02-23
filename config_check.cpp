b35d88a1ce14f3d0cb50042ea2aa9a79916ca011
Markus Makela <markus.makela@mariadb.com>
2016-02-23 10:30:16 +0200

    Added more output to config_check.cpp

--- config_check.cpp
+++ config_check.cpp
@@ -51,6 +51,7 @@ bool test_config_works(const char* config, const char **expected_output = NULL)
     {
         int rc = 0;
         wait(&rc);
+        printf("Process exited with status %d\n", rc);
         return rc == 0;
     }
     else
