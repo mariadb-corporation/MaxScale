2ee7ad486738c48abd26187f3ce5a6ca3b3c737d
Markus Makela <markus.makela@mariadb.com>
2016-03-02 08:50:12 +0200

    Changed multistatements in bug694 to individual statements
    This will prevent triggering the multistatement handling in readwritesplit.

--- bug694.cpp
+++ bug694.cpp
@@ -23,19 +23,23 @@ int main(int argc, char *argv[])
 
     Test->connect_maxscale();
 
-    Test->tprintf("Trying SELECT @a:=@a+1 as a, test.b FROM test\n");
-    Test->try_query(Test->conn_rwsplit, "DROP TABLE IF EXISTS test; CREATE TABLE test (b integer);");
-    for (int i=0; i<10000;i++) {
+    Test->try_query(Test->conn_rwsplit, "USE test");
+    Test->try_query(Test->conn_rwsplit, "DROP TABLE IF EXISTS test");
+    Test->try_query(Test->conn_rwsplit, "CREATE TABLE test (b integer)");
+
+    for (int i=0; i < Test->smoke ? 10 : 100; i++) {
         Test->set_timeout(5);
         execute_query(Test->conn_rwsplit, "insert into test value(2);");
     }
+
+    Test->tprintf("Trying SELECT @a:=@a+1 as a, test.b FROM test\n");
     if (execute_query(Test->conn_rwsplit, "SELECT @a:=@a+1 as a, test.b FROM test;") == 0) {
         Test->add_result(1, "Query succeded, but expected to fail.\n");
     }
     Test->tprintf("Trying USE test\n");
     Test->try_query(Test->conn_rwsplit, "USE test");
 
-    Test->try_query(Test->conn_rwsplit, "DROP TABLE IF EXISTS test;");
+    Test->try_query(Test->conn_rwsplit, "DROP TABLE IF EXISTS test");
 
     Test->tprintf("Checking if MaxScale alive\n");
     Test->close_maxscale_connections();
