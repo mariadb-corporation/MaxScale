f0956276aee98cde22dc74feb27c0913da2268d7
Markus Makela <markus.makela@mariadb.com>
2016-03-05 06:04:36 +0200

    Fixed FindMariaDBConnector.cmake failing even if connector is installed
    This will remove the need to build the connector if it already exists as
    a system library.

--- cmake/FindMariaDBConnector.cmake
+++ cmake/FindMariaDBConnector.cmake
@@ -6,7 +6,7 @@
 # MARIADB_CONNECTOR_STATIC_LIBRARIES - The static connector libraries
 # MARIADB_CONNECTOR_INCLUDE_DIR - The connector headers
 
-find_library(MARIADB_CONNECTOR_LIBRARIES NAMES mysqlclient PATH_SUFFIXES mysql mariadb)
+find_library(MARIADB_CONNECTOR_LIBRARIES NAMES mysqlclient PATH_SUFFIXES mariadb mysql)
 if(${MARIADB_CONNECTOR_LIBRARIES} MATCHES "NOTFOUND")
   set(MARIADB_CONNECTOR_FOUND FALSE CACHE INTERNAL "")
   message(STATUS "Dynamic MySQL client library not found.")
@@ -18,7 +18,7 @@ endif()
 
 set(OLD_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-find_library(MARIADB_CONNECTOR_STATIC_LIBRARIES NAMES mysqlclient PATH_SUFFIXES mysql mariadb)
+find_library(MARIADB_CONNECTOR_STATIC_LIBRARIES NAMES mysqlclient PATH_SUFFIXES mariadb mysql)
 set(CMAKE_FIND_LIBRARY_SUFFIXES ${OLD_SUFFIXES})
 
 if(${MARIADB_CONNECTOR_STATIC_LIBRARIES} MATCHES "NOTFOUND")
@@ -30,10 +30,13 @@ else()
   message(STATUS "Found statc MySQL client library: ${MARIADB_CONNECTOR_STATIC_LIBRARIES}")
 endif()
 
-find_path(MARIADB_CONNECTOR_INCLUDE_DIR mysql.h PATH_SUFFIXES mysql mariadb)
+find_path(MARIADB_CONNECTOR_INCLUDE_DIR mysql.h PATH_SUFFIXES mariadb mysql)
 
-include(CheckSymbolExists)
-check_symbol_exists(LIBMARIADB ${MARIADB_CONNECTOR_INCLUDE_DIR}/mysql.h HAVE_MARIADB_CONNECTOR)
+if(NOT (${MARIADB_CONNECTOR_INCLUDE_DIR}  MATCHES "NOTFOUND"))
+  include(CheckSymbolExists)
+  set(CMAKE_REQUIRED_INCLUDES ${MARIADB_CONNECTOR_INCLUDE_DIR})
+  check_symbol_exists(LIBMARIADB mysql.h HAVE_MARIADB_CONNECTOR)
+endif()
 
 if(HAVE_MARIADB_CONNECTOR)
   message(STATUS "Found MariaDB Connector-C")
