29c9b3bdda703c696114ceceeaec1b7edab5a3cf
Timofey Turenko <timofey.turenko@skysql.com>
2016-03-11 21:01:02 +0200

    temporal hack for open_close_connections

--- open_close_connections.cpp
+++ open_close_connections.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file open_close_connections.cpp Simple test which creates load which is very short sessions
+ *
+ * - 20 threads are opening and immediatelly closing connection in the loop
+ */
+
 #include <my_config.h>
 #include "testconnections.h"
 //#include "sql_t1.h"
@@ -11,12 +17,13 @@ typedef struct  {
     TestConnections * Test;
 } openclose_thread_data;
 void *query_thread1( void *ptr );
+int threads_num = 20;
 int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
     Test->set_timeout(20);
 
-    int threads_num = 20;
+
     openclose_thread_data data[threads_num];
     int i;
 
@@ -38,9 +45,9 @@ int main(int argc, char *argv[])
 
     int  iret1[threads_num];
 
-    Test->repl->execute_query_all_nodes((char *) "set global max_connections = 500;");
+    Test->repl->execute_query_all_nodes((char *) "set global max_connections = 50000;");
     Test->connect_maxscale();
-    Test->try_query(Test->conn_rwsplit, (char *) "set global max_connections = 500;");
+    Test->try_query(Test->conn_rwsplit, (char *) "set global max_connections = 50000;");
     Test->close_maxscale_connections();
 
     /* Create independent threads each of them will execute function */
@@ -78,24 +85,65 @@ void *query_thread1( void *ptr )
     MYSQL * conn1;
     MYSQL * conn2;
     MYSQL * conn3;
+
+    int k;
+
+    unsigned int m_errno;
     openclose_thread_data * data = (openclose_thread_data *) ptr;
+    MYSQL * conn_node[data->Test->repl->N];
     int rw_o = data->rwsplit_only;
+    //sleep(data->thread_id);
+    for (k = 0; k < data->Test->repl->N; k++)
+    {
+        conn_node[k] = open_conn(data->Test->repl->port[k],
+                                 data->Test->repl->IP[k],
+                                 data->Test->repl->user_name,
+                                 data->Test->repl->password,
+                                 data->Test->repl->ssl);
+    }
 
     while (data->exit_flag == 0 && data->Test->global_result == 0) {
         conn1 = data->Test->open_rwsplit_connection();
-        data->Test->add_result(mysql_errno(conn1), "Error opening RWsplit conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn1));
+        m_errno = mysql_errno(conn1);
+        if (m_errno != 0)
+        {
+            data->Test->add_result(1, "Error opening RWsplit conn, thread num is %d, iteration %d, error is: %s\n",
+                                   data->thread_id, data->i, mysql_error(conn1));
+            for (k = 0; k < data->Test->repl->N; k++)
+            {
+                data->Test->tprintf("conn to node%d is %u (thread is is %d)\n",
+                                    k,
+                                    get_conn_num(conn_node[k], data->Test->maxscale_IP, data->Test->maxscale_hostname, (char *) "test"),
+                                    data->thread_id);
+            }
+        }
         if (rw_o == 0) {
             conn2 = data->Test->open_readconn_master_connection();
             data->Test->add_result(mysql_errno(conn2), "Error opening ReadConn master conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn2));
             conn3 = data->Test->open_readconn_slave_connection();
             data->Test->add_result(mysql_errno(conn3), "Error opening ReadConn master conn, thread num is %d, iteration %d, error is: %s\n", data->thread_id, data->i, mysql_error(conn3));
         }
-        if (conn1 != NULL) {mysql_close(conn1);}
+        /*if (data->i > 20)
+        {
+            data->Test->tprintf("thread %d is waiting\n", data->thread_id);
+            while (get_conn_num(conn_node[0], data->Test->maxscale_IP, data->Test->maxscale_hostname, (char *) "test") > threads_num)
+            {
+                sleep(5);
+            }
+            data->Test->tprintf("thread %d continues\n", data->thread_id);
+        }*/
+
+        // USE test here is a hack to prevent Maxscale from failure; should be removed when fixed
+        if (conn1 != NULL) {data->Test->try_query(conn1, (char*) "USE test");mysql_close(conn1);}
         if (rw_o == 0) {
-            if (conn2 != NULL) {mysql_close(conn2);}
-            if (conn3 != NULL) {mysql_close(conn3);}
+            if (conn2 != NULL) {data->Test->try_query(conn2, (char*) "USE test");mysql_close(conn2);}
+            if (conn3 != NULL) {data->Test->try_query(conn3, (char*) "USE test");mysql_close(conn3);}
         }
-        data->i++;
+        data->i++; //if ((data->i / 10) * 10 == data->i) {sleep(10);}
+    }
+    for (k = 0; k < data->Test->repl->N; k++)
+    {
+        mysql_close(conn_node[k]);
     }
     return NULL;
 }
