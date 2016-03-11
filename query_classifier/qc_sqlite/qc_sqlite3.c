b7880f14242cbe164e4a2ea62f0b1f9dcafe3315
Johan Wikman <johan.wikman@mariadb.com>
2016-03-11 13:22:40 +0200

    Fix qc_sqlite CMakeLists.txt
    Adding a generated file as source to add_library() proved to be
    quite difficult. So instead, there's a static file (qc_sqlite3.c)
    that includes the generated file (sqlite3.c).

---
+++ query_classifier/qc_sqlite/qc_sqlite3.c
@@ -0,0 +1,7 @@
+/*
+ * The sqlite3.c included here, is the sqlite3.c file generated as part
+ * of the building of the target maxscale_sqlite (see CMakeLists.txt).
+ *
+ * It proved to be difficult to otherwise get cmake to do the right thing.
+ */
+#include <sqlite3.c>
