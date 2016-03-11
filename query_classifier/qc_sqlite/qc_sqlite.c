cb646ca92b4ba42f3f225970f2f4029fbf856b49
Johan Wikman <johan.wikman@mariadb.com>
2016-03-11 10:36:50 +0200

    Add minimal select recognition to qc_sqlite
    This change now implements in principle the basic mechanism of the
    sqlite based query classifier.
    
    When a information about a query residing in a GWBUF is asked for,
    the procedure (if the query has not been parsed yet) is as follows
    (see parse_query):
    
    - Allocate a QC_SQLITE_INFO object for holding information about
      the query. The status is set to QC_INFO_INVALID.
    - Store a pointer to the info object in the thread local struct
      this_thread.
    - Call sqlite3_prepare(). That'll parse the query and in the end one
      of our replacement functions will be called (currently only
      qc_sqlite3Select).
    - In that function we collect information about the query and
      set the info status to QC_INFO_OK.
    - That is, when sqlite3_prepare() returns, if the status field in
      the info object is QC_INFO_OK, we know the call chain has
      reached one of our "overriding" functions and that we have
      collected information about the query.
    - The info object, valid or not, is always added to the GWBUF.
      If the parsing failed this time, it will fail next time as well.
    - Thereafter, if more information is asked about the query, we can
      consult the info object directly.
    
    The following step is to create a test-suite for comparing the
    behaviour of the MySQL based query classifier (qc_mysqlembedded)
    and this, and then fix discrepancies.

--- query_classifier/qc_sqlite/qc_sqlite.c
+++ query_classifier/qc_sqlite/qc_sqlite.c
@@ -25,6 +25,7 @@
 #include <sqliteInt.h>
 #include <log_manager.h>
 #include <modinfo.h>
+#include <mysql_client_server_protocol.h>
 #include <platform.h>
 #include <query_classifier.h>
 
@@ -37,16 +38,200 @@
 #define QC_TRACE()
 #endif
 
+/**
+ * The status of the information in QC_SQLITE_INFO.
+ */
+typedef enum qc_info_status
+{
+    QC_INFO_OK      = 0, // The information is valid.
+    QC_INFO_INVALID = 1  // The information is invalid.
+} qc_info_status_t;
+
+/**
+ * Contains information about a particular query.
+ */
+typedef struct qc_sqlite_info
+{
+    qc_info_status_t status;  // The validity of the information in this structure.
+    // TODO: More to be added.
+    qc_query_op_t operation;
+} QC_SQLITE_INFO;
+
+/**
+ * The state of qc_sqlite.
+ */
 static struct
 {
     bool initialized;
 } this_unit;
 
+/**
+ * The qc_sqlite thread-specific state.
+ */
 static thread_local struct
 {
     bool initialized;
-    sqlite3* db;
+    sqlite3* db;      // Thread specific database handle.
+    QC_SQLITE_INFO* info;
 } this_thread;
+
+
+/**
+ * HELPERS
+ */
+
+static void buffer_object_free(void* data);
+static bool ensure_query_is_parsed(GWBUF* query);
+static QC_SQLITE_INFO* get_query_info(GWBUF* query);
+static QC_SQLITE_INFO* info_alloc(void);
+static void info_finish(QC_SQLITE_INFO* info);
+static void info_free(QC_SQLITE_INFO* info);
+static QC_SQLITE_INFO* info_init(QC_SQLITE_INFO* info);
+static bool parse_query(GWBUF* query);
+static void parse_query_string(const char* query, size_t len);
+static bool query_is_parsed(GWBUF* query);
+
+
+/**
+ * Used for freeing a QC_SQLITE_INFO object added to a GWBUF.
+ *
+ * @param object A pointer to a QC_SQLITE_INFO object.
+ */
+static void buffer_object_free(void* data)
+{
+    info_free((QC_SQLITE_INFO*) data);
+}
+
+static bool ensure_query_is_parsed(GWBUF* query)
+{
+    bool parsed = query_is_parsed(query);
+
+    if (!parsed)
+    {
+        parsed = parse_query(query);
+    }
+
+    return parsed;
+}
+
+static QC_SQLITE_INFO* get_query_info(GWBUF* query)
+{
+    QC_SQLITE_INFO* info = NULL;
+
+    if (ensure_query_is_parsed(query))
+    {
+        info = (QC_SQLITE_INFO*) gwbuf_get_buffer_object_data(query, GWBUF_PARSING_INFO);
+        ss_dassert(info);
+    }
+
+    return info;
+}
+
+static QC_SQLITE_INFO* info_alloc(void)
+{
+    QC_SQLITE_INFO* info = malloc(sizeof(*info));
+
+    if (info)
+    {
+        info_init(info);
+    }
+
+    return info;
+}
+
+static void info_finish(QC_SQLITE_INFO* info)
+{
+    (void) info;
+}
+
+static void info_free(QC_SQLITE_INFO* info)
+{
+    if (info)
+    {
+        info_finish(info);
+        free(info);
+    }
+}
+
+static QC_SQLITE_INFO* info_init(QC_SQLITE_INFO* info)
+{
+    memset(info, 0, sizeof(*info));
+
+    info->status = QC_INFO_INVALID;
+    info->operation = (qc_query_op_t) -1;
+
+    return info;
+}
+
+static void parse_query_string(const char* query, size_t len)
+{
+    sqlite3_stmt* stmt = NULL;
+    const char* tail = NULL;
+
+    ss_dassert(this_thread.db);
+    int rc = sqlite3_prepare(this_thread.db, query, len, &stmt, &tail);
+
+    if (rc != SQLITE_OK)
+    {
+        MXS_DEBUG("qc_sqlite: sqlite3_prepare returned something else but ok: %d, %s",
+                  rc, sqlite3_errstr(rc));
+    }
+
+    if (stmt)
+    {
+        sqlite3_finalize(stmt);
+    }
+}
+
+static bool parse_query(GWBUF* query)
+{
+    bool parsed = false;
+    ss_dassert(!query_is_parsed(query));
+
+    QC_SQLITE_INFO* info = info_alloc();
+
+    if (info)
+    {
+        this_thread.info = info;
+
+        // TODO: Somewhere it needs to be ensured that this buffer is contiguous.
+        // TODO: Where is it checked that the GWBUF really contains a query?
+        uint8_t* data = (uint8_t*) GWBUF_DATA(query);
+        size_t len = MYSQL_GET_PACKET_LEN(data) - 1; // Subtract 1 for packet type byte.
+
+        const char* s = (const char*) &data[5]; // TODO: Are there symbolic constants somewhere?
+
+        parse_query_string(s, len);
+
+        if (this_thread.info->status == QC_INFO_OK)
+        {
+            MXS_INFO("qc_sqlite: SQL statement \"%.*s\", was recognized.", (int)len, s);
+        }
+        else
+        {
+            MXS_ERROR("qc_sqlite: SQL statement \"%.*s\", was not recognized.", (int)len, s);
+        }
+
+        // TODO: Add return value to gwbuf_add_buffer_object.
+        // Always added; also when it was not recognized. If it was not recognized now,
+        // it won't be if we try a second time.
+        gwbuf_add_buffer_object(query, GWBUF_PARSING_INFO, info, buffer_object_free);
+        parsed = true;
+
+        this_thread.info = NULL;
+    }
+    else
+    {
+        MXS_ERROR("Could not allocate structure for containing parse data.");
+    }
+
+    return parsed;
+}
+
+static bool query_is_parsed(GWBUF* query)
+{
+    return query && GWBUF_IS_PARSED(query);
+}
 
 
 /**
@@ -59,7 +244,11 @@ int qc_sqlite3Select(Parse* pParse, Select* p, SelectDest* pDest)
 {
     MXS_NOTICE("qc_sqlite: qc_sqlite3Select called.");
 
-    // TODO: Collect information and return error to abort sqlite3's processing.
+    QC_SQLITE_INFO* info = this_thread.info;
+    ss_dassert(info);
+
+    info->status = QC_INFO_OK;
+    info->operation = QUERY_OP_SELECT;
 
     return -1;
 }
@@ -82,7 +271,6 @@ static bool qc_sqlite_query_has_clause(GWBUF* query);
 static char* qc_sqlite_get_qtype_str(qc_query_type_t qtype);
 static char* qc_sqlite_get_affected_fields(GWBUF* query);
 static char** qc_sqlite_get_database_names(GWBUF* query, int* sizep);
-
 
 static bool qc_sqlite_init(void)
 {
@@ -179,9 +367,26 @@ static qc_query_op_t qc_sqlite_get_operation(GWBUF* query)
     ss_dassert(this_unit.initialized);
     ss_dassert(this_thread.initialized);
 
-    MXS_ERROR("qc_sqlite: qc_get_operation not implemented yet.");
+    qc_query_op_t op = QUERY_OP_UNDEFINED;
+    QC_SQLITE_INFO* info = get_query_info(query);
 
-    return QUERY_OP_UNDEFINED;
+    if (info)
+    {
+        if (info->status == QC_INFO_OK)
+        {
+            op = info->operation;
+        }
+        else
+        {
+            MXS_ERROR("qc_sqlite: The query operation was not resolved. Response not valid.");
+        }
+    }
+    else
+    {
+        MXS_ERROR("qc_sqlite: The query could not be parsed. Response not valid.");
+    }
+
+    return op;
 }
 
 static char* qc_sqlite_get_created_table_name(GWBUF* query)
