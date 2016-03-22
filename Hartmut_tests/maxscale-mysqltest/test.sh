f411209e0aa176272d56f137dbb6bcaf0d99b386
Timofey Turenko <timofey.turenko@skysql.com>
2016-03-22 09:34:06 +0200

    remove irrelevant MariaDB test

--- Hartmut_tests/maxscale-mysqltest/test.sh
+++ Hartmut_tests/maxscale-mysqltest/test.sh
@@ -2,10 +2,26 @@
 
 port=$1
 
+#TESTS="t/test_transaction_routing1.test \
+#        t/test_transaction_routing2.test \
+#        t/test_transaction_routing2b.test \
+#        t/test_transaction_routing3.test \
+#        t/test_transaction_routing3b.test \
+#        t/test_transaction_routing4.test \
+#        t/test_transaction_routing4b.test t/select_for_var_set.test \
+#        t/test_implicit_commit1.test t/test_implicit_commit2.test \
+#        t/test_implicit_commit3.test t/test_implicit_commit4.test \
+#        t/test_implicit_commit5.test t/test_implicit_commit6.test \
+#        t/test_implicit_commit7.test t/test_autocommit_disabled1.test \
+#        t/test_autocommit_disabled1b.test \
+#        t/test_autocommit_disabled2.test \
+#        t/test_autocommit_disabled3.test \
+#        t/set_autocommit_disabled.test \
+#        t/test_after_autocommit_disabled.test t/test_sescmd.test"
+
 TESTS="t/test_transaction_routing1.test \
         t/test_transaction_routing2.test \
         t/test_transaction_routing2b.test \
-        t/test_transaction_routing3.test \
         t/test_transaction_routing3b.test \
         t/test_transaction_routing4.test \
         t/test_transaction_routing4b.test t/select_for_var_set.test \
