60c496e3b77304b4de56d1bcf41fbb4a8426c412
Markus Makela <markus.makela@mariadb.com>
2016-04-15 11:59:25 +0300

    Added python based test for MXS-598
    The test uses the MariaDB Connector/J with SSL.

---
+++ mxs598.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import maxpython
+
+test1 = maxpython.MaxScaleTest("mxs598")
+
+for i in test1.maxscale:
+    i.connect()
+
+for i in range(0,100):
+    for x in test1.maxscale:
+        x.begin()
+        x.query("insert into test.t1 values (1)")
+        x.query("select * from test.t1")
+        x.commit()
+
+for i in test1.maxscale:
+    i.disconnect()
