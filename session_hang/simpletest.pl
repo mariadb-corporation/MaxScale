5742dab2450255d5f08c8e973d2d2f75a5dd9d99
Markus Makela <markus.makela@mariadb.com>
2016-05-03 05:57:44 +0300

    Cleaned up some test printing to make it less verbose
    The run_session_hang test printed useless information which flooded the logs.

--- session_hang/simpletest.pl
+++ session_hang/simpletest.pl
@@ -15,7 +15,9 @@ my $sth = $dbh->prepare("SELECT id, \@\@server_id from test.t1 where id=(?)");
 $sth->bind_param(1, "%");  # placeholders are numbered from 1
 
 for (my $i=0; $i<100000; $i++) {
-    print "$i\n";
+    if ($i % 5000 == 0) {
+        print "$i\n";
+    }
     if (defined($sth)) {
         $sth->execute($i) or warn "Did not execute successfully: ".$dbh->errstr;
         #DBI::dump_results($sth);
