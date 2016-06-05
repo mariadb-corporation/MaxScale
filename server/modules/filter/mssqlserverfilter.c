f7712df1ba452bbc191978b7ba117bea15ff1434
Lisa Brinson <liz.brinz@gmail.com>
2016-06-05 12:20:52 -0400

    Fixed Travis build error

--- server/modules/filter/mssqlserverfilter.c
+++ server/modules/filter/mssqlserverfilter.c
@@ -1,1 +1,22 @@
-##include "filter.h"
+#include <stdio.h>
+#include <filter.h>
+#include <modinfo.h>
+#include <modutil.h>
+#include <skygw_utils.h>
+#include <log_manager.h>
+#include <string.h>
+#include <pcre2.h>
+#include <atomic.h>
+#include "maxconfig.h"
+
+/**
+ * @file mssqlserverfilter.c - a very filter  rewrite MS SQL Server 
+ * Syntax to MariaDB syntax.
+ * @verbatim
+ *
+ * A SQL server syntax translation filter.
+ *
+ * Date         Who             Description
+ * 03/06/2016   Lisa Reilly Brinson    Addition of source and user parameters
+ * @endverbatim
+ */
