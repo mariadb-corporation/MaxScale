67ebcc126c38059813966fc6d5f86b46279fac77
Lisa Brinson <liz.brinz@gmail.com>
2016-06-05 12:44:08 -0400

    Created filter skeleton

--- server/modules/filter/mssqlserverfilter.c
+++ server/modules/filter/mssqlserverfilter.c
@@ -20,3 +20,191 @@
  * 03/06/2016   Lisa Reilly Brinson    Addition of source and user parameters
  * @endverbatim
  */
+
+
+MODULE_INFO info =
+{
+    MODULE_API_FILTER,
+    MODULE_GA,
+    FILTER_VERSION,
+    "A query rewrite filter that uses regular expressions to SQL Server syntax to MariaDB syntax"
+};
+
+static char *version_str = "V1.0.0";
+
+static FILTER *createInstance(char **options, FILTER_PARAMETER **params);
+static void *newSession(FILTER *instance, SESSION *session);
+static void closeSession(FILTER *instance, void *session);
+static void freeSession(FILTER *instance, void *session);
+static void setDownstream(FILTER *instance, void *fsession, DOWNSTREAM *downstream);
+static int routeQuery(FILTER *instance, void *fsession, GWBUF *queue);
+static void diagnostic(FILTER *instance, void *fsession, DCB *dcb);
+
+static FILTER_OBJECT MyObject =
+{
+    createInstance,
+    newSession,
+    closeSession,
+    freeSession,
+    setDownstream,
+    NULL, // No Upstream requirement
+    routeQuery,
+    NULL,
+    diagnostic,
+};
+
+typedef struct
+{
+    
+}MSSQLSERVER_INSTANCE;
+
+/**
+ * The session structure for this  filter
+ */
+typedef struct
+{
+
+} MSSQLSERVER_SESSION;
+
+/**
+ * Implementation of the mandatory version entry point
+ *
+ * @return version string of the module
+ */
+char *
+version()
+{
+    return version_str;
+}
+
+
+/**
+ * The module initialisation routine, called when the module
+ * is first loaded.
+ */
+void
+ModuleInit()
+{
+}
+
+/**
+ * The module entry point routine. It is this routine that
+ * must populate the structure that is referred to as the
+ * "module object", this is a structure with the set of
+ * external entry points for this module.
+ *
+ * @return The module object
+ */
+FILTER_OBJECT *
+GetModuleObject()
+{
+    return &MyObject;
+}
+
+/**
+ * Free a regexfilter instance.
+ * @param instance instance to free
+ */
+void free_instance(MSSQLSERVER_INSTANCE *instance)
+{
+    
+}
+
+/**
+ * Create an instance of the filter for a particular service
+ * within MaxScale.
+ *
+ * @param options   The options for this filter
+ * @param params    The array of name/value pair parameters for the filter
+ *
+ * @return The instance data for this new instance
+ */
+static FILTER *
+createInstance(char **options, FILTER_PARAMETER **params)
+{
+   return NULL; 
+}
+
+/**
+ * Associate a new session with this instance of the filter.
+ *
+ * @param instance  The filter instance data
+ * @param session   The session itself
+ * @return Session specific data for this session
+ */
+static void *
+newSession(FILTER *instance, SESSION *session)
+{
+   return NULL;
+}
+
+/**
+ * Close a session with the filter, this is the mechanism
+ * by which a filter may cleanup data structure etc.
+ *
+ * @param instance  The filter instance data
+ * @param session   The session being closed
+ */
+static void
+closeSession(FILTER *instance, void *session)
+{
+}
+
+/**
+ * Free the memory associated with this filter session.
+ *
+ * @param instance  The filter instance data
+ * @param session   The session being closed
+ */
+static void
+freeSession(FILTER *instance, void *session)
+{
+    free(session);
+    return;
+}
+
+/**
+ * Set the downstream component for this filter.
+ *
+ * @param instance  The filter instance data
+ * @param session   The session being closed
+ * @param downstream    The downstream filter or router
+ */
+static void
+setDownstream(FILTER *instance, void *session, DOWNSTREAM *downstream)
+{
+    
+}
+
+/**
+ * The routeQuery entry point. This is passed the query buffer
+ * to which the filter should be applied. Once applied the
+ * query shoudl normally be passed to the downstream component
+ * (filter or router) in the filter chain.
+ *
+ * @param instance  The filter instance data
+ * @param session   The filter session
+ * @param queue     The query data
+ */
+static int
+routeQuery(FILTER *instance, void *session, GWBUF *queue)
+{
+   return 0; 
+}
+
+/**
+ * Diagnostics routine
+ *
+ * If fsession is NULL then print diagnostics on the filter
+ * instance as a whole, otherwise print diagnostics for the
+ * particular session.
+ *
+ * @param   instance    The filter instance
+ * @param   fsession    Filter session, may be NULL
+ * @param   dcb     The DCB for diagnostic output
+ */
+static void
+diagnostic(FILTER *instance, void *fsession, DCB *dcb)
+{
+   
+}
