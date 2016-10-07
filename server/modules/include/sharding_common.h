2fb9e995499ba868ce4eceda022bd614d8d70778
Markus Makela <markus.makela@mariadb.com>
2016-10-07 09:33:17 +0300

    Add missing license blurbs
    Some file were missing the license blurb at the top of the file.

--- server/modules/include/sharding_common.h
+++ server/modules/include/sharding_common.h
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl.
+ *
+ * Change Date: 2019-07-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
 #ifndef _SHARDING_COMMON_HG
 #define _SHARDING_COMMON_HG
 
