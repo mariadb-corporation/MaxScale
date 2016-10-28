3c26ea16e280932d741c1d0841f1d586810da5ce
Markus Makela <markus.makela@mariadb.com>
2016-10-28 12:23:47 +0300

    Add and use FindSQLite.cmake
    Added a find_package module for SQLite development libraries.

--- cmake/FindJansson.cmake
+++ cmake/FindJansson.cmake
@@ -1,9 +1,9 @@
 # This CMake file locates the Jansson libraries and headers
 #
 # The following variables are set:
-# JANSSON_FOUND - If the Avro C library was found
+# JANSSON_FOUND - If the Jansson library was found
 # JANSSON_LIBRARIES - Path to the static library
-# JANSSON_INCLUDE_DIR - Path to Avro headers
+# JANSSON_INCLUDE_DIR - Path to Jansson headers
 
 find_path(JANSSON_INCLUDE_DIR jansson.h)
 find_library(JANSSON_LIBRARIES NAMES libjansson.so libjansson.a)
