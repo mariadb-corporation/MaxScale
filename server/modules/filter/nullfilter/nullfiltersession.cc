b46bc47c879766f20f45c8216ab86b2444c61c58
Johan Wikman <johan.wikman@mariadb.com>
2017-02-07 15:14:17 +0200

    Add NullFilter filter module
    NullFilter is a filter module that does nothing, except reports
    capabilities as defined in the configuration file. It's purpose
    is only to make it simple to benchmark the performance impact
    various routing capabilities have.
    
    Note that since getCapabilities() currently does *not* take an
    instance pointer as parameter, all NullFilter instances will
    report the same capabilities, the ones specified for the last
    filter to have been loaded.

---
+++ server/modules/filter/nullfilter/nullfiltersession.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl.
+ *
+ * Change Date: 2019-07-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#define MXS_MODULE_NAME "nullfilter"
+#include "nullfiltersession.hh"
+
+NullFilterSession::NullFilterSession(MXS_SESSION* pSession, const NullFilter* pFilter)
+    : maxscale::FilterSession(pSession)
+    , m_filter(*pFilter)
+{
+}
+
+NullFilterSession::~NullFilterSession()
+{
+}
+
+//static
+NullFilterSession* NullFilterSession::create(MXS_SESSION* pSession, const NullFilter* pFilter)
+{
+    return new NullFilterSession(pSession, pFilter);
+}
