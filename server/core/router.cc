7f9fdd0f3d844e56f5fc6b98a2da9a87342033bf
Johan Wikman <johan.wikman@mariadb.com>
2017-03-03 15:28:18 +0200

    Add router template
    Using the class RouterSession and the template Router, a router
    module can be defined. The way they are intended to be used are
    as follows:
    
      class MyRouterSession : public maxscale::RouterSession
      {
          ...
      };
    
      class MyRouter : public maxscale::Router<MyRouter, MyRouterSession>
      {
          ...
      }
    
      ...
    
      extern "C" MXS_MODULE* MXS_CREATE_MODULE()
      {
         static MXS_MODULE module =
         {
             ...
             &MyRouter::s_object,
             ...
         };
    
         return &module;
      }

---
+++ server/core/router.cc
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2019-07-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <maxscale/router.hh>
+
+namespace maxscale
+{
+
+//
+// RouterSession
+//
+RouterSession::RouterSession(MXS_SESSION* pSession)
+    : m_pSession(pSession)
+{
+}
+
+RouterSession::~RouterSession()
+{
+}
+
+void RouterSession::close()
+{
+}
+
+int32_t RouterSession::routeQuery(GWBUF* pPacket)
+{
+    return 0;
+}
+
+void RouterSession::clientReply(GWBUF* pPacket, DCB* pBackend)
+{
+}
+
+void RouterSession::handleError(GWBUF*             pMessage,
+                                DCB*               pProblem,
+                                mxs_error_action_t action,
+                                bool*              pSuccess)
+{
+}
+
+}
