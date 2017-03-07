83d696cddeba43ed74d4e89563d58f79a6a7a25d
Markus Mäkelä <markus.makela@mariadb.com>
2017-03-07 08:07:06 +0200

    Enable JIT support for PCRE2
    The bundled PCRE2 library was built without JIT support.

--- cmake/BuildPCRE2.cmake
+++ cmake/BuildPCRE2.cmake
@@ -6,7 +6,7 @@
 # to the CMakeLists.txt. You don't need to link against the pcre2 library
 # because the static symbols will be in MaxScale.
 ExternalProject_Add(pcre2 SOURCE_DIR ${CMAKE_SOURCE_DIR}/pcre2/
-  CMAKE_ARGS -DCMAKE_C_FLAGS=-fPIC -DBUILD_SHARED_LIBS=N -DPCRE2_BUILD_PCRE2GREP=N  -DPCRE2_BUILD_TESTS=N
+  CMAKE_ARGS -DCMAKE_C_FLAGS=-fPIC -DBUILD_SHARED_LIBS=N -DPCRE2_BUILD_PCRE2GREP=N  -DPCRE2_BUILD_TESTS=N -DPCRE2_SUPPORT_JIT=Y
   BINARY_DIR ${CMAKE_BINARY_DIR}/pcre2/
   BUILD_COMMAND make
   INSTALL_COMMAND "")
