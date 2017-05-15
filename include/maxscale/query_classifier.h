49897a22ad8812324e1bebed255b8a969177ae59
Johan Wikman <johan.wikman@mariadb.com>
2017-05-15 10:06:30 +0300

    MXS-1196: Make operation constants sequential
    The operations do not form a bitmask. Consequently their
    values can be sequential.

--- include/maxscale/query_classifier.h
+++ include/maxscale/query_classifier.h
@@ -82,19 +82,19 @@ typedef enum qc_query_type
  */
 typedef enum qc_query_op
 {
-    QUERY_OP_UNDEFINED     = 0,
-    QUERY_OP_SELECT        = (1 << 0),
-    QUERY_OP_UPDATE        = (1 << 1),
-    QUERY_OP_INSERT        = (1 << 2),
-    QUERY_OP_DELETE        = (1 << 3),
-    QUERY_OP_TRUNCATE      = (1 << 4),
-    QUERY_OP_ALTER         = (1 << 5),
-    QUERY_OP_CREATE        = (1 << 6),
-    QUERY_OP_DROP          = (1 << 7),
-    QUERY_OP_CHANGE_DB     = (1 << 8),
-    QUERY_OP_LOAD          = (1 << 9),
-    QUERY_OP_GRANT         = (1 << 10),
-    QUERY_OP_REVOKE        = (1 << 11)
+    QUERY_OP_UNDEFINED = 0,
+    QUERY_OP_SELECT,
+    QUERY_OP_UPDATE,
+    QUERY_OP_INSERT,
+    QUERY_OP_DELETE,
+    QUERY_OP_TRUNCATE,
+    QUERY_OP_ALTER,
+    QUERY_OP_CREATE,
+    QUERY_OP_DROP,
+    QUERY_OP_CHANGE_DB,
+    QUERY_OP_LOAD,
+    QUERY_OP_GRANT,
+    QUERY_OP_REVOKE
 } qc_query_op_t;
 
 /**
