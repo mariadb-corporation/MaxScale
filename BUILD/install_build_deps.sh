49cb3f3f65bedc45da580c54880401eccc36a57a
Timofey Turenko <timofey.turenko@skysql.com>
2017-05-29 14:02:55 +0300

    add wget installation

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -15,7 +15,7 @@ then
   sudo apt-get install -y --force-yes dpkg-dev git gcc g++ ncurses-dev bison \
        build-essential libssl-dev libaio-dev perl make libtool libcurl4-openssl-dev \
        libpcre3-dev flex tcl libeditline-dev uuid-dev liblzma-dev libsqlite3-dev \
-       sqlite3 liblua5.1 liblua5.1-dev libgnutls-dev libgcrypt20 
+       sqlite3 liblua5.1 liblua5.1-dev libgnutls-dev libgcrypt20 wget
 else
   ## RPM-based distro
   command -v yum
@@ -40,7 +40,7 @@ else
     sudo yum clean all
     sudo yum install -y --nogpgcheck gcc gcc-c++ ncurses-devel bison glibc-devel \
          libgcc perl make libtool openssl-devel libaio libaio-devel libedit-devel \
-         libedit-devel libcurl-devel curl-devel systemtap-sdt-devel rpm-sign \
+         libedit-devel libcurl-devel curl-devel systemtap-sdt-devel rpm-sign wget \
          gnupg pcre-devel flex rpmdevtools git wget tcl openssl libuuid-devel xz-devel \
          sqlite sqlite-devel pkgconfig lua lua-devel rpm-build createrepo yum-utils \
          gnutls gcrypt
