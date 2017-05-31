a79a492601db1d30a5690aa3aebacfa64e6213e1
Markus Mäkelä <markus.makela@mariadb.com>
2017-05-31 10:52:19 +0300

    Tell libmicrohttpd where to install libraries
    Explicitly specify the library location for the configure script. This
    should prevent problems with different library paths that the configure
    script can generate.

--- cmake/BuildMicroHttpd.cmake
+++ cmake/BuildMicroHttpd.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(libmicrohttpd
   URL http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.54.tar.gz
   SOURCE_DIR ${CMAKE_BINARY_DIR}/libmicrohttpd/
-  CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/libmicrohttpd//configure --prefix=${CMAKE_BINARY_DIR}/libmicrohttpd/ --enable-shared --with-pic
+  CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/libmicrohttpd//configure --prefix=${CMAKE_BINARY_DIR}/libmicrohttpd/ --enable-shared --with-pic --libdir=${CMAKE_BINARY_DIR}/libmicrohttpd/lib/
   BINARY_DIR ${CMAKE_BINARY_DIR}/libmicrohttpd/
   BUILD_COMMAND make
   INSTALL_COMMAND make install)
