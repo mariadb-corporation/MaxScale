988988922165254d9c3f86b196816423d0eee54a
Timofey Turenko <timofey.turenko@skysql.com>
2017-06-09 15:07:24 +0300

    remove gnutls and gcrypt from install_build_deps.sh

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -15,7 +15,7 @@ then
   sudo apt-get install -y --force-yes dpkg-dev git gcc g++ ncurses-dev bison \
        build-essential libssl-dev libaio-dev perl make libtool libcurl4-openssl-dev \
        libpcre3-dev flex tcl libeditline-dev uuid-dev liblzma-dev libsqlite3-dev \
-       sqlite3 liblua5.1 liblua5.1-dev libgnutls-dev libgcrypt20 wget
+       sqlite3 liblua5.1 liblua5.1-dev wget
 else
   ## RPM-based distro
   command -v yum
@@ -26,8 +26,7 @@ else
     sudo zypper -n install gcc gcc-c++ ncurses-devel bison glibc-devel libgcc_s1 perl \
          make libtool libopenssl-devel libaio libaio-devel flex libcurl-devel \
          pcre-devel git wget tcl libuuid-devel \
-         xz-devel sqlite3 sqlite3-devel pkg-config lua lua-devel \
-         gnutls gcrypt
+         xz-devel sqlite3 sqlite3-devel pkg-config lua lua-devel
     sudo zypper -n install rpm-build
     cat /etc/*-release | grep "SUSE Linux Enterprise Server 11"
 
@@ -42,8 +41,7 @@ else
          libgcc perl make libtool openssl-devel libaio libaio-devel libedit-devel \
          libedit-devel libcurl-devel curl-devel systemtap-sdt-devel rpm-sign wget \
          gnupg pcre-devel flex rpmdevtools git wget tcl openssl libuuid-devel xz-devel \
-         sqlite sqlite-devel pkgconfig lua lua-devel rpm-build createrepo yum-utils \
-         gnutls gcrypt
+         sqlite sqlite-devel pkgconfig lua lua-devel rpm-build createrepo yum-utils
 
     cat /etc/redhat-release | grep "release 5"
     if [ $? == 0 ]
