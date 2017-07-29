d0d19f3dedcf2a8a93b8255f55d1ea8105aba769
Markus Mäkelä <markus.makela@mariadb.com>
2017-07-29 05:26:06 +0300

    Tune timeouts for bug662
    The timeouts were a bit too demanding and increasing them should lower the
    rate of false positives.

--- maxscale-system-test/bug662.cpp
+++ maxscale-system-test/bug662.cpp
@@ -1,39 +1,18 @@
 /**
  * @file bug662.cpp regression case for bug 662 ("MaxScale hangs in startup if backend server is not responsive"), covers also bug680 ("RWSplit can't load DB user if backend is not available at MaxScale start")
  *
- * - block all Mariadb servers  Firewall
- * - restart MaxScale
- * - check it took no more then 20 seconds
- * - unblock Mariadb servers
- * - sleep one minute
- * - check if Maxscale is alive
+ * - Block all Mariadb servers
+ * - Restart MaxScale
+ * - Unblock Mariadb servers
+ * - Sleep and check if Maxscale is alive
  */
 
-/*
-Vilho Raatikka 2014-12-29 08:38:28 UTC
-During startup, load_mysql_users tries to read the contents of the mysql.user table. If the chosen backend is not responsive, connection hangs for a long time.
-Comment 1 Vilho Raatikka 2014-12-29 11:41:32 UTC
-The issue causes long stalls for the executing thread whenever getUsers function is called and one or more backends are not responsive.
-Comment 2 Vilho Raatikka 2014-12-29 11:50:10 UTC
-dbusers.c: Added function for setting read, write and connection timeout values. Set default timeouts for getUsers. Defaults are listed in service.c
-    gateway.c:shutdown_server is called whenever MaxScale is to be shut down. Added call for service_shutdown to shutdown_server.
-    service.c:service_alloc: replaced malloc with calloc and removed unnecessary zero/NULL initialization statements as a consequence.
-        serviceStart: Exit serviceStartPort loop if shutdown flag is set for the service.
-        serviceStartAll: Exit serviceStart loop if shutdown flag is set for the service.
-    service.c: Added service_shutdown which sets shutdown flag for each service found in allServices list.
-    service.h: Added prototype for service_shutdown
-*/
-
-
-
-#include <iostream>
 #include "testconnections.h"
 #include "maxadmin_operations.h"
 
 int main(int argc, char *argv[])
 {
     TestConnections * Test = new TestConnections(argc, argv);
-    Test->set_timeout(20);
     int i;
 
     Test->tprintf("Connecting to Maxscale %s\n", Test->maxscale_IP);
@@ -44,20 +23,21 @@ int main(int argc, char *argv[])
 
     for (i = 0; i < Test->repl->N; i++)
     {
+        Test->set_timeout(30);
         Test->tprintf("Setup firewall to block mysql on node %d\n", i);
         Test->repl->block_node(i);
         fflush(stdout);
     }
 
-    Test->set_timeout(100);
+    Test->set_timeout(120);
     Test->restart_maxscale();
 
-    Test->set_timeout(20);
     Test->tprintf("Checking if MaxScale is alive by connecting to MaxAdmin\n");
     Test->add_result(Test->execute_maxadmin_command((char* ) "show servers"), "Maxadmin execution failed.\n");
 
     for (i = 0; i < Test->repl->N; i++)
     {
+        Test->set_timeout(30);
         Test->tprintf("Setup firewall back to allow mysql on node %d\n", i);
         Test->repl->unblock_node(i);
         fflush(stdout);
@@ -67,13 +47,11 @@ int main(int argc, char *argv[])
     Test->tprintf("Sleeping 30 seconds\n");
     sleep(30);
 
-    Test->set_timeout(20);
-
+    Test->set_timeout(30);
     Test->tprintf("Checking Maxscale is alive\n");
     Test->check_maxscale_alive();
 
     int rval = Test->global_result;
     delete Test;
     return rval;
-    //}
 }
