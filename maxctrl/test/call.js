d64cb9fa803abe12b92913f276764e42b30bf8dd
Markus Mäkelä <markus.makela@mariadb.com>
2017-08-08 11:24:06 +0300

    MXS-1300: Test calling of module commands via MaxCtrl
    The calling of module commands is tested by using the qlafilter log
    command.

---
+++ maxctrl/test/call.js
@@ -0,0 +1,24 @@
+require('../test_utils.js')()
+
+describe("Module Commands", function() {
+    before(startMaxScale)
+
+    it('call command', function() {
+        return doCommand('call command qlafilter log QLA')
+            .then(function(output) {
+                JSON.parse(output).meta.should.have.lengthOf(1)
+            })
+    })
+
+    it('call command with missing parameters', function() {
+        return doCommand('call command qlafilter log')
+            .should.be.rejected
+    })
+
+    it('call command with too many parameters', function() {
+        return doCommand('call command qlafilter log QLA too many arguments for this command')
+            .should.be.rejected
+    })
+
+    after(stopMaxScale)
+});
