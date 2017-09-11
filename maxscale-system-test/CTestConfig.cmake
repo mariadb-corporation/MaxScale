7a8a12dd28d378650187da055c64c857823fd63f
Markus Mäkelä <markus.makela@mariadb.com>
2017-09-11 21:55:59 +0300

    Update CDash drop site address
    The drop site was not set to the correct one.

--- maxscale-system-test/CTestConfig.cmake
+++ maxscale-system-test/CTestConfig.cmake
@@ -10,6 +10,6 @@ set(CTEST_PROJECT_NAME "MaxScale")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "jenkins.engskysql.com")
+set(CTEST_DROP_SITE "maxscale-jenkins.mariadb.com")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=MaxScale")
 set(CTEST_DROP_SITE_CDASH TRUE)
