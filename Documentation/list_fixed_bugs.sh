e8a9a393ae9ecdf1be839d12b935fa8258f5d774
Markus Mäkelä <markus.makela@mariadb.com>
2017-09-18 22:55:05 +0300

    Fix bug and issue list scripts
    The scripts now properly handle quoted CSV values with the help of a Perl
    script.

--- Documentation/list_fixed_bugs.sh
+++ Documentation/list_fixed_bugs.sh
@@ -8,4 +8,4 @@ then
 fi
 
 version=$1
-curl -s "https://jira.mariadb.org/sr/jira.issueviews:searchrequest-csv-current-fields/temp/SearchRequest.csv?jqlQuery=project+%3D+MXS+AND+issuetype+%3D+Bug+AND+status+%3D+Closed+AND+fixVersion+%3D+$version"|cut -f 1,2 -d ,|tail -n+2|sed 's/\(.*\),\(.*\)/* (\2)[https:\/\/jira.mariadb.org\/browse\/\2] \1/'
+curl -s "https://jira.mariadb.org/sr/jira.issueviews:searchrequest-csv-current-fields/temp/SearchRequest.csv?jqlQuery=project+%3D+MXS+AND+issuetype+%3D+Bug+AND+status+%3D+Closed+AND+fixVersion+%3D+$version" | process.pl
