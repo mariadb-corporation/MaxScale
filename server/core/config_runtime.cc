e55968b808becd74d6b280a272a96bb2f46ade43
Markus Mäkelä <markus.makela@mariadb.com>
2017-09-28 13:03:04 +0300

    Fix listener creation via REST API
    The check for SSL parameters was wrong as it tested whether all SSL
    parameters were defined even when the check was not needed.

--- server/core/config_runtime.cc
+++ server/core/config_runtime.cc
@@ -1205,9 +1205,12 @@ static bool validate_ssl_json(json_t* params)
         is_string_or_null(params, CN_SSL_VERSION) &&
         is_count_or_null(params, CN_SSL_CERT_VERIFY_DEPTH))
     {
-        if (!mxs_json_pointer(params, CN_SSL_KEY) ||
-            !mxs_json_pointer(params, CN_SSL_CERT) ||
-            !mxs_json_pointer(params, CN_SSL_CA_CERT))
+        if ((mxs_json_pointer(params, CN_SSL_KEY) ||
+             mxs_json_pointer(params, CN_SSL_CERT) ||
+             mxs_json_pointer(params, CN_SSL_CA_CERT)) &&
+            (!mxs_json_pointer(params, CN_SSL_KEY) ||
+             !mxs_json_pointer(params, CN_SSL_CERT) ||
+             !mxs_json_pointer(params, CN_SSL_CA_CERT)))
         {
             runtime_error("SSL configuration requires '%s', '%s' and '%s' parameters",
                           CN_SSL_KEY, CN_SSL_CERT, CN_SSL_CA_CERT);
