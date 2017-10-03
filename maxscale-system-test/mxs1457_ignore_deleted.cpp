7dfa1577fc21139a699e78a88c94f68d801ff66f
Markus Mäkelä <markus.makela@mariadb.com>
2017-10-04 00:47:31 +0300

    Use new MaxScale object names in mxs1457_ignore_deleted
    The names of the monitors and services in tests that use MaxScale 2.2 need
    to use the hyphen-transformed versions of the object names.

--- maxscale-system-test/mxs1457_ignore_deleted.cpp
+++ maxscale-system-test/mxs1457_ignore_deleted.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
      * The monitor needs to be stopped before the slaves are stopped to prevent
      * it from detecting the broken replication.
      */
-    test.ssh_maxscale(true, "maxadmin shutdown monitor \"MySQL Monitor\"");
+    test.ssh_maxscale(true, "maxadmin shutdown monitor MySQL-Monitor");
     // Stop slaves and drop the user on the master
     test.repl->stop_slaves();
     test.repl->connect();
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     test.add_result(mysql_errno(conn) == 0, "Connection with users from master should fail");
     mysql_close(conn);
 
-    test.ssh_maxscale(true, "maxadmin remove server server1 \"RW Split Router\"");
+    test.ssh_maxscale(true, "maxadmin remove server server1 RW-Split-Router");
     conn = open_conn_db(test.rwsplit_port, test.maxscale_ip(), "test", "auth_test", "test", false);
     test.add_result(mysql_errno(conn), "Connection should be OK: %s", mysql_error(conn));
     test.try_query(conn, "SELECT 1");
