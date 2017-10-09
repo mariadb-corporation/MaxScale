36dfcd4319521442ce0bcd64913caaf68030cc1e
Markus Mäkelä <markus.makela@mariadb.com>
2017-10-09 12:29:55 +0300

    Add MaxCtrl test for start/stop maxscale
    Added the missing test case for starting and stopping MaxScale.

--- maxctrl/test/startstop.js
+++ maxctrl/test/startstop.js
@@ -1,33 +1,51 @@
 require('../test_utils.js')()
 
-describe("Start/Stop Commands", function() {
+describe('Start/Stop Commands', function() {
     before(startMaxScale)
 
     it('stop service', function() {
         return verifyCommand('stop service Read-Connection-Router', 'services/Read-Connection-Router')
             .then(function(res) {
-                res.data.attributes.state.should.equal("Stopped")
+                res.data.attributes.state.should.equal('Stopped')
             })
     })
 
     it('start service', function() {
         return verifyCommand('start service Read-Connection-Router', 'services/Read-Connection-Router')
             .then(function(res) {
-                res.data.attributes.state.should.equal("Started")
+                res.data.attributes.state.should.equal('Started')
             })
     })
 
     it('stop monitor', function() {
         return verifyCommand('stop monitor MySQL-Monitor', 'monitors/MySQL-Monitor')
             .then(function(res) {
-                res.data.attributes.state.should.equal("Stopped")
+                res.data.attributes.state.should.equal('Stopped')
             })
     })
 
     it('start monitor', function() {
         return verifyCommand('start monitor MySQL-Monitor', 'monitors/MySQL-Monitor')
             .then(function(res) {
-                res.data.attributes.state.should.equal("Running")
+                res.data.attributes.state.should.equal('Running')
+            })
+    })
+
+    it('stop maxscale', function() {
+        return verifyCommand('stop maxscale', 'services')
+            .then(function(res) {
+                res.data.forEach((i) => {
+                    i.attributes.state.should.equal('Stopped')
+                })
+            })
+    })
+
+    it('start maxscale', function() {
+        return verifyCommand('start maxscale', 'services')
+            .then(function(res) {
+                res.data.forEach((i) => {
+                    i.attributes.state.should.equal('Started')
+                })
             })
     })
 
