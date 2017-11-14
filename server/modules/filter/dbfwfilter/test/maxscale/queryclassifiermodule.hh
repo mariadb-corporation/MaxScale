d2c80d2e91ca65dfa303e73dfd92bc866b38221d
Johan Wikman <johan.wikman@mariadb.com>
2017-11-14 14:12:24 +0200

    MXS-1461 Add QueryClassifierModule

---
+++ server/modules/filter/dbfwfilter/test/maxscale/queryclassifiermodule.hh
@@ -0,0 +1,48 @@
+#pragma once
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2020-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <maxscale/cppdefs.hh>
+#include <memory>
+#include <maxscale/query_classifier.h>
+#include "module.hh"
+
+namespace maxscale
+{
+
+/**
+ * A QueryClassfierModule instance is an abstraction for a query
+ * classifier module.
+ */
+class QueryClassifierModule : public Module<QueryClassifierModule>
+{
+    QueryClassifierModule(const QueryClassifierModule&);
+    QueryClassifierModule& operator = (const QueryClassifierModule&);
+
+public:
+    static const char*       zName;  /*< The name describing the module type. */
+    typedef QUERY_CLASSIFIER type_t; /*< The type of the module object. */
+
+private:
+    friend class Module<QueryClassifierModule>;
+
+    QueryClassifierModule(QUERY_CLASSIFIER* pApi)
+        : m_pApi(pApi)
+    {
+    }
+
+private:
+    QUERY_CLASSIFIER* m_pApi;
+};
+
+}
---
+++ server/modules/filter/dbfwfilter/test/queryclassifiermodule.cc
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2020-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include "maxscale/queryclassifiermodule.hh"
+#include "../../../../core/maxscale/modules.h"
+
+namespace maxscale
+{
+
+//static
+const char* QueryClassifierModule::zName = MODULE_QUERY_CLASSIFIER;
+
+}
