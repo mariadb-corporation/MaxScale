7cc4018c1584fe1ebab086235db45d73a22ac00d
Markus Mäkelä <markus.makela@mariadb.com>
2017-11-15 15:00:40 +0200

    Use SQLSTATE 08S01 for hangup errors
    This will tell the connectors that the connection cannot be used.

--- server/modules/protocol/MySQL/MySQLClient/mysql_client.c
+++ server/modules/protocol/MySQL/MySQLClient/mysql_client.c
@@ -1446,7 +1446,7 @@ static int gw_client_hangup_event(DCB *dcb)
         goto retblock;
     }
 
-    modutil_send_mysql_err_packet(dcb, 0, 0, 1927, "70100", "Connection killed by MaxScale");
+    modutil_send_mysql_err_packet(dcb, 0, 0, 1927, "08S01", "Connection killed by MaxScale");
     dcb_close(dcb);
 
 retblock:
