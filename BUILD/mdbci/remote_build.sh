6555fc0babc33a75c04f66db1b50432a59f1d5fc
Timofey Turenko <timofey.turenko@skysql.com>
2017-11-17 15:33:17 +0200

    add MDBCI build scripts

---
+++ BUILD/mdbci/remote_build.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Copyies stuff to VM, run build on VM and copies binaries 
+# to $pre_repo_dir/$target/$box
+
+set -x
+
+
+rm -rf $pre_repo_dir/$target/$box
+mkdir -p $pre_repo_dir/$target/SRC
+mkdir -p $pre_repo_dir/$target/$box
+
+export work_dir="MaxScale"
+export orig_image=$box
+
+ssh $sshopt "sudo rm -rf $work_dir"
+echo "copying stuff to $image machine"
+ssh $sshopt "mkdir -p $work_dir"
+
+rsync -avz  --progress --delete -e "ssh $scpopt" ${script_dir}/../../../MaxScale/* $sshuser@$IP:$work_dir/ 
+if [ $? -ne 0 ] ; then
+  echo "Error copying stuff to $box machine"
+  exit 2
+fi
+
+export install_script="install_build_deps.sh"
+
+if [ "$box_type" == "RPM" ] ; then
+  build_script="build_rpm_local.sh"
+  files="*.rpm"
+  tars="$product_name*.tar.gz"
+else
+  build_script="build_deb_local.sh"
+  files="../*.deb"
+  tars="$product_name*.tar.gz"
+fi
+
+export remote_build_cmd="export already_running=\"$already_running\"; \
+  export build_experimental=\"$build_experimental\"; \
+  export cmake_flags=\"$cmake_flags\"; \
+  export work_dir=\"$work_dir\"; \
+  export platform=\"$platform\"; \
+  export platform_version=\"$platform_version\"; \
+  export source=\"$source\"; \
+  export BUILD_TAG=\"$BUILD_TAG\"; \
+  "
+
+if [ "$already_running" != "ok" ]
+then
+  echo "install packages on $image"
+  ssh $sshopt "$remote_build_cmd ./MaxScale/BUILD/$install_script"
+  installres=$?
+  if [ $installres -ne 0 ]
+  then
+    exit $installres
+  fi
+
+  $HOME/mdbci/mdbci snapshot take --path-to-nodes $box --snapshot-name clean
+
+else
+  echo "already running VM, not installing deps"
+fi
+
+echo "run build on $box"
+ssh $sshopt "$remote_build_cmd ./MaxScale/BUILD/$build_script"
+if [ $? -ne 0 ] ; then
+  echo "Error build on $box"
+  exit 4
+fi
+
+echo "copying binaries to the '$pre_repo_dir/$target/$box'"
+scp $scpopt $sshuser@$IP:$work_dir/$files $pre_repo_dir/$target/$box/
+scp $scpopt $sshuser@$IP:$work_dir/$tars $pre_repo_dir/$target/$box/
+
+
+echo "package building for '$target' for '$platform' '$platform_version' done!"
+
+
+
---
+++ BUILD/mdbci/set_build_variables.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Defines defaults values for all uninitialized environmental variables
+# In case of running from Jenkins all values go from Jenkins parameters
+
+export dir=`pwd`
+
+export MDBCI_VM_PATH=${MDBCI_VM_PATH:-$HOME/vms}
+mkdir -p $MDBCI_VM_PATH
+echo "MDBCI_VM_PATH=$MDBCI_VM_PATH"
+
+export box=${box:-"centos_7_libvirt"}
+echo "box=$box"
+
+# get commit ID
+commitID=`git log | head -1 | sed "s/commit //"`
+echo "commitID $commitID"
+
+export branch=`git symbolic-ref --short HEAD`
+export curr_date=`date '+%Y-%m-%d_%H-%M'`
+
+export source=${source:-"$branch"}
+echo "source=$source"
+
+#hack to get rid of Jenkins artifacts 
+export target=`echo $target | tr -cd "[:print:]" | sed "s/?//g" | sed "s/ //g"`
+
+export target=${target:-"$source-$curr_date"}
+echo "target=$target"
+
+export product_name=${product_name:-"maxscale"}
+
+export build_experimental=${build_experimental:-"yes"}
+
+export gpg_keys_path=${gpg_keys_path:-"$HOME/maxscale_gpg_keys/"}
+
+export pre_repo_dir=${pre_repo_dir:-"$HOME/pre-repo/"}
+
+export unsorted_repo_dir=${unsorted_repo_dir:-"$HOME/repo/"}
+
+export box_type="RPM"
+echo $box | grep -i ubuntu
+if [ $? == 0 ] ; then
+  export box_type="DEB"
+  export platform_family="ubuntu"
+fi
+echo $box | grep -i deb
+if [ $? == 0 ] ; then
+  export box_type="DEB"
+  export platform_family="debian"
+fi
+
+export cmake_flags=${cmake_flags:-"-DBUILD_TESTS=Y -DCMAKE_BUILD_TYPE=Debug -DBUILD_MMMON=Y -DBUILD_AVRO=Y -DBUILD_CDC=Y"}
+echo "cmake_flags=$cmake_flags"
+
+export do_not_destroy_vm=${do_not_destroy_vm:-"no"}
+
+export try_already_running=${try_already_running:-"no"}
+
+export JOB_NAME=${JOB_NAME:-"local_build"}
+
+export BUILD_NUMBER=${BUILD_NUMBER:-`date '+%Y%m%d%H%M'`}
+
+export BUILD_TAG=${BUILD_TAG:-jenkins-${JOB_NAME}-${BUILD_NUMBER}}
+
+export mdbci_dir=${mdbci_dir:-"$HOME/mdbci/"}
+
+export repo_name=$target
+
+export repo_path=${repo_path:-$HOME/repository}
+
+export path_prefix="$repo_path/$repo_name/mariadb-$product_name/"
+
