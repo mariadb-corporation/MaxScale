bbe99e458d9b8fbaed0c67185d91badcfcb4a22a
Markus Mäkelä <markus.makela@mariadb.com>
2018-01-19 09:30:21 +0200

    Fix test build failure
    MXS-1543 test failed to build in 2.2.

--- maxscale-system-test/mxs1543.cpp
+++ maxscale-system-test/mxs1543.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
 
     // Wait for the avrorouter to process the data
     sleep(10);
-    test.check_log_err("Possible STATEMENT or MIXED", true);
+    test.check_log_err(0, "Possible STATEMENT or MIXED", true);
 
     return test.global_result;
 }
