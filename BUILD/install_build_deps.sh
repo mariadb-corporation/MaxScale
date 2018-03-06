0a4bb6fe7aab065847b5a263d84e13cbd273fa56
Markus Mäkelä <markus.makela@mariadb.com>
2018-03-06 12:53:37 +0200

    MXS-1697: Install pkg-config for Debian systems
    The build scripts did not install the pkg-config package that the Avro-C
    API depends on.

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -15,7 +15,7 @@ then
   sudo apt-get install -y --force-yes dpkg-dev git gcc g++ ncurses-dev bison \
        build-essential libssl-dev libaio-dev perl make libtool libcurl4-openssl-dev \
        libpcre3-dev flex tcl libeditline-dev uuid-dev liblzma-dev libsqlite3-dev \
-       sqlite3 liblua5.1 liblua5.1-dev wget zlib1g-dev
+       sqlite3 liblua5.1 liblua5.1-dev wget zlib1g-dev pkg-config
 
 else
   ## RPM-based distro
