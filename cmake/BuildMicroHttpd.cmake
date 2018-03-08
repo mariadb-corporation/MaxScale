003ad03e8ddc25d9868824256e295bf8f664e87a
dapeng huang <7xerocha@gmail.com>
2018-03-08 16:42:27 +0800

    make libmircohttpd source url configurable (#170)

--- cmake/BuildMicroHttpd.cmake
+++ cmake/BuildMicroHttpd.cmake
@@ -1,5 +1,8 @@
+set(LIBMICROHTTPD_URL "http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.54.tar.gz"
+  CACHE STRING "GNU libmicrochttpd source code")
+
 ExternalProject_Add(libmicrohttpd
-  URL http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.54.tar.gz
+  URL ${LIBMICROHTTPD_URL}
   SOURCE_DIR ${CMAKE_BINARY_DIR}/libmicrohttpd/
   CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/libmicrohttpd//configure --prefix=${CMAKE_BINARY_DIR}/libmicrohttpd/ --enable-shared --with-pic --libdir=${CMAKE_BINARY_DIR}/libmicrohttpd/lib/
   BINARY_DIR ${CMAKE_BINARY_DIR}/libmicrohttpd/
