4167e88719d53229ecc1afb0f6adc991019abe4d
Markus Mäkelä <markus.makela@mariadb.com>
2018-03-29 18:00:12 +0300

    MXS-1751: Fix crash with available_when_donor=true
    The `MYSQL_ROW row` variable was being overwritten by the extra query done
    by the SST method detection code. Moving it into its own function prevents
    this and makes the code significantly easier to comprehend.
    
    Added a test case that reproduced the problem (MaxScale crashed) and
    verifies that the patch fixes the problem.

---
+++ maxscale-system-test/mxs1751_available_when_donor_crash.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2020-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include "testconnections.h"
+
+int main(int argc, char* argv[])
+{
+    TestConnections test(argc, argv);
+
+    for (int i = 0; i < 2; i++)
+    {
+        test.galera->stop_node(0);
+        test.galera->stop_node(1);
+        test.galera->start_node(1);
+        test.galera->start_node(0);
+        test.galera->fix_replication();
+    }
+
+    return test.global_result;
+}
