5c534acf6bf34a6a3444d704ae8391ed4055e4ec
Johan Wikman <johan.wikman@mariadb.com>
2018-04-26 15:21:16 +0300

    MXS-1830 Remove erroneous free
    The libdir is set elsewhere as well, which causes the set
    buffer to be freed. Consequently it cannot be freed.

--- server/modules/filter/cache/test/testlrustorage.cc
+++ server/modules/filter/cache/test/testlrustorage.cc
@@ -54,7 +54,5 @@ int main(int argc, char* argv[])
     TestLRUStorage test(&cout);
     int rv = test.run(argc, argv);
 
-    // TODO: Remove this once globally allocated memory is freed
-    MXS_FREE(libdir);
     return rv;
 }
