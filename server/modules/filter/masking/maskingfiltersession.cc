5eb6718b752b12e8b7eb5f3cd8c9ed25efd0172e
Johan Wikman <johan.wikman@mariadb.com>
2018-05-08 15:06:47 +0300

    MXS-1861 Detect and handle multistatement responses
    Multi-statement SELECTs were properly detected and handled,
    but e.g. multi-statement UPDATESs were not, with the result
    that erronous warnings were logged.
    
    Now the responses are detected and handled properly.

--- server/modules/filter/masking/maskingfiltersession.cc
+++ server/modules/filter/masking/maskingfiltersession.cc
@@ -131,7 +131,21 @@ void MaskingFilterSession::handle_response(GWBUF* pPacket)
     switch (response.type())
     {
     case ComResponse::OK_PACKET:
-    // We'll end up here also in the case of a multi-result.
+        {
+            ComOK ok(response);
+
+            if (ok.status() & SERVER_MORE_RESULTS_EXIST)
+            {
+                m_res.reset_multi();
+                m_state = EXPECTING_RESPONSE;
+            }
+            else
+            {
+                m_state = EXPECTING_NOTHING;
+            }
+        }
+        break;
+
     case ComResponse::LOCAL_INFILE_PACKET: // GET_MORE_CLIENT_DATA/SEND_MORE_CLIENT_DATA
         m_state = EXPECTING_NOTHING;
         break;
