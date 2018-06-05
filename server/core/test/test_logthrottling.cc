19a0c9466140ce3a408d82db3a8cf6d5525891c0
Marko <marko.rajala@mariadb.com>
2018-06-05 15:49:22 +0300

    Typo fix in logthrottling_test

--- server/core/test/test_logthrottling.cc
+++ server/core/test/test_logthrottling.cc
@@ -216,7 +216,7 @@ int main(int argc, char* argv[])
         // window, we should get no messages.
         if (!run(t, LOG_WARNING, 100, 0))
         {
-            rc = EXIT_FAILURE;<< strerror(errno) <<
+            rc = EXIT_FAILURE;
         }
 
         cout << "Sleeping 6 seconds." << endl;
@@ -276,7 +276,7 @@ int main(int argc, char* argv[])
     cmd += logdir;
     if (system(cmd.c_str()) == -1)
     {
-        cerr << "Could not remove all files due to " << strerror(errno) << endl;
+        cerr << "Could not remove all files";
     }
 
     return rc;
