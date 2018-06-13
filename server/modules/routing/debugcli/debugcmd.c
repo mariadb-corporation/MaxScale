mariadb-corporation/MaxScale

commit c850336199c3c19508a3d280fb7000291d66b80c

MXS-1907: Allow ssl_verify_peer_certificate when creating listener
When a listener is created at runtime or SSL is being enabled for an
already created listener, the ssl_verify_peer_certificate parameter can
now be defined.

server/modules/routing/debugcli/debugcmd.c
@@ -62,7 +62,7 @@
 #include "../../../core/maxscale/poll.h"
 #include "../../../core/maxscale/session.h"
 
-#define MAXARGS 12
+#define MAXARGS 14
 
 #define ARG_TYPE_NONE           0
 #define ARG_TYPE_ADDRESS        1
@@ -1143,11 +1143,11 @@ static void createServer(DCB *dcb, char *name, char *address, char *port,
 static void createListener(DCB *dcb, SERVICE *service, char *name, char *address,
                            char *port, char *protocol, char *authenticator,
                            char *authenticator_options, char *key, char *cert,
-                           char *ca, char *version, char *depth)
+                           char *ca, char *version, char *depth, char *verify)
 {
     if (runtime_create_listener(service, name, address, port, protocol,
                                 authenticator, authenticator_options,
-                                key, cert, ca, version, depth))
+                                key, cert, ca, version, depth, verify))
     {
         dcb_printf(dcb, "Listener '%s' created\n", name);
     }
@@ -1355,6 +1355,7 @@ static void alterServer(DCB *dcb, SERVER *server, char *v1, char *v2, char *v3,
     char *ssl_ca = NULL;
     char *ssl_version = NULL;
     char *ssl_depth = NULL;
+    char *ssl_verify = NULL;
     bool enable = false;
 
     for (int i = 0; i < items && values[i]; i++)
@@ -1388,6 +1389,10 @@ static void alterServer(DCB *dcb, SERVER *server, char *v1, char *v2, char *v3,
                 {
                     ssl_depth = value;
                 }
+                else if (strcmp("ssl_verify_peer_certificate", key) == 0)
+                {
+                    ssl_verify = value;
+                }
                 else
                 {
                     enable = strcmp("ssl", key) == 0 && strcmp(value, "required") == 0;
@@ -1411,7 +1416,7 @@ static void alterServer(DCB *dcb, SERVER *server, char *v1, char *v2, char *v3,
         {
             /** We have SSL parameters, try to process them */
             if (!runtime_enable_server_ssl(server, ssl_key, ssl_cert, ssl_ca,
-                                           ssl_version, ssl_depth))
+                                           ssl_version, ssl_depth, ssl_verify))
             {
                 dcb_printf(dcb, "Enabling SSL for server '%s' failed, see log "
                            "for more details.\n", server->unique_name);
@@ -1464,7 +1469,7 @@ static void alterMonitor(DCB *dcb, MXS_MONITOR *monitor, char *v1, char *v2, cha
 struct subcommand alteroptions[] =
 {
     {
-        "server", 2, 12, alterServer,
+        "server", 2, 14, alterServer,
         "Alter server parameters",
         "Usage: alter server NAME KEY=VALUE ...\n"
         "\n"
@@ -1474,18 +1479,19 @@ struct subcommand alteroptions[] =
         "\n"
         "This will alter an existing parameter of a server. The accepted values for KEY are:\n"
         "\n"
-        "address               Server address\n"
-        "port                  Server port\n"
-        "monitoruser           Monitor user for this server\n"
-        "monitorpw             Monitor password for this server\n"
-        "ssl                   Enable SSL, value must be 'required'\n"
-        "ssl_key               Path to SSL private key\n"
-        "ssl_cert              Path to SSL certificate\n"
-        "ssl_ca_cert           Path to SSL CA certificate\n"
-        "ssl_version           SSL version\n"
-        "ssl_cert_verify_depth Certificate verification depth\n"
-        "persistpoolmax        Persisted connection pool size\n"
-        "persistmaxtime        Persisted connection maximum idle time\n"
+        "address                     Server address\n"
+        "port                        Server port\n"
+        "monitoruser                 Monitor user for this server\n"
+        "monitorpw                   Monitor password for this server\n"
+        "ssl                         Enable SSL, value must be 'required'\n"
+        "ssl_key                     Path to SSL private key\n"
+        "ssl_cert                    Path to SSL certificate\n"
+        "ssl_ca_cert                 Path to SSL CA certificate\n"
+        "ssl_version                 SSL version\n"
+        "ssl_cert_verify_depth       Certificate verification depth\n"
+        "ssl_verify_peer_certificate Peer certificate verification\n"
+        "persistpoolmax              Persisted connection pool size\n"
+        "persistmaxtime              Persisted connection maximum idle time\n"
         "\n"
         "To configure SSL for a newly created server, the 'ssl', 'ssl_cert',\n"
         "'ssl_key' and 'ssl_ca_cert' parameters must be given at the same time.\n"

server/core/config_runtime.c
@@ -206,7 +206,8 @@ bool runtime_destroy_server(SERVER *server)
 }
 
 static SSL_LISTENER* create_ssl(const char *name, const char *key, const char *cert,
-                                const char *ca, const char *version, const char *depth)
+                                const char *ca, const char *version, const char *depth,
+                                const char *verify)
 {
     SSL_LISTENER *rval = NULL;
     CONFIG_CONTEXT *obj = config_context_create(name);
@@ -218,7 +219,8 @@ static SSL_LISTENER* create_ssl(const char *name, const char *key, const char *c
             config_add_param(obj, "ssl_cert", cert) &&
             config_add_param(obj, "ssl_ca_cert", ca) &&
             (!version || config_add_param(obj, "ssl_version", version)) &&
-            (!depth || config_add_param(obj, "ssl_cert_verify_depth", depth)))
+            (!depth || config_add_param(obj, "ssl_cert_verify_depth", depth)) &&
+            (!verify || config_add_param(obj, "ssl_verify_peer_certificate", verify)))
         {
             int err = 0;
             SSL_LISTENER *ssl = make_ssl_structure(obj, true, &err);
@@ -236,14 +238,15 @@ static SSL_LISTENER* create_ssl(const char *name, const char *key, const char *c
 }
 
 bool runtime_enable_server_ssl(SERVER *server, const char *key, const char *cert,
-                               const char *ca, const char *version, const char *depth)
+                               const char *ca, const char *version, const char *depth,
+                               const char *verify)
 {
     bool rval = false;
 
     if (key && cert && ca)
     {
         spinlock_acquire(&crt_lock);
-        SSL_LISTENER *ssl = create_ssl(server->unique_name, key, cert, ca, version, depth);
+        SSL_LISTENER *ssl = create_ssl(server->unique_name, key, cert, ca, version, depth, verify);
 
         if (ssl)
         {
@@ -494,7 +497,8 @@ bool runtime_create_listener(SERVICE *service, const char *name, const char *add
                              const char *port, const char *proto, const char *auth,
                              const char *auth_opt, const char *ssl_key,
                              const char *ssl_cert, const char *ssl_ca,
-                             const char *ssl_version, const char *ssl_depth)
+                             const char *ssl_version, const char *ssl_depth,
+                             const char *verify_ssl)
 {
 
     if (addr == NULL || strcasecmp(addr, "default") == 0)
@@ -535,7 +539,7 @@ bool runtime_create_listener(SERVICE *service, const char *name, const char *add
 
         if (ssl_key && ssl_cert && ssl_ca)
         {
-            ssl = create_ssl(name, ssl_key, ssl_cert, ssl_ca, ssl_version, ssl_depth);
+            ssl = create_ssl(name, ssl_key, ssl_cert, ssl_ca, ssl_version, ssl_depth, verify_ssl);
 
             if (ssl == NULL)
             {

server/core/maxscale/config_runtime.h
@@ -103,10 +103,13 @@ bool runtime_alter_server(SERVER *server, char *key, char *value);
  * @param ca Path to certificate authority
  * @param version Required SSL Version
  * @param depth Certificate verification depth
+ * @param verify Verify peer certificate
+ *
  * @return True if SSL was successfully enabled
  */
 bool runtime_enable_server_ssl(SERVER *server, const char *key, const char *cert,
-                               const char *ca, const char *version, const char *depth);
+                               const char *ca, const char *version, const char *depth,
+                               const char *verify);
 
 /**
  * @brief Alter monitor parameters
@@ -135,6 +138,7 @@ bool runtime_alter_monitor(MXS_MONITOR *monitor, char *key, char *value);
  * @param ssl_ca      SSL CA cert, NULL for no CA cert
  * @param ssl_version SSL version, NULL for default of "MAX"
  * @param ssl_depth   SSL cert verification depth, NULL for default
+ * @param verify_ssl  SSL peer certificate verification, NULL for default
  *
  * @return True if the listener was successfully created and started
  */
@@ -142,7 +146,8 @@ bool runtime_create_listener(SERVICE *service, const char *name, const char *add
                              const char *port, const char *proto, const char *auth,
                              const char *auth_opt, const char *ssl_key,
                              const char *ssl_cert, const char *ssl_ca,
-                             const char *ssl_version, const char *ssl_depth);
+                             const char *ssl_version, const char *ssl_depth,
+                             const char *verify_ssl);
 
 /**
  * @brief Destroy a listener

commit 1ea7c65861cff83a7424af569cb3ee285d7fbc74

Fix runtime alteration of servers
The server runtime alteration was broken by commit
c850336199c3c19508a3d280fb7000291d66b80c when it increased the maximum
argument count of the `alter server` command to 14.

server/modules/routing/debugcli/debugcmd.c
@@ -1345,9 +1345,9 @@ struct subcommand destroyoptions[] =
  */
 static void alterServer(DCB *dcb, SERVER *server, char *v1, char *v2, char *v3,
                         char *v4, char *v5, char *v6, char *v7, char *v8, char *v9,
-                        char *v10, char *v11)
+                        char *v10, char *v11, char *v12, char *v13)
 {
-    char *values[11] = {v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11};
+    char *values[] = {v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13};
     const int items = sizeof(values) / sizeof(values[0]);
     CONFIG_CONTEXT *obj = NULL;
     char *ssl_key = NULL;
@@ -1976,8 +1976,23 @@ execute_cmd(CLI_SESSION *cli)
                                                           arg_list[6], arg_list[7], arg_list[8],
                                                           arg_list[9], arg_list[10], arg_list[11]);
                                     break;
+                                case 13:
+                                    cmds[i].options[j].fn(dcb, arg_list[0], arg_list[1], arg_list[2],
+                                                          arg_list[3], arg_list[4], arg_list[5],
+                                                          arg_list[6], arg_list[7], arg_list[8],
+                                                          arg_list[9], arg_list[10], arg_list[11],
+                                                          arg_list[12]);
+                                    break;
+                                case 14:
+                                    cmds[i].options[j].fn(dcb, arg_list[0], arg_list[1], arg_list[2],
+                                                          arg_list[3], arg_list[4], arg_list[5],
+                                                          arg_list[6], arg_list[7], arg_list[8],
+                                                          arg_list[9], arg_list[10], arg_list[11],
+                                                          arg_list[12], arg_list[13]);
+                                    break;
                                 default:
                                     dcb_printf(dcb, "Error: Maximum argument count is %d.\n", MAXARGS);
+                                    ss_info_dassert(!true, "Command has too many arguments");
                                     break;
                                 }
                             }