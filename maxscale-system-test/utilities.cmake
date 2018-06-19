be853a448c7aa8053148e787cbd223254d7fb5b6
Markus Mäkelä <markus.makela@mariadb.com>
2018-06-19 14:39:04 +0300

    Link tests against maxutils
    The tests now link against the maxutils libraries. Currently, the maxutils
    libraries consist of only the maxbase library.

--- maxscale-system-test/utilities.cmake
+++ maxscale-system-test/utilities.cmake
@@ -147,3 +147,12 @@ set(JANSSON_STATIC_FOUND TRUE CACHE INTERNAL "")
 set(JANSSON_INCLUDE_DIR ${CMAKE_BINARY_DIR}/jansson/install/include CACHE INTERNAL "")
 set(JANSSON_STATIC_LIBRARIES ${CMAKE_BINARY_DIR}/jansson/install/lib/libjansson.a CACHE INTERNAL "")
 set(JANSSON_LIBRARIES ${JANSSON_STATIC_LIBRARIES} CACHE INTERNAL "")
+
+# Build the maxutils library
+ExternalProject_Add(maxutils
+  SOURCE_DIR ${CMAKE_SOURCE_DIR}/../maxutils/
+  BINARY_DIR ${CMAKE_BINARY_DIR}/maxutils
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/maxutils/install)
+
+set(MAXUTILS_INCLUDE_DIR ${CMAKE_BINARY_DIR}/maxutils/install/include CACHE INTERNAL "")
+set(MAXUTILS_LIBRARIES ${CMAKE_BINARY_DIR}/maxutils/install/lib/libmaxbase.a CACHE INTERNAL "")
