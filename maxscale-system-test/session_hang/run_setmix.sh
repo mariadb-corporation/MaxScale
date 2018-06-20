249e66756f0027f68db828bc9061b77e6473ebeb
Markus Mäkelä <markus.makela@mariadb.com>
2018-06-20 22:42:29 +0300

    Fix run_session_hang.sh
    The script it called used the wrong variable name.

--- maxscale-system-test/session_hang/run_setmix.sh
+++ maxscale-system-test/session_hang/run_setmix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for ((i=0 ; i<100 ; i++)) ;
 do
-	mysql --host=$maxscale_IP -P 4006 -u $node_user -p$node_password --verbose --force --unbuffered=true --disable-reconnect $ssl_options > /dev/null < $test_dir/session_hang/setmix.sql >& /dev/null
+	mysql --host=$maxscale_IP -P 4006 -u $node_user -p$node_password --verbose --force --unbuffered=true --disable-reconnect $ssl_options > /dev/null < $src_dir/session_hang/setmix.sql >& /dev/null
 done
 
