fd202c9339443520184b7807c538273bc1a12436
Johan Wikman <johan.wikman@mariadb.com>
2018-06-25 11:01:08 +0300

    Look for more ASAN llibrary versions
    On Ubuntu libasan.so.1 and libasan.so.2 are present.

--- cmake/FindASAN.cmake
+++ cmake/FindASAN.cmake
@@ -4,7 +4,7 @@
 # ASAN_FOUND - If AddressSanitizer was found
 # ASAN_LIBRARIES - Path to the libasan library
 
-find_library(ASAN_LIBRARIES NAMES libasan.so.0 libasan.so.3 libasan.so.4)
+find_library(ASAN_LIBRARIES NAMES libasan.so.0 libasan.so.1 libasan.so.2 libasan.so.3 libasan.so.4)
 
 if (ASAN_LIBRARIES)
   message(STATUS "Found AdressSanitizer libraries: ${ASAN_LIBRARIES}")
