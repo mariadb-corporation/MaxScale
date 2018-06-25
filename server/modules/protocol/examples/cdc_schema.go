cc0299aee6000d913cf63d90331719a06aa7003a
Johan Wikman <johan.wikman@mariadb.com>
2018-06-25 09:49:01 +0300

    Update change date of 2.3

--- server/modules/protocol/examples/cdc_schema.go
+++ server/modules/protocol/examples/cdc_schema.go
@@ -3,7 +3,7 @@
 // Use of this software is governed by the Business Source License included
 // in the LICENSE.TXT file and at www.mariadb.com/bsl11.
 //
-// Change Date: 2020-01-01
+// Change Date: 2022-01-01
 //
 // On the date above, in accordance with the Business Source License, use
 // of this software will be governed by version 2 or later of the General
