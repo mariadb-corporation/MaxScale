b2f52590a33e96bf60ea7d72a56bb3eb1d3e957e
Timofey Turenko <timofey.turenko@mariadb.com>
2018-07-04 09:10:55 +0300

    fix typo in chmod for all binary repository dirs and files

--- BUILD/mdbci/copy_repos.sh
+++ BUILD/mdbci/copy_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyies repo from ${unsorted_repo_dir}/$target/$box to 
+# Copies repo from ${unsorted_repo_dir}/$target/$box to proper place in the public repo dir
 
 dir=`pwd`
 if [ "$box_type" == "RPM" ] ; then
@@ -9,9 +9,10 @@ if [ "$box_type" == "RPM" ] ; then
 
         rm -rf $path_prefix/$platform/$platform_version/$arch/
         mkdir -p $path_prefix/$platform/$platform_version/$arch/
-        chmod 775 $path_prefix/$platform/$platform_version/$arch
         rsync -avz  --progress --delete ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform/$platform_version/$arch/
 	env > $build_info_path
+        find $path_prefix/.. -type d -exec chmod 755 {} \;
+        find $path_prefix/.. -type f -exec chmod 644 {} \;
         cd $path_prefix/$platform
         ln -s $platform_version "$platform_version"server
         ln -s $platform_version "$platform_version"Server
@@ -28,9 +29,10 @@ else
         rm -rf $path_prefix/$platform_family/dists/$platform_version/main/binary-"$arch"
         rm -rf $path_prefix/$platform_family/dists/$platform_version/main/binary-i386
         mkdir -p $path_prefix/$platform_family/
-        chmod 775 $path_prefix/$platform_family
         rsync -avz  --progress --delete ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform_family/
         env > $build_info_path
+        find $path_prefix/.. -type d -exec chmod 755 {} \;
+        find $path_prefix/.. -type f -exec chmod 644 {} \;
   eval "cat <<EOF
 $(<${script_dir}/templates/repository-config/deb.json.template)
 " 2> /dev/null > ${path_prefix}/${platform}_${platform_version}.json
