1ebdadcc2b051ac3feff98333fce4602069e0e0c
Markus Mäkelä <markus.makela@mariadb.com>
2018-07-04 15:17:21 +0300

    Do rsync without --delete
    rsync with the --delete flag will destroy artifacts generated by other
    jobs. This broke the build_all job.

--- BUILD/mdbci/copy_repos.sh
+++ BUILD/mdbci/copy_repos.sh
@@ -9,7 +9,7 @@ if [ "$box_type" == "RPM" ] ; then
 
         rm -rf $path_prefix/$platform/$platform_version/$arch/
         mkdir -p $path_prefix/$platform/$platform_version/$arch/
-        rsync -avz  --progress --delete ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform/$platform_version/$arch/
+        rsync -avz  --progress ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform/$platform_version/$arch/
 	env > $build_info_path
         find $path_prefix/.. -type d -exec chmod 755 {} \;
         find $path_prefix/.. -type f -exec chmod 644 {} \;
@@ -29,7 +29,7 @@ else
         rm -rf $path_prefix/$platform_family/dists/$platform_version/main/binary-"$arch"
         rm -rf $path_prefix/$platform_family/dists/$platform_version/main/binary-i386
         mkdir -p $path_prefix/$platform_family/
-        rsync -avz  --progress --delete ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform_family/
+        rsync -avz  --progress ${unsorted_repo_dir}/$repo_name/$box/* $path_prefix/$platform_family/
         env > $build_info_path
         find $path_prefix/.. -type d -exec chmod 755 {} \;
         find $path_prefix/.. -type f -exec chmod 644 {} \;
