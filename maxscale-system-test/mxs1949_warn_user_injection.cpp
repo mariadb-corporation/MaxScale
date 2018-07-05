5c610503ba325f6ce3ab13e707f7da2ff4966348
Markus Mäkelä <markus.makela@mariadb.com>
2018-07-05 13:47:08 +0300

    MXS-1949: Add test case
    Added a test case that verifies the bug is fixed.

---
+++ maxscale-system-test/mxs1949_warn_user_injection.cpp
@@ -0,0 +1,15 @@
+/**
+ * MXS-1949: Warning for user load failure logged even when service has no users
+ *
+ * Check that the message is not logged when services have no servers and
+ * 'inject_service_user' is enabled.
+ */
+
+#include "testconnections.h"
+
+int main(int argc, char *argv[])
+{
+    TestConnections test(argc, argv);
+    test.check_log_err(0, " No users were loaded but 'inject_service_user' is enabled", false);
+    return test.global_result;
+}
