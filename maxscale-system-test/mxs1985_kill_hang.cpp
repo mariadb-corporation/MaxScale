ee6e2b28b2b100fc4964254f190fe71f394bab90
Markus Mäkelä <markus.makela@mariadb.com>
2018-07-19 20:25:01 +0300

    MXS-1985: Add concurrent KILL test case
    The test case runs parallel KILL queries and reproduces the problem.

---
+++ maxscale-system-test/mxs1985_kill_hang.cpp
@@ -0,0 +1,55 @@
+/**
+ * MXS-1985: MaxScale hangs on concurrent KILL processing
+ */
+
+#include "testconnections.h"
+
+#include <atomic>
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+static atomic<bool> running{true};
+
+int main(int argc, char *argv[])
+{
+    TestConnections test(argc, argv);
+    vector<thread> threads;
+
+    for (int i = 0; i < 20; i++)
+    {
+        threads.emplace_back([&, i]()
+        {
+            while (running)
+            {
+                MYSQL* c = test.maxscales->open_rwsplit_connection();
+
+                // It doesn't really matter if the connection ID exists, this is just a
+                // handy way of generating cross-thread communication.
+                for (auto&& a: get_result(c, "SELECT id FROM information_schema.processlist"
+                                          " WHERE user like '%skysql%'"))
+                {
+                    if (execute_query_silent(c, std::string("KILL " + a[0]).c_str()))
+                    {
+                        break;
+                    }
+                }
+
+                mysql_close(c);
+            }
+        });
+    }
+
+    sleep(10);
+    running = false;
+
+    // If MaxScale hangs, at least one thread will not return in time
+    test.set_timeout(30);
+    for (auto&& a: threads)
+    {
+        a.join();
+    }
+
+    return test.global_result;
+}
