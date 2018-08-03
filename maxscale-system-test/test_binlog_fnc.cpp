0795d6000705c27fbc44d27f41f4f469f8e9b07a
Markus Mäkelä <markus.makela@mariadb.com>
2018-08-03 17:00:48 +0300

    Use MASTER_GTID_WAIT in binlog tests
    This should help with the slave synchronization problem.

--- maxscale-system-test/test_binlog_fnc.cpp
+++ maxscale-system-test/test_binlog_fnc.cpp
@@ -134,22 +134,13 @@ void test_binlog(TestConnections* Test)
     Test->add_result(insert_into_t1(Test->repl->nodes[0], 4), "Data inserting to t1 failed");
     Test->stop_timeout();
     Test->tprintf("Waiting for replication to catch up");
-
-    Test->set_timeout(500);
-    // Create a table and wait for it to replicate to all servers
-    Test->try_query(Test->repl->nodes[0], "CREATE TABLE test.t2(id INT)");
+    Row row = get_row(Test->repl->nodes[0], "SELECT @@gtid_current_pos");
 
     for (int i = 1; i < Test->repl->N; i++)
     {
-        // The timeout set before will terminate the loop if a fatal error occurs
-        while (execute_query_silent(Test->repl->nodes[i], "SELECT * FROM test.t2"))
-        {
-            sleep(1);
-        }
-        mysql_free_result(mysql_store_result(Test->repl->nodes[i]));
+        std::string query = "SELECT MASTER_GTID_WAIT('" + row[0] + "', 120)";
+        get_row(Test->repl->nodes[i], query);
     }
-
-    Test->try_query(Test->repl->nodes[0], "DROP TABLE test.t2");
 
     for (i = 0; i < Test->repl->N; i++)
     {
