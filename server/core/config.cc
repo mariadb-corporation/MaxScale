4a47b4f3344145828daefc975ab4967e02800d14
Markus Mäkelä <markus.makela@mariadb.com>
2018-08-11 22:31:50 +0300

    Fix ssl_version
    The parameter used wrong keywords for the enum values. The comparison
    should be changed to be case-insensitive to prevent similar problems.

--- server/core/config.cc
+++ server/core/config.cc
@@ -251,11 +251,11 @@ static const MXS_ENUM_VALUE ssl_version_values[] =
 {
     {"MAX", 1},
 #ifndef OPENSSL_1_1
-    {"TLSV10", 1},
+    {"TLSv10", 1},
 #endif
 #ifdef OPENSSL_1_0
-    {"TLSV11", 1},
-    {"TLSV12", 1},
+    {"TLSv11", 1},
+    {"TLSv12", 1},
 #endif
     {NULL}
 };
