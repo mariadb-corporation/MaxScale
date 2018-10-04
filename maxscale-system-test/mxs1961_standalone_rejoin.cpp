1af43d4ff5f36d92d622b4ac4550dfdec6741e09
Esa Korhonen <esa.korhonen@mariadb.com>
2018-10-04 16:27:33 +0300

    Speed up mxs1961_standalone_rejoin
    The waits were needlessly long. Also cleaned up a bit.

--- maxscale-system-test/mxs1961_standalone_rejoin.cpp
+++ maxscale-system-test/mxs1961_standalone_rejoin.cpp
@@ -4,12 +4,13 @@
 
 #include "testconnections.h"
 #include <iostream>
+#include <maxbase/assert.h>
 
 using namespace std;
 
 void checkpoint(TestConnections& test)
 {
-    const int v = 5;
+    const int v = 2;
     test.maxscales->wait_for_monitor(v);
 
     for (auto&& s : {
@@ -37,10 +38,8 @@ int main(int argc, char* argv[])
 
     auto comment = [&](const char* comment) {
             cout << comment << endl;
-            test.maxscales->ssh_node_f(0,
-                                       true,
-                                       "echo '----- %s -----' >> /var/log/maxscale/maxscale.log",
-                                       comment);
+            test.maxscales->ssh_node_f(0, true,
+                                       "echo '----- %s -----' >> /var/log/maxscale/maxscale.log", comment);
         };
 
     auto slave = [&](const char* name) {
@@ -58,77 +57,79 @@ int main(int argc, char* argv[])
             test.expect(status(name) == down, "'%s' should be down", name);
         };
 
-    auto block = [&](int node) {
-            test.repl->block_node(node);
+    auto block = [&](int servernum) {
+            mxb_assert(servernum >= 1);
+            test.repl->block_node(servernum - 1);
             checkpoint(test);
         };
 
-    auto unblock = [&](int node) {
-            test.repl->unblock_node(node);
+    auto unblock = [&](int servernum) {
+            mxb_assert(servernum >= 1);
+            test.repl->unblock_node(servernum - 1);
             checkpoint(test);
         };
 
-    test.maxscales->wait_for_monitor(1);
+    checkpoint(test);
 
     master("server1");
     slave("server2");
     slave("server3");
 
     comment("Blocking server1");
-    block(0);
-    comment("Blocking server2");
     block(1);
+    comment("Blocking server2");
+    block(2);
 
     down("server1");
     down("server2");
     master("server3");
 
     comment("Unblocking server2");
-    unblock(1);
+    unblock(2);
 
     down("server1");
     slave("server2");
     master("server3");
 
     comment("Blocking server3");
-    block(2);
+    block(3);
     comment("Unblocking server3");
-    unblock(2);
+    unblock(3);
 
     down("server1");
     master("server2");
     slave("server3");
 
     comment("Blocking server3");
-    block(2);
+    block(3);
 
     down("server1");
     master("server2");
     down("server3");
 
     comment("Unblocking server1");
-    unblock(0);
+    unblock(1);
 
     slave("server1");
     master("server2");
     down("server3");
 
     comment("Blocking server2");
-    block(1);
+    block(2);
 
     master("server1");
     down("server2");
     down("server3");
 
     comment("Unblocking server2");
-    unblock(1);
+    unblock(2);
 
     master("server1");
     slave("server2");
     down("server3");
 
     comment("Unblocking server3");
-    unblock(2);
+    unblock(3);
 
     master("server1");
     slave("server2");
