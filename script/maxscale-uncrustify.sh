e52741679e9dfe0cb5f2ae4b60c9fd88b52da0b1
Niclas Antti <niclas.antti@mariadb.com>
2018-11-01 13:43:34 +0200

    Change uncrustify to add newline before constructor initializer list.
    Also add exclusion of "build" directory to maxscale-uncrustify.sh script.

--- script/maxscale-uncrustify.sh
+++ script/maxscale-uncrustify.sh
@@ -2,5 +2,5 @@
 
 currdir=$(dirname $(realpath $0))
 
-$currdir/list-src -x 'sqlite-src-3110100 pcre2' $currdir/..|xargs uncrustify -c $currdir/../uncrustify.cfg --no-backup --mtime
+$currdir/list-src -x 'sqlite-src-3110100 pcre2 build' $currdir/.. | xargs uncrustify -c $currdir/../uncrustify.cfg --no-backup --mtime
 
