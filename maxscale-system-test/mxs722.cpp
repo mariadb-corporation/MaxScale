1f8180b8ee23548049556a9cf00b6dc88b351739
Markus Mäkelä <markus.makela@mariadb.com>
2018-11-16 02:20:41 +0200

    Use explicit file in mxs722
    Using an explicit file also checks whether the correct file is used when a
    specific file is given as an argument.

--- maxscale-system-test/mxs722.cpp
+++ maxscale-system-test/mxs722.cpp
@@ -22,34 +22,34 @@ int main(int argc, char* argv[])
     test->maxscales->stop_maxscale(0);
 
     /** Copy original config so we can easily reset the testing environment */
-    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf /etc/maxscale.cnf.backup");
+    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf /tmp/maxscale.cnf");
 
     /** Get a baseline result with a good configuration */
-    int baseline = test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale");
+    int baseline = test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale -f /tmp/maxscale.cnf");
 
     /** Configure bad parameter for a listener */
-    test->maxscales->ssh_node_f(0, true, "sed -i -e 's/service/ecivres/' /etc/maxscale.cnf");
-    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale"),
+    test->maxscales->ssh_node_f(0, true, "sed -i -e 's/service/ecivres/' /tmp/maxscale.cnf");
+    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale -f /tmp/maxscale.cnf"),
                      "Bad parameter name should be detected.\n");
-    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf.backup /etc/maxscale.cnf");
+    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf /tmp/maxscale.cnf");
 
     /** Set router_options to a bad value */
     test->maxscales->ssh_node_f(0,
                                 true,
-                                "sed -i -e 's/router_options.*/router_options=bad_option=true/' /etc/maxscale.cnf");
-    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale"),
+                                "sed -i -e 's/router_options.*/router_options=bad_option=true/' /tmp/maxscale.cnf");
+    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale -f /tmp/maxscale.cnf"),
                      "Bad router_options should be detected.\n");
 
-    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf.backup /etc/maxscale.cnf");
+    test->maxscales->ssh_node_f(0, true, "cp /etc/maxscale.cnf /tmp/maxscale.cnf");
 
     /** Configure bad filter parameter */
-    test->maxscales->ssh_node_f(0, true, "sed -i -e 's/filebase/basefile/' /etc/maxscale.cnf");
-    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale"),
+    test->maxscales->ssh_node_f(0, true, "sed -i -e 's/filebase/basefile/' /tmp/maxscale.cnf");
+    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale -f /tmp/maxscale.cnf"),
                      "Bad filter parameter should be detected.\n");
 
     /** Remove configuration file */
-    test->maxscales->ssh_node_f(0, true, "rm -f /etc/maxscale.cnf");
-    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale"),
+    test->maxscales->ssh_node_f(0, true, "rm -f /tmp/maxscale.cnf");
+    test->add_result(baseline == test->maxscales->ssh_node_f(0, true, "maxscale -c --user=maxscale -f /tmp/maxscale.cnf"),
                      "Missing configuration file should be detected.\n");
 
     int rval = test->global_result;
