b72d6ad2a0003653b81baba101520c22ad060f8f
Timofey Turenko <timofey.turenko@mariadb.com>
2018-12-30 00:56:20 +0200

    remove unsupported in 1.0 sysbench options

--- maxscale-system-test/sysbench_commands.h
+++ maxscale-system-test/sysbench_commands.h
@@ -3,7 +3,7 @@
 
 /*const char * SYSBENCH_PREPARE =
          "sysbench oltp_read_write \
-         --oltp-table-size=1000000 --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
+         --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
          --mysql-port=4006 --mysql-host=%s  prepare";
 
 
@@ -11,10 +11,10 @@
 const char * SYSBENCH_COMMAND =
          "sysbench oltp_read_write \
          --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
-         --mysql-db=test --mysql-table-engine=innodb  \
-         --num-threads=32 --oltp-table-size=1000000  --oltp-read-only=off \
+         --mysql-db=test \
+         --num-threads=32 \
          --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
-         --max-requests=0  --max-time=600 run";*/
+         --max-requests=0 --time=600 run";*/
 
 const char * SYSBENCH_PREPARE =
                 "sysbench oltp_read_write \
@@ -24,33 +24,30 @@ const char * SYSBENCH_PREPARE =
 const char * SYSBENCH_COMMAND =
                 "sysbench oltp_read_write \
                  --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
-                 --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on \
-                 --num-threads=32 --oltp-table-size=1000000 --oltp-tables-count=2 --oltp-read-only=%s \
-                 --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
-                 --max-requests=0 --report-interval=5 --max-time=100 run";
+                 --mysql-db=test \
+                 --threads=32 --oltp-tables-count=2 --oltp-read-only=%s \
+                 --max-requests=0 --report-interval=5 --time=100 run";
 
 
 const char * SYSBENCH_PREPARE1 =
                 "sysbench oltp_read_write \
-                 --oltp-table-size=1000 --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
+                 --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
                  --mysql-port=4006 --mysql-host=%s --oltp-tables-count=1 prepare";
 
 const char * SYSBENCH_COMMAND1 =
                  "sysbench oltp_read_write \
                   --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
-                  --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on \
-                  --num-threads=32 --oltp-table-size=1000 --oltp-tables-count=1 --oltp-read-only=%s \
-                  --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
-                  --max-requests=0 --report-interval=5 --max-time=100 run";
+                  --mysql-db=test \
+                  --threads=32 --oltp-tables-count=1 --oltp-read-only=%s \
+                  --max-requests=0 --report-interval=5 --time=100 run";
 
 
 const char * SYSBENCH_COMMAND_LONG =
                "sysbench oltp_read_write \
                 --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
-                --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on \
-                --num-threads=32 --oltp-table-size=1000000 --oltp-tables-count=2 --oltp-read-only=%s \
-                --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
-                --max-requests=0 --report-interval=5 --max-time=2592000 run";
+                --mysql-db=test \
+                --threads=32 --oltp-tables-count=2 --oltp-read-only=%s \
+                --max-requests=0 --report-interval=5 --time=2592000 run";
 
 
 const char * SYSBENCH_PREPARE_SHORT =
@@ -61,11 +58,8 @@ const char * SYSBENCH_PREPARE_SHORT =
 const char * SYSBENCH_COMMAND_SHORT =
                        "sysbench oltp_read_write \
                         --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
-                        --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on \
-                        --num-threads=32 --oltp-table-size=10000 --oltp-tables-count=2 --oltp-read-only=%s \
-                        --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
-                        --max-requests=0 --report-interval=5 --max-time=300 run";
-
-
+                        --mysql-db=test \
+                        --threads=32 --oltp-tables-count=2 --oltp-read-only=%s \
+                        --max-requests=0 --report-interval=5 --time=300 run";
 
 #endif // SYSBENCH_COMMANDS_H
