b9e376c27fce9eed008c3f485864dc41a064934f
Alexander Kovtunenko <akovtunenko@slice.com>
2019-01-01 10:04:57 +0200

    use sort -v

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -86,29 +86,9 @@ if [ $? != 0 ] ; then
     wget -q https://cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz --no-check-certificate
 fi
 sudo tar xzf cmake-3.7.1-Linux-x86_64.tar.gz -C /usr/ --strip-components=1
-check_cmake_version_is_greater_than_default()
-{
-  compared_major_version=3
-  compared_minor_version=7
-  compared_patch_version=1
-  IFS='.' declare -a 'arr=($1)'
-  major_version=${arr[0]}
-  minor_version=${arr[1]}
-  patch_version=${arr[2]}
-  unset IFS
- if [[ $major_version -ne $compared_major_version ]] ; then
-    [[ $major_version -gt $compared_major_version ]] && return 0 || return 1
- fi
- if [[ $minor_version -ne $compared_minor_version ]] ; then
-    [[ $minor_version -gt $compared_minor_version ]] && return 0 || return 1
- fi
- [[ $patch_version -ge $compared_patch_version ]] && return 0 || return 1
-
-}
 
 cmake_version=`cmake --version | grep "cmake version" | awk '{ print $3 }'`
-check_cmake_version_is_greater_than_default $cmake_version
-if [ $? -gt 0 ] ; then
+if [ "`echo -e "3.7.1\n$cmake_version"|sort -V|head -n 1`" != "3.7.1" ] ; then
     echo "cmake does not work! Trying to build from source"
     wget -q https://cmake.org/files/v3.7/cmake-3.7.1.tar.gz --no-check-certificate
     tar xzf cmake-3.7.1.tar.gz
