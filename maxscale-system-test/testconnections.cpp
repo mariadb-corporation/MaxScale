5cdea12ea69a753802898d3ac8b906c16af37d28
Timofey Turenko <timofey.turenko@mariadb.com>
2019-04-05 10:31:51 +0300

    restore test_name processing

--- maxscale-system-test/testconnections.cpp
+++ maxscale-system-test/testconnections.cpp
@@ -217,10 +217,13 @@ TestConnections::TestConnections(int argc, char *argv[]):
         }
     }
 
-    test_name = basename(argv[0]);
-    if (!strcmp(test_name, "non_native_setup"))
+    if (optind < argc)
     {
-        test_name = argv[1];
+        test_name = argv[optind];
+    }
+    else
+    {
+        test_name = basename(argv[0]);
     }
 
     const char * labels_string = NULL;
