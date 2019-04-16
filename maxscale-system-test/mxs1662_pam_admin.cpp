11814762b5cc7fda67792b95b078e10c80396bd4
Markus Mäkelä <markus.makela@mariadb.com>
2019-04-16 20:31:41 +0300

    MXS-1662: Test PAM admin authentication
    The test checks that the vagrant user has access to the REST API.

---
+++ maxscale-system-test/mxs1662_pam_admin.cpp
@@ -0,0 +1,15 @@
+/**
+ * MXS-1662: PAM authenticator for admin users
+ */
+
+#include "testconnections.h"
+
+int main(int argc, char** argv)
+{
+    TestConnections test(argc, argv);
+
+    // TODO: Store login information
+    test.check_maxctrl("-u vagrant -p vagrant show maxscale");
+
+    return test.global_result;
+}
