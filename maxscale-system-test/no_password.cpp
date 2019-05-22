c57764a84fcca59ed93ecdd8832a756ad8fd1f36
Markus Mäkelä <markus.makela@mariadb.com>
2019-05-22 19:45:55 +0300

    Fix no_password
    The test used freed memory.

--- maxscale-system-test/no_password.cpp
+++ maxscale-system-test/no_password.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     test.tprintf("MySQL error: %s", mysql_error(mysql));
     mysql_close(mysql);
 
-    open_conn(test.maxscales->rwsplit_port[0], test.maxscales->IP[0], "testuser", "testpassword", false);
+    mysql = open_conn(test.maxscales->rwsplit_port[0], test.maxscales->IP[0], "testuser", "testpassword", false);
     test.add_result(mysql_errno(mysql) == 0, "Connecting to MaxScale should fail");
     test.add_result(strstr(mysql_error(mysql), "using password: YES") == NULL,
                     "Missing (using password: YES) error message, got this instead: %s",
