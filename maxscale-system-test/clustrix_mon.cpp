dcc7d195a3968c0cf9d8ba02cd9d2d7aceaf7703
Johan Wikman <johan.wikman@mariadb.com>
2019-05-27 12:26:50 +0300

    MXS-2481 Add/fix copyright blurb

--- maxscale-system-test/clustrix_mon.cpp
+++ maxscale-system-test/clustrix_mon.cpp
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2019 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2022-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
 /**
  * @file clustrix_mon.cpp - simple Clustrix monitor test
  * Just creates Clustrix cluster and connect Maxscale to it
