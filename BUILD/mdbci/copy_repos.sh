169fb256edb014af0068be4861d0de6565c6a21e
Timofey Turenko <timofey.turenko@mariadb.com>
2019-08-07 16:02:40 +0300

    Create symlink for sles15
    Currently build is executed for OpenSuse 15 and SLES15 repo is not available. To avoid
    missing repo symlink sles -> opensuse is created

--- BUILD/mdbci/copy_repos.sh
+++ BUILD/mdbci/copy_repos.sh
@@ -28,10 +28,15 @@ if [ "$box_type" == "RPM" ] ; then
                 cd $path_prefix/$platform
                 ln -s $platform_version "$platform_version"server
                 ln -s $platform_version "$platform_version"Server
+                cd ..
                 if [ "$platform" == "centos" ] ; then
-                        cd ..
                         ln -s centos rhel
                 fi
+                if [ "$platform" == "opensuse" ] ; then
+                        mkdir -p sles
+                        ln -s opensuse/$platform_version sles/$platform_version
+                fi
+
 
   eval "cat <<EOF
 $(<${script_dir}/templates/repository-config/rpm.json.template)
