7a79208aff4172731f4a015ff23e2a67d469e218
Markus Mäkelä <markus.makela@mariadb.com>
2019-08-14 22:32:45 +0300

    Remove unused RWBackend code

--- include/maxscale/protocol/rwbackend.hh
+++ include/maxscale/protocol/rwbackend.hh
@@ -38,16 +38,6 @@ std::vector<typename Smart::pointer> sptr_vec_to_ptr_vec(const std::vector<Smart
     return pVec;
 }
 
-/** Enum for tracking client reply state */
-enum reply_state_t
-{
-    REPLY_STATE_START,          /**< Query sent to backend */
-    REPLY_STATE_DONE,           /**< Complete reply received */
-    REPLY_STATE_RSET_COLDEF,    /**< Resultset response, waiting for column definitions */
-    REPLY_STATE_RSET_COLDEF_EOF,/**< Resultset response, waiting for EOF for column definitions */
-    REPLY_STATE_RSET_ROWS       /**< Resultset response, waiting for rows */
-};
-
 typedef std::map<uint32_t, uint32_t> BackendHandleMap;      /** Internal ID to external ID */
 
 class RWBackend;
@@ -64,127 +54,11 @@ class RWBackend : public mxs::Backend
     RWBackend& operator=(const RWBackend&);
 
 public:
-    class Error
-    {
-    public:
-        Error() = default;
-
-        explicit operator bool() const
-        {
-            return m_code != 0;
-        }
-
-        bool is_rollback() const
-        {
-            bool rv = false;
-
-            if (m_code != 0)
-            {
-                mxb_assert(m_sql_state.length() == 5);
-                // The 'sql_state' of all transaction rollbacks is "40XXX".
-                if (m_sql_state[0] == '4' && m_sql_state[1] == '0')
-                {
-                    rv = true;
-                }
-            }
-
-            return rv;
-        }
-
-        bool is_wsrep_error() const
-        {
-            return m_code == 1047
-                   && m_sql_state == "08S01"
-                   && m_message == "WSREP has not yet prepared node for application use";
-        }
-
-        bool is_unexpected_error() const
-        {
-            switch (m_code)
-            {
-            case ER_CONNECTION_KILLED:
-            case ER_SERVER_SHUTDOWN:
-            case ER_NORMAL_SHUTDOWN:
-            case ER_SHUTDOWN_COMPLETE:
-                return true;
-
-            default:
-                return false;
-            }
-        }
-
-        uint32_t code() const
-        {
-            return m_code;
-        }
-
-        const std::string& sql_state() const
-        {
-            return m_sql_state;
-        }
-
-        const std::string& message() const
-        {
-            return m_message;
-        }
-
-        template<class InputIterator>
-        void set(uint32_t code,
-                 InputIterator sql_state_begin, InputIterator sql_state_end,
-                 InputIterator message_begin, InputIterator message_end)
-        {
-            mxb_assert(std::distance(sql_state_begin, sql_state_end) == 5);
-            m_code = code;
-            m_sql_state.assign(sql_state_begin, sql_state_end);
-            m_message.assign(message_begin, message_end);
-        }
-
-        void clear()
-        {
-            m_code = 0;
-            m_sql_state.clear();
-            m_message.clear();
-        }
-
-    private:
-        uint16_t    m_code {0};
-        std::string m_sql_state;
-        std::string m_message;
-    };
 
     static SRWBackends from_endpoints(const Endpoints& endpoints);
 
     RWBackend(mxs::Endpoint* endpoint);
     virtual ~RWBackend();
-
-    inline reply_state_t get_reply_state() const
-    {
-        return m_reply_state;
-    }
-
-    const char* reply_state_str() const
-    {
-        switch (m_reply_state)
-        {
-        case REPLY_STATE_START:
-            return "START";
-
-        case REPLY_STATE_DONE:
-            return "DONE";
-
-        case REPLY_STATE_RSET_COLDEF:
-            return "COLDEF";
-
-        case REPLY_STATE_RSET_COLDEF_EOF:
-            return "COLDEF_EOF";
-
-        case REPLY_STATE_RSET_ROWS:
-            return "ROWS";
-
-        default:
-            return "UNKNOWN";
-        }
-    }
 
     void     add_ps_handle(uint32_t id, uint32_t handle);
     uint32_t get_ps_handle(uint32_t id) const;
@@ -208,53 +82,6 @@ public:
 
     void close(close_type type = CLOSE_NORMAL);
 
-    // For COM_STMT_FETCH processing
-    bool consume_fetched_rows(GWBUF* buffer);
-
-    inline uint8_t current_command() const
-    {
-        return m_command;
-    }
-
-    bool local_infile_requested() const
-    {
-        return m_local_infile_requested;
-    }
-
-    void process_reply(GWBUF* buffer);
-
-    /**
-     * Updated during the call to @c process_reply().
-     *
-     * @return The current error state.
-     */
-    const Error& error() const
-    {
-        return m_error;
-    }
-
-    /**
-     * Check whether the response from the server is complete
-     *
-     * @return True if no more results are expected from this server
-     */
-    bool reply_is_complete() const
-    {
-        return m_reply_state == REPLY_STATE_DONE;
-    }
-
-    /**
-     * Check if a partial response has been received from the backend
-     *
-     * @return True if some parts of the reply have been received
-     */
-    bool reply_has_started() const
-    {
-        return m_size > 0 && m_reply_state != REPLY_STATE_DONE;
-    }
-
-    void process_packets(GWBUF* buffer);
-
     // Controlled by the session
     ResponseStat& response_stat();
 
@@ -264,49 +91,10 @@ public:
     }
 
 private:
-    reply_state_t    m_reply_state;
     BackendHandleMap m_ps_handles;      /**< Internal ID to backend PS handle mapping */
-    modutil_state    m_modutil_state;   /**< @see modutil_count_signal_packets */
-    uint8_t          m_command;
-    bool             m_opening_cursor;          /**< Whether we are opening a cursor */
-    uint32_t         m_expected_rows;           /**< Number of rows a COM_STMT_FETCH is retrieving */
-    bool             m_local_infile_requested;  /**< Whether a LOCAL INFILE was requested */
     ResponseStat     m_response_stat;
-    uint64_t         m_num_coldefs = 0;
     bool             m_large_query = false;
-    bool             m_skip_next = false;
-    Error            m_error;
-    uint64_t         m_size = 0;/**< Size of the response */
 
     std::chrono::steady_clock::time_point m_last_write;
-
-    /**
-     * @param it   Iterator pointing to the command byte of an error packet.
-     * @param end  Iterator pointing one past the end of the error packet.
-     */
-    void process_reply_start(mxs::Buffer::iterator it, mxs::Buffer::iterator end);
-
-    /**
-     * Update @c m_error.
-     *
-     * @param it   Iterator that points to the first byte of the error code in an error packet.
-     * @param end  Iterator pointing one past the end of the error packet.
-     */
-    void update_error(mxs::Buffer::iterator it, mxs::Buffer::iterator end);
-
-    inline bool is_opening_cursor() const
-    {
-        return m_opening_cursor;
-    }
-
-    inline void set_cursor_opened()
-    {
-        m_opening_cursor = false;
-    }
-
-    inline void set_reply_state(reply_state_t state)
-    {
-        m_reply_state = state;
-    }
 };
 }
--- server/modules/protocol/MySQL/rwbackend.cc
+++ server/modules/protocol/MySQL/rwbackend.cc
@@ -26,12 +26,6 @@ namespace maxscale
 
 RWBackend::RWBackend(mxs::Endpoint* ref)
     : mxs::Backend(ref)
-    , m_reply_state(REPLY_STATE_DONE)
-    , m_modutil_state{0}
-    , m_command(0)
-    , m_opening_cursor(false)
-    , m_expected_rows(0)
-    , m_local_infile_requested(false)
     , m_last_write(Clock::now())
 {
 }
@@ -43,17 +37,7 @@ RWBackend::~RWBackend()
 bool RWBackend::execute_session_command()
 {
     m_last_write = Clock::now();
-    m_command = next_session_command()->get_command();
-    bool expect_response = mxs_mysql_command_will_respond(m_command);
-    bool rval = mxs::Backend::execute_session_command();
-
-    if (rval && expect_response)
-    {
-        set_reply_state(REPLY_STATE_START);
-        m_size = 0;
-    }
-
-    return rval;
+    return mxs::Backend::execute_session_command();
 }
 
 bool RWBackend::continue_session_command(GWBUF* buffer)
@@ -92,16 +76,7 @@ bool RWBackend::write(GWBUF* buffer, response_type type)
         return mxs::Backend::write(buffer, Backend::NO_RESPONSE);
     }
 
-    if (type == mxs::Backend::EXPECT_RESPONSE)
-    {
-        /** The server will reply to this command */
-        set_reply_state(REPLY_STATE_START);
-        m_size = 0;
-    }
-
     uint8_t cmd = mxs_mysql_get_command(buffer);
-
-    m_command = cmd;
 
     if (mxs_mysql_is_ps_command(cmd))
     {
@@ -114,7 +89,7 @@ bool RWBackend::write(GWBUF* buffer, response_type type)
         buffer = tmp;
 
         uint32_t id = mxs_mysql_extract_ps_id(buffer);
-        BackendHandleMap::iterator it = m_ps_handles.find(id);
+        auto it = m_ps_handles.find(id);
 
         if (it != m_ps_handles.end())
         {
@@ -122,25 +97,9 @@ bool RWBackend::write(GWBUF* buffer, response_type type)
             uint8_t* ptr = GWBUF_DATA(buffer) + MYSQL_PS_ID_OFFSET;
             gw_mysql_set_byte4(ptr, it->second);
 
-            if (cmd == MXS_COM_STMT_EXECUTE)
-            {
-                // Extract the flag byte after the statement ID
-                uint8_t flags = 0;
-                gwbuf_copy_data(buffer, MYSQL_PS_ID_OFFSET + MYSQL_PS_ID_SIZE, 1, &flags);
-
-                // Any non-zero flag value means that we have an open cursor
-                m_opening_cursor = flags != 0;
-            }
-            else if (cmd == MXS_COM_STMT_CLOSE)
+            if (cmd == MXS_COM_STMT_CLOSE)
             {
                 m_ps_handles.erase(it);
-            }
-            else if (cmd == MXS_COM_STMT_FETCH)
-            {
-                // Number of rows to fetch is a 4 byte integer after the ID
-                uint8_t buf[4];
-                gwbuf_copy_data(buffer, MYSQL_PS_ID_OFFSET + MYSQL_PS_ID_SIZE, 4, buf);
-                m_expected_rows = gw_mysql_get_byte4(buf);
             }
         }
     }
@@ -150,293 +109,7 @@ bool RWBackend::write(GWBUF* buffer, response_type type)
 
 void RWBackend::close(close_type type)
 {
-    m_reply_state = REPLY_STATE_DONE;
     mxs::Backend::close(type);
-}
-
-bool RWBackend::consume_fetched_rows(GWBUF* buffer)
-{
-    bool rval = false;
-    bool more = false;
-    int n_eof = modutil_count_signal_packets(buffer, 0, &more, &m_modutil_state);
-
-    // If the server responded with an error, n_eof > 0
-    if (n_eof > 0)
-    {
-        rval = true;
-    }
-    else
-    {
-        m_expected_rows -= modutil_count_packets(buffer);
-        mxb_assert(m_expected_rows >= 0);
-        rval = m_expected_rows == 0;
-    }
-
-    return rval;
-}
-
-static inline bool have_next_packet(GWBUF* buffer)
-{
-    uint32_t len = MYSQL_GET_PAYLOAD_LEN(GWBUF_DATA(buffer)) + MYSQL_HEADER_LEN;
-    return gwbuf_length(buffer) > len;
-}
-
-uint64_t get_encoded_int(Iter it)
-{
-    uint64_t len = *it++;
-
-    switch (len)
-    {
-    case 0xfc:
-        len = *it++;
-        len |= ((uint64_t)*it++) << 8;
-        break;
-
-    case 0xfd:
-        len = *it++;
-        len |= ((uint64_t)*it++) << 8;
-        len |= ((uint64_t)*it++) << 16;
-        break;
-
-    case 0xfe:
-        len = *it++;
-        len |= ((uint64_t)*it++) << 8;
-        len |= ((uint64_t)*it++) << 16;
-        len |= ((uint64_t)*it++) << 24;
-        len |= ((uint64_t)*it++) << 32;
-        len |= ((uint64_t)*it++) << 40;
-        len |= ((uint64_t)*it++) << 48;
-        len |= ((uint64_t)*it++) << 56;
-        break;
-
-    default:
-        break;
-    }
-
-    return len;
-}
-
-Iter skip_encoded_int(Iter it)
-{
-    switch (*it)
-    {
-    case 0xfc:
-        it.advance(3);
-        break;
-
-    case 0xfd:
-        it.advance(4);
-        break;
-
-    case 0xfe:
-        it.advance(9);
-        break;
-
-    default:
-        ++it;
-        break;
-    }
-
-    return it;
-}
-
-bool is_last_ok(Iter it)
-{
-    ++it;                       // Skip the command byte
-    it = skip_encoded_int(it);  // Affected rows
-    it = skip_encoded_int(it);  // Last insert ID
-    uint16_t status = *it++;
-    status |= (*it++) << 8;
-    return (status & SERVER_MORE_RESULTS_EXIST) == 0;
-}
-
-bool is_last_eof(Iter it)
-{
-    std::advance(it, 3);    // Skip the command byte and warning count
-    uint16_t status = *it++;
-    status |= (*it++) << 8;
-    return (status & SERVER_MORE_RESULTS_EXIST) == 0;
-}
-
-void RWBackend::process_reply_start(Iter it, Iter end)
-{
-    uint8_t cmd = *it;
-    m_local_infile_requested = false;
-
-    switch (cmd)
-    {
-    case MYSQL_REPLY_OK:
-        if (is_last_ok(it))
-        {
-            // No more results
-            set_reply_state(REPLY_STATE_DONE);
-        }
-        break;
-
-    case MYSQL_REPLY_LOCAL_INFILE:
-        m_local_infile_requested = true;
-        set_reply_state(REPLY_STATE_DONE);
-        break;
-
-    case MYSQL_REPLY_ERR:
-        // Nothing ever follows an error packet
-        ++it;
-        update_error(it, end);
-        set_reply_state(REPLY_STATE_DONE);
-        break;
-
-    case MYSQL_REPLY_EOF:
-        // EOF packets are never expected as the first response
-        mxb_assert(!true);
-        break;
-
-    default:
-        if (current_command() == MXS_COM_FIELD_LIST)
-        {
-            // COM_FIELD_LIST sends a strange kind of a result set
-            set_reply_state(REPLY_STATE_RSET_ROWS);
-        }
-        else
-        {
-            // Start of a result set
-            m_num_coldefs = get_encoded_int(it);
-            set_reply_state(REPLY_STATE_RSET_COLDEF);
-        }
-
-        break;
-    }
-}
-
-void RWBackend::process_packets(GWBUF* result)
-{
-    mxs::Buffer buffer(result);
-    auto it = buffer.begin();
-    MXB_AT_DEBUG(size_t total_len = buffer.length());
-    MXB_AT_DEBUG(size_t used_len = 0);
-
-    while (it != buffer.end())
-    {
-        // Extract packet length and command byte
-        uint32_t len = *it++;
-        len |= (*it++) << 8;
-        len |= (*it++) << 16;
-        ++it;   // Skip the sequence
-        mxb_assert(it != buffer.end());
-        mxb_assert(used_len + len <= total_len);
-        MXB_AT_DEBUG(used_len += len);
-        auto end = it;
-        end.advance(len);
-        uint8_t cmd = *it;
-
-        m_size += len;
-
-        // Ignore the tail end of a large packet large packet. Only resultsets can generate packets this large
-        // and we don't care what the contents are and thus it is safe to ignore it.
-        bool skip_next = m_skip_next;
-        m_skip_next = len == GW_MYSQL_MAX_PACKET_LEN;
-
-        if (skip_next)
-        {
-            it = end;
-            continue;
-        }
-
-        switch (m_reply_state)
-        {
-        case REPLY_STATE_START:
-            process_reply_start(it, end);
-            break;
-
-        case REPLY_STATE_DONE:
-            if (cmd == MYSQL_REPLY_ERR)
-            {
-                update_error(++it, end);
-            }
-            else
-            {
-                // This should never happen
-                MXS_ERROR("Unexpected result state. cmd: 0x%02hhx, len: %u", cmd, len);
-                mxb_assert(!true);
-            }
-            break;
-
-        case REPLY_STATE_RSET_COLDEF:
-            mxb_assert(m_num_coldefs > 0);
-            --m_num_coldefs;
-
-            if (m_num_coldefs == 0)
-            {
-                set_reply_state(REPLY_STATE_RSET_COLDEF_EOF);
-                // Skip this state when DEPRECATE_EOF capability is supported
-            }
-            break;
-
-        case REPLY_STATE_RSET_COLDEF_EOF:
-            mxb_assert(cmd == MYSQL_REPLY_EOF && len == MYSQL_EOF_PACKET_LEN - MYSQL_HEADER_LEN);
-            set_reply_state(REPLY_STATE_RSET_ROWS);
-
-            if (is_opening_cursor())
-            {
-                set_cursor_opened();
-                MXS_INFO("Cursor successfully opened");
-                set_reply_state(REPLY_STATE_DONE);
-            }
-            break;
-
-        case REPLY_STATE_RSET_ROWS:
-            if (cmd == MYSQL_REPLY_EOF && len == MYSQL_EOF_PACKET_LEN - MYSQL_HEADER_LEN)
-            {
-                set_reply_state(is_last_eof(it) ? REPLY_STATE_DONE : REPLY_STATE_START);
-            }
-            else if (cmd == MYSQL_REPLY_ERR)
-            {
-                ++it;
-                update_error(it, end);
-                set_reply_state(REPLY_STATE_DONE);
-            }
-            break;
-        }
-
-        it = end;
-    }
-
-    buffer.release();
-}
-
-/**
- * @brief Process a possibly partial response from the backend
- *
- * @param buffer  Buffer containing the response
- */
-void RWBackend::process_reply(GWBUF* buffer)
-{
-    m_error.clear();
-
-    if (current_command() == MXS_COM_STMT_FETCH)
-    {
-        // TODO: m_error is not updated here.
-        // If the server responded with an error, n_eof > 0
-        if (consume_fetched_rows(buffer))
-        {
-            set_reply_state(REPLY_STATE_DONE);
-        }
-    }
-    else if (current_command() == MXS_COM_STATISTICS || GWBUF_IS_COLLECTED_RESULT(buffer))
-    {
-        // COM_STATISTICS returns a single string and thus requires special handling.
-        // Collected result are all in one buffer and need no processing.
-        set_reply_state(REPLY_STATE_DONE);
-    }
-    else
-    {
-        // Normal result, process it one packet at a time
-        process_packets(buffer);
-    }
-
-    if (get_reply_state() == REPLY_STATE_DONE && is_waiting_result())
-    {
-        ack_write();
-    }
 }
 
 ResponseStat& RWBackend::response_stat()
@@ -455,20 +128,5 @@ mxs::SRWBackends RWBackend::from_endpoints(const Endpoints& endpoints)
     }
 
     return backends;
-}
-
-void RWBackend::update_error(Iter it, Iter end)
-{
-    uint16_t code = 0;
-    code |= (*it++);
-    code |= (*it++) << 8;
-    ++it;
-    auto sql_state_begin = it;
-    it.advance(5);
-    auto sql_state_end = it;
-    auto message_begin = sql_state_end;
-    auto message_end = end;
-
-    m_error.set(code, sql_state_begin, sql_state_end, message_begin, message_end);
 }
 }
