cc038278d67c8f04eed3e6f8b81dbd901062c627
Marko <marko.rajala@mariadb.com>
2019-08-27 15:21:22 +0300

    MXS-2631 Add system test

---
+++ maxscale-system-test/mxs2631_ignore_system_tables.cpp
@@ -0,0 +1,25 @@
+/**
+ * MXS-2631: Dublicate system tables found
+ *
+ * https://jira.mariadb.org/browse/MXS-2631
+ */
+
+
+#include <iostream>
+#include "testconnections.h"
+
+int main(int argc, char* argv[])
+{
+    TestConnections test(argc, argv);
+    test.set_timeout(30);
+
+    MYSQL* conn = test.maxscales->open_rwsplit_connection(0);
+
+    test.add_result(execute_query(conn, "SELECT 1"), "Query should succeed.");
+
+    mysql_close(conn);
+    test.stop_timeout();
+    sleep(1);
+    test.repl->fix_replication();
+    return test.global_result;
+}
