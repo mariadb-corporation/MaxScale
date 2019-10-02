mariadb-corporation/MaxScale

commit f9a33bf8efd6365ef639115110c5b2fc3e78be36

Fix double free of GWBUF in Session::handleError
The handleError method does not own the buffer.

server/core/session.cc
@@ -1521,7 +1521,7 @@ int32_t Session::routeQuery(GWBUF* buffer)
 
 int32_t Session::clientReply(GWBUF* buffer, mxs::ReplyRoute& down, const mxs::Reply* reply)
 {
-    return client_dcb->protocol_write(buffer);
+    return client_dcb->protocol_write(gwbuf_clone(buffer));
 }
 
 bool Session::handleError(GWBUF* error, Endpoint* down)

commit d44b865cd37ac7cc461b1ce6f5a8030d7386919e

Fix memory leak
This reverts commit f9a33bf8efd6365ef639115110c5b2fc3e78be36,
which caused a memory leak, and fixes what that commit intended to
fix: Fix double free of GWBUF in Session::handleError.

server/core/session.cc
@@ -1480,13 +1480,13 @@ int32_t Session::routeQuery(GWBUF* buffer)
 
 int32_t Session::clientReply(GWBUF* buffer, mxs::ReplyRoute& down, const mxs::Reply& reply)
 {
-    return client_dcb->protocol_write(gwbuf_clone(buffer));
+    return client_dcb->protocol_write(buffer);
 }
 
 bool Session::handleError(GWBUF* error, Endpoint* down, const mxs::Reply& reply)
 {
     mxs::ReplyRoute route;
-    clientReply(error, route, reply);
+    clientReply(gwbuf_clone(error), route, reply);
     terminate();
     return false;
 }