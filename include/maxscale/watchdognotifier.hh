f238ee5271729d8613c6fc817dcc3e9f877aa417
Johan Wikman <johan.wikman@mariadb.com>
2019-10-08 13:11:14 +0300

    MXS-2716 Introduce dedicated systemd watchdog notifier
    The systemd watchdog notifier must be a dedicated thread, not used
    for anything else.

---
+++ include/maxscale/watchdognotifier.hh
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2018 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2023-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#include <maxscale/ccdefs.hh>
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <unordered_set>
+#include <maxbase/assert.h>
+#include <maxbase/atomic.hh>
+#include <maxbase/semaphore.hh>
+#include <maxbase/stopwatch.hh>
+
+namespace maxscale
+{
+
+/**
+ * @class WatchdogNotifier
+ *
+ * An instance of this class performs systemd watchdog notifications at
+ * regular intervals, provided all dependents are deemed to be alive.
+ * Note that there at any moment may be at most one instance alive of
+ * WatchdogNotifier.
+ */
+class WatchdogNotifier
+{
+public:
+    /**
+     * @class Dependent
+     *
+     * The liveness of an instance of a class derived from
+     * WatchdogNotifier::Dependent will be considered when deciding whether
+     * MaxScale itself is alive.
+     */
+    class Dependent
+    {
+    public:
+        /**
+         * @class WatchdogWorkaround
+         *
+         * RAII-class using which the systemd watchdog notification can be
+         * handled during synchronous worker activity that causes the epoll
+         * event handling to be stalled.
+         *
+         * The constructor turns on the workaround and the destructor
+         * turns it off.
+         */
+        class WatchdogWorkaround
+        {
+        public:
+            WatchdogWorkaround(const WatchdogWorkaround&) = delete;
+            WatchdogWorkaround& operator=(const WatchdogWorkaround&) = delete;
+
+            /**
+             * Turns on the watchdog workaround for a specific dependent.
+             *
+             * @param pDependent  The dependent for which the systemd notification
+             *                    should be arranged.
+             */
+            WatchdogWorkaround(Dependent* pDependent)
+                : m_dependent(*pDependent)
+            {
+                mxb_assert(pDependent);
+                m_dependent.start_watchdog_workaround();
+            }
+
+            /**
+             * Turns off the watchdog workaround.
+             */
+            ~WatchdogWorkaround()
+            {
+                m_dependent.stop_watchdog_workaround();
+            }
+
+        private:
+            Dependent& m_dependent;
+        };
+
+        virtual ~Dependent();
+
+        const WatchdogNotifier& notifier() const
+        {
+            return m_notifier;
+        }
+
+        /**
+         * Starts the watchdog workaround that will ensure that the systemd
+         * watchdog is notified, even if the dependent would perform a lengthy
+         * synchronous operation.
+         *
+         * It is permissible to call this function multiple times, but
+         * each call should be matched with a call to
+         * @c stop_watchdog_workaround().
+         *
+         * @note This should be considered as the last resort, the right
+         *       approach is to replace the synchronous operation with
+         *       an asynchronous one.
+         */
+        void start_watchdog_workaround();
+
+        /**
+         * Stops the watchdog workaround.
+         */
+        void stop_watchdog_workaround();
+
+        /**
+         * @return True, if the dependent is alive and kicking.
+         */
+        bool is_ticking() const
+        {
+            return m_ticking.load(std::memory_order_acquire);
+        }
+
+    protected:
+        Dependent(WatchdogNotifier* pNotifier);
+
+        /**
+         * To be regularly called by a derived class.
+         */
+        void mark_ticking_if_currently_not()
+        {
+            if (m_ticking.load(std::memory_order_acquire) == false)
+            {
+                m_ticking.store(true, std::memory_order_release);
+            }
+        }
+
+    private:
+        friend class WatchdogNotifier;
+
+        void mark_not_ticking()
+        {
+            m_ticking.store(false, std::memory_order_release);
+        }
+
+    private:
+        class Ticker;
+        friend Ticker;
+
+        WatchdogNotifier& m_notifier;
+        std::atomic<bool> m_ticking;
+        Ticker*           m_pTicker { nullptr }; /*< Watchdog ticker, if systemd enabled. */
+    };
+
+    WatchdogNotifier(const WatchdogNotifier&) = delete;
+    WatchdogNotifier& operator=(const WatchdogNotifier&) = delete;
+
+    /**
+     * Constructor
+     *
+     * @param usec  The systemd notification interval in micro seconds.
+     *              If 0, then there will be no notifications.
+     */
+    WatchdogNotifier(uint64_t usec);
+    ~WatchdogNotifier();
+
+    const mxb::Duration& interval() const
+    {
+        return m_interval;
+    }
+
+    void start();
+
+    void stop();
+
+private:
+    friend Dependent;
+
+    friend class Dependent;
+
+    void add(Dependent* pDependent);
+    void remove(Dependent* pDependent);
+
+private:
+    void run();
+    void notify_systemd_watchdog();
+
+    std::thread                    m_thread;
+    mxb::Semaphore                 m_sem;
+    mxb::Duration                  m_interval;        /*< Duration between notifications, if any. */
+    std::unordered_set<Dependent*> m_dependents;
+    std::mutex                     m_dependents_lock;
+};
+
+}
---
+++ server/core/watchdognotifier.cc
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2018 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2023-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#include <maxscale/watchdognotifier.hh>
+#ifdef HAVE_SYSTEMD
+#include <systemd/sd-daemon.h>
+#endif
+
+using std::lock_guard;
+using std::mutex;
+using std::unique_lock;
+
+namespace
+{
+
+static struct ThisUnit
+{
+    maxscale::WatchdogNotifier* pNotifier = nullptr;
+} this_unit;
+
+}
+
+namespace maxscale
+{
+
+/**
+ * @class WatchdogNotifier::Dependent::Ticker
+ *
+ * This class is capable making an instance of Dependent appear
+ * to be ticking, even if it actually is not while performing some
+ * lengthy synchronous operation.
+ */
+class WatchdogNotifier::Dependent::Ticker
+{
+    Ticker(const Ticker&) = delete;
+    Ticker& operator=(const Ticker&) = delete;
+
+public:
+    Ticker(Dependent* pOwner)
+        : m_owner(*pOwner)
+        , m_nClients(0)
+        , m_terminate(false)
+    {
+        m_thread = std::thread([this] {
+                                   uint32_t interval = m_owner.notifier().interval().secs();
+                                   timespec timeout = {interval, 0};
+
+                                   while (!mxb::atomic::load(&m_terminate, mxb::atomic::RELAXED))
+                                   {
+                                        // We will wakeup when someone wants the notifier to run,
+                                        // or when MaxScale is going down.
+                                       m_sem_start.wait();
+
+                                       if (!mxb::atomic::load(&m_terminate, mxb::atomic::RELAXED))
+                                       {
+                                            // If MaxScale is not going down...
+                                           do
+                                           {
+                                                // we ensure the worker appears to be ticking
+                                               m_owner.mark_ticking_if_currently_not();
+                                           }
+                                           while (!m_sem_stop.timedwait(timeout));
+                                            // until the semaphore is actually posted, which it will be
+                                            // once the notification should stop.
+                                       }
+                                   }
+                               });
+    }
+
+    ~Ticker()
+    {
+        mxb_assert(m_nClients == 0);
+        mxb::atomic::store(&m_terminate, true, mxb::atomic::RELAXED);
+        m_sem_start.post();
+        m_thread.join();
+    }
+
+    void start()
+    {
+        Guard guard(m_lock);
+        mxb::atomic::add(&m_nClients, 1, mxb::atomic::RELAXED);
+
+        if (m_nClients == 1)
+        {
+            m_sem_start.post();
+        }
+    }
+
+    void stop()
+    {
+        Guard guard(m_lock);
+        mxb::atomic::add(&m_nClients, -1, mxb::atomic::RELAXED);
+        mxb_assert(m_nClients >= 0);
+
+        if (m_nClients == 0)
+        {
+            m_sem_stop.post();
+        }
+    }
+
+private:
+    using Guard = std::lock_guard<std::mutex>;
+
+    Dependent&     m_owner;
+    int            m_nClients;
+    bool           m_terminate;
+    std::thread    m_thread;
+    std::mutex     m_lock;
+    mxb::Semaphore m_sem_start;
+    mxb::Semaphore m_sem_stop;
+};
+
+WatchdogNotifier::Dependent::Dependent(WatchdogNotifier* pNotifier)
+    : m_notifier(*pNotifier)
+    , m_ticking(true)
+{
+    if (m_notifier.interval().count() != 0)
+    {
+        m_pTicker = new Ticker(this);
+    }
+
+    m_notifier.add(this);
+}
+
+WatchdogNotifier::Dependent::~Dependent()
+{
+    m_notifier.remove(this);
+
+    delete m_pTicker;
+}
+
+void WatchdogNotifier::Dependent::start_watchdog_workaround()
+{
+    if (m_pTicker)
+    {
+        m_pTicker->start();
+    }
+}
+
+void WatchdogNotifier::Dependent::stop_watchdog_workaround()
+{
+    if (m_pTicker)
+    {
+        m_pTicker->stop();
+    }
+}
+
+WatchdogNotifier::WatchdogNotifier(uint64_t usecs)
+    // The internal timeout is 1/3 of the systemd configured interval.
+    : m_interval(maxbase::Duration(1.0 * usecs / 2000000))
+{
+    mxb_assert(this_unit.pNotifier == nullptr);
+    this_unit.pNotifier = this;
+
+    if (m_interval.count() != 0)
+    {
+        MXS_NOTICE("The systemd watchdog is Enabled. Internal timeout = %s\n",
+                   to_string(m_interval).c_str());
+    }
+}
+
+WatchdogNotifier::~WatchdogNotifier()
+{
+    mxb_assert(m_dependents.size() == 0);
+    mxb_assert(this_unit.pNotifier == this);
+    this_unit.pNotifier = nullptr;
+}
+
+void WatchdogNotifier::start()
+{
+    mxb_assert(m_thread.get_id() == std::thread::id());
+
+    if (m_interval.count() != 0)
+    {
+        m_thread = std::thread(&WatchdogNotifier::run, this);
+    }
+}
+
+void WatchdogNotifier::stop()
+{
+    if (m_interval.count() != 0)
+    {
+        mxb_assert(m_thread.get_id() != std::thread::id());
+
+        m_sem.post();
+        m_thread.join();
+    }
+}
+
+void WatchdogNotifier::add(Dependent* pDependent)
+{
+    lock_guard<mutex> guard(m_dependents_lock);
+
+    mxb_assert(m_dependents.find(pDependent) == m_dependents.end());
+
+    m_dependents.insert(pDependent);
+}
+
+void WatchdogNotifier::remove(Dependent* pDependent)
+{
+    lock_guard<mutex> guard(m_dependents_lock);
+
+    auto it = m_dependents.find(pDependent);
+    mxb_assert(it != m_dependents.end());
+
+    m_dependents.erase(it);
+}
+
+void WatchdogNotifier::run()
+{
+    mxb_assert(m_interval.count() != 0);
+
+    time_t secs = m_interval.count();
+    long nsecs = 0;
+
+    while (m_sem.timedwait(secs, nsecs, mxb::Semaphore::IGNORE_SIGNALS) == false)
+    {
+        notify_systemd_watchdog();
+    }
+
+    // If the waiting succeeded, it's because someone has called stop().
+}
+
+void WatchdogNotifier::notify_systemd_watchdog()
+{
+    unique_lock<mutex> guard(m_dependents_lock);
+
+    bool all_ticking = std::all_of(m_dependents.begin(), m_dependents.end(), [](Dependent* pDependent) {
+            return pDependent->is_ticking();
+        });
+
+    if (all_ticking)
+    {
+        std::for_each(m_dependents.begin(), m_dependents.end(), [](Dependent* pDependent) {
+                pDependent->mark_not_ticking();
+            });
+    }
+
+    guard.unlock();
+
+    if (all_ticking)
+    {
+#ifdef HAVE_SYSTEMD
+        MXS_DEBUG("systemd watchdog keep-alive ping: sd_notify(false, \"WATCHDOG=1\")");
+        sd_notify(false, "WATCHDOG=1");
+#endif
+    }
+}
+
+}
