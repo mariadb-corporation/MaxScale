7e15161f7cd3869aeaf6d4686573d485deb091e3
Markus Mäkelä <markus.makela@mariadb.com>
2019-10-17 14:05:45 +0300

    Add DDL tokenizer
    By fully tokenizing the SQL for DDLs, a recursive descent parser can be
    written to handle all DDL processing. This should be a clearer solution
    compared to the old string processing based parsing.

---
+++ server/modules/routing/avrorouter/tokenizer.cc
@@ -0,0 +1,255 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2023-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include "tokenizer.hh"
+
+#include <cstring>
+#include <unordered_map>
+
+#define MAKE_VALUE(a) { #a, a}
+
+namespace tok
+{
+
+// SQL keyword to enum value map, must stay in sync with the enum values defined in tokenizer.hh
+const std::unordered_map<std::string, Type> Tokenizer::s_tokens =
+{
+    MAKE_VALUE(ADD),
+    MAKE_VALUE(AFTER),
+    MAKE_VALUE(ALTER),
+    MAKE_VALUE(AUTO_INCREMENT),
+    MAKE_VALUE(CASCADE),
+    MAKE_VALUE(CHANGE),
+    MAKE_VALUE(COLUMN_FORMAT),
+    MAKE_VALUE(COLUMN),
+    MAKE_VALUE(COMMENT),
+    MAKE_VALUE(CONSTRAINT),
+    MAKE_VALUE(CREATE),
+    MAKE_VALUE(DEFAULT),
+    MAKE_VALUE(DROP),
+    MAKE_VALUE(DYNAMIC),
+    MAKE_VALUE(EXISTS),
+    MAKE_VALUE(FIRST),
+    MAKE_VALUE(FIXED),
+    MAKE_VALUE(FOREIGN),
+    MAKE_VALUE(FULLTEXT),
+    MAKE_VALUE(IF),
+    MAKE_VALUE(IGNORE),
+    MAKE_VALUE(INDEX),
+    MAKE_VALUE(INVISIBLE),
+    MAKE_VALUE(KEY),
+    MAKE_VALUE(KEYS),
+    MAKE_VALUE(LIKE),
+    MAKE_VALUE(MODIFY),
+    MAKE_VALUE(NOT),
+    MAKE_VALUE(ONLINE),
+    MAKE_VALUE(OR),
+    MAKE_VALUE(PERIOD),
+    MAKE_VALUE(PRIMARY),
+    MAKE_VALUE(REF_SYSTEM_ID),
+    MAKE_VALUE(REMOVE),
+    MAKE_VALUE(RENAME),
+    MAKE_VALUE(REPLACE),
+    MAKE_VALUE(RESTRICT),
+    MAKE_VALUE(SPATIAL),
+    MAKE_VALUE(SYSTEM),
+    MAKE_VALUE(TABLE),
+    MAKE_VALUE(TO),
+    MAKE_VALUE(UNIQUE),
+    MAKE_VALUE(UNSIGNED),
+    MAKE_VALUE(VERSIONING),
+    MAKE_VALUE(WITH),
+    MAKE_VALUE(WITHOUT),
+    MAKE_VALUE(ZEROFILL),
+
+    {"NULL",                   SQLNULL},
+};
+
+std::string Tokenizer::Token::to_string() const
+{
+    for (const auto& a : Tokenizer::s_tokens)
+    {
+        if (a.second == type())
+        {
+            return a.first;
+        }
+    }
+
+    switch (type())
+    {
+    case DOT:
+        return ".";
+
+    case COMMA:
+        return ",";
+
+    case LP:
+        return "(";
+
+    case RP:
+        return ")";
+
+    case EQ:
+        return "=";
+
+    case ID:
+        return "ID[" + value() + "]";
+
+    default:
+        return "UNKNOWN";
+    }
+}
+
+std::string Tokenizer::Token::value() const
+{
+    return m_sanitizer(m_str, m_len);
+}
+
+const char* find_char(const char* s, char c)
+{
+    while (*s)
+    {
+        if (*s == '\\')
+        {
+            ++s;
+        }
+        else if (*s == c)
+        {
+            break;
+        }
+
+        ++s;
+    }
+
+    return s;
+}
+
+bool is_special(char c)
+{
+    switch (c)
+    {
+    case '.':
+    case ',':
+    case '(':
+    case ')':
+    case '`':
+    case '\'':
+    case '"':
+    case '=':
+        return true;
+
+    default:
+        return isspace(c);
+    }
+}
+
+Tokenizer::Chain Tokenizer::tokenize(const char* sql, Sanitizer sanitizer)
+{
+    Tokenizer::Chain rval;
+    std::string buf;
+
+    while (char c = *sql)
+    {
+        switch (c)
+        {
+        case '.':
+            rval.m_tokens.emplace_back(DOT, sql);
+            ++sql;
+            break;
+
+        case '=':
+            rval.m_tokens.emplace_back(EQ, sql);
+            ++sql;
+            break;
+
+        case ',':
+            rval.m_tokens.emplace_back(COMMA, sql);
+            ++sql;
+            break;
+
+        case '(':
+            rval.m_tokens.emplace_back(LP, sql);
+            ++sql;
+            break;
+
+        case ')':
+            rval.m_tokens.emplace_back(RP, sql);
+            ++sql;
+            break;
+
+        case '`':
+            ++sql;
+            if (auto s = find_char(sql, '`'))
+            {
+                rval.m_tokens.emplace_back(ID, sql, s - sql, sanitizer);
+                sql = s + 1;
+            }
+            else
+            {
+                return rval;    // Abort tokenization
+            }
+
+            break;
+
+        case '\'':
+        case '"':
+            ++sql;
+            if (auto s = find_char(sql, c))
+            {
+                rval.m_tokens.emplace_back(ID, sql, s - sql, sanitizer);
+                sql = s + 1;
+            }
+            else
+            {
+                return rval;    // Abort tokenization
+            }
+            break;
+
+        default:
+            if (isspace(c))
+            {
+                ++sql;
+            }
+            else
+            {
+                const char* start = sql;
+                buf.clear();
+
+                while (*sql && !is_special(*sql))
+                {
+                    buf += toupper(*sql++);
+                }
+
+                Type type = ID;
+                auto it = s_tokens.find(buf);
+
+                if (it != s_tokens.end())
+                {
+                    type = it->second;
+                }
+
+                rval.m_tokens.emplace_back(type, start, sql - start, sanitizer);
+            }
+
+            break;
+        }
+    }
+
+    return rval;
+}
+
+bool operator==(const Tokenizer::Token& lhs, const Tokenizer::Token& rhs)
+{
+    return lhs.type() == rhs.type();
+}
+}
---
+++ server/modules/routing/avrorouter/tokenizer.hh
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2023-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#include <deque>
+#include <functional>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace tok
+{
+
+using Sanitizer = std::function<std::string (const char*, int)>;
+
+static std::string default_sanitizer(const char* sql, int len)
+{
+    return std::string(sql, len);
+}
+
+enum Type
+{
+    // Used for non-keyword tokens e.g. string literals and identifiers
+    ID,
+
+    // SQL keywords
+    ADD,
+    AFTER,
+    ALTER,
+    AUTO_INCREMENT,
+    CASCADE,
+    CHANGE,
+    COLUMN_FORMAT,
+    COLUMN,
+    COMMENT,
+    CONSTRAINT,
+    CREATE,
+    DEFAULT,
+    DROP,
+    DYNAMIC,
+    EXISTS,
+    FIRST,
+    FIXED,
+    FOREIGN,
+    FULLTEXT,
+    IF,
+    IGNORE,
+    INDEX,
+    INVISIBLE,
+    KEY,
+    KEYS,
+    LIKE,
+    MODIFY,
+    NOT,
+    ONLINE,
+    OR,
+    PERIOD,
+    PRIMARY,
+    REF_SYSTEM_ID,
+    REMOVE,
+    RENAME,
+    REPLACE,
+    RESTRICT,
+    SPATIAL,
+    SYSTEM,
+    TABLE,
+    TO,
+    UNIQUE,
+    UNSIGNED,
+    VERSIONING,
+    WITH,
+    WITHOUT,
+    ZEROFILL,
+
+    // Special characters
+    DOT,
+    COMMA,
+    LP,
+    RP,
+    EQ,
+    SQLNULL,
+
+    // Marks the end of the token list. Returned by Tokenizer::Chain when tokenizer is exhausted to remove the
+    // need for bounds checks.
+    EXHAUSTED
+};
+
+class Tokenizer
+{
+public:
+
+    struct Token
+    {
+        Token(Type t, const char* s)
+            : m_type(t)
+            , m_str(s)
+        {
+        }
+
+        Token(Type t, const char* s, int l, Sanitizer sanitizer)
+            : m_type(t)
+            , m_str(s)
+            , m_len(l)
+            , m_sanitizer(sanitizer)
+        {
+        }
+
+        Token(Type t)
+            : m_type(t)
+        {
+        }
+
+        Token()
+            : m_type(EXHAUSTED)
+        {
+        }
+
+        static std::string to_string(Type t)
+        {
+            return Token(t).to_string();
+        }
+
+        std::string to_string() const;
+
+        Type type() const
+        {
+            return m_type;
+        }
+
+        const char* position() const
+        {
+            return m_str;
+        }
+
+        std::string value() const;
+
+    private:
+        Type        m_type;
+        const char* m_str {nullptr};
+        int         m_len {0};
+        Sanitizer   m_sanitizer {default_sanitizer};
+    };
+
+    class Chain
+    {
+    public:
+        using Container = std::deque<Token>;
+
+        Token chomp()
+        {
+            Token rv;
+
+            if (!m_tokens.empty())
+            {
+                rv = std::move(m_tokens.front());
+                m_tokens.pop_front();
+            }
+
+            return rv;
+        }
+
+        Token front() const
+        {
+            Token rv;
+
+            if (!m_tokens.empty())
+            {
+                rv = m_tokens.front();
+            }
+
+            return rv;
+        }
+
+        Container::iterator begin()
+        {
+            return m_tokens.begin();
+        }
+
+        Container::iterator end()
+        {
+            return m_tokens.end();
+        }
+
+    private:
+        friend class Tokenizer;
+        Container m_tokens;
+    };
+
+    static Chain tokenize(const char* sql, Sanitizer sanitizer = default_sanitizer);
+
+private:
+    static const std::unordered_map<std::string, Type> s_tokens;
+};
+
+bool operator==(const Tokenizer::Token& lhs, const Tokenizer::Token& rhs);
+}
