mariadb-corporation/MaxScale

commit 187c48fc82d698aeb9d2ea07235112bdf4cd9fbe

MXS-2628: Route queries to backend more evenly
In the case of a tie, the backend which was used least recently will be
chosen. This doesn't guarantee absolutely even load balancing but in
practice it is enough to appear as if it did.

server/modules/routing/readwritesplit/rwsplit_select_backends.cc
@@ -30,6 +30,10 @@
 
 using namespace maxscale;
 
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+using Clock = std::chrono::steady_clock;
+
 /**
  * Check whether it's possible to use this server as a slave
  *
@@ -70,6 +74,18 @@ RWBackend* best_score(PRWBackends& sBackends, std::function<double(mxs::Endpoint
         {
             min = score;
             best = b;
+        }
+        else if (min == score && best)
+        {
+            // In the case of a tie, use the least recently used backend
+            auto now = Clock::now();
+            auto left = duration_cast<microseconds>(now - best->last_write()).count();
+            auto right = duration_cast<microseconds>(now - b->last_write()).count();
+
+            if (left < right)
+            {
+                best = b;
+            }
         }
     }
 

commit 752dc7a642a3c1a5a11039a7eb0da96a81316ea0

Fix test_hints
The test failed as it expected a specific slave to respond. This was
caused by commit 187c48fc82d698aeb9d2ea07235112bdf4cd9fbe which
round-robins queries to slaves when they are all equally as good.

maxscale-system-test/test_hints.cpp
@@ -12,6 +12,7 @@
 #define SERVER2 1
 #define SERVER3 2
 #define SERVER4 3
+#define NOT_MASTER -1
 
 static struct result
 {
@@ -23,50 +24,50 @@ static struct result
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id; -- maxscale route to server server3",                     SERVER3},
     {"select @@server_id;",                                                         SERVER1},
-    {"select @@server_id; -- maxscale end",                                         SERVER2},
-    {"select @@server_id; -- maxscale named1 prepare route to master",              SERVER2},
+    {"select @@server_id; -- maxscale end",                                         NOT_MASTER},
+    {"select @@server_id; -- maxscale named1 prepare route to master",              NOT_MASTER},
     {"select @@server_id; -- maxscale named1 begin",                                SERVER1},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id; -- maxscale route to server server3",                     SERVER3},
     {"select @@server_id;",                                                         SERVER1},
-    {"select @@server_id; -- maxscale end",                                         SERVER2},
+    {"select @@server_id; -- maxscale end",                                         NOT_MASTER},
     {"select @@server_id; -- maxscale shorthand1 begin route to server server2",    SERVER2},
     {"select @@server_id;",                                                         SERVER2},
     {"select @@server_id; -- maxscale route to server server3",                     SERVER3},
     {"select @@server_id;",                                                         SERVER2},
-    {"select @@server_id; -- maxscale end",                                         SERVER2},
+    {"select @@server_id; -- maxscale end",                                         NOT_MASTER},
     {"select @@server_id; # maxscale begin route to master",                        SERVER1},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id; # maxscale route to server server3",                      SERVER3},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id; # maxscale end",                                          SERVER2},
-    {"select @@server_id; # maxscale named2 prepare route to master",               SERVER2},
+    {"select @@server_id; # maxscale named2 prepare route to master",               NOT_MASTER},
     {"select @@server_id; # maxscale named2 begin",                                 SERVER1},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id; # maxscale route to server server3",                      SERVER3},
     {"select @@server_id;",                                                         SERVER1},
-    {"select @@server_id; # maxscale end",                                          SERVER2},
+    {"select @@server_id; # maxscale end",                                          NOT_MASTER},
     {"select @@server_id; # maxscale shorthand2 begin route to server server2",     SERVER2},
     {"select @@server_id;",                                                         SERVER2},
     {"select @@server_id; # maxscale route to server server3",                      SERVER3},
     {"select @@server_id;",                                                         SERVER2},
-    {"select @@server_id; # maxscale end",                                          SERVER2},
+    {"select @@server_id; # maxscale end",                                          NOT_MASTER},
     {"select @@server_id/* maxscale begin route to master */;",                     SERVER1},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id/* maxscale route to server server3 */;",                   SERVER3},
     {"select @@server_id;",                                                         SERVER1},
-    {"select @@server_id/* maxscale end */;",                                       SERVER2},
-    {"select @@server_id/* maxscale named3 prepare route to master */;",            SERVER2},
+    {"select @@server_id/* maxscale end */;",                                       NOT_MASTER},
+    {"select @@server_id/* maxscale named3 prepare route to master */;",            NOT_MASTER},
     {"select @@server_id/* maxscale named3 begin */;",                              SERVER1},
     {"select @@server_id;",                                                         SERVER1},
     {"select @@server_id/* maxscale route to server server3 */;",                   SERVER3},
     {"select @@server_id;",                                                         SERVER1},
-    {"select @@server_id/* maxscale end */;",                                       SERVER2},
+    {"select @@server_id/* maxscale end */;",                                       NOT_MASTER},
     {"select @@server_id/* maxscale shorthand3 begin route to server server2 */; ", SERVER2},
     {"select @@server_id;",                                                         SERVER2},
     {"select @@server_id/* maxscale route to server server3 */;",                   SERVER3},
     {"select @@server_id;",                                                         SERVER2},
-    {"select @@server_id/* maxscale end */;",                                       SERVER2},
+    {"select @@server_id/* maxscale end */;",                                       NOT_MASTER},
     {NULL,                                                                          SERVER1}
 };
 
@@ -88,7 +89,12 @@ int main(int argc, char** argv)
     {
         char str[1024];
         find_field(test->maxscales->conn_rwsplit[0], queries[i].query, "@@server_id", str);
-        if (strcmp(server_id[queries[i].reply], str) != 0)
+        if (queries[i].reply == NOT_MASTER)
+        {
+            test->expect(strcmp(server_id[0], str) != 0,
+                         "%s: Query should not go to master.", queries[i].query);
+        }
+        else if (strcmp(server_id[queries[i].reply], str) != 0)
         {
             test->add_result(1,
                              "%s: Expected %s but got %s.\n",