552c6e4d5fef594f7c52207b1e348e61b7d8e9ed
Markus Mäkelä <markus.makela@mariadb.com>
2019-11-01 15:51:30 +0200

    MXS-2443: Add causal_reads_mode=global test
    The test verifies that writes are visible across connections by leveraging
    the fact that large inserts take time to replicate.

---
+++ maxscale-system-test/mxs2443_global_causal_reads.cpp
@@ -0,0 +1,55 @@
+/**
+ * MXS-1778: Use GTID from OK packets for consistent reads
+ *
+ * https://jira.mariadb.org/browse/MXS-1778
+ */
+
+#include "testconnections.h"
+
+int main(int argc, char** argv)
+{
+    Mariadb_nodes::require_gtid(true);
+    TestConnections::require_repl_version("10.3.8");
+    TestConnections test(argc, argv);
+    test.repl->execute_query_all_nodes("SET GLOBAL session_track_system_variables='last_gtid'");
+
+    auto conn = test.maxscales->rwsplit();
+    conn.connect();
+    test.expect(conn.query("CREATE OR REPLACE TABLE test.t1 (a LONGTEXT)"),
+                "Table creation should work: %s", conn.error());
+    conn.disconnect();
+
+    std::string data(1000000, 'a');
+    auto secondary = test.maxscales->rwsplit();
+    secondary.connect();
+
+    for (int i = 0; i < 50 && test.ok(); i++)
+    {
+        test.set_timeout(60);
+        conn.connect();
+        test.expect(conn.query("INSERT INTO test.t1 VALUES ('" + data + "')"),
+                    "INSERT should work: %s", conn.error());
+
+        // Existing connections should also see the inserted rows
+        auto second_count = atoi(secondary.field("SELECT COUNT(*) FROM test.t1").c_str());
+        test.expect(second_count == i + 1, "Missing `%d` rows from open connection.", (i + 1) - count);
+
+        conn.disconnect();
+
+        // New connections should see the inserted rows
+        conn.connect();
+        auto count = atoi(conn.field("SELECT COUNT(*) FROM test.t1").c_str());
+        test.expect(count == i + 1, "Missing `%d` rows.", (i + 1) - count);
+        conn.disconnect();
+
+    }
+
+    test.stop_timeout();
+
+    conn.connect();
+    test.expect(conn.query("DROP TABLE test.t1"),
+                "Table creation should work: %s", conn.error());
+    conn.disconnect();
+
+    return test.global_result;
+}
