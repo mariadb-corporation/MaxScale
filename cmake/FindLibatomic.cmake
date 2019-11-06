b793c1f12b4ff2aacbac26c23aaa800f10431efa
Markus Mäkelä <markus.makela@mariadb.com>
2019-11-06 23:52:06 +0200

    Improve libatomic searches
    A custom CMake file for finding libatomic is required as some systems,
    namely CentOS, don't have proper symlinks in the libatomic package.

---
+++ cmake/FindLibatomic.cmake
@@ -0,0 +1,18 @@
+# Find libatomic.so
+#
+# Some systems (I'm looking at you, CentOS 7) don't have the proper symlink for
+# libatomic.so and only have the libatomic.so.1 symlink. This is why we need to
+# be a bit smarter than usual when linking the atomic libraries.
+#
+# The following variables are set:
+#
+# LIBATOMIC_FOUND - Found libatomic
+# LIBATOMIC_LIBRARIES - The libuuid headers
+
+find_library(LIBATOMIC_LIBRARIES NAMES libatomic.so libatomic.so.1)
+
+if (NOT LIBATOMIC_LIBRARIES)
+  message(STATUS "Could not find libatomic")
+else()
+  message(STATUS "Found libatomic: ${LIBATOMIC_LIBRARIES}")
+endif()
