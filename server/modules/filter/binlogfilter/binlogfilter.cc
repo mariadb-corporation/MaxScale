52eafb79268c2be8432663faf0ba02a49c65731a
Markus Mäkelä <markus.makela@mariadb.com>
2019-12-03 11:06:52 +0200

    Fix binlogfilter capabilities
    The binlogfilter needs to read results one packet at a time but it needs
    resultsets to be collected into a single buffer. This behavior is
    guaranteed implicitly when the binlogrouter is used but is not present
    when it is used without it. To support the use of the binlogfilter with
    readconnroute, the filter must properly declare the capabilities.

--- server/modules/filter/binlogfilter/binlogfilter.cc
+++ server/modules/filter/binlogfilter/binlogfilter.cc
@@ -27,7 +27,7 @@ extern "C" MXS_MODULE* MXS_CREATE_MODULE()
         MXS_FILTER_VERSION,
         desc,
         "V1.0.0",
-        RCAP_TYPE_NONE,
+        RCAP_TYPE_STMT_OUTPUT,
         &BinlogFilter::s_object,
         NULL,
         NULL,
@@ -81,5 +81,5 @@ json_t* BinlogFilter::diagnostics_json() const
 // static
 uint64_t BinlogFilter::getCapabilities()
 {
-    return RCAP_TYPE_NONE;
+    return RCAP_TYPE_STMT_OUTPUT;
 }
