mariadb-corporation/MaxScale

commit 6306519e5e75575ba083ee2f0edfe7e624da5d26

MXS-2710: Move client_count handling inside Session
By incrementing the counters when the session is created, we know that the
counter will always be decremented correctly. This does cause the listener
session to be counted as an actual session but this is already present in
the statistics calculations and is something we have to live with in 2.3
This change also makes it possible to overshoot the connection count
limitation as the session creation is delayed until authentication
fails. Both of these problems are fixed in 2.4.

server/core/dcb.cc
@@ -2533,11 +2533,6 @@ DCB* dcb_accept(DCB* dcb)
         }
     }
 
-    if (client_dcb)
-    {
-        mxb::atomic::add(&client_dcb->service->client_count, 1);
-    }
-
     return client_dcb;
 }
 

server/core/session.cc
@@ -361,18 +361,7 @@ private:
 static void session_free(MXS_SESSION* session)
 {
     MXS_INFO("Stopped %s client session [%" PRIu64 "]", session->service->name, session->ses_id);
-    Service* service = static_cast<Service*>(session->service);
-
     session_final_free(session);
-    bool should_destroy = !mxb::atomic::load(&service->active);
-
-    if (mxb::atomic::add(&service->client_count, -1) == 1 && should_destroy)
-    {
-        // Destroy the service in the main routing worker thread
-        mxs::RoutingWorker* main_worker = mxs::RoutingWorker::get(mxs::RoutingWorker::MAIN);
-        main_worker->execute(std::unique_ptr<ServiceDestroyTask>(new ServiceDestroyTask(service)),
-                             Worker::EXECUTE_AUTO);
-    }
 }
 
 static void session_final_free(MXS_SESSION* ses)
@@ -1264,6 +1253,7 @@ Session::Session(SERVICE* service)
 
     mxb::atomic::add(&service->stats.n_current, 1, mxb::atomic::RELAXED);
     mxb_assert(service->stats.n_current >= 0);
+    mxb::atomic::add(&service->client_count, 1, mxb::atomic::RELAXED);
 }
 
 Session::~Session()
@@ -1281,6 +1271,17 @@ Session::~Session()
 
     mxb::atomic::add(&service->stats.n_current, -1, mxb::atomic::RELAXED);
     mxb_assert(service->stats.n_current >= 0);
+
+    bool should_destroy = !mxb::atomic::load(&service->active);
+
+    if (mxb::atomic::add(&service->client_count, -1) == 1 && should_destroy)
+    {
+        // Destroy the service in the main routing worker thread
+        mxs::RoutingWorker* main_worker = mxs::RoutingWorker::get(mxs::RoutingWorker::MAIN);
+        main_worker->execute(
+            std::unique_ptr<ServiceDestroyTask>(new ServiceDestroyTask(static_cast<Service*>(service))),
+            Worker::EXECUTE_AUTO);
+    }
 }
 
 namespace

server/modules/filter/test/mock_session.cc
@@ -44,6 +44,9 @@ Session::Session(Client* pClient)
 
     m_client_dcb.data = &m_mysql_session;
     service = &dummy_service;
+
+    // This prevents the destruction of the dummy service
+    service->active = true;
 }
 
 Session::~Session()

server/modules/protocol/MySQL/mariadbclient/mysql_client.cc
@@ -740,20 +740,12 @@ static void check_packet(DCB* dcb, GWBUF* buf, int bytes)
  */
 static int gw_read_do_authentication(DCB* dcb, GWBUF* read_buffer, int nbytes_read)
 {
-    // If this function fails and dcb is closed, the service client count must be decremented manually.
-    // After authentication, the session closing code decrements the count.
-    auto decrement_and_close = [](DCB* dcb)
-    {
-        mxb::atomic::add(&dcb->service->client_count, -1);
-        dcb_close(dcb);
-    };
-
     MXB_AT_DEBUG(check_packet(dcb, read_buffer, nbytes_read));
 
     /** Allocate the shared session structure */
     if (dcb->data == NULL && (dcb->data = mysql_session_alloc()) == NULL)
     {
-        decrement_and_close(dcb);
+        dcb_close(dcb);
         return 1;
     }
 
@@ -820,7 +812,7 @@ static int gw_read_do_authentication(DCB* dcb, GWBUF* read_buffer, int nbytes_re
             MYSQL_session* ses = (MYSQL_session*)dcb->data;
             if ((dcb->user = MXS_STRDUP(ses->user)) == NULL)
             {
-                decrement_and_close(dcb);
+                dcb_close(dcb);
                 gwbuf_free(read_buffer);
                 return 0;
             }
@@ -873,7 +865,7 @@ static int gw_read_do_authentication(DCB* dcb, GWBUF* read_buffer, int nbytes_re
         /**
          * Close DCB and which will release MYSQL_session
          */
-        decrement_and_close(dcb);
+        dcb_close(dcb);
     }
     /* One way or another, the buffer is now fully processed */
     gwbuf_free(read_buffer);

commit 1d8139587b5cf39a39edc9b5bb5b0bddcb42038c

MXS-2710: Move client_count handling into client DCB
Due to the fact that both client connections and listeners use sessions in
2.3, the client_count tracking must be done inside the client DCB. In
addition to this, the max_connections check didn't take the current
pending connection into account which caused an off-by-one error.

This commit fixes the connection_limit test failure that was introduced by
commit 6306519e5e75575ba083ee2f0edfe7e624da5d26.

server/core/dcb.cc
@@ -56,6 +56,7 @@
 
 #include "internal/modules.h"
 #include "internal/session.h"
+#include "internal/service.hh"
 
 using maxscale::RoutingWorker;
 using maxbase::Worker;
@@ -218,6 +219,11 @@ DCB* dcb_alloc(dcb_role_t role, SERV_LISTENER* listener)
         newdcb->poll.owner = RoutingWorker::get_current();
     }
 
+    if (role == DCB_ROLE_CLIENT_HANDLER)
+    {
+        mxb::atomic::add(&listener->service->client_count, 1, mxb::atomic::RELAXED);
+    }
+
     return newdcb;
 }
 
@@ -280,6 +286,21 @@ void dcb_free_all_memory(DCB* dcb)
     if (this_thread.current_dcb == dcb)
     {
         this_thread.current_dcb = NULL;
+    }
+
+    if (dcb->dcb_role == DCB_ROLE_CLIENT_HANDLER)
+    {
+        Service* service = static_cast<Service*>(dcb->service);
+        bool should_destroy = !mxb::atomic::load(&service->active);
+
+        if (mxb::atomic::add(&service->client_count, -1) == 1 && should_destroy)
+        {
+            // Destroy the service in the main routing worker thread
+            mxs::RoutingWorker* main_worker = mxs::RoutingWorker::get(mxs::RoutingWorker::MAIN);
+            main_worker->execute([service]() {
+                                     service_free(service);
+                                 }, Worker::EXECUTE_AUTO);
+        }
     }
 
     DCB_CALLBACK* cb_dcb;
@@ -2518,7 +2539,7 @@ DCB* dcb_accept(DCB* dcb)
             }
 
             if (client_dcb->service->max_connections
-                && client_dcb->service->client_count >= client_dcb->service->max_connections)
+                && client_dcb->service->client_count > client_dcb->service->max_connections)
             {
                 // TODO: If connections can be queued, this is the place to put the
                 // TODO: connection on that queue.

server/core/session.cc
@@ -336,23 +336,6 @@ void session_close(MXS_SESSION* session)
     }
 }
 
-class ServiceDestroyTask : public Worker::DisposableTask
-{
-public:
-    ServiceDestroyTask(Service* service)
-        : m_service(service)
-    {
-    }
-
-    void execute(Worker& worker) override
-    {
-        service_free(m_service);
-    }
-
-private:
-    Service* m_service;
-};
-
 /**
  * Deallocate the specified session
  *
@@ -1253,7 +1236,6 @@ Session::Session(SERVICE* service)
 
     mxb::atomic::add(&service->stats.n_current, 1, mxb::atomic::RELAXED);
     mxb_assert(service->stats.n_current >= 0);
-    mxb::atomic::add(&service->client_count, 1, mxb::atomic::RELAXED);
 }
 
 Session::~Session()
@@ -1271,17 +1253,6 @@ Session::~Session()
 
     mxb::atomic::add(&service->stats.n_current, -1, mxb::atomic::RELAXED);
     mxb_assert(service->stats.n_current >= 0);
-
-    bool should_destroy = !mxb::atomic::load(&service->active);
-
-    if (mxb::atomic::add(&service->client_count, -1) == 1 && should_destroy)
-    {
-        // Destroy the service in the main routing worker thread
-        mxs::RoutingWorker* main_worker = mxs::RoutingWorker::get(mxs::RoutingWorker::MAIN);
-        main_worker->execute(
-            std::unique_ptr<ServiceDestroyTask>(new ServiceDestroyTask(static_cast<Service*>(service))),
-            Worker::EXECUTE_AUTO);
-    }
 }
 
 namespace

server/core/test/test_poll.cc
@@ -53,6 +53,7 @@ static int test1()
 
     SERVICE service;
     service.routerModule = (char*)"required by a check in dcb.cc";
+    dummy.service = &service;
 
     /* Poll tests */
     fprintf(stderr,