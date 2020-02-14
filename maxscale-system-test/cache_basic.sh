3863ca6a59d0361302b47c6b317be3c33de12929
Johan Wikman <johan.wikman@gmail.com>
2020-02-14 16:28:25 +0200

    MXS-2841 Update cache_basic for memcached and redis

--- maxscale-system-test/cache_basic.sh
+++ maxscale-system-test/cache_basic.sh
@@ -4,16 +4,43 @@
 user=$maxscale_user
 password=$maxscale_password
 
-# See cnf/maxscale.cnf.template.cache_basic
-port=4008
 # Ensure that these are EXACTLY like the corresponding values
 # in cnf/maxscale.cnf.template.cache_basic
 soft_ttl=5
 hard_ttl=10
 
+function install_package
+{
+    local keyfile=$1
+    local user=$2
+    local host=$3
+    local package=$4
+
+    echo "Installing $package on $host."
+
+    ssh -i ${keyfile} -o StrictHostKeyChecking=no ${user}@${host} sudo yum -y install ${package}
+
+    if [ $? -ne 0 ]
+    then
+        echo "error: Could not install $package on $host."
+        exit 1
+    fi
+}
+
+function install_redis_on_node_000
+{
+    install_package ${node_000_keyfile} ${node_000_whoami} ${node_000_network} redis
+}
+
+function install_memcached_on_node_000
+{
+    install_package ${node_000_keyfile} ${node_000_whoami} ${node_000_network} memcached
+}
+
 function run_test
 {
-    local test_name=$1
+    local port=$1
+    local test_name=$2
 
     echo $test_name
     logdir=log_$test_name
@@ -63,37 +90,54 @@ ssh  -i $maxscale_000_keyfile -o StrictHostKeyChecking=no -o UserKnownHostsFile=
 # kicks in.
 let seconds=$soft_ttl+2
 
-run_test create || exit 1
-run_test insert1 || exit 1
-# We should now get result 1, as this is the first select.
-run_test select1 || exit 1
+function run_tests
+{
+    local port=$1
 
-run_test update2 || exit 1
-# We should now get result 1, as ttl has NOT passed.
-run_test select1 || exit 1
+    run_test $port create || exit 1
+    run_test $port insert1 || exit 1
+    # We should now get result 1, as this is the first select.
+    run_test $port select1 || exit 1
 
-echo "Sleeping $seconds"
-sleep $seconds
-# We should now get result 2, as soft ttl has PASSED.
-run_test select2 || exit 1
+    run_test $port update2 || exit 1
+    # We should now get result 1, as ttl has NOT passed.
+    run_test $port select1 || exit 1
 
-run_test update3 || exit 1
-# We should now get result 2, as ttl has NOT passed.
-run_test select2 || exit 1
+    echo "Sleeping $seconds"
+    sleep $seconds
+    # We should now get result 2, as soft ttl has PASSED.
+    run_test $port select2 || exit 1
 
-echo "Sleeping $seconds"
-sleep $seconds
-# We should now get result 3, as soft ttl has PASSED.
-run_test select3 || exit 1
+    run_test $port update3 || exit 1
+    # We should now get result 2, as ttl has NOT passed.
+    run_test $port select2 || exit 1
 
-run_test delete || exit 1
-# We should now get result 3, as soft ttl has NOT passed.
-run_test select3 || exit 1
+    echo "Sleeping $seconds"
+    sleep $seconds
+    # We should now get result 3, as soft ttl has PASSED.
+    run_test $port select3 || exit 1
 
-echo "Sleeping $seconds"
-sleep $seconds
-# We should now get result 0, as soft ttl has PASSED.
-run_test select0 || exit 1
+    run_test $port delete || exit 1
+    # We should now get result 3, as soft ttl has NOT passed.
+    run_test $port select3 || exit 1
 
-# Cleanup
-run_test drop || exit 1
+    echo "Sleeping $seconds"
+    sleep $seconds
+    # We should now get result 0, as soft ttl has PASSED.
+    run_test $port select0 || exit 1
+
+    # Cleanup
+    run_test $port drop || exit 1
+}
+
+# See cnf/maxscale.cnf.template.cache_basic for the port
+echo Testing with local storage
+run_tests 4008
+
+echo Testing with memcached storage
+install_memcached_on_node_000
+run_tests 4009
+
+echo Testing with redis storage
+install_redis_on_node_000
+run_tests 4010
