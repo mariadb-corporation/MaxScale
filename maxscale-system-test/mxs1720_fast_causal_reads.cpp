942f2bfb3f3d8252e2c637dd7272f1f05eee0660
Markus Mäkelä <markus.makela@mariadb.com>
2020-02-26 08:18:41 +0200

    MXS-1720: Add test case
    Test that the queries always return the latest data with
    causal_reads_mode=fast.

---
+++ maxscale-system-test/mxs1720_fast_causal_reads.cpp
@@ -0,0 +1,41 @@
+/**
+ * MXS-1720: Test for causal_reads=fast
+ *
+ * https://jira.mariadb.org/browse/MXS-1720
+ */
+
+#include "testconnections.h"
+
+void basic_test(TestConnections& test)
+{
+    test.maxscales->connect();
+    test.try_query(test.maxscales->conn_rwsplit[0], "CREATE OR REPLACE TABLE test.t1(id INT)");
+
+    for (int i = 0; i < 100; i++)
+    {
+        std::string value = std::to_string(i);
+        std::string insert = "INSERT INTO test.t1 VALUES (" + value + ")";
+        std::string select = "SELECT @@server_id, COUNT(*) FROM test.t1 WHERE id = " + value;
+
+        test.try_query(test.maxscales->conn_rwsplit[0], "%s", insert.c_str());
+        Row row = get_row(test.maxscales->conn_rwsplit[0], select);
+        test.expect(!row.empty() && row[1] == "1", "At %d: Row is %s", i,
+                    row.empty() ? "empty" : (row[0] + " " + row[1]).c_str());
+    }
+
+    test.try_query(test.maxscales->conn_rwsplit[0], "DROP TABLE test.t1");
+    test.maxscales->disconnect();
+}
+
+int main(int argc, char** argv)
+{
+    Mariadb_nodes::require_gtid(true);
+    TestConnections::require_repl_version("10.3.8");
+    TestConnections test(argc, argv);
+
+    test.repl->execute_query_all_nodes("SET GLOBAL session_track_system_variables='last_gtid'");
+
+    basic_test(test);
+
+    return test.global_result;
+}
