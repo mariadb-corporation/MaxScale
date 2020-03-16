5121aa296e1ed6b02de89ec2c0a3a4937797b80e
Markus Mäkelä <markus.makela@mariadb.com>
2020-03-16 15:09:41 +0200

    Add timeout to rwsplit_readonly_stress
    This prevents the test from hanging.

--- maxscale-system-test/rwsplit_readonly_stress.cpp
+++ maxscale-system-test/rwsplit_readonly_stress.cpp
@@ -102,6 +102,7 @@ int main(int argc, char* argv[])
     }
 
     Test->tprintf("Waiting for all threads to finish\n");
+    Test->set_timeout(60);
     running = 0;
 
     for (int i = 0; i < THREADS; i++)
