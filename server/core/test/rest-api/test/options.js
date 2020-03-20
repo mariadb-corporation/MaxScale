697a268a3e4c00a7b4bd9b7551740554d75bbca3
Markus Mäkelä <markus.makela@mariadb.com>
2020-03-20 14:01:15 +0200

    MXS-2923: Add test cases
    Added test cases that make sure only the expected fields are returned.

--- server/core/test/rest-api/test/options.js
+++ server/core/test/rest-api/test/options.js
@@ -8,5 +8,34 @@ describe("Request Options", function() {
             .should.eventually.satisfy(validate)
     })
 
+    it("Sparse fieldset with one field", function() {
+        return request.get(base_url + "/services/RW-Split-Router?fields[services]=state", {json: true})
+            .then((res) => {
+                validate_func(res).should.be.true
+                res.data.should.have.keys("attributes", "id", "type", "links")
+                res.data.attributes.should.have.keys("state")
+                res.data.attributes.should.not.have.keys("parameters")
+            })
+    })
+
+    it("Sparse fieldset with two fields", function() {
+        return request.get(base_url + "/services/RW-Split-Router?fields[services]=state,router", {json: true})
+            .then((res) => {
+                validate_func(res).should.be.true
+                res.data.should.have.keys("attributes", "id", "type", "links")
+                res.data.attributes.should.have.keys("state", "router")
+                res.data.attributes.should.not.have.keys("parameters")
+            })
+    })
+
+    it("Sparse fieldset with relationships", function() {
+        return request.get(base_url + "/services/RW-Split-Router?fields[services]=servers", {json: true})
+            .then((res) => {
+                validate_func(res).should.be.true
+                res.data.should.have.keys("relationships", "id", "type", "links")
+                res.data.relationships.should.have.keys("servers")
+            })
+    })
+
     after(stopMaxScale)
 });
