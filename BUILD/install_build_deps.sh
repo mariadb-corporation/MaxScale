bfaeeb63f027651e0b772fc76c288f9c8348681b
Markus Mäkelä <markus.makela@mariadb.com>
2020-04-01 10:30:54 +0300

    Install cyrus-sasl-devel on SLES
    The package was missing in the zypper installation list.

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -162,7 +162,8 @@ then
          make libtool libopenssl-devel libaio libaio-devel flex \
          pcre-devel git wget tcl tcl-devel libuuid-devel \
          xz-devel sqlite3 sqlite3-devel pkg-config lua lua-devel \
-         gnutls-devel libgcrypt-devel pam-devel systemd-devel libcurl-devel libatomic1
+         gnutls-devel libgcrypt-devel pam-devel systemd-devel libcurl-devel libatomic1 \
+         cyrus-sasl-devel
     sudo zypper -n install rpm-build
     cat /etc/*-release | grep "SUSE Linux Enterprise Server 11"
 
