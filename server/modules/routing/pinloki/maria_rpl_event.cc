e7fa169bcbb624d4c0cb2cce967dc8cbff00567d
Markus Mäkelä <markus.makela@mariadb.com>
2020-04-16 23:01:55 +0300

    Add license blurb

--- server/modules/routing/pinloki/maria_rpl_event.cc
+++ server/modules/routing/pinloki/maria_rpl_event.cc
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2024-03-10
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
 #include "maria_rpl_event.hh"
 #include "dbconnection.hh"
 
