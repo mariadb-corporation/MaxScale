56988cb5c76327e7bc395911fbd092956fa8173f
Markus Mäkelä <markus.makela@mariadb.com>
2020-04-18 09:20:12 +0300

    Add rudimentary SQL processing
    The hard-coded queries that MariaDB slaves execute are now handled in a
    limited manner. This allows the replication to proceed to the
    COM_BINLOG_DUMP phase with certain limitations:
    
    - Only GTID domain 0 is supported
    - The server_id is always reported as 1
    - @@binlog_checksum is always CRC32

--- server/modules/routing/pinloki/pinlokisession.cc
+++ server/modules/routing/pinloki/pinlokisession.cc
@@ -14,6 +14,19 @@
 #include "pinlokisession.hh"
 
 #include <maxscale/modutil.hh>
+#include <maxscale//protocol/mariadb/resultset.hh>
+#include <maxscale/protocol/mariadb/mysql.hh>
+
+namespace
+{
+GWBUF* create_resultset(const std::initializer_list<std::string>& columns,
+                        const std::initializer_list<std::string>& row)
+{
+    auto rset = ResultSet::create(columns);
+    rset->add_row(row);
+    return rset->as_buffer().release();
+}
+}
 
 namespace pinloki
 {
@@ -28,20 +41,88 @@ void PinlokiSession::close()
 
 int32_t PinlokiSession::routeQuery(GWBUF* pPacket)
 {
-    const mxs::ReplyRoute down;
-    const mxs::Reply reply;
-    mxs::RouterSession::clientReply(modutil_create_ok(), down, reply);
-    return 0;
+    int rval = 0;
+    GWBUF* response = nullptr;
+    mxs::Buffer buf(pPacket);
+    auto cmd = mxs_mysql_get_command(buf.get());
+
+    switch (cmd)
+    {
+    case MXS_COM_REGISTER_SLAVE:
+        // Register slave (maybe grab the slave's server_id if we need it)
+        MXS_INFO("COM_REGISTER_SLAVE");
+        response = modutil_create_ok();
+        break;
+
+    case MXS_COM_BINLOG_DUMP:
+        // Start dumping binlogs (not yet implemented)
+        MXS_INFO("COM_BINLOG_DUMP");
+        rval = 1;
+        break;
+
+    case MXS_COM_QUERY:
+        {
+            auto sql = mxs::extract_sql(buf.get());
+            mxb::lower_case(sql);
+            MXS_INFO("COM_QUERY: %s", sql.c_str());
+
+            // TODO: Implement a proper SQL parser instead of comparing to hard-coded queries
+            if (sql == "select unix_timestamp()")
+            {
+                response = create_resultset({"UNIX_TIMESTAMP()"}, {std::to_string(time(nullptr))});
+            }
+            else if (sql == "show variables like 'server_id'")
+            {
+                response = create_resultset({"Variable_name", "Value"}, {"server_id", "1"});
+            }
+            else if (sql == "select @master_binlog_checksum")
+            {
+                response = create_resultset({"@master_binlog_checksum"}, {"CRC32"});
+            }
+            else if (sql == "select 1")
+            {
+                response = create_resultset({"1"}, {"1"});
+            }
+            else if (sql == "select @@global.gtid_domain_id")
+            {
+                response = create_resultset({"@@GLOBAL.gtid_domain_id"}, {"0"});
+            }
+            else if (sql.substr(0, 4) == "set ")
+            {
+                response = modutil_create_ok();
+            }
+            else
+            {
+                mxb_assert(!true);
+            }
+        }
+        break;
+    }
+
+    if (response)
+    {
+        const mxs::ReplyRoute down;
+        const mxs::Reply reply;
+        mxs::RouterSession::clientReply(response, down, reply);
+        rval = 1;
+    }
+    else
+    {
+        mxb_assert(rval == 1);
+    }
+
+    return rval;
 }
 
 void PinlokiSession::clientReply(GWBUF* pPacket, const mxs::ReplyRoute& down, const mxs::Reply& reply)
 {
-    mxs::RouterSession::clientReply(pPacket, down, reply);
+    mxb_assert_message(!true, "This should not happen");
 }
 
 bool PinlokiSession::handleError(mxs::ErrorType type, GWBUF* pMessage,
                                  mxs::Endpoint* pProblem, const mxs::Reply& pReply)
 {
+    mxb_assert_message(!true, "This should not happen");
     return false;
 }
 }
