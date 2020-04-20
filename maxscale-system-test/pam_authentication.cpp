8f9bba4d463f03078e7fb8c186c63ea4fe8831bf
Esa Korhonen <esa.korhonen@mariadb.com>
2020-04-20 15:51:44 +0300

    MXS-2958 Test various authenticator options
    Adds test cases for skip_authentication=true, match_host=false,
    lower_case_table_names=1&2 and log_password_mismatch=true.

--- maxscale-system-test/pam_authentication.cpp
+++ maxscale-system-test/pam_authentication.cpp
@@ -19,6 +19,11 @@
 using std::string;
 using std::cout;
 
+bool test_pam_login(TestConnections& test, int port, const string& user, const string& pass,
+                    const string& database);
+bool test_normal_login(TestConnections& test, int port, const string& user, const string& pass,
+                       const string& db = "");
+
 int main(int argc, char** argv)
 {
     TestConnections test(argc, argv);
@@ -124,49 +129,10 @@ int main(int argc, char** argv)
 
     // Helper function for checking PAM-login. If db is empty, log to null database.
     auto try_log_in = [&test](const string& user, const string& pass, const string& database) {
-        const char* host = test.maxscales->IP[0];
         int port = test.maxscales->rwsplit_port[0];
-        const char* db = nullptr;
-        if (!database.empty())
+        if (!test_pam_login(test, port, user, pass, database))
         {
-            db = database.c_str();
-        }
-
-        if (db)
-        {
-            printf("Trying to log in to [%s]:%i as %s with database %s.\n", host, port, user.c_str(), db);
-        }
-        else
-        {
-            printf("Trying to log in to [%s]:%i as %s.\n", host, port, user.c_str());
-        }
-
-        MYSQL* maxconn = mysql_init(NULL);
-        test.expect(maxconn, "mysql_init failed");
-        if (maxconn)
-        {
-            // Need to set plugin directory so that dialog.so is found.
-            const char plugin_path[] = "../connector-c/install/lib/mariadb/plugin";
-            mysql_optionsv(maxconn, MYSQL_PLUGIN_DIR, plugin_path);
-            mysql_real_connect(maxconn, host, user.c_str(), pass.c_str(), db, port, NULL, 0);
-            auto err = mysql_error(maxconn);
-            if (*err)
-            {
-                test.expect(false, "Could not log in: '%s'", err);
-            }
-            else
-            {
-                test.try_query(maxconn, "SELECT rand();");
-                if (test.ok())
-                {
-                    cout << "Logged in and queried successfully.\n";
-                }
-                else
-                {
-                    cout << "Query rejected: '" << mysql_error(maxconn) << "'\n";
-                }
-            }
-            mysql_close(maxconn);
+            test.expect(false, "PAM login failed.");
         }
     };
 
@@ -285,7 +251,7 @@ int main(int argc, char** argv)
 
     if (test.ok())
     {
-        // Finally, test that normal authentication on the same port works. This tests MXS-2497.
+        // Test that normal authentication on the same port works. This tests MXS-2497.
         auto maxconn = test.maxscales->open_rwsplit_connection();
         int port = test.maxscales->rwsplit_port[0];
         test.try_query(maxconn, "SELECT rand();");
@@ -293,7 +259,154 @@ int main(int argc, char** argv)
         mysql_close(maxconn);
     }
 
-    // Cleanup: remove the linux users on the backends and MaxScale node, unload pam plugin.
+    // Cleanup: remove the linux users on the MaxScale node.
+    test.maxscales->ssh_node_f(0, true, "%s", remove_user_cmd.c_str());
+    test.maxscales->ssh_node_f(0, true, "%s", read_shadow_off.c_str());
+    test.maxscales->ssh_node_f(0, true, "%s", delete_pam_conf_cmd.c_str());
+    test.maxscales->ssh_node_f(0, true, "%s", delete_pam_message_cmd.c_str());
+
+    int normal_port = test.maxscales->rwsplit_port[0];
+    int skip_auth_port = 4007;
+    int nomatch_port = 4008;
+    int caseless_port = 4009;
+
+    const char login_failed_msg[] = "Login to port %i failed.";
+    if (test.ok())
+    {
+        // Recreate the pam user.
+        MYSQL* conn = test.repl->nodes[0];
+        test.try_query(conn, create_pam_user_fmt, pam_user, pam_config_name);
+        // Normal listener should not work anymore, but the one with skip_authentication should work
+        // even with the Linux user removed.
+
+        bool login_success = test_pam_login(test, normal_port, pam_user, pam_pw, "");
+        test.expect(!login_success, "Normal login succeeded when it should not have.");
+        login_success = test_pam_login(test, skip_auth_port, pam_user, pam_pw, "");
+        cout << "Testing listener with skip_authentication.\n";
+        test.expect(login_success, login_failed_msg, skip_auth_port);
+        if (test.ok())
+        {
+            cout << "skip_authentication works.\n";
+        }
+        test.try_query(conn, "DROP USER '%s'@'%%';", pam_user);
+    }
+
+    const char create_fmt[] = "CREATE OR REPLACE USER '%s'@'%s' IDENTIFIED BY '%s';";
+    if (test.ok())
+    {
+        // Create a user which can only connect from MaxScale IP. This should work with the listener with
+        // authenticator_options=match_host=false.
+        string user = "maxhost_user";
+        auto userz = user.c_str();
+        string host = test.maxscales->IP[0];
+        auto hostz = host.c_str();
+        string pass = "maxhost_pass";
+        MYSQL* conn = test.repl->nodes[0];
+        test.try_query(conn, create_fmt, userz, hostz, pass.c_str());
+
+        if (test.ok())
+        {
+            const char unexpected[] = "Login to port %i succeeded when it should have failed.";
+            bool login_success = test_normal_login(test, normal_port, user, pass);
+            test.expect(!login_success, unexpected, normal_port);
+            login_success = test_normal_login(test, skip_auth_port, user, pass);
+            test.expect(!login_success, unexpected, normal_port);
+            cout << "Testing listener with match_host=false.\n";
+            login_success = test_normal_login(test, nomatch_port, user, pass);
+            test.expect(login_success, login_failed_msg, normal_port);
+            if (test.ok())
+            {
+                cout << "match_host=false works.\n";
+            }
+        }
+        test.try_query(conn, "DROP USER '%s'@'%s';", userz, hostz);
+    }
+
+    if (test.ok())
+    {
+        // Test lower_case_table_names. Only test the MaxScale-side of authentication, as testing
+        // the server is not really the purpose here.
+        MYSQL* conn = test.repl->nodes[0];
+        string user = "low_case_user";
+        string pass = "low_case_pass";
+        auto userz = user.c_str();
+        const char host[] = "%";
+        test.try_query(conn, create_fmt, userz, host, pass.c_str());
+
+        const char create_db_fmt[] = "CREATE OR REPLACE DATABASE %s;";
+        const char grant_sel_fmt[] = "GRANT select on %s.* TO '%s'@'%s';";
+
+        const char test_db1[] = "test_db1";
+        test.try_query(conn, create_db_fmt, test_db1);
+        test.try_query(conn, grant_sel_fmt, test_db1, userz, host);
+
+        const char test_db2[] = "tEsT_db2";
+        test.try_query(conn, create_db_fmt, test_db2);
+        test.try_query(conn, grant_sel_fmt, test_db2, userz, host);
+
+        auto test_normal_login_short = [&test, &user, pass](int port, const string& db)
+        {
+            auto host = test.maxscales->IP[0];
+            MYSQL* maxconn = nullptr;
+            maxconn = open_conn_db(port, host, db, user, pass);
+            auto err = mysql_error(maxconn);
+            bool rval = (*err == '\0');
+            if (*err)
+            {
+                test.tprintf("Could not log in: '%s'", err);
+            }
+            return rval;
+        };
+
+        const string login_db1 = "TeSt_dB1";
+        const string login_db2 = "tESt_Db2";
+        const char unexpected_login[] = "Login to db %s worked when it should not have.";
+        const char failed_login[] = "Login to db %s failed.";
+
+        if (test.ok())
+        {
+            // Should not work, as requested db is not equal to real db.
+            bool login_success = test_normal_login_short(normal_port, login_db1);
+            test.expect(!login_success, unexpected_login, login_db1.c_str());
+
+            cout << "Testing listener with lower_case_table_names=1\n";
+            // Should work, as the login db is converted to lower case.
+            login_success = test_normal_login_short(nomatch_port, login_db1);
+            test.expect(login_success, failed_login, login_db1.c_str());
+            if (test.ok())
+            {
+                cout << "lower_case_table_names=1 works.\n";
+            }
+
+            // Should not work, as target db is not lower case.
+            login_success = test_normal_login_short(nomatch_port, login_db2);
+            test.expect(!login_success, unexpected_login, login_db2.c_str());
+
+            cout << "Testing listener with lower_case_table_names=2\n";
+            // Should work, as listener compares db names case-insensitive.
+            login_success = test_normal_login_short(caseless_port, login_db2);
+            test.expect(login_success, failed_login, login_db1.c_str());
+            if (test.ok())
+            {
+                cout << "lower_case_table_names=2 works.\n";
+            }
+
+            // Check that log_password_mismatch works.
+            login_success = test_normal_login(test, caseless_port, user, "wrong_pw");
+            test.expect(!login_success, "Login using wrong password worked when it should not have.");
+            test.log_includes(0, "Client gave wrong password. Got hash");
+            if (test.ok())
+            {
+                cout << "log_password_mismatch works.\n";
+            }
+        }
+
+        test.try_query(conn, "DROP USER '%s'@'%s';", user.c_str(), host);
+        test.try_query(conn, "DROP DATABASE %s;", test_db1);
+        test.try_query(conn, "DROP DATABASE %s;", test_db2);
+    }
+
+    // Cleanup: remove the linux users on the backends, unload pam plugin.
     for (int i = 0; i < test.repl->N; i++)
     {
         MYSQL* conn = test.repl->nodes[i];
@@ -303,11 +416,90 @@ int main(int argc, char** argv)
         test.repl->ssh_node_f(i, true, "%s", delete_pam_conf_cmd.c_str());
         test.repl->ssh_node_f(i, true, "%s", delete_pam_message_cmd.c_str());
     }
-    test.maxscales->ssh_node_f(0, true, "%s", remove_user_cmd.c_str());
-    test.maxscales->ssh_node_f(0, true, "%s", read_shadow_off.c_str());
-    test.maxscales->ssh_node_f(0, true, "%s", delete_pam_conf_cmd.c_str());
-    test.maxscales->ssh_node_f(0, true, "%s", delete_pam_message_cmd.c_str());
 
     test.repl->disconnect();
     return test.global_result;
+}
+
+
+// Helper function for checking PAM-login. If db is empty, log to null database.
+bool test_pam_login(TestConnections& test, int port, const string& user, const string& pass,
+                    const string& database) {
+    const char* host = test.maxscales->IP[0];
+    const char* db = nullptr;
+    if (!database.empty())
+    {
+        db = database.c_str();
+    }
+
+    if (db)
+    {
+        printf("Trying to log in to [%s]:%i as %s with database %s.\n", host, port, user.c_str(), db);
+    }
+    else
+    {
+        printf("Trying to log in to [%s]:%i as %s.\n", host, port, user.c_str());
+    }
+
+    bool rval = false;
+    MYSQL* maxconn = mysql_init(NULL);
+    // Need to set plugin directory so that dialog.so is found.
+    const char plugin_path[] = "../connector-c/install/lib/mariadb/plugin";
+    mysql_optionsv(maxconn, MYSQL_PLUGIN_DIR, plugin_path);
+    mysql_real_connect(maxconn, host, user.c_str(), pass.c_str(), db, port, NULL, 0);
+    auto err = mysql_error(maxconn);
+    if (*err)
+    {
+        test.tprintf("Could not log in: '%s'", err);
+    }
+    else
+    {
+        if (execute_query_silent(maxconn, "SELECT rand();") == 0)
+        {
+            rval = true;
+            cout << "Logged in and queried successfully.\n";
+        }
+        else
+        {
+            cout << "Query rejected: '" << mysql_error(maxconn) << "'\n";
+        }
+    }
+    mysql_close(maxconn);
+    return rval;
+}
+
+bool test_normal_login(TestConnections& test, int port, const string& user, const string& pass,
+                       const string& db)
+{
+    bool rval = false;
+    auto host = test.maxscales->IP[0];
+    MYSQL* maxconn = nullptr;
+    if (db.empty())
+    {
+        maxconn = open_conn_no_db(port, host, user, pass);
+    }
+    else
+    {
+        maxconn = open_conn_db(port, host, db, user, pass);
+    }
+
+    auto err = mysql_error(maxconn);
+    if (*err)
+    {
+        test.tprintf("Could not log in: '%s'", err);
+    }
+    else
+    {
+        if (execute_query_silent(maxconn, "SELECT rand();") == 0)
+        {
+            rval = true;
+            cout << "Logged in and queried successfully.\n";
+        }
+        else
+        {
+            cout << "Query rejected: '" << mysql_error(maxconn) << "'\n";
+        }
+    }
+    mysql_close(maxconn);
+    return rval;
 }
