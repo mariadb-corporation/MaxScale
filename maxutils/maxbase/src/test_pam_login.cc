fbba1a24e7fcd4e961d8eefcf94abadd8e463bc2
Esa Korhonen <esa.korhonen@mariadb.com>
2020-04-29 22:28:39 +0300

    MXS-2958 Add PAM authentication tester program
    Adds a simple executable which asks for username, password and PAM service
    name, then tries to login. Allows one to easily test if the local PAM
    setup is working as intended.

---
+++ maxutils/maxbase/src/test_pam_login.cc
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2025-03-10
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <iostream>
+#include <termios.h>
+#include <maxbase/log.hh>
+#include <maxbase/pam_utils.hh>
+
+using std::string;
+using std::cin;
+using std::cout;
+using PamResult = mxb::PamResult::Result;
+
+int main()
+{
+    mxb::Log log;
+    string username, password, service;
+
+    cout << "Username:\n";
+    cin >> username;
+
+    // Disable echo when reading password.
+    termios orig_flags {0};
+    const auto fd = fileno(stdin);
+    tcgetattr(fd, &orig_flags);
+    auto new_flags = orig_flags;
+    new_flags.c_lflag &= ~ECHO;
+    new_flags.c_lflag |= ECHONL;
+
+    bool tc_error = (tcsetattr(fd, TCSANOW, &new_flags) != 0);
+    if (!tc_error)
+    {
+        cout << "Password (no echo):\n";
+        cin >> password;
+        // Re-enable echo.
+        tc_error = (tcsetattr(fd, TCSANOW, &orig_flags) != 0);
+    }
+
+    if (tc_error)
+    {
+        perror("tcsetattr");
+        return EXIT_FAILURE;
+    }
+
+    cout << "PAM service:\n";
+    cin >> service;
+
+    int rval = EXIT_FAILURE;
+    auto res = mxb::pam_authenticate(username, password, service);
+    if (res.type == PamResult::SUCCESS)
+    {
+        cout << "Authentication successful.\n";
+        rval = EXIT_SUCCESS;
+    }
+    else if (res.error.empty())
+    {
+        string failtype = (res.type == PamResult::WRONG_USER_PW) ? "wrong username/password" :
+            (res.type == PamResult::ACCOUNT_INVALID) ? "account error" : "unknown error";
+        cout << "Authentication failed: " << failtype << ".\n";
+    }
+    else
+    {
+        cout << res.error << "\n";
+    }
+    return rval;
+}
