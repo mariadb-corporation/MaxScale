ba2f1e68a40edd1ddfb198f2287f2e2043fe4339
Markus Mäkelä <markus.makela@mariadb.com>
2020-05-21 10:31:23 +0300

    Add replication restart test
    The test stops and starts the replication while inserting new rows.

---
+++ maxscale-system-test/pinloki/pinloki_start_stop.cc
@@ -0,0 +1,48 @@
+#include <testconnections.h>
+#include "test_base.hh"
+
+class StartStopTest : public TestCase
+{
+public:
+    using TestCase::TestCase;
+
+    void pre() override
+    {
+        test.expect(master.query("CREATE TABLE test.t1(id INT)"), "CREATE failed: %s", master.error());
+        sync(master, maxscale);
+        sync(maxscale, slave);
+    }
+
+    void run() override
+    {
+        for (int i = 0; i < 100 && test.ok(); i++)
+        {
+            test.set_timeout(30);
+            test.expect(master.query("INSERT INTO test.t1 VALUES (" + std::to_string(i) + ")"),
+                        "INSERT failed: %s", master.error());
+            test.expect(maxscale.query("STOP SLAVE"), "STOP SLAVE failed: %s", maxscale.error());
+            test.expect(maxscale.query("START SLAVE"), "START SLAVE failed: %s", maxscale.error());
+        }
+
+        test.stop_timeout();
+        sync(master, maxscale);
+        sync(maxscale, slave);
+
+        // All servers should be at the same GTID
+        check_gtid();
+    }
+
+    void post() override
+    {
+        test.expect(master.query("DROP TABLE test.t1"), "DROP failed: %s", master.error());
+    }
+
+private:
+};
+
+int main(int argc, char** argv)
+{
+    Mariadb_nodes::require_gtid(true);
+    TestConnections test(argc, argv);
+    return StartStopTest(test).result();
+}
