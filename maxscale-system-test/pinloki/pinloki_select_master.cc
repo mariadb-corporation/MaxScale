096fe2b3427b99218b1f45cbfb445522f1e4e8ae
Markus Mäkelä <markus.makela@mariadb.com>
2020-05-22 16:57:34 +0300

    Add automatic master selection test
    Test verifies that pinloki is capable of selecting the master it
    replicates from.

---
+++ maxscale-system-test/pinloki/pinloki_select_master.cc
@@ -0,0 +1,48 @@
+#include <testconnections.h>
+#include "test_base.hh"
+
+class MasterSelectTest : public TestCase
+{
+public:
+    using TestCase::TestCase;
+
+    void setup() override
+    {
+        test.expect(maxscale.connect(), "Pinloki connection should work: %s", maxscale.error());
+        test.expect(master.connect(), "Master connection should work: %s", master.error());
+        test.expect(slave.connect(), "Slave connection should work: %s", slave.error());
+
+        sync(master, maxscale);
+
+        slave.query("STOP SLAVE; RESET SLAVE ALL;");
+        slave.query(change_master_sql(test.maxscales->ip(0), test.maxscales->rwsplit_port[0]));
+        slave.query("START SLAVE");
+        sync(maxscale, slave);
+    }
+
+    void run() override
+    {
+        test.expect(!maxscale.query(change_master_sql(test.repl->ip(0), test.repl->port[0])),
+                    "CHANGE MASTER should fail");
+        test.expect(maxscale.query("STOP SLAVE"), "STOP SLAVE should work: %s", maxscale.error());
+        test.expect(maxscale.query("START SLAVE"), "START SLAVE should work: %s", maxscale.error());
+
+        check_gtid();
+
+        test.expect(master.query("CREATE TABLE test.t1(id INT)"), "CREATE failed: %s", master.error());
+        test.expect(master.query("INSERT INTO test.t1 VALUES (1)"), "INSERT failed: %s", master.error());
+        test.expect(master.query("DROP TABLE test.t1"), "DROP failed: %s", master.error());
+
+        sync(master, maxscale);
+        sync(maxscale, slave);
+
+        check_gtid();
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Mariadb_nodes::require_gtid(true);
+    TestConnections test(argc, argv);
+    return MasterSelectTest(test).result();
+}
