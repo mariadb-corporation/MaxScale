6f77ec384374991c006df34d97de2933c62b0ae6
Markus Mäkelä <markus.makela@mariadb.com>
2020-05-26 14:17:06 +0300

    Improve maxpasswd test
    The test verifies that the password decrypts into the same value that was
    encrypted. This is better as the ciphertext can change depending on the
    encryption algorithm.

--- server/core/test/test_maxpasswd.sh
+++ server/core/test/test_maxpasswd.sh
@@ -5,31 +5,21 @@ SECRETS_DIR=`mktemp -d`
 
 # Generate a .secrets file.
 ../maxkeys -u $(whoami) ${SECRETS_DIR} || exit 1
+rv=0
 
-# Generate a key.
-KEY1=$(../maxpasswd ${SECRETS_DIR} dummy)
-KEY2=$(../maxpasswd ${SECRETS_DIR} dummy)
+for ((i=0;i<10;i++))
+do
+    RES=$(../maxpasswd ${SECRETS_DIR} -d $(../maxpasswd ${SECRETS_DIR} dummy))
+
+    if [ "$RES" != "dummy" ]
+    then
+        echo "Encrypting 'dummy' decrypted into '$RES'"
+        rv=1
+        break
+    fi
+done
 
 # Remove the temporary directory.
 rm -rf ${SECRETS_DIR}
 
-# Get the length of the generated KEY.
-LENGTH=${#KEY1}
-
-if [ ${LENGTH} -eq 32 ]
-then
-    # Exactly 32 characters => Ok!
-
-    if [ "$KEY1" == "$KEY2" ]
-    then
-        # Password encryption returns the same results when invoked multiple times
-        exit 0
-    else
-        echo "First generated key ($KEY1) and second generated key ($KEY2) do not match."
-        exit 1
-    fi
-else
-    # Something else; chances are maxpasswd outputs garbage.
-    echo "Expected 32 characters, got $LENGTH"
-    exit 1
-fi
+exit $rv
