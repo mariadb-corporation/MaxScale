5df44646fe890e54ddec46e1db90523554a9fa31
Johan Wikman <johan.wikman@gmail.com>
2020-05-26 11:59:53 +0300

    MXS-2922 Add tests for the adding/removing of dbroots

--- server/modules/monitor/csmon/test/test_csxml.cc
+++ server/modules/monitor/csmon/test/test_csxml.cc
@@ -17,6 +17,7 @@
 #include <maxbase/log.hh>
 
 using namespace std;
+namespace xml = cs::xml;
 
 namespace
 {
@@ -28,6 +29,7 @@ bool equal(xmlDoc& lhs, xmlDoc& rhs);
 bool equal(const unique_ptr<xmlDoc>& sLhs, const unique_ptr<xmlDoc>& sRhs);
 
 }
+
 
 namespace
 {
@@ -81,7 +83,7 @@ int test_convert_to_first_multi_node()
     const char IP[] = "123.45.67.89";
     const char MANAGER[] = "MaxScale";
 
-    cs::xml::convert_to_first_multi_node(*sDoc.get(), MANAGER, IP);
+    xml::convert_to_first_multi_node(*sDoc.get(), MANAGER, IP);
 
     unique_ptr<xmlDoc> sExpected = compile_xml(ZFIRST_MULTI_NODE);
 
@@ -89,7 +91,7 @@ int test_convert_to_first_multi_node()
     {
         cout << "Single -> Multi Conversion ok" << endl;
 
-        cs::xml::convert_to_single_node(*sDoc.get());
+        xml::convert_to_single_node(*sDoc.get());
 
         sExpected = compile_xml(ZSINGLE_NODE);
 
@@ -114,12 +116,231 @@ int test_convert_to_first_multi_node()
 
 }
 
+namespace
+{
+
+const char ZSCAN_1_2[] = R"(
+<Columnstore Version="V1.0.0">
+  <SystemConfig>
+    <DBRootCount>2</DBRootCount>
+    <DBRoot1>/var/lib/columnstore/data1</DBRoot1>
+    <DBRoot2>/var/lib/columnstore/data2</DBRoot2>
+  </SystemConfig>
+  <SystemModuleConfig>
+    <ModuleIPAddr1-1-3>192.168.0.1</ModuleIPAddr1-1-3>
+    <ModuleDBRootCount1-3>1</ModuleDBRootCount1-3>
+    <ModuleDBRootID1-1-3>1</ModuleDBRootID1-1-3>
+    <ModuleIPAddr2-1-3>192.168.0.2</ModuleIPAddr2-1-3>
+    <ModuleDBRootCount2-3>1</ModuleDBRootCount2-3>
+    <ModuleDBRootID2-1-3>2</ModuleDBRootID2-1-3>
+  </SystemModuleConfig>
+</Columnstore>
+)";
+
+const char ZSCAN_1_2_3[] = R"(
+<Columnstore Version="V1.0.0">
+  <SystemConfig>
+    <DBRootCount>3</DBRootCount>
+    <DBRoot1>/var/lib/columnstore/data1</DBRoot1>
+    <DBRoot2>/var/lib/columnstore/data2</DBRoot2>
+    <DBRoot3>/var/lib/columnstore/data3</DBRoot3>
+  </SystemConfig>
+  <SystemModuleConfig>
+    <ModuleIPAddr1-1-3>192.168.0.1</ModuleIPAddr1-1-3>
+    <ModuleDBRootCount1-3>2</ModuleDBRootCount1-3>
+    <ModuleDBRootID1-1-3>1</ModuleDBRootID1-1-3>
+    <ModuleDBRootID1-2-3>3</ModuleDBRootID1-2-3>
+    <ModuleIPAddr2-1-3>192.168.0.2</ModuleIPAddr2-1-3>
+    <ModuleDBRootCount2-3>1</ModuleDBRootCount2-3>
+    <ModuleDBRootID2-1-3>2</ModuleDBRootID2-1-3>
+  </SystemModuleConfig>
+</Columnstore>
+)";
+
+const char ZSCAN_1_2_3_4[] = R"(
+<Columnstore Version="V1.0.0">
+  <SystemConfig>
+    <DBRootCount>4</DBRootCount>
+    <DBRoot1>/var/lib/columnstore/data1</DBRoot1>
+    <DBRoot2>/var/lib/columnstore/data2</DBRoot2>
+    <DBRoot3>/var/lib/columnstore/data3</DBRoot3>
+    <DBRoot4>/var/lib/columnstore/data4</DBRoot4>
+  </SystemConfig>
+  <SystemModuleConfig>
+    <ModuleIPAddr1-1-3>192.168.0.1</ModuleIPAddr1-1-3>
+    <ModuleDBRootCount1-3>2</ModuleDBRootCount1-3>
+    <ModuleDBRootID1-1-3>1</ModuleDBRootID1-1-3>
+    <ModuleDBRootID1-2-3>3</ModuleDBRootID1-2-3>
+    <ModuleIPAddr2-1-3>192.168.0.2</ModuleIPAddr2-1-3>
+    <ModuleDBRootCount2-3>2</ModuleDBRootCount2-3>
+    <ModuleDBRootID2-1-3>2</ModuleDBRootID2-1-3>
+    <ModuleDBRootID2-2-3>4</ModuleDBRootID2-2-3>
+  </SystemModuleConfig>
+</Columnstore>
+)";
+
+const char ZSCAN_1_2_4[] = R"(
+<Columnstore Version="V1.0.0">
+  <SystemConfig>
+    <DBRootCount>3</DBRootCount>
+    <DBRoot1>/var/lib/columnstore/data1</DBRoot1>
+    <DBRoot2>/var/lib/columnstore/data2</DBRoot2>
+    <DBRoot4>/var/lib/columnstore/data4</DBRoot4>
+  </SystemConfig>
+  <SystemModuleConfig>
+    <ModuleIPAddr1-1-3>192.168.0.1</ModuleIPAddr1-1-3>
+    <ModuleDBRootCount1-3>1</ModuleDBRootCount1-3>
+    <ModuleDBRootID1-1-3>1</ModuleDBRootID1-1-3>
+    <ModuleIPAddr2-1-3>192.168.0.2</ModuleIPAddr2-1-3>
+    <ModuleDBRootCount2-3>2</ModuleDBRootCount2-3>
+    <ModuleDBRootID2-1-3>2</ModuleDBRootID2-1-3>
+    <ModuleDBRootID2-2-3>4</ModuleDBRootID2-2-3>
+  </SystemModuleConfig>
+</Columnstore>
+)";
+
+bool update_dbroots(const char* zCase,
+                    const unique_ptr<xmlDoc>& sDoc,
+                    const string& address,
+                    const vector<int>& dbroots,
+                    const char* zExpected)
+{
+    bool rv = false;
+    json_t* pOutput = json_object();
+
+    auto status = xml::update_dbroots(*sDoc.get(), address, dbroots, pOutput);
+
+    switch (status)
+    {
+    case xml::DbRoots::UPDATED:
+        {
+            auto sExpected = compile_xml(zExpected);
+
+            if (equal(sDoc, sExpected))
+            {
+                cout << zCase << ": Correctly handled.";
+                rv = true;
+            }
+            else
+            {
+                cout << zCase << ": Config updated, but result not the expected one.\n"
+                     << "\n"
+                     << "EXPECTED:\n" << xml::dump(sExpected) << "\n"
+                     << "\n"
+                     << "OBTAINED:\n" << xml::dump(sDoc) << endl;
+            }
+        }
+        break;
+
+    case xml::DbRoots::NO_CHANGE:
+        cout << zCase << ": Change was not detected." << endl;
+        break;
+
+    case xml::DbRoots::ERROR:
+        cout << zCase << ": DbRoot update faile." << endl;
+        break;
+    }
+
+    cout << endl;
+
+    json_decref(pOutput);
+
+    return rv;
+}
+
+int test_scan_for_dbroots()
+{
+    int rv = 0;
+    unique_ptr<xmlDoc> sDoc = compile_xml(ZSCAN_1_2);
+
+    json_t* pOutput = json_object();
+    string address { "192.168.0.1" };
+    vector<int> dbroots;
+
+    if (rv == 0)
+    {
+        // Try the same dbroots as there are in the configuration.
+        dbroots.push_back(1);
+
+        auto status = xml::update_dbroots(*sDoc.get(), address, dbroots, pOutput);
+
+        if (status == xml::DbRoots::NO_CHANGE)
+        {
+            cout << "Identical configuration was detected as such." << endl;
+        }
+        else
+        {
+            cout << "No change not detected." << endl;
+            rv = 1;
+        }
+    }
+
+    if (rv == 0)
+    {
+        dbroots.push_back(3);
+        // Now there are dbroots 1,3 but the config only has 1, so the config must be updated.
+
+        if (!update_dbroots("(1) -> (1,3)", sDoc, address, dbroots, ZSCAN_1_2_3))
+        {
+            rv = 1;
+        }
+    }
+
+    if (rv == 0)
+    {
+        // Now we add root 4 to the other node.
+        dbroots.clear();
+        dbroots.push_back(2); // Present in the initial config.
+        dbroots.push_back(4);
+
+        address = "192.168.0.2";
+
+        if (!update_dbroots("(2) -> (2, 4)", sDoc, address, dbroots, ZSCAN_1_2_3_4))
+        {
+            rv = 1;
+        }
+    }
+
+    if (rv == 0)
+    {
+        // Now we remove dbroot 3 from node 1.
+        dbroots.clear();
+        dbroots.push_back(1);
+
+        address = "192.168.0.1";
+
+        if (!update_dbroots("(1,3) -> (1)", sDoc, address, dbroots, ZSCAN_1_2_4))
+        {
+            rv = 1;
+        }
+    }
+
+    if (rv == 0)
+    {
+        // Now we remove dbroot 4 from node2.
+        dbroots.clear();
+        dbroots.push_back(2);
+
+        address = "192.168.0.2";
+
+        if (!update_dbroots("(2,4) -> (2)", sDoc, address, dbroots, ZSCAN_1_2))
+        {
+            rv = 1;
+        }
+    }
+
+    return rv;
+}
+}
+
 int main()
 {
-    mxb::Log log(MXB_LOG_TARGET_STDOUT);
+    mxb::Log log;
 
     int rv = 0;
     rv += test_convert_to_first_multi_node();
+    rv += test_scan_for_dbroots();
+
     return rv;
 }
 
