8957a77948f7abf94b95d31566ad04e2676bba3d
Johan Wikman <johan.wikman@gmail.com>
2020-06-17 15:54:54 +0300

    MXS-3042 Factor out installation and starting of storages
    - memcached
    - redis
    
    Other tests will call the installation/start script as well,
    so that the order of tests is not significant.

---
+++ maxscale-system-test/cache_install_and_start_storages.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+function install_package
+{
+    local keyfile=$1
+    local user=$2
+    local host=$3
+    local package=$4
+
+    echo "Installing $package on $host."
+
+    ssh -i ${keyfile} -o StrictHostKeyChecking=no ${user}@${host} sudo yum -y install ${package}
+
+    if [ $? -ne 0 ]
+    then
+        echo "error: Could not install $package on $host."
+        exit 1
+    fi
+}
+
+function start_service
+{
+    local keyfile=$1
+    local user=$2
+    local host=$3
+    local service=$4
+
+    echo "Starting $package on $host."
+
+    ssh -i ${keyfile} -o StrictHostKeyChecking=no ${user}@${host} sudo systemctl start ${service}
+
+    if [ $? -ne 0 ]
+    then
+        echo "error: Could not start $service on $host."
+        exit 1
+    fi
+}
+
+function install_redis_on_maxscale_000
+{
+    install_package ${maxscale_000_keyfile} ${maxscale_000_whoami} ${maxscale_000_network} redis
+}
+
+function start_redis_on_maxscale_000
+{
+    start_service ${maxscale_000_keyfile} ${maxscale_000_whoami} ${maxscale_000_network} redis
+}
+
+function install_memcached_on_maxscale_000
+{
+    install_package ${maxscale_000_keyfile} ${maxscale_000_whoami} ${maxscale_000_network} memcached
+}
+
+function start_memcached_on_maxscale_000
+{
+    start_service ${maxscale_000_keyfile} ${maxscale_000_whoami} ${maxscale_000_network} memcached
+}
+
+install_memcached_on_maxscale_000
+start_memcached_on_maxscale_000
+
+install_redis_on_maxscale_000
+start_redis_on_maxscale_000
