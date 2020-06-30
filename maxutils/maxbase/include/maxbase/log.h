31591dbf00824c0792927e7bc73cb62c928aa9ff
Markus Mäkelä <markus.makela@mariadb.com>
2020-06-30 10:42:46 +0300

    MXS-3057: Fix session_trace
    The log priority check must not check session trace as it is used inside
    the logging code.

--- maxutils/maxbase/include/maxbase/log.h
+++ maxutils/maxbase/include/maxbase/log.h
@@ -165,8 +165,7 @@ bool mxb_log_get_session_trace();
 static inline bool mxb_log_is_priority_enabled(int priority)
 {
     assert((priority & ~LOG_PRIMASK) == 0);
-    return ((mxb_log_enabled_priorities & (1 << priority)) != 0) || (priority == LOG_ALERT)
-           || mxb_log_get_session_trace();
+    return ((mxb_log_enabled_priorities & (1 << priority)) != 0) || (priority == LOG_ALERT);
 }
 
 /**
