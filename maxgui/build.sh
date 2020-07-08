e19599c6215ab094915d6bd6c7ac813452837666
Markus Mäkelä <markus.makela@mariadb.com>
2020-07-08 09:34:45 +0300

    MXS-3066: Change maxgui build files
    Changed the build process to work similarly to maxctrl: sources are copied
    to the build directory to avoid polluting the source tree. The actual
    sources will be added in a later commit.

---
+++ maxgui/build.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 SRC"
+    exit 1
+fi
+
+src=$1
+
+if [ "$PWD" != "$src" ]
+then
+    # Copy sources to working directory
+    cp -r -t $PWD/ $src/maxgui/
+fi
+
+cd $PWD/maxgui
+
+npm install
+buildPath=$PWD npm run build
