758fdf292b18b2fb4b907a3715bb074fe8dd228d
Esa Korhonen <esa.korhonen@mariadb.com>
2020-07-15 19:30:07 +0300

    MXS-2900 Add more features to ServersInfo
    Allows further simplifying of multimaster test.

--- maxscale-system-test/mariadbmonitor/mysqlmon_multimaster.cpp
+++ maxscale-system-test/mariadbmonitor/mysqlmon_multimaster.cpp
@@ -18,145 +18,17 @@
  */
 
 
-#include <iostream>
 #include <string>
-#include <set>
 #include <maxtest/testconnections.hh>
-#include <maxtest/sql_t1.hh>
-#include <jansson.h>
-#include <maxbase/string.hh>
 
-using std::cout;
 using std::string;
-using std::set;
 using std::vector;
 
-json_t* get_json_data(TestConnections& test, const char* query)
-{
-    json_t* rval = nullptr;
-    auto res = test.maxscales->ssh_output(query);
-    if (res.output.empty())
-    {
-        test.add_result(1, "Query '%s' execution error, no output.", query);
-    }
-    else
-    {
-        json_error_t error;
-        rval = json_loads(res.output.c_str(), 0, &error);
-        if (!rval)
-        {
-            test.add_result(1, "JSON decode error: %s\n", error.text);
-        }
-    }
-    return rval;
-}
-
-json_t* traverse_json(TestConnections& test, json_t* object, const std::vector<string>& keys)
-{
-    test.expect(object, "JSON object is NULL\n");
-
-    json_t* current_object = object;
-    for (auto iter = keys.begin(); iter != keys.end() && current_object; ++iter)
-    {
-        current_object = json_object_get(current_object, (*iter).c_str());
-        test.expect(current_object, "Key %s was not found in json data.\n", (*iter).c_str());
-    }
-    return current_object;
-}
-
-json_t* find_array_elem_json(TestConnections& test, json_t* object,
-                             std::string key, std::string expected_val)
-{
-    bool is_array = json_is_array(object);
-    test.expect(is_array, "JSON object is not an array\n");
-    json_t* found_elem = NULL;
-
-    if (is_array)
-    {
-        size_t arr_size = json_array_size(object);
-        for (size_t i = 0; i < arr_size; i++)
-        {
-            json_t* arr_elem = json_array_get(object, i);
-            json_t* elem_val = json_object_get(arr_elem, key.c_str());
-            bool is_string = json_is_string(elem_val);
-            test.expect(is_string,
-                        "Key %s was not found in json data or the data is not string.\n",
-                        key.c_str());
-            if (is_string)
-            {
-                std::string elem_field = json_string_value(elem_val);
-                if (elem_field == expected_val)
-                {
-                    found_elem = arr_elem;
-                    break;
-                }
-            }
-        }
-
-        test.expect(found_elem, "Array element with %s->%s was not found in json array\n",
-                    key.c_str(), expected_val.c_str());
-    }
-    return found_elem;
-}
-
-void check_group(TestConnections& test, const char* server, int expected_group)
-{
-    json_t* monitor_data = get_json_data(test, "maxctrl api get monitors/MySQL-Monitor");
-    if (monitor_data == NULL)
-    {
-        return;
-    }
-
-    monitor_data = traverse_json(test, monitor_data,
-                                 {"data", "attributes", "monitor_diagnostics", "server_info"});
-    if (monitor_data)
-    {
-        auto server_data = find_array_elem_json(test, monitor_data, "name", server);
-        if (server_data)
-        {
-            int found_group = json_integer_value(json_object_get(server_data, "master_group"));
-            test.expect(found_group == expected_group, "Server '%s', expected group '%d', not '%d'",
-                        server, expected_group, found_group);
-        }
-    }
-}
-
-void check_rlag(TestConnections& test, const char* server, int min_rlag, int max_rlag)
-{
-    json_t* object_data = get_json_data(test, "maxctrl api get servers");
-    if (object_data)
-    {
-        auto servers_data = traverse_json(test, object_data, {"data"});
-        auto server_data = find_array_elem_json(test, servers_data, "id", server);
-        auto rlag_object = traverse_json(test, server_data, {"attributes", "replication_lag"});
-        if (rlag_object)
-        {
-            int found_rlag = json_integer_value(rlag_object);
-            if (found_rlag >= min_rlag && found_rlag <= max_rlag)
-            {
-                cout << "Replication lag of " << server << " is " << found_rlag << " seconds.\n";
-            }
-            else
-            {
-                test.expect(false,
-                            "Replication lag of %s is out of bounds: value: %i min: %i max: %i\n",
-                            server, found_rlag, min_rlag, max_rlag);
-            }
-        }
-    }
-}
+void
+check_rlag(TestConnections& test, const ServersInfo& servers_info, size_t ind, int min_rlag, int max_rlag);
 
 void change_master(TestConnections& test, int slave, int master, const string& conn_name = "",
-                   int replication_delay = 0)
-{
-    const char query[] = "CHANGE MASTER '%s' TO master_host='%s', master_port=%d, "
-                         "master_log_file='mar-bin.000001', master_log_pos=4, "
-                         "master_user='repl', master_password='repl', master_delay=%d; "
-                         "START SLAVE '%s';";
-    test.try_query(test.repl->nodes[slave], query, conn_name.c_str(),
-                   test.repl->IP_private[master], test.repl->port[master],
-                   replication_delay, conn_name.c_str());
-}
+                   int replication_delay = 0);
 
 int main(int argc, char* argv[])
 {
@@ -164,6 +36,7 @@ int main(int argc, char* argv[])
     auto mm_slave_status = ServerInfo::RELAY | ServerInfo::SLAVE | ServerInfo::RUNNING;
     auto slave_status = ServerInfo::SLAVE | ServerInfo::RUNNING;
     auto running_status = ServerInfo::RUNNING;
+    auto grp_none = ServerInfo::GROUP_NONE;
 
     const char reset_query[] = "STOP SLAVE; RESET SLAVE ALL; RESET MASTER; SET GLOBAL read_only='OFF'";
     const char readonly_on_query[] = "SET GLOBAL read_only='ON'";
@@ -186,13 +59,13 @@ int main(int argc, char* argv[])
     test.try_query(maxconn, "FLUSH TABLES;");
     test.maxscale()->wait_monitor_ticks(1);
 
-    test.maxscale()->check_servers_status({mm_master_status, mm_slave_status, mm_slave_status, slave_status});
+    auto servers_info = test.maxscale()->get_servers();
+    auto phase1_2_groups = {1, 1, 1, grp_none};
+    servers_info.check_servers_status(test,
+                                      {mm_master_status, mm_slave_status, mm_slave_status, slave_status});
+    servers_info.check_master_groups(test, phase1_2_groups);
+    check_rlag(test, servers_info, 3, 1, max_rlag);
 
-    check_group(test, "server1", 1);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 1);
-    check_group(test, "server4", 0);
-    check_rlag(test, "server4", 1, max_rlag);
     // Need to send a read query so that rwsplit detects replication lag.
     test.try_query(maxconn, "SHOW DATABASES;");
     mysql_close(maxconn);
@@ -205,13 +78,11 @@ int main(int argc, char* argv[])
     execute_query(test.repl->nodes[1], readonly_on_query);
     test.maxscale()->wait_monitor_ticks(1);
 
-    test.maxscale()->check_servers_status({mm_slave_status, mm_slave_status, mm_master_status, slave_status});
-
-    check_group(test, "server1", 1);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 1);
-    check_group(test, "server4", 0);
-    check_rlag(test, "server4", 1, max_rlag);
+    servers_info = test.maxscale()->get_servers();
+    servers_info.check_servers_status(test, {mm_slave_status, mm_slave_status, mm_master_status,
+                                             slave_status});
+    servers_info.check_master_groups(test, phase1_2_groups);
+    check_rlag(test, servers_info, 3, 1, max_rlag);
 
     test.tprintf("Test 3 - Configure nodes 1 and 2 into a master-master pair, make node 0 "
                  "a slave of node 1 and node 3 a slave of node 2");
@@ -235,13 +106,11 @@ int main(int argc, char* argv[])
     mysql_close(maxconn);
     test.maxscale()->wait_monitor_ticks(1);
 
-    test.maxscale()->check_servers_status({slave_status, mm_master_status, mm_slave_status, slave_status});
-
-    check_group(test, "server1", 0);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 1);
-    check_group(test, "server4", 0);
-    check_rlag(test, "server3", 1, max_rlag);
+    servers_info = test.maxscale()->get_servers();
+    auto phase3_4_groups = {grp_none, 1, 1, grp_none};
+    servers_info.check_servers_status(test, {slave_status, mm_master_status, mm_slave_status, slave_status});
+    servers_info.check_master_groups(test, phase3_4_groups);
+    check_rlag(test, servers_info, 2, 1, max_rlag);
 
     test.tprintf("Test 4 - Set node 1 into read-only mode");
 
@@ -249,12 +118,9 @@ int main(int argc, char* argv[])
     execute_query(test.repl->nodes[1], readonly_on_query);
     test.maxscale()->wait_monitor_ticks(1);
 
-    test.maxscale()->check_servers_status({slave_status, mm_slave_status, mm_master_status, slave_status});
-
-    check_group(test, "server1", 0);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 1);
-    check_group(test, "server4", 0);
+    servers_info = test.maxscale()->get_servers();
+    servers_info.check_servers_status(test, {slave_status, mm_slave_status, mm_master_status, slave_status});
+    servers_info.check_master_groups(test, phase3_4_groups);
 
     test.tprintf("Test 5 - Create two distinct groups");
 
@@ -276,13 +142,11 @@ int main(int argc, char* argv[])
     // contains a master and a slave. Only one master may exist in a cluster
     // at once, since by definition this is the server to which routers may
     // direct writes.
-    test.maxscale()->check_servers_status({mm_master_status, mm_slave_status,
-                                           running_status, running_status});
-
-    check_group(test, "server1", 1);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 2);
-    check_group(test, "server4", 2);
+    servers_info = test.maxscale()->get_servers();
+    auto phase5_6_groups = {1, 1, 2, 2};
+    auto phase5_6_status = {mm_master_status, mm_slave_status, running_status, running_status};
+    servers_info.check_servers_status(test, phase5_6_status);
+    servers_info.check_master_groups(test, phase5_6_groups);
 
     test.tprintf("Test 6 - Set nodes 1 and 3 into read-only mode");
 
@@ -292,13 +156,9 @@ int main(int argc, char* argv[])
 
     test.maxscale()->wait_monitor_ticks(1);
 
-    test.maxscale()->check_servers_status({mm_master_status, mm_slave_status,
-                                           running_status, running_status});
-
-    check_group(test, "server1", 1);
-    check_group(test, "server2", 1);
-    check_group(test, "server3", 2);
-    check_group(test, "server4", 2);
+    servers_info = test.maxscale()->get_servers();
+    servers_info.check_servers_status(test, phase5_6_status);
+    servers_info.check_master_groups(test, phase5_6_groups);
 
     test.tprintf("Test 7 - Diamond topology with delay");
 
@@ -315,13 +175,12 @@ int main(int argc, char* argv[])
     test.maxscale()->wait_monitor_ticks(2);
     test.try_query(maxconn, "SHOW DATABASES;");
 
-    test.maxscale()->check_servers_status({slave_status, mm_slave_status, mm_slave_status, mm_master_status});
-
-    check_group(test, "server1", 0);
-    check_group(test, "server2", 0);
-    check_group(test, "server3", 0);
-    check_group(test, "server4", 0);
-    check_rlag(test, "server1", 1, max_rlag);
+    servers_info = test.maxscale()->get_servers();
+    auto phase7_8_status = {slave_status, mm_slave_status, mm_slave_status, mm_master_status};
+    auto phase7_8_groups = {grp_none, grp_none, grp_none, grp_none};
+    servers_info.check_servers_status(test, phase7_8_status);
+    servers_info.check_master_groups(test, phase7_8_groups);
+    check_rlag(test, servers_info, 0, 1, max_rlag);
 
     test.tprintf("Test 8 - Diamond topology with no delay");
 
@@ -329,8 +188,11 @@ int main(int argc, char* argv[])
     test.try_query(test.repl->nodes[0], remove_delay, "a", "a", "a");
     test.maxscale()->wait_monitor_ticks(2);
 
-    test.maxscale()->check_servers_status({slave_status});
-    check_rlag(test, "server1", 0, 0);
+    servers_info = test.maxscale()->get_servers();
+    servers_info.check_servers_status(test, phase7_8_status);
+    servers_info.check_master_groups(test, phase7_8_groups);
+    check_rlag(test, servers_info, 0, 0, 0);
+
     // Rwsplit should detects that replication lag is 0.
     test.try_query(maxconn, "SHOW DATABASES;");
     mysql_close(maxconn);
@@ -348,4 +210,35 @@ int main(int argc, char* argv[])
     change_master(test, 3, 0);
 
     return test.global_result;
+}
+
+void
+check_rlag(TestConnections& test, const ServersInfo& servers_info, size_t ind, int min_rlag, int max_rlag)
+{
+    if (ind < servers_info.size())
+    {
+        auto& srv_info = servers_info.get(ind);
+        auto found_rlag = srv_info.rlag;
+        if (found_rlag >= min_rlag && found_rlag <= max_rlag)
+        {
+            test.tprintf("Replication lag of %s is %li seconds.", srv_info.name.c_str(), found_rlag);
+        }
+        else
+        {
+            test.add_failure("Replication lag of %s is out of bounds: value: %li min: %i max: %i\n",
+                             srv_info.name.c_str(), found_rlag, min_rlag, max_rlag);
+        }
+    }
+}
+
+void
+change_master(TestConnections& test, int slave, int master, const string& conn_name, int replication_delay)
+{
+    const char query[] = "CHANGE MASTER '%s' TO master_host='%s', master_port=%d, "
+                         "master_log_file='mar-bin.000001', master_log_pos=4, "
+                         "master_user='repl', master_password='repl', master_delay=%d; "
+                         "START SLAVE '%s';";
+    test.try_query(test.repl->nodes[slave], query, conn_name.c_str(),
+                   test.repl->IP_private[master], test.repl->port[master],
+                   replication_delay, conn_name.c_str());
 }
