34cb5b74dd1213445d791b64565f7288e9ca1e85
Thien Ly <thien.ly@mariadb.com>
2020-07-16 17:59:37 +0300

    Refactor DataTable.spec.js
    Each unit test should test one aspect of a feature. This helps to
    track test fails easier.

--- maxgui/src/components/common/DataTable/DataTable.spec.js
+++ maxgui/src/components/common/DataTable/DataTable.spec.js
@@ -15,6 +15,70 @@ import { expect } from 'chai'
 import mount from '@tests/unit/setup'
 import DataTable from '@/components/common/DataTable'
 
+const keepPrimitiveValue = true
+let rowspanTableHeaders = [
+    { text: `Monitor`, value: 'groupId' },
+    { text: 'State', value: 'monitorState' },
+    { text: 'Servers', value: 'id' },
+    { text: 'Address', value: 'serverAddress' },
+    { text: 'Port', value: 'serverPort' },
+    { text: 'Connections', value: 'serverConnections' },
+    { text: 'State', value: 'serverState' },
+    { text: 'GTID', value: 'gtid' },
+    { text: 'Services', value: 'serviceIds' },
+]
+let rowspanTableData = [
+    {
+        id: 'row_server_2',
+        serverAddress: '127.0.0.1',
+        serverPort: 4002,
+        serverConnections: 0,
+        serverState: 'Slave, Running',
+        serviceIds: ['RCR-Router', 'RCR-Writer', 'RWS-Router'],
+        gtid: '0-1000-9',
+        groupId: 'Monitor', // will be assigned rowspan 2 via handleDisplayRowspan
+        monitorState: 'Running', //rowspan 2
+    },
+    {
+        id: 'row_server_1',
+        serverAddress: '127.0.0.1',
+        serverPort: 4001,
+        serverConnections: 0,
+        serverState: 'Master, Running',
+        serviceIds: ['RCR-Router', 'RCR-Writer', 'RWS-Router'],
+        gtid: '0-1000-9',
+        groupId: 'Monitor', // will be assigned rowspan 2 via handleDisplayRowspan
+        monitorState: 'Running', //rowspan 2
+    },
+    {
+        id: 'row_server_3',
+        serverAddress: '127.0.0.1',
+        serverPort: 4003,
+        serverConnections: 0,
+        serverState: 'Down',
+        serviceIds: 'No services',
+        gtid: null,
+        groupId: 'monitor-test', // will be assigned rowspan 1 via handleDisplayRowspan
+        monitorState: 'Running', //rowspan 1
+    },
+]
+/**
+ * This function mockup processing tree data
+ * @param {Object} wrapper A Wrapper is an object that contains a mounted component and methods to test the component
+ * @return {Array} Tree data array that is used to display in data-table
+ */
+function mockupTreeData(wrapper) {
+    /* Tree props data*/
+    const objParameter = {
+        root_node: {
+            node_child: { grand_child: { great_grand_child: 'great_grand_child' } },
+            node_child_1: 'node_child_1 value',
+        },
+    }
+
+    let level = 0
+    return wrapper.vm.$help.objToArrOfObj(objParameter, keepPrimitiveValue, level)
+}
 describe('DataTable.vue', () => {
     let wrapper
     // mockup parent value passing to Collapse
@@ -25,11 +89,11 @@ describe('DataTable.vue', () => {
             shallow: false,
             component: DataTable,
             props: {
-                /* 
-                    object in headers may have these properties: 
+                /*
+                    object in headers may have these properties:
                     sortable(true || false), editableCol (true || false), align ("center || left || right"),
                     cellTruncated (true || false), width (String), padding (String)
-                   
+
                     - editableCol, align is mainly used for styling purpose which is applied in table-cell.
                     - sortable is used for disable sorting on table, by default it's always true.
                     - cellTruncated is a condition to emit 'get-truncated-info' event in table-cell
@@ -56,11 +120,10 @@ describe('DataTable.vue', () => {
         })
     })
 
-    it(`Normal table: 
-      - Component processes data as expected when keepPrimitiveValue 
+    it(`Should process data as expected when keepPrimitiveValue
         props is true or false`, async () => {
-        /* 
-            by default keepPrimitiveValue is set to false which means 
+        /*
+            by default keepPrimitiveValue is set to false which means
             null, undefined become a string i.e. 'null', 'undefined' respectively.
             This allows null or undefined value to be shown on the table and can be searched using global-search
             component
@@ -75,7 +138,7 @@ describe('DataTable.vue', () => {
 
         // this keep original value
         await wrapper.setProps({
-            keepPrimitiveValue: true,
+            keepPrimitiveValue: keepPrimitiveValue,
         })
         expect(wrapper.vm.$props.keepPrimitiveValue).to.equal(true)
         let oriData = wrapper.vm.processingData
@@ -83,40 +146,58 @@ describe('DataTable.vue', () => {
         expect(oriData[1].value).to.be.an('undefined')
     })
 
-    it(`Tree data table: 
-      - Component processes data as expected when isTree props is true. 
-      - The collapse and expansion of node works as expected
-        Note: data passed to component must be preliminary processed using 
-        objToArrOfObj helper method`, async () => {
+    it(`Tree data table:
+      - It processes data as expected when isTree props is true.`, async () => {
         // check default value isTree, should be false
         expect(wrapper.vm.$props.isTree).to.equal(false)
-
-        /* ---------------  mockup processing data from parent component  --------------- */
-        const objParameter = {
-            root_node: {
-                node_child: { grand_child: { great_grand_child: 'great_grand_child' } },
-                node_child_1: 'node_child_1 value',
-            },
-        }
-
-        let level = 0
-        const keepPrimitiveValue = true
-        let tableRow = wrapper.vm.$help.objToArrOfObj(objParameter, keepPrimitiveValue, level)
-
         await wrapper.setProps({
             isTree: true,
-            data: tableRow,
+            data: mockupTreeData(wrapper),
         })
-
-        /* ---------------  initial render should not expand all node except in editableCell mode --------------- */
-
+        expect(wrapper.vm.$props.isTree).to.equal(true)
+    })
+    it(`Tree data table:
+      - Should not expand all nodes in first render`, async () => {
+        await wrapper.setProps({
+            isTree: true,
+            data: mockupTreeData(wrapper),
+        })
         expect(wrapper.vm.$data.hasValidChild).to.equal(true)
         expect(wrapper.vm.tableRows.length).to.equal(1)
         expect(wrapper.vm.tableRows[0].expanded).to.equal(false) // root_node is collapsed
         expect(wrapper.vm.tableRows[0].id).to.equal('root_node') //  only root_node
+    })
+    it(`Tree data table:
+      - Should expand all nodes when editableCell is true`, async () => {
+        await wrapper.setProps({
+            isTree: true,
+            data: mockupTreeData(wrapper),
+        })
 
+        /* ---------------  expand all nodes------------------------ */
+        await wrapper.setProps({
+            editableCell: true, // expand all nodes ( expandAllNodes function will be called)
+        })
+        expect(wrapper.vm.tableRows.length).to.equal(5)
+        // tableRows should now includes all tree levels i.e. 0, 1, 2 and 3
+        let allNodeIds = [
+            'root_node',
+            'node_child',
+            'grand_child',
+            'great_grand_child',
+            'node_child_1',
+        ]
+        for (let i = 0; i < wrapper.vm.tableRows.length; ++i) {
+            expect(wrapper.vm.tableRows[i].id).to.equal(allNodeIds[i])
+        }
+    })
+    it(`Tree data table:
+      - The collapse and expansion of node works as expected`, async () => {
+        await wrapper.setProps({
+            isTree: true,
+            data: mockupTreeData(wrapper),
+        })
         /* ---------------  expand root_node ------------------------ */
-
         wrapper.vm.toggleNode(wrapper.vm.tableRows[0]) //expand
         expect(wrapper.vm.tableRows[0].expanded).to.equal(true) // root_node is expanded
         expect(wrapper.vm.tableRows.length).to.equal(3)
@@ -142,79 +223,45 @@ describe('DataTable.vue', () => {
         expect(wrapper.vm.tableRows[0].expanded).to.equal(false) // root_node is collapsed
         expect(wrapper.vm.tableRows.length).to.equal(1)
         expect(wrapper.vm.tableRows[0].id).to.equal('root_node') // only root_node
+    })
 
-        /* ---------------  expand all nodes------------------------ */
+    it(`Rowspan data table:
+      - Should set background color of cells have same groupId to #fafcfc if
+        a cell or a rowspan cell (rowspanCell) is hovered`, async () => {
         await wrapper.setProps({
-            editableCell: true, // expand all nodes ( expandAllNodes function will be called)
+            colsHasRowSpan: 2,
+            headers: rowspanTableHeaders,
+            data: rowspanTableData,
         })
-        expect(wrapper.vm.tableRows.length).to.equal(5)
-        // tableRows should now includes all tree levels i.e. 0, 1, 2 and 3
-        let allNodeIds = [
-            'root_node',
-            'node_child',
-            'grand_child',
-            'great_grand_child',
-            'node_child_1',
-        ]
-        for (let i = 0; i < wrapper.vm.tableRows.length; ++i) {
-            expect(wrapper.vm.tableRows[i].id).to.equal(allNodeIds[i])
+
+        // get all table-cell components with ref = MonitorRowspanCell
+        const rowspanCells = wrapper.findAllComponents({ ref: 'MonitorRowspanCell' })
+        expect(rowspanCells.length).to.equal(4)
+        /*
+            trigger mouseenter at cell 0 having header as groupId and value equal to 'Monitor'
+            cell-hover event should be emitted, setRowspanBg is called when
+            colsHasRowSpan > 0
+        */
+        await rowspanCells.at(0).trigger('mouseenter')
+
+        // testing for setRowspanBg method
+        const normalCell = wrapper.findAllComponents({ ref: 'MonitorCell' })
+        expect(normalCell.length).to.equal(14) // 7 columns with 2 rows have same groupId
+        for (let i = 0; i < normalCell.length; ++i) {
+            expect(normalCell.at(i).vm.$el.style['_values']['background-color']).to.equal(
+                'rgb(250, 252, 252)'
+            )
         }
     })
 
     it(`Rowspan data table:
-      - If a cell or a rowspan cell (rowspanCell) is hovered, 
-        automatically set background color of cells have same groupId to #fafcfc.
-      - When hovering, component emits cell-hover event and  passes data to that 
-        event correctly `, async () => {
+      - Should emit cell-hover event and return accurate data when hovering a cell`, async () => {
         await wrapper.setProps({
             colsHasRowSpan: 2,
-            headers: [
-                { text: `Monitor`, value: 'groupId' },
-                { text: 'State', value: 'monitorState' },
-                { text: 'Servers', value: 'id' },
-                { text: 'Address', value: 'serverAddress' },
-                { text: 'Port', value: 'serverPort' },
-                { text: 'Connections', value: 'serverConnections' },
-                { text: 'State', value: 'serverState' },
-                { text: 'GTID', value: 'gtid' },
-                { text: 'Services', value: 'serviceIds' },
-            ],
-            data: [
-                {
-                    id: 'row_server_2',
-                    serverAddress: '127.0.0.1',
-                    serverPort: 4002,
-                    serverConnections: 0,
-                    serverState: 'Slave, Running',
-                    serviceIds: ['RCR-Router', 'RCR-Writer', 'RWS-Router'],
-                    gtid: '0-1000-9',
-                    groupId: 'Monitor', // will be assigned rowspan 2 via handleDisplayRowspan
-                    monitorState: 'Running', //rowspan 2
-                },
-                {
-                    id: 'row_server_1',
-                    serverAddress: '127.0.0.1',
-                    serverPort: 4001,
-                    serverConnections: 0,
-                    serverState: 'Master, Running',
-                    serviceIds: ['RCR-Router', 'RCR-Writer', 'RWS-Router'],
-                    gtid: '0-1000-9',
-                    groupId: 'Monitor', // will be assigned rowspan 2 via handleDisplayRowspan
-                    monitorState: 'Running', //rowspan 2
-                },
-                {
-                    id: 'row_server_3',
-                    serverAddress: '127.0.0.1',
-                    serverPort: 4003,
-                    serverConnections: 0,
-                    serverState: 'Down',
-                    serviceIds: 'No services',
-                    gtid: null,
-                    groupId: 'monitor-test', // will be assigned rowspan 1 via handleDisplayRowspan
-                    monitorState: 'Running', //rowspan 1
-                },
-            ],
+            headers: rowspanTableHeaders,
+            data: rowspanTableData,
         })
+
         // add cb for cell-hover event
         let eventFired = 0
         wrapper.vm.$on('cell-hover', ({ item }) => {
@@ -225,25 +272,16 @@ describe('DataTable.vue', () => {
         // get all table-cell components with ref = MonitorRowspanCell
         const rowspanCells = wrapper.findAllComponents({ ref: 'MonitorRowspanCell' })
         expect(rowspanCells.length).to.equal(4)
-        /* 
-        trigger mouseenter at cell 0 having header as groupId and value equal to 'Monitor'
-        cell-hover event should be emitted, setRowspanBg is called when
-        colsHasRowSpan > 0
-    */
-
+        /*
+            trigger mouseenter at cell 0 having header as groupId and value equal to 'Monitor'
+            cell-hover event should be emitted, setRowspanBg is called when
+            colsHasRowSpan > 0
+        */
         await rowspanCells.at(0).trigger('mouseenter')
 
-        /*  after hovering 'Monitor' cell, cb and cell-hover event have been triggered 
+        /*  after hovering 'Monitor' cell, cb and cell-hover event have been triggered
             cell-hover event is emitted correctly
         */
         expect(eventFired).to.equal(1)
-        // testing for setRowspanBg method
-        const normalCell = wrapper.findAllComponents({ ref: 'MonitorCell' })
-        expect(normalCell.length).to.equal(14) // 7 columns with 2 rows have same groupId
-        for (let i = 0; i < normalCell.length; ++i) {
-            expect(normalCell.at(i).vm.$el.style['_values']['background-color']).to.equal(
-                'rgb(250, 252, 252)'
-            )
-        }
     })
 })
