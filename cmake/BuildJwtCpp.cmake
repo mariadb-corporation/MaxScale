130f08814a967b683a0668368f8ee368cb4035a0
Markus Mäkelä <markus.makela@mariadb.com>
2020-07-24 12:35:18 +0300

    Fix jwt-cpp build failure with GCC 10.1
    A patch step is required to fix the code until the maintainer fixes the
    problem: https://github.com/Thalhammer/jwt-cpp/issues/92

--- cmake/BuildJwtCpp.cmake
+++ cmake/BuildJwtCpp.cmake
@@ -1,13 +1,14 @@
 # Downloads jwt-cpp: https://github.com/Thalhammer/jwt-cpp
 ExternalProject_Add(jwt-cpp
-  URL "https://github.com/Thalhammer/jwt-cpp/archive/v0.3.1.tar.gz"
-  SOURCE_DIR ${CMAKE_BINARY_DIR}/jwt-cpp/
+  GIT_REPOSITORY "https://github.com/Thalhammer/jwt-cpp.git"
+  GIT_TAG "v0.4.0"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
+  PATCH_COMMAND cd ${CMAKE_BINARY_DIR}/jwt-cpp-prefix/src/jwt-cpp/ && git apply ${CMAKE_SOURCE_DIR}/cmake/jwt-cpp.patch
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
   LOG_CONFIGURE 1
   LOG_BUILD 1
   LOG_INSTALL 1)
-include_directories(${CMAKE_BINARY_DIR}/jwt-cpp/include/)
+include_directories(${CMAKE_BINARY_DIR}/jwt-cpp-prefix/src/jwt-cpp/include/)
