7cc65582306491c4248135a8dadf638f0e71b9f5
Esa Korhonen <esa.korhonen@mariadb.com>
2020-08-03 13:49:32 +0300

    MXS-2383 Improve password prompt message matching
    The sent answer depends on the expected messages from PAM api or
    backend. Uses case-insensitive matching for some leeway.

--- maxutils/maxbase/src/pam_utils.cc
+++ maxutils/maxbase/src/pam_utils.cc
@@ -24,7 +24,7 @@ using std::string;
 namespace
 {
 
-const string password_query = "Password: ";
+const string password_query = "Password";
 
 /** Used by the PAM conversation function */
 struct ConversationData
@@ -63,6 +63,8 @@ int conversation_func(int num_msg, const struct pam_message** messages, struct p
     auto pwds = appdata->pwds;
     auto expected_msgs = appdata->exp_msgs;
 
+    const char unexpected_prompt[] = "Unexpected prompt from PAM api when authenticating '%s'. Got '%s', "
+                                     "expected '%s'.";
     // The responses are saved as an array of structures. This is unlike the input messages, which is an
     // array of pointers to struct. Each message should have an answer, even if empty.
     auto responses = static_cast<pam_response*>(MXS_CALLOC(num_msg, sizeof(pam_response)));
@@ -72,34 +74,46 @@ int conversation_func(int num_msg, const struct pam_message** messages, struct p
     }
 
     bool conv_error = false;
-    string userhost = userdata->remote.empty() ? userdata->username :
-        userdata->username + "@" + userdata->remote;
+    auto userhost = [&userdata]() {
+            string rval = userdata->username;
+            if (!userdata->remote.empty())
+            {
+                (rval += '@') += userdata->remote;
+            }
+            return rval;
+        };
+
     for (int i = 0; i < num_msg; i++)
     {
-        const pam_message* message = messages[i];   // This may crash on Solaris, see PAM documentation.
+        const pam_message* message = messages[i];
         pam_response* response = &responses[i];
         int msg_type = message->msg_style;
+
+        auto query_match = [message](const string& expected_start) {
+                return mxb::pam::match_prompt(message->msg, expected_start);
+            };
+
         // In an ideal world, these messages would be sent to the client instead of the log. The problem
         // is that the messages should be sent with the AuthSwitchRequest-packet, requiring the blocking
         // PAM api to work with worker-threads. Not worth the trouble unless really required.
         if (msg_type == PAM_ERROR_MSG)
         {
             MXB_WARNING("Error message from PAM api when authenticating '%s': '%s'",
-                        userhost.c_str(), message->msg);
+                        userhost().c_str(), message->msg);
         }
         else if (msg_type == PAM_TEXT_INFO)
         {
             MXB_NOTICE("Message from PAM api when authenticating '%s': '%s'",
-                       userhost.c_str(), message->msg);
+                       userhost().c_str(), message->msg);
         }
         else if (msg_type == PAM_PROMPT_ECHO_ON || msg_type == PAM_PROMPT_ECHO_OFF)
         {
-            auto& pwq = expected_msgs->password_query;
             if (mode == mxb::pam::AuthMode::PW)
             {
+                auto& expected_query = expected_msgs->password_query;
                 // PAM system is asking for something. We only know how to answer the expected question,
                 // anything else is an error.
-                if (pwq.empty() || (strncasecmp(pwq.c_str(), message->msg, pwq.length()) == 0))
+                if (expected_query.empty() || query_match(expected_query))
                 {
                     response->resp = MXS_STRDUP(pwds->password.c_str());
                     MXB_DEBUG("PAM api asked for '%s'.", message->msg);
@@ -107,39 +121,73 @@ int conversation_func(int num_msg, const struct pam_message** messages, struct p
                 }
                 else
                 {
-                    MXB_ERROR("Unexpected prompt from PAM api when authenticating '%s'. Got '%s', "
-                              "expected '%s'.",
-                              userhost.c_str(), message->msg, pwq.c_str());
+                    MXB_ERROR(unexpected_prompt, userhost().c_str(), message->msg, expected_query.c_str());
                     conv_error = true;
                 }
             }
             else
             {
+                auto& prompt_ind = appdata->prompt_ind;
+                auto& exp_pwq = expected_msgs->password_query;
+                auto& exp_2faq = expected_msgs->two_fa_query;
+                bool have_exp_pwq = !exp_pwq.empty();
+                bool have_exp_2faq = !exp_2faq.empty();
+                const string* answer = nullptr;
+
                 // When performing two-factor auth, try to match the expected messages to the
-                // pam api messages. If no expected messages are given, answer first with password and
-                // then with 2FA. TODO: add message matching
-                if (pwq.empty() && expected_msgs->two_fa_query.empty())
+                // pam api messages.
+                if (have_exp_pwq && have_exp_2faq)
                 {
-                    auto& prompt_ind = appdata->prompt_ind;
+                    // Match according to expected message.
+                    if (query_match(exp_pwq))
+                    {
+                        answer = &pwds->password;
+                    }
+                    else if (query_match(exp_2faq))
+                    {
+                        answer = &pwds->two_fa_code;
+                    }
+                }
+                else if (!have_exp_pwq && !have_exp_2faq)
+                {
+                    // If no expected messages are given, answer first with password and
+                    // then with 2FA.
                     if (prompt_ind == 0)
                     {
-                        response->resp = MXS_STRDUP(pwds->password.c_str());
-                        MXB_DEBUG("PAM api asked for '%s'.", message->msg);
-                        prompt_ind++;
+                        answer = &pwds->password;
                     }
                     else if (prompt_ind == 1)
                     {
-                        response->resp = MXS_STRDUP(pwds->two_fa_code.c_str());
-                        MXB_DEBUG("PAM api asked for '%s'.", message->msg);
-                        prompt_ind++;
+                        answer = &pwds->two_fa_code;
                     }
-                    else
+                }
+                else if (have_exp_pwq)
+                {
+                    // If only expected password query is given, default to 2FA-response.
+                    answer = query_match(exp_pwq) ? &pwds->password :  &pwds->two_fa_code;
+                }
+                else
+                {
+                    // If only expected 2FA-query is given, default to password-response.
+                    answer = query_match(exp_2faq) ? &pwds->two_fa_code : &pwds->password;
+                }
+
+                if (answer)
+                {
+                    response->resp = MXS_STRDUP(answer->c_str());
+                    MXB_DEBUG("PAM api asked for '%s'.", message->msg);
+                    prompt_ind++;
+                }
+                else
+                {
+                    string expected_msgs_str = "none";
+                    if ((have_exp_pwq && have_exp_2faq))
                     {
-                        MXB_ERROR("Unexpected prompt from PAM api when authenticating '%s'. Got '%s', "
-                                  "expected none.",
-                                  userhost.c_str(), message->msg);
-                        conv_error = true;
+                        expected_msgs_str = mxb::string_printf("'%s' or '%s'",
+                                                               exp_pwq.c_str(), exp_2faq.c_str());
                     }
+                    MXB_ERROR(unexpected_prompt, userhost().c_str(), message->msg, expected_msgs_str.c_str());
+                    conv_error = true;
                 }
             }
         }
@@ -243,22 +291,17 @@ authenticate(AuthMode mode, const UserData& user, const PwdData& pwds, const std
     return result;
 }
 
-AuthResult
-authenticate(const std::string& user, const std::string& password, const std::string& client_remote,
-             const std::string& service, const std::string& expected_msg)
-{
-    UserData usr = {user, client_remote};
-    PwdData pwds = {password, ""};
-    ExpectedMsgs exp_msg = {expected_msg, ""};
-    return authenticate(AuthMode::PW, usr, pwds, service, exp_msg);
-}
-
 AuthResult authenticate(const string& user, const string& password, const string& service)
 {
     UserData usr = {user, ""};
     PwdData pwds = {password, ""};
     ExpectedMsgs exp_msg = {password_query, ""};
     return authenticate(AuthMode::PW, usr, pwds, service, exp_msg);
+}
+
+bool match_prompt(const char* prompt, const std::string& expected_start)
+{
+    return strncasecmp(prompt, expected_start.c_str(), expected_start.length()) == 0;
 }
 }
 }
