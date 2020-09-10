a541b95f9bba0dcd01abbfa3f76ee6853d0b89d6
Niclas Antti <niclas.antti@mariadb.com>
2020-09-10 15:27:16 +0300

    MXS-3147 Append to last logfile when possible

--- server/modules/routing/pinloki/file_writer.hh
+++ server/modules/routing/pinloki/file_writer.hh
@@ -48,12 +48,16 @@ private:
         int           write_pos;
     };
 
+
+    bool open_for_appending(const maxsql::Rotate& rotate, const maxsql::RplEvent& fmt_event);
     void perform_rotate(const maxsql::Rotate& rotate);
     void write_to_file(WritePosition& fn, const maxsql::RplEvent& rpl_event);
     void write_stop(const std::string& file_name);
     void write_rotate(WritePosition& fn, const std::string& to_file_name);
     void write_gtid_list(WritePosition& fn);
 
+    bool           m_newborn = true;
+    bool           m_ignore_preamble = false;
     Inventory&     m_inventory;
     const Writer&  m_writer;
     WritePosition  m_current_pos;
