679c892ed88089f9f199b89efaa2faf4d9e2ab3c
Markus Mäkelä <markus.makela@mariadb.com>
2020-09-16 16:59:03 +0300

    Initialize upstream and downstream pointers
    This helps detect cases where they aren't set correctly.

--- include/maxscale/filter.hh
+++ include/maxscale/filter.hh
@@ -163,10 +163,10 @@ protected:
     }
 
 protected:
-    MXS_SESSION*   m_pSession;  /*< The MXS_SESSION this filter session is associated with. */
-    SERVICE*       m_pService;  /*< The service for which this session was created. */
-    mxs::Routable* m_down;      /*< The downstream component. */
-    mxs::Routable* m_up;        /*< The upstream component. */
+    MXS_SESSION*   m_pSession;      /*< The MXS_SESSION this filter session is associated with. */
+    SERVICE*       m_pService;      /*< The service for which this session was created. */
+    mxs::Routable* m_down = nullptr;/*< The downstream component. */
+    mxs::Routable* m_up = nullptr;  /*< The upstream component. */
 };
 
 /**
