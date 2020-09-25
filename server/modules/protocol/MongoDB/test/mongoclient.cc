9f28858622097d7e4695c1fa70716716fdf70700
Johan Wikman <johan.wikman@gmail.com>
2020-09-25 11:17:31 +0300

    MXS-2646 Add Mongo client program
    Now possible to trace the client-server connection sequence.

---
+++ server/modules/protocol/MongoDB/test/mongoclient.cc
@@ -0,0 +1,30 @@
+/*
+ * Copied from http://mongocxx.org/mongocxx-v3/installation
+ *
+ * Presumably the license is Apache 2.
+ */
+
+#include <iostream>
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
+int main(int, char**) {
+    mongocxx::instance inst{};
+    mongocxx::client conn{mongocxx::uri{}};
+
+    bsoncxx::builder::stream::document document{};
+
+    auto collection = conn["testdb"]["testcollection"];
+    document << "hello" << "world";
+
+    collection.insert_one(document.view());
+    auto cursor = collection.find({});
+
+    for (auto&& doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << std::endl;
+    }
+}
