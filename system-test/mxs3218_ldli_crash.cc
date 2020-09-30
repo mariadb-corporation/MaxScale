9882b3c912fc1cdbe748dddec7be49aa9558dd99
Markus Mäkelä <markus.makela@mariadb.com>
2020-09-30 18:08:39 +0300

    MXS-3218: Add test case
    The test reproduces the problem and verifies that the fix solves it.

---
+++ system-test/mxs3218_ldli_crash.cc
@@ -0,0 +1,36 @@
+/**
+ * MXS-3218: Crash with LOAD DATA LOCAL INFILE
+ *
+ * The protocol parsed the data during the LOAD DATA LOCAL INFILE and confused it
+ * with a `USE <database>` query.
+ */
+
+#include <maxtest/testconnections.hh>
+
+
+// The payload must have a leading space so that it is interpreted as the command byte
+const char data[] = " USE test";
+
+int main(int argc, char** argv)
+{
+    TestConnections test(argc, argv);
+
+    char filename[1024] = "/tmp/mxs3218.XXXXXX";
+    int fd = mkstemp(filename);
+    write(fd, data, sizeof(data) - 1);
+
+    auto conn = test.maxscales->rwsplit();
+    test.expect(conn.connect(), "Connect failed: %s", conn.error());
+
+    test.expect(conn.query("CREATE OR REPLACE TABLE test.t1(id INT)"),
+                "CREATE failed: %s", conn.error());
+    test.expect(conn.query("LOAD DATA LOCAL INFILE '" + std::string(filename) + "' INTO TABLE test.t1"),
+                "LOAD DATA LOCAL INFILE failed: %s", conn.error());
+    test.expect(conn.query("DROP TABLE test.t1"),
+                "DROP failed: %s", conn.error());
+
+    close(fd);
+    remove(filename);
+
+    return test.global_result;
+}
