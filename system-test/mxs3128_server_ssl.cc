f582b8df5f6aa467c46ce864026156cbdaad82d4
Markus Mäkelä <markus.makela@mariadb.com>
2020-10-07 16:19:33 +0300

    MXS-3128: Test ssl_version
    Test that it actually changes the version that's being used.

--- system-test/mxs3128_server_ssl.cc
+++ system-test/mxs3128_server_ssl.cc
@@ -7,7 +7,8 @@
 int main(int argc, char* argv[])
 {
     TestConnections test(argc, argv);
-    const char* query = "SELECT variable_value FROM information_schema.session_status WHERE variable_name = 'ssl_version'";
+    const char* query = "SELECT variable_value FROM information_schema.session_status "
+                        "WHERE variable_name = 'ssl_version'";
     std::string ssl_ca = std::string(test.maxscales->access_homedir(0)) + "/certs/ca.pem";
 
     auto c = test.repl->get_connection(0);
@@ -26,6 +27,26 @@ int main(int argc, char* argv[])
 
     test.expect(conn.connect(), "Connection with SSL should work: %s", conn.error());
     test.expect(!conn.field(query).empty(), "SSL should be enabled");
+
+    for (int i = 1; i <= 4; i++)
+    {
+        test.check_maxctrl("alter server server" + std::to_string(i) + " ssl_version TLSv12");
+    }
+
+    test.expect(conn.connect(), "Connection with SSL should work: %s", conn.error());
+    auto res = conn.field(query);
+    test.tprintf("TLS version: %s", res.c_str());
+    test.expect(res == "TLSv1.2", "TLSv1.2 should be in use: %s", res.c_str());
+
+    for (int i = 1; i <= 4; i++)
+    {
+        test.check_maxctrl("alter server server" + std::to_string(i) + " ssl_version TLSv13");
+    }
+
+    test.expect(conn.connect(), "Connection with SSL should work: %s", conn.error());
+    res = conn.field(query);
+    test.tprintf("TLS version: %s", res.c_str());
+    test.expect(res == "TLSv1.3", "TLSv1.3 should be in use: %s", res.c_str());
 
     for (int i = 1; i <= 4; i++)
     {
