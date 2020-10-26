8f2db4b654ad54a022516c258538a7daf8604044
Markus Mäkelä <markus.makela@mariadb.com>
2020-10-26 20:54:17 +0200

    MXS-1687: Add kafka HA test case
    The test checks that the sending of events moves to the other instance
    when the ownership changes.

---
+++ system-test/kafka/mxs1687_kafka_ha.cpp
@@ -0,0 +1,69 @@
+#include <maxtest/testconnections.hh>
+#include "consumer.hh"
+
+using namespace std::literals::string_literals;
+
+void read_messages(TestConnections& test, Consumer& consumer, int n_expected)
+{
+    int i = consumer.consume_messages();
+    test.expect(i == n_expected, "Expected %d messages, got %d", n_expected, i);
+}
+
+int main(int argc, char** argv)
+{
+    TestConnections test(argc, argv);
+    Kafka kafka(test);
+
+    test.repl->stop_slaves();
+    auto conn = test.repl->get_connection(0);
+
+    // Stop B-Monitor, A-Monitor will take ownership of the cluster
+    test.maxctrl("stop monitor B-Monitor");
+    sleep(5);
+    test.maxctrl("start monitor B-Monitor");
+
+    // Connect to Kafka
+    Consumer consumer(test);
+
+    test.tprintf("Inserting data");
+    conn.connect();
+    conn.query("RESET MASTER");
+    conn.query("CREATE TABLE t1(id INT)");
+    conn.query("INSERT INTO t1 VALUES (1), (2), (3)");
+    conn.query("UPDATE t1 SET id = 4 WHERE id = 2");
+    conn.query("DELETE FROM t1 WHERE id = 3");
+    auto gtid = conn.field("SELECT @@gtid_binlog_pos");
+    test.tprintf("GTID: %s", gtid.c_str());
+
+    test.tprintf("Give MaxScale some time to process the events");
+    sleep(5);
+
+    read_messages(test, consumer, 7);
+
+    // Pass ownership to B-Monitor
+    test.maxctrl("stop monitor A-Monitor");
+    sleep(5);
+    test.maxctrl("start monitor A-Monitor");
+
+    conn.query("INSERT INTO t1 VALUES (4), (5), (6)");
+    gtid = conn.field("SELECT @@gtid_binlog_pos");
+    test.tprintf("GTID: %s", gtid.c_str());
+    sleep(5);
+
+    read_messages(test, consumer, 3);
+
+    // Stop both monitors, no events should be sent
+    test.maxctrl("stop monitor A-Monitor");
+    test.maxctrl("stop monitor B-Monitor");
+    sleep(5);
+
+    conn.query("INSERT INTO t1 VALUES (7), (8), (9)");
+    gtid = conn.field("SELECT @@gtid_binlog_pos");
+    test.tprintf("GTID: %s", gtid.c_str());
+    sleep(5);
+
+    read_messages(test, consumer, 0);
+    test.repl->fix_replication();
+
+    return test.global_result;
+}
