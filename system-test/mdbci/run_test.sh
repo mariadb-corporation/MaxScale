4300f1d80e3cc1a429aedd31934019598c8413e2
DmitriyKarpovskiy1 <58940841+DmitriyKarpovskiy1@users.noreply.github.com>
2020-11-18 16:35:42 +0300

    Add a check for the existence of the LastTestsFailed.log file to rest… (#213)
    Remove rerun failed tests

--- system-test/mdbci/run_test.sh
+++ system-test/mdbci/run_test.sh
@@ -81,7 +81,6 @@ else
     eval "arguments=(${test_set})"
     ctest -N "${arguments[@]}"
     ctest -VV "${arguments[@]}"
-    ctest --rerun-failed -VV
 fi
 cp core.* ${logs_publish_dir}
 ${script_dir}/copy_logs.sh
