mariadb-corporation/MaxScale

commit 43068d20b43a34d5f3b4b4db0fcce701b3cd7cad

MXS-3337: Fix galeramon query
The query now properly queries over both the status and the variables.

server/modules/monitor/galeramon/galeramon.cc
@@ -137,7 +137,11 @@ void GaleraMonitor::update_server_status(MXS_MONITORED_SERVER* monitored_server)
 
     /* Check if the the Galera FSM shows this node is joined to the cluster */
     const char* cluster_member =
-        "SHOW STATUS WHERE Variable_name IN"
+        " SELECT * FROM ("
+        " SELECT * FROM information_schema.SESSION_STATUS"
+        " UNION"
+        " SELECT * FROM information_schema.SESSION_VARIABLES) AS t"
+        " WHERE Variable_name IN"
         " ('wsrep_cluster_state_uuid',"
         " 'wsrep_cluster_size',"
         " 'wsrep_local_index',"

commit d869dd358eba740da7162205e30ea09d37fd24d4

MXS-3337: Select lowercase variable names
The current code assumes that the variable names are in lowercase. This
fixes the galera monitoring that was broken by commit
43068d20b43a34d5f3b4b4db0fcce701b3cd7cad. In addition, lowercase names
also helps when comparisons are done with std::string.

server/modules/monitor/galeramon/galeramon.cc
@@ -137,7 +137,7 @@ void GaleraMonitor::update_server_status(MXS_MONITORED_SERVER* monitored_server)
 
     /* Check if the the Galera FSM shows this node is joined to the cluster */
     const char* cluster_member =
-        " SELECT * FROM ("
+        " SELECT LOWER(VARIABLE_NAME), VARIABLE_VALUE FROM ("
         " SELECT * FROM information_schema.SESSION_STATUS"
         " UNION"
         " SELECT * FROM information_schema.SESSION_VARIABLES) AS t"