45fff711b4e7fecc249a0e31b8b0c153989720f5
Johan Wikman <johan.wikman@gmail.com>
2020-12-14 10:31:51 +0200

    2.5.6 Use correct maxctrl credentials

--- system-test/mxs1662_pam_admin.cpp
+++ system-test/mxs1662_pam_admin.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     TestConnections test(argc, argv);
 
     // TODO: Store login information
-    test.check_maxctrl("-u vagrant -p vagrant show maxscale");
+    test.check_maxctrl("-u admin -p mariadb show maxscale");
 
     return test.global_result;
 }
