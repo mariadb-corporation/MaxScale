2e87d6fe56e7cb6f1a53fd0aa2e499a74376cbbe
Johan Wikman <johan.wikman@gmail.com>
2020-12-16 09:38:19 +0200

    MXS-3340 Remove creation of '...'@'localhost' users
    The tests and MaxScale are not run on the same VM, so those
    localhost users cannot be used.

--- system-test/create_user.sh
+++ system-test/create_user.sh
@@ -17,31 +17,17 @@ DROP USER IF EXISTS 'repl'@'%';
 CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
 GRANT ALL ON *.* TO 'repl'@'%' WITH GRANT OPTION;
 
-DROP USER IF EXISTS 'repl'@'localhost';
-CREATE USER 'repl'@'localhost' IDENTIFIED BY 'repl';
-GRANT ALL ON *.* TO 'repl'@'localhost' WITH GRANT OPTION;
-
 DROP USER IF EXISTS 'skysql'@'%';
 CREATE USER 'skysql'@'%' IDENTIFIED BY 'skysql';
 GRANT ALL ON *.* TO 'skysql'@'%' $require_ssl WITH GRANT OPTION;
-
-DROP USER IF EXISTS 'skysql'@'localhost';
 
 DROP USER IF EXISTS 'maxskysql'@'%';
 CREATE USER 'maxskysql'@'%' IDENTIFIED BY 'skysql';
 GRANT ALL ON *.* TO 'maxskysql'@'%' $require_ssl WITH GRANT OPTION;
 
-DROP USER IF EXISTS 'maxskysql'@'localhost';
-CREATE USER 'maxskysql'@'localhost' IDENTIFIED BY 'skysql';
-GRANT ALL ON *.* TO 'maxskysql'@'localhost' $require_ssl WITH GRANT OPTION;
-
 DROP USER IF EXISTS 'maxuser'@'%';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'%' $require_ssl WITH GRANT OPTION;
-
-DROP USER IF EXISTS 'maxuser'@'localhost';
-CREATE USER 'maxuser'@'localhost' IDENTIFIED BY 'maxpwd';
-GRANT ALL ON *.* TO 'maxuser'@'localhost' $require_ssl WITH GRANT OPTION;
 
 RESET MASTER;
 EOF
--- system-test/create_user_galera.sh
+++ system-test/create_user_galera.sh
@@ -29,10 +29,6 @@ DROP USER IF EXISTS 'maxskysql'@'%';
 CREATE USER 'maxskysql'@'%' IDENTIFIED BY 'skysql';
 GRANT ALL PRIVILEGES ON *.* TO 'maxskysql'@'%' WITH GRANT OPTION;
 
-DROP USER IF EXISTS 'maxskysql'@'localhost';
-CREATE USER 'maxskysql'@'localhost' IDENTIFIED BY 'skysql';
-GRANT ALL ON *.* TO 'maxskysql'@'localhost' WITH GRANT OPTION;
-
 DROP USER IF EXISTS 'repl'@'%';
 CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
 GRANT ALL PRIVILEGES ON *.* TO 'repl'@'%' WITH GRANT OPTION;
@@ -41,18 +37,9 @@ DROP USER IF EXISTS 'skysql'@'%';
 CREATE USER 'skysql'@'%' IDENTIFIED BY 'skysql';
 GRANT ALL PRIVILEGES ON *.* TO 'skysql'@'%' WITH GRANT OPTION;
 
-DROP USER IF EXISTS 'skysql'@'localhost';
-CREATE USER 'skysql'@'localhost' IDENTIFIED BY 'skysql';
-GRANT ALL PRIVILEGES ON *.* TO 'skysql'@'localhost' WITH GRANT OPTION;
-
 DROP USER IF EXISTS 'maxuser'@'%';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
-
-DROP USER IF EXISTS 'maxuser'@'localhost';
-CREATE USER 'maxuser'@'localhost' IDENTIFIED BY 'maxpwd';
-GRANT ALL ON *.* TO 'maxuser'@'localhost' WITH GRANT OPTION;
-
 
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE test;
