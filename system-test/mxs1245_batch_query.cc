770ec85e331f082fe250f68c48350e865b27696d
Markus Mäkelä <markus.makela@mariadb.com>
2020-12-17 10:18:50 +0200

    MXS-1245: Extend test case
    The connector does support streaming of queries that return resultsets,
    they must simply be consumed before the next call to
    mysql_read_query_result is made.

--- system-test/mxs1245_batch_query.cc
+++ system-test/mxs1245_batch_query.cc
@@ -20,6 +20,33 @@ void run_test(TestConnections& test, MYSQL* mysql, const std::string& query)
         test.set_timeout(10);
         test.expect(mysql_read_query_result(mysql) == 0,
                     "Reading batch result failed: %s", mysql_error(mysql));
+        mysql_free_result(mysql_use_result(mysql));
+    }
+
+    test.stop_timeout();
+}
+
+void test_master_failure(TestConnections& test, MYSQL* mysql)
+{
+    const std::string query = "DO LAST_INSERT_ID(), SLEEP(5)";
+    const int NUM_QUERY = 6;
+
+    for (int i = 0; i < NUM_QUERY && test.ok(); i++)
+    {
+        test.set_timeout(10);
+        test.expect(mysql_send_query(mysql, query.c_str(), query.length()) == 0,
+                    "Batch query failed for '%s': %s", query.c_str(), mysql_error(mysql));
+    }
+
+    test.tprintf("Block master while batched queries are being executed");
+    test.repl->block_node(0);
+    test.maxscales->wait_for_monitor(2);
+    test.repl->unblock_node(0);
+
+    for (int i = 0; i < NUM_QUERY && test.ok(); i++)
+    {
+        test.set_timeout(10);
+        mysql_read_query_result(mysql);
     }
 
     test.stop_timeout();
@@ -35,11 +62,14 @@ int main(int argc, char** argv)
         "UPDATE test.t1 SET id = id + 1 WHERE id MOD 2 != 0",
         "DELETE FROM test.t1 LIMIT 1",
         "SET @a = 1",   // Currently this won't be executed in a pipeline manner
-        // "SELECT * FROM test.t1", // TODO: Enable this when the connector supports it
+        "SELECT * FROM test.t1",
+        "SELECT LAST_INSERT_ID()",
     };
 
     test.maxscales->connect_rwsplit();
     mysql_query(test.maxscales->conn_rwsplit[0], "CREATE TABLE test.t1(id INT)");
+
+    test.tprintf("Testing streaming of various queries");
 
     for (const auto& query : queries)
     {
@@ -54,6 +84,9 @@ int main(int argc, char** argv)
     }
 
     mysql_query(test.maxscales->conn_rwsplit[0], "DROP TABLE test.t1");
+
+    test.tprintf("Testing master failure during query streaming");
+    test_master_failure(test, test.maxscales->conn_rwsplit[0]);
 
     return test.global_result;
 }
