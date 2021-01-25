47bd15fea4f2eb3298f5b9405e608c5a17c41d8d
Johan Wikman <johan.wikman@gmail.com>
2021-01-25 12:29:23 +0200

    2.5.7 Update change date

--- maxgui/src/pages/ListenerDetail/PageHeader.vue
+++ maxgui/src/pages/ListenerDetail/PageHeader.vue
@@ -51,7 +51,7 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
  *
- * Change Date: 2024-11-26
+ * Change Date: 2025-01-25
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2 or later of the General
