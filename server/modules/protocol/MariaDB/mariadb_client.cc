mariadb-corporation/MaxScale

commit bde1878f6dff072c8250ab44ed3209af617a7e3f

Make special command parsing optional
If a module in the routing chain requires query classification, the client
protocol code can reuse the query classification result to determine
whether the custom parsers need to be used. This speeds up the execution
of all normal commands.

Exposed the query classification requirement as a routing capability bit
and added it to the relevant routers that already use the query
classifier.

server/modules/protocol/MariaDB/mariadb_client.cc
@@ -651,6 +651,13 @@ char* MariaDBClientConnection::handle_variables(mxs::Buffer& buffer)
     SetParser set_parser;
     SetParser::Result result;
 
+    // If the router needs query classification, we can skip the custom parsing for all non-SET SQL commands.
+    if (rcap_type_required(m_session->service->capabilities(), RCAP_TYPE_QUERY_CLASSIFICATION)
+        && qc_get_operation(read_buffer) != QUERY_OP_SET)
+    {
+        return nullptr;
+    }
+
     switch (set_parser.check(&read_buffer, &result))
     {
     case SetParser::ERROR:
@@ -759,7 +766,11 @@ void MariaDBClientConnection::track_transaction_state(MXS_SESSION* session, GWBU
 
     if (mxs_mysql_get_command(packetbuf) == MXS_COM_QUERY)
     {
-        uint32_t type = qc_get_trx_type_mask(packetbuf);
+        const auto parser_type = rcap_type_required(
+            m_session->service->capabilities(), RCAP_TYPE_QUERY_CLASSIFICATION) ?
+            QC_TRX_PARSE_USING_QC : QC_TRX_PARSE_USING_PARSER;
+
+        uint32_t type = qc_get_trx_type_mask_using(packetbuf, parser_type);
 
         if (type & QUERY_TYPE_BEGIN_TRX)
         {
@@ -882,6 +893,31 @@ void MariaDBClientConnection::handle_use_database(GWBUF* read_buffer)
     }
 }
 
+bool MariaDBClientConnection::is_special_query(mxs::Buffer& buffer)
+{
+    bool rval = true;
+
+    if (rcap_type_required(m_session->service->capabilities(), RCAP_TYPE_QUERY_CLASSIFICATION))
+    {
+        switch (qc_get_operation(buffer.get()))
+        {
+        case QUERY_OP_CHANGE_DB:
+        case QUERY_OP_SET:
+            // TODO: Add KILL to qc_query_op_t
+            // case QUERY_OP_KILL:
+
+            // We need to process this command
+            break;
+
+        default:
+            rval = false;
+            break;
+        }
+    }
+
+    return rval;
+}
+
 /**
  * Some SQL commands/queries need to be detected and handled by the protocol
  * and MaxScale instead of being routed forward as is.
@@ -893,41 +929,46 @@ MariaDBClientConnection::SpecialCmdRes
 MariaDBClientConnection::process_special_queries(mxs::Buffer& buffer)
 {
     auto rval = SpecialCmdRes::CONTINUE;
-    auto packet_len = buffer.length();
-    /* The packet must be at least HEADER + cmd + 5 (USE d) chars in length. Also, if the packet is rather
-     * long, assume that it is not a tracked query. This assumption allows avoiding the make_contiquous-call
-     * on e.g. big inserts. The long packets can only contain one of the tracked queries by having lots of
-     * comments. */
-    const size_t min_len = MYSQL_HEADER_LEN + 1 + 5;
-    const size_t max_len = 10000;
 
-    if (packet_len >= min_len && packet_len <= max_len)
+    if (is_special_query(buffer))
     {
-        char* sql = nullptr;
-        int len = 0;
+        auto packet_len = buffer.length();
+        /* The packet must be at least HEADER + cmd + 5 (USE d) chars in length. Also, if the packet is rather
+         * long, assume that it is not a tracked query. This assumption allows avoiding the
+         * make_contiquous-call
+         * on e.g. big inserts. The long packets can only contain one of the tracked queries by having lots of
+         * comments. */
+        const size_t min_len = MYSQL_HEADER_LEN + 1 + 5;
+        const size_t max_len = 10000;
 
-        buffer.make_contiguous();
-        if (modutil_extract_SQL(buffer.get(), &sql, &len))
+        if (packet_len >= min_len && packet_len <= max_len)
         {
-            auto fields = parse_special_query(sql, len);
-            switch (fields.type)
+            char* sql = nullptr;
+            int len = 0;
+
+            buffer.make_contiguous();
+            if (modutil_extract_SQL(buffer.get(), &sql, &len))
             {
-            case SpecialQueryDesc::Type::NONE:
-                break;
+                auto fields = parse_special_query(sql, len);
+                switch (fields.type)
+                {
+                case SpecialQueryDesc::Type::NONE:
+                    break;
 
-            case SpecialQueryDesc::Type::KILL:
-                handle_query_kill(fields);
-                // The kill-query is not routed to backends, as the id:s would be wrong.
-                rval = SpecialCmdRes::END;
-                break;
+                case SpecialQueryDesc::Type::KILL:
+                    handle_query_kill(fields);
+                    // The kill-query is not routed to backends, as the id:s would be wrong.
+                    rval = SpecialCmdRes::END;
+                    break;
 
-            case SpecialQueryDesc::Type::USE_DB:
-                handle_use_database(buffer.get());
-                break;
+                case SpecialQueryDesc::Type::USE_DB:
+                    handle_use_database(buffer.get());
+                    break;
 
-            case SpecialQueryDesc::Type::SET_ROLE:
-                start_change_role(move(fields.target));
-                break;
+                case SpecialQueryDesc::Type::SET_ROLE:
+                    start_change_role(move(fields.target));
+                    break;
+                }
             }
         }
     }

include/maxscale/protocol/mariadb/client_connection.hh
@@ -132,6 +132,7 @@ private:
     void  handle_use_database(GWBUF* read_buffer);
     char* handle_variables(mxs::Buffer& buffer);
 
+    bool          is_special_query(mxs::Buffer& buffer);
     SpecialCmdRes process_special_queries(mxs::Buffer& buffer);
     void          handle_query_kill(const SpecialQueryDesc& kill_contents);
 

include/maxscale/routing.hh
@@ -119,6 +119,15 @@ enum mxs_routing_capability_t
      * Binary: 0b0000000001000100
      */
     RCAP_TYPE_SESSION_STATE_TRACKING = (1 << 6) | RCAP_TYPE_PACKET_OUTPUT,
+
+    /**
+     * Query classification is always done. This lets the protocol module know that at least one module in the
+     * routing chain will do query classification on each query. This allows some optimizations to be done
+     * that skip some of the custom mini-parsers for the majority of commands.
+     *
+     * Binary: 0b0000000010000000
+     */
+    RCAP_TYPE_QUERY_CLASSIFICATION = (1 << 7),
 };
 
 #define RCAP_TYPE_NONE 0

server/modules/routing/readwritesplit/readwritesplit.cc
@@ -322,7 +322,7 @@ json_t* RWSplit::diagnostics() const
 }
 
 constexpr uint64_t CAPABILITIES = RCAP_TYPE_REQUEST_TRACKING | RCAP_TYPE_TRANSACTION_TRACKING
-    | RCAP_TYPE_SESSION_STATE_TRACKING | RCAP_TYPE_RUNTIME_CONFIG;
+    | RCAP_TYPE_SESSION_STATE_TRACKING | RCAP_TYPE_RUNTIME_CONFIG | RCAP_TYPE_QUERY_CLASSIFICATION;
 
 uint64_t RWSplit::getCapabilities() const
 {

server/modules/routing/schemarouter/schemarouterinstance.cc
@@ -212,7 +212,7 @@ json_t* SchemaRouter::diagnostics() const
 }
 
 static const uint64_t CAPABILITIES = RCAP_TYPE_STMT_INPUT | RCAP_TYPE_RUNTIME_CONFIG
-    | RCAP_TYPE_REQUEST_TRACKING;
+    | RCAP_TYPE_REQUEST_TRACKING | RCAP_TYPE_QUERY_CLASSIFICATION;
 
 uint64_t SchemaRouter::getCapabilities() const
 {

server/modules/routing/smartrouter/smartrouter.cc
@@ -158,7 +158,8 @@ json_t* SmartRouter::diagnostics() const
 
 uint64_t SmartRouter::getCapabilities() const
 {
-    return RCAP_TYPE_TRANSACTION_TRACKING | RCAP_TYPE_STMT_INPUT | RCAP_TYPE_STMT_OUTPUT;
+    return RCAP_TYPE_TRANSACTION_TRACKING | RCAP_TYPE_STMT_INPUT | RCAP_TYPE_STMT_OUTPUT
+           | RCAP_TYPE_QUERY_CLASSIFICATION;
 }
 
 // Eviction schedule

commit d62841aa460bc1bf5e616cadc2ad15ab59e3a1aa

Fix crash due to assert in get_canonical()
This should be a minimal fix to changes introduced by commit
bde1878f6 Make special command parsing optional

server/modules/protocol/MariaDB/mariadb_client.cc
@@ -2318,6 +2318,11 @@ bool MariaDBClientConnection::process_normal_packet(mxs::Buffer&& buffer)
 
     case MXS_COM_QUERY:
         {
+            if (rcap_type_required(m_session->service->capabilities(), RCAP_TYPE_QUERY_CLASSIFICATION))
+            {
+                buffer.make_contiguous();
+            }
+
             // Track MaxScale-specific sql. If the variable setting succeeds, the query is routed normally
             // so that the same variable is visible on backend.
             char* errmsg = handle_variables(buffer);