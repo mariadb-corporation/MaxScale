98054ce5a4f58b1e88c1c6357452443893ab9ad0
Markus Mäkelä <markus.makela@mariadb.com>
2021-03-08 10:23:44 +0200

    MXS-3332: Format capabilities better
    The formatting made it hard to read what the actual binary values were.

--- include/maxscale/routing.hh
+++ include/maxscale/routing.hh
@@ -36,25 +36,69 @@
  */
 typedef enum routing_capability
 {
-    /**< Statements are delivered one per buffer (currently always on). */
-    RCAP_TYPE_STMT_INPUT = 0x0001,      /* 0b0000000000000001 */
-    /**< Each delivered buffer is contiguous; implies RCAP_TYPE_STMT_INPUT (currently always on). */
-    RCAP_TYPE_CONTIGUOUS_INPUT = 0x0003,    /* 0b0000000000000011 */
-    /**< The transaction state and autocommit mode of the session are tracked;
-     *    implies RCAP_TYPE_CONTIGUOUS_INPUT and RCAP_TYPE_STMT_INPUT. */
-    RCAP_TYPE_TRANSACTION_TRACKING = 0x0007,    /* 0b0000000000000111 */
-    /**< Responses are delivered one per buffer. */
-    RCAP_TYPE_STMT_OUTPUT = 0x0010,     /* 0b0000000000010000 */
-    /**< Each delivered buffer is contiguous; implies RCAP_TYPE_STMT_OUTPUT. */
-    RCAP_TYPE_CONTIGUOUS_OUTPUT = 0x0030,   /* 0b0000000000110000 */
-    /** Result sets are delivered in one buffer; implies RCAP_TYPE_STMT_OUTPUT. */
-    RCAP_TYPE_RESULTSET_OUTPUT = 0x0050,    /* 0b0000000001110000 */
-    /** Results are delivered as a set of complete packets */
-    RCAP_TYPE_PACKET_OUTPUT = 0x0080,   /* 0b0000000010000000 */
-    /** Track session state changes, implies packet output */
-    RCAP_TYPE_SESSION_STATE_TRACKING = 0x0180,      /* 0b0000000011000000 */
-    /** Request and response tracking: tells when a the response to a query is complete */
-    RCAP_TYPE_REQUEST_TRACKING = 0x0183,    /* 0b0000000110000011 */
+    /**
+     * Statements are delivered one per buffer (currently always on).
+     *
+     * Binary: 0b0000 0000 0000 0001
+     */
+    RCAP_TYPE_STMT_INPUT = 0x0001,
+
+    /**
+     * Each delivered buffer is contiguous; implies RCAP_TYPE_STMT_INPUT (currently always on).
+     *
+     * Binary: 0b0000 0000 0000 0011
+     */
+    RCAP_TYPE_CONTIGUOUS_INPUT = 0x0003,
+
+    /**
+     * The transaction state and autocommit mode of the session are tracked; implies
+     * RCAP_TYPE_CONTIGUOUS_INPUT and RCAP_TYPE_STMT_INPUT.
+     *
+     * Binary: 0b0000 0000 0000 0111
+     */
+    RCAP_TYPE_TRANSACTION_TRACKING = 0x0007,
+
+    /**
+     * Responses are delivered one per buffer.
+     *
+     * Binary: 0b0000 0000 0001 0000
+     */
+    RCAP_TYPE_STMT_OUTPUT = 0x0010,
+
+    /**
+     * Each delivered buffer is contiguous; implies RCAP_TYPE_STMT_OUTPUT.
+     *
+     * Binary: 0b0000 0000 0011 0000
+     */
+    RCAP_TYPE_CONTIGUOUS_OUTPUT = 0x0030,
+
+    /**
+     * Result sets are delivered in one buffer; implies RCAP_TYPE_STMT_OUTPUT.
+     *
+     * Binary: 0b0000 0000 0111 0000
+     */
+    RCAP_TYPE_RESULTSET_OUTPUT = 0x0050,
+
+    /**
+     * Results are delivered as a set of complete packets
+     *
+     * Binary: 0b0000 0000 1000 0000
+     */
+    RCAP_TYPE_PACKET_OUTPUT = 0x0080,
+
+    /**
+     * Track session state changes, implies packet output.
+     *
+     * Binary: 0b0000 0001 1000 0000
+     */
+    RCAP_TYPE_SESSION_STATE_TRACKING = 0x0180,
+
+    /**
+     * Request and response tracking: tells when a the response to a query is complete.
+     *
+     * Binary: 0b0000 0001 1000 0011
+     */
+    RCAP_TYPE_REQUEST_TRACKING = 0x0183,
 } mxs_routing_capability_t;
 
 #define RCAP_TYPE_NONE 0
