73dcdd520fa9a82841904197cce0a8f0df303ba2
Johan Wikman <johan.wikman@gmail.com>
2021-03-08 15:03:55 +0200

    2.5 Update change date
    In preparation for merge from 2.4

--- maxgui/src/pages/Dashboard/Sessions.vue
+++ maxgui/src/pages/Dashboard/Sessions.vue
@@ -36,7 +36,7 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
  *
- * Change Date: 2025-02-16
+ * Change Date: 2025-03-08
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2 or later of the General
