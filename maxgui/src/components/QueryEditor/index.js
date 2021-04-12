e701882bb217d8dbd262acf432d8a24846172d8c
Thien Ly <thien.ly@mariadb.com>
2021-04-12 17:51:22 +0300

    MXS-3465 Add monaco editor
    This adds monaco editor to maxgui.
    Monaco editor doesn't provide `mariadb` language by default,
    so this creates `mariadb` as a custom language for the editor.

---
+++ maxgui/src/components/QueryEditor/index.js
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2025-03-24
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+import { format } from 'sql-formatter'
+import { languageConfiguration, languageTokens } from './mariadbLang'
+import reservedWords from './reservedWords.js'
+export default {
+    name: 'query-editor',
+    props: {
+        value: { type: String, required: true },
+        tableDist: { type: Array, default: () => [] },
+    },
+
+    model: {
+        prop: 'value',
+        event: 'change',
+    },
+
+    data() {
+        return {
+            completionList: [],
+            language: 'mariadb',
+        }
+    },
+    computed: {
+        completionListLabels() {
+            return this.completionList.map(item => item.label)
+        },
+    },
+    mounted() {
+        this.monaco = require('monaco-editor')
+        this.createCompletionList()
+        this.$nextTick(() => this.initMonaco(this.monaco))
+    },
+
+    beforeDestroy() {
+        if (this.editor) this.editor.dispose()
+    },
+
+    methods: {
+        createCompletionList() {
+            const keywordList = reservedWords.map(w => ({
+                label: w.keyword,
+                kind: this.monaco.languages.CompletionItemKind.Keyword,
+                insertText: w.keyword,
+            }))
+
+            const dist = this.$help.lodash.cloneDeep(this.tableDist)
+            for (const item of dist) {
+                switch (item.type) {
+                    case 'table':
+                    case 'column':
+                        item.kind = this.monaco.languages.CompletionItemKind.Text
+                }
+            }
+
+            this.completionList = [...dist, ...keywordList]
+        },
+        codeFormatter(v) {
+            return format(v, {
+                language: this.language,
+                indent: '   ',
+                uppercase: true,
+                linesBetweenQueries: 1,
+            })
+        },
+        initMonaco(monaco) {
+            // Register a new language
+            monaco.languages.register({ id: this.language })
+            monaco.languages.setLanguageConfiguration(this.language, languageConfiguration)
+
+            // Register a tokens provider for the language
+            monaco.languages.setMonarchTokensProvider(this.language, languageTokens)
+
+            monaco.editor.defineTheme('mariadb-theme', {
+                base: 'vs',
+                inherit: true, // false to completely replace the builtin rules
+                rules: [
+                    { token: '', foreground: '333333', background: 'fffffe' },
+                    { token: 'comment', foreground: '60a0b0' },
+                    { token: 'number', foreground: '40a070' },
+                    { token: 'delimiter', foreground: '000000' },
+                    { token: 'string.mariadb', foreground: '4070a0' },
+                    { token: 'keyword', foreground: '007020', fontStyle: 'bold' },
+                    { token: 'operator.mariadb', foreground: '666666' },
+                    { token: 'predefined.mariadb', foreground: 'FF00FF' },
+                    { token: 'builtinFunctions', foreground: 'eb5757' },
+                ],
+                //https://microsoft.github.io/monaco-editor/playground.html#customizing-the-appearence-exposed-colors
+                colors: {
+                    foreground: '#525a65',
+                    errorForeground: '#eb5757',
+                    'editor.foreground': '#525a65', // code color
+                    'editor.background': '#ffffff', // editor background color
+                    'editorCursor.foreground': '#424f62', // cursor color
+                    'editorLineNumber.foreground': '#a3bac0', // line number color:
+                    'editorRuler.foreground': '#e7eef1', // Color of the editor rulers.
+                    'editorError.foreground': '#eb5757',
+                    'editorError.border': '#eb5757',
+                    'editorWarning.foreground': '#f59d34',
+                    'editorWarning.border': '#f59d34',
+                    'editorSuggestWidget.background': '#ffffff',
+                    'editorSuggestWidget.border': '#e7eef1',
+                    'editorSuggestWidget.foreground': '#525a65',
+                    'editorSuggestWidget.selectedBackground': '#f2fcff',
+                    'editorSuggestWidget.highlightForeground': '#0e9bc0',
+                },
+            })
+
+            const options = {
+                value: this.codeFormatter(this.value),
+                theme: 'mariadb-theme',
+                language: this.language,
+            }
+
+            this.editor = monaco.editor.create(this.$el, options)
+
+            const scope = this
+            //TODO: Move below computes to web worker
+            monaco.languages.registerCompletionItemProvider(this.language, {
+                provideCompletionItems: function(model, position) {
+                    const wordObj = model.getWordUntilPosition(position)
+                    const range = {
+                        startLineNumber: position.lineNumber,
+                        endLineNumber: position.lineNumber,
+                        startColumn: wordObj.startColumn,
+                        endColumn: wordObj.endColumn,
+                    }
+
+                    const match = scope.completionListLabels.find(label =>
+                        label.includes(wordObj.word)
+                    )
+
+                    const suggestions = match ? scope.createCompleters(range) : []
+                    return { suggestions }
+                },
+            })
+            monaco.languages.registerDocumentFormattingEditProvider(this.language, {
+                provideDocumentFormattingEdits: function(model) {
+                    return [
+                        {
+                            range: model.getFullModelRange(),
+                            text: scope.codeFormatter(model.getValue()),
+                        },
+                    ]
+                },
+            })
+            this.editor.onDidChangeModelContent(event => {
+                const editorValue = this.getEditorValue()
+                if (this.value !== editorValue) this.$emit('change', editorValue, event)
+            })
+        },
+        createCompleters(range) {
+            return this.completionList.map(item => ({ ...item, range }))
+        },
+        getEditorValue() {
+            return this.editor.getValue()
+        },
+    },
+
+    render(createElement) {
+        return createElement('div')
+    },
+}
---
+++ maxgui/src/components/QueryEditor/reservedWords.js
@@ -0,0 +1,815 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2025-03-24
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+export default [
+    {
+        keyword: 'ACCESSIBLE',
+    },
+    {
+        keyword: 'ADD',
+    },
+    {
+        keyword: 'ALL',
+    },
+    {
+        keyword: 'ALTER',
+    },
+    {
+        keyword: 'ANALYZE',
+    },
+    {
+        keyword: 'AND',
+    },
+    {
+        keyword: 'AS',
+    },
+    {
+        keyword: 'ASC',
+    },
+    {
+        keyword: 'ASENSITIVE',
+    },
+    {
+        keyword: 'BEFORE',
+    },
+    {
+        keyword: 'BETWEEN',
+    },
+    {
+        keyword: 'BIGINT',
+    },
+    {
+        keyword: 'BINARY',
+    },
+    {
+        keyword: 'BLOB',
+    },
+    {
+        keyword: 'BOTH',
+    },
+    {
+        keyword: 'BY',
+    },
+    {
+        keyword: 'CALL',
+    },
+    {
+        keyword: 'CASCADE',
+    },
+    {
+        keyword: 'CASE',
+    },
+    {
+        keyword: 'CHANGE',
+    },
+    {
+        keyword: 'CHAR',
+    },
+    {
+        keyword: 'CHARACTER',
+    },
+    {
+        keyword: 'CHECK',
+    },
+    {
+        keyword: 'COLLATE',
+    },
+    {
+        keyword: 'COLUMN',
+    },
+    {
+        keyword: 'CONDITION',
+    },
+    {
+        keyword: 'CONSTRAINT',
+    },
+    {
+        keyword: 'CONTINUE',
+    },
+    {
+        keyword: 'CONVERT',
+    },
+    {
+        keyword: 'CREATE',
+    },
+    {
+        keyword: 'CROSS',
+    },
+    {
+        keyword: 'CURRENT_DATE',
+    },
+    {
+        keyword: 'CURRENT_ROLE',
+    },
+    {
+        keyword: 'CURRENT_TIME',
+    },
+    {
+        keyword: 'CURRENT_TIMESTAMP',
+    },
+    {
+        keyword: 'CURRENT_USER',
+    },
+    {
+        keyword: 'CURSOR',
+    },
+    {
+        keyword: 'DATABASE',
+    },
+    {
+        keyword: 'DATABASES',
+    },
+    {
+        keyword: 'DAY_HOUR',
+    },
+    {
+        keyword: 'DAY_MICROSECOND',
+    },
+    {
+        keyword: 'DAY_MINUTE',
+    },
+    {
+        keyword: 'DAY_SECOND',
+    },
+    {
+        keyword: 'DEC',
+    },
+    {
+        keyword: 'DECIMAL',
+    },
+    {
+        keyword: 'DECLARE',
+    },
+    {
+        keyword: 'DEFAULT',
+    },
+    {
+        keyword: 'DELAYED',
+    },
+    {
+        keyword: 'DELETE',
+    },
+    {
+        keyword: 'DESC',
+    },
+    {
+        keyword: 'DESCRIBE',
+    },
+    {
+        keyword: 'DETERMINISTIC',
+    },
+    {
+        keyword: 'DISTINCT',
+    },
+    {
+        keyword: 'DISTINCTROW',
+    },
+    {
+        keyword: 'DIV',
+    },
+    {
+        keyword: 'DO_DOMAIN_IDS',
+    },
+    {
+        keyword: 'DOUBLE',
+    },
+    {
+        keyword: 'DROP',
+    },
+    {
+        keyword: 'DUAL',
+    },
+    {
+        keyword: 'EACH',
+    },
+    {
+        keyword: 'ELSE',
+    },
+    {
+        keyword: 'ELSEIF',
+    },
+    {
+        keyword: 'ENCLOSED',
+    },
+    {
+        keyword: 'ESCAPED',
+    },
+    {
+        keyword: 'EXCEPT',
+    },
+    {
+        keyword: 'EXISTS',
+    },
+    {
+        keyword: 'EXIT',
+    },
+    {
+        keyword: 'EXPLAIN',
+    },
+    {
+        keyword: 'FALSE',
+    },
+    {
+        keyword: 'FETCH',
+    },
+    {
+        keyword: 'FLOAT',
+    },
+    {
+        keyword: 'FLOAT4',
+    },
+    {
+        keyword: 'FLOAT8',
+    },
+    {
+        keyword: 'FOR',
+    },
+    {
+        keyword: 'FORCE',
+    },
+    {
+        keyword: 'FOREIGN',
+    },
+    {
+        keyword: 'FROM',
+    },
+    {
+        keyword: 'FULLTEXT',
+    },
+    {
+        keyword: 'GENERAL',
+    },
+    {
+        keyword: 'GRANT',
+    },
+    {
+        keyword: 'GROUP',
+    },
+    {
+        keyword: 'HAVING',
+    },
+    {
+        keyword: 'HIGH_PRIORITY',
+    },
+    {
+        keyword: 'HOUR_MICROSECOND',
+    },
+    {
+        keyword: 'HOUR_MINUTE',
+    },
+    {
+        keyword: 'HOUR_SECOND',
+    },
+    {
+        keyword: 'IF',
+    },
+    {
+        keyword: 'IGNORE',
+    },
+    {
+        keyword: 'IGNORE_DOMAIN_IDS',
+    },
+    {
+        keyword: 'IGNORE_SERVER_IDS',
+    },
+    {
+        keyword: 'IN',
+    },
+    {
+        keyword: 'INDEX',
+    },
+    {
+        keyword: 'INFILE',
+    },
+    {
+        keyword: 'INNER',
+    },
+    {
+        keyword: 'INOUT',
+    },
+    {
+        keyword: 'INSENSITIVE',
+    },
+    {
+        keyword: 'INSERT',
+    },
+    {
+        keyword: 'INT',
+    },
+    {
+        keyword: 'INT1',
+    },
+    {
+        keyword: 'INT2',
+    },
+    {
+        keyword: 'INT3',
+    },
+    {
+        keyword: 'INT4',
+    },
+    {
+        keyword: 'INT8',
+    },
+    {
+        keyword: 'INTEGER',
+    },
+    {
+        keyword: 'INTERSECT',
+    },
+    {
+        keyword: 'INTERVAL',
+    },
+    {
+        keyword: 'INTO',
+    },
+    {
+        keyword: 'IS',
+    },
+    {
+        keyword: 'ITERATE',
+    },
+    {
+        keyword: 'JOIN',
+    },
+    {
+        keyword: 'KEY',
+    },
+    {
+        keyword: 'KEYS',
+    },
+    {
+        keyword: 'KILL',
+    },
+    {
+        keyword: 'LEADING',
+    },
+    {
+        keyword: 'LEAVE',
+    },
+    {
+        keyword: 'LEFT',
+    },
+    {
+        keyword: 'LIKE',
+    },
+    {
+        keyword: 'LIMIT',
+    },
+    {
+        keyword: 'LINEAR',
+    },
+    {
+        keyword: 'LINES',
+    },
+    {
+        keyword: 'LOAD',
+    },
+    {
+        keyword: 'LOCALTIME',
+    },
+    {
+        keyword: 'LOCALTIMESTAMP',
+    },
+    {
+        keyword: 'LOCK',
+    },
+    {
+        keyword: 'LONG',
+    },
+    {
+        keyword: 'LONGBLOB',
+    },
+    {
+        keyword: 'LONGTEXT',
+    },
+    {
+        keyword: 'LOOP',
+    },
+    {
+        keyword: 'LOW_PRIORITY',
+    },
+    {
+        keyword: 'MASTER_HEARTBEAT_PERIOD',
+    },
+    {
+        keyword: 'MASTER_SSL_VERIFY_SERVER_CERT',
+    },
+    {
+        keyword: 'MATCH',
+    },
+    {
+        keyword: 'MAXVALUE',
+    },
+    {
+        keyword: 'MEDIUMBLOB',
+    },
+    {
+        keyword: 'MEDIUMINT',
+    },
+    {
+        keyword: 'MEDIUMTEXT',
+    },
+    {
+        keyword: 'MIDDLEINT',
+    },
+    {
+        keyword: 'MINUTE_MICROSECOND',
+    },
+    {
+        keyword: 'MINUTE_SECOND',
+    },
+    {
+        keyword: 'MOD',
+    },
+    {
+        keyword: 'MODIFIES',
+    },
+    {
+        keyword: 'NATURAL',
+    },
+    {
+        keyword: 'NOT',
+    },
+    {
+        keyword: 'NO_WRITE_TO_BINLOG',
+    },
+    {
+        keyword: 'NULL',
+    },
+    {
+        keyword: 'NUMERIC',
+    },
+    {
+        keyword: 'ON',
+    },
+    {
+        keyword: 'OPTIMIZE',
+    },
+    {
+        keyword: 'OPTION',
+    },
+    {
+        keyword: 'OPTIONALLY',
+    },
+    {
+        keyword: 'OR',
+    },
+    {
+        keyword: 'ORDER',
+    },
+    {
+        keyword: 'OUT',
+    },
+    {
+        keyword: 'OUTER',
+    },
+    {
+        keyword: 'OUTFILE',
+    },
+    {
+        keyword: 'OVER',
+    },
+    {
+        keyword: 'PAGE_CHECKSUM',
+    },
+    {
+        keyword: 'PARSE_VCOL_EXPR',
+    },
+    {
+        keyword: 'PARTITION',
+    },
+    {
+        keyword: 'POSITION',
+    },
+    {
+        keyword: 'PRECISION',
+    },
+    {
+        keyword: 'PRIMARY',
+    },
+    {
+        keyword: 'PROCEDURE',
+    },
+    {
+        keyword: 'PURGE',
+    },
+    {
+        keyword: 'RANGE',
+    },
+    {
+        keyword: 'READ',
+    },
+    {
+        keyword: 'READS',
+    },
+    {
+        keyword: 'READ_WRITE',
+    },
+    {
+        keyword: 'REAL',
+    },
+    {
+        keyword: 'RECURSIVE',
+    },
+    {
+        keyword: 'REF_SYSTEM_ID',
+    },
+    {
+        keyword: 'REFERENCES',
+    },
+    {
+        keyword: 'REGEXP',
+    },
+    {
+        keyword: 'RELEASE',
+    },
+    {
+        keyword: 'RENAME',
+    },
+    {
+        keyword: 'REPEAT',
+    },
+    {
+        keyword: 'REPLACE',
+    },
+    {
+        keyword: 'REQUIRE',
+    },
+    {
+        keyword: 'RESIGNAL',
+    },
+    {
+        keyword: 'RESTRICT',
+    },
+    {
+        keyword: 'RETURN',
+    },
+    {
+        keyword: 'RETURNING',
+    },
+    {
+        keyword: 'REVOKE',
+    },
+    {
+        keyword: 'RIGHT',
+    },
+    {
+        keyword: 'RLIKE',
+    },
+    {
+        keyword: 'ROWS',
+    },
+    {
+        keyword: 'SCHEMA',
+    },
+    {
+        keyword: 'SCHEMAS',
+    },
+    {
+        keyword: 'SECOND_MICROSECOND',
+    },
+    {
+        keyword: 'SELECT',
+    },
+    {
+        keyword: 'SENSITIVE',
+    },
+    {
+        keyword: 'SEPARATOR',
+    },
+    {
+        keyword: 'SET',
+    },
+    {
+        keyword: 'SHOW',
+    },
+    {
+        keyword: 'SIGNAL',
+    },
+    {
+        keyword: 'SLOW',
+    },
+    {
+        keyword: 'SMALLINT',
+    },
+    {
+        keyword: 'SPATIAL',
+    },
+    {
+        keyword: 'SPECIFIC',
+    },
+    {
+        keyword: 'SQL',
+    },
+    {
+        keyword: 'SQLEXCEPTION',
+    },
+    {
+        keyword: 'SQLSTATE',
+    },
+    {
+        keyword: 'SQLWARNING',
+    },
+    {
+        keyword: 'SQL_BIG_RESULT',
+    },
+    {
+        keyword: 'SQL_CALC_FOUND_ROWS',
+    },
+    {
+        keyword: 'SQL_SMALL_RESULT',
+    },
+    {
+        keyword: 'SSL',
+    },
+    {
+        keyword: 'STARTING',
+    },
+    {
+        keyword: 'STATS_AUTO_RECALC',
+    },
+    {
+        keyword: 'STATS_PERSISTENT',
+    },
+    {
+        keyword: 'STATS_SAMPLE_PAGES',
+    },
+    {
+        keyword: 'STRAIGHT_JOIN',
+    },
+    {
+        keyword: 'TABLE',
+    },
+    {
+        keyword: 'TERMINATED',
+    },
+    {
+        keyword: 'THEN',
+    },
+    {
+        keyword: 'TINYBLOB',
+    },
+    {
+        keyword: 'TINYINT',
+    },
+    {
+        keyword: 'TINYTEXT',
+    },
+    {
+        keyword: 'TO',
+    },
+    {
+        keyword: 'TRAILING',
+    },
+    {
+        keyword: 'TRIGGER',
+    },
+    {
+        keyword: 'TRUE',
+    },
+    {
+        keyword: 'UNDO',
+    },
+    {
+        keyword: 'UNION',
+    },
+    {
+        keyword: 'UNIQUE',
+    },
+    {
+        keyword: 'UNLOCK',
+    },
+    {
+        keyword: 'UNSIGNED',
+    },
+    {
+        keyword: 'UPDATE',
+    },
+    {
+        keyword: 'USAGE',
+    },
+    {
+        keyword: 'USE',
+    },
+    {
+        keyword: 'USING',
+    },
+    {
+        keyword: 'UTC_DATE',
+    },
+    {
+        keyword: 'UTC_TIME',
+    },
+    {
+        keyword: 'UTC_TIMESTAMP',
+    },
+    {
+        keyword: 'VALUES',
+    },
+    {
+        keyword: 'VARBINARY',
+    },
+    {
+        keyword: 'VARCHAR',
+    },
+    {
+        keyword: 'VARCHARACTER',
+    },
+    {
+        keyword: 'VARYING',
+    },
+    {
+        keyword: 'WHEN',
+    },
+    {
+        keyword: 'WHERE',
+    },
+    {
+        keyword: 'WHILE',
+    },
+    {
+        keyword: 'WINDOW',
+    },
+    {
+        keyword: 'WITH',
+    },
+    {
+        keyword: 'WRITE',
+    },
+    {
+        keyword: 'XOR',
+    },
+    {
+        keyword: 'YEAR_MONTH',
+    },
+    {
+        keyword: 'ZEROFILL',
+    },
+    {
+        keyword: 'BIT',
+    },
+    {
+        keyword: 'DATE',
+    },
+    {
+        keyword: 'ENUM',
+    },
+    {
+        keyword: 'NO',
+    },
+    {
+        keyword: 'TEXT',
+    },
+    {
+        keyword: 'TIME',
+    },
+    {
+        keyword: 'TIMESTAMP',
+    },
+    {
+        keyword: 'BODY',
+    },
+    {
+        keyword: 'GOTO',
+    },
+    {
+        keyword: 'HISTORY',
+    },
+    {
+        keyword: 'OTHERS',
+    },
+    {
+        keyword: 'PACKAGE',
+    },
+    {
+        keyword: 'PERIOD',
+    },
+    {
+        keyword: 'RAISE',
+    },
+    {
+        keyword: 'ROWTYPE',
+    },
+    {
+        keyword: 'SYSTEM',
+    },
+    {
+        keyword: 'SYSTEM_TIME',
+    },
+    {
+        keyword: 'VERSIONING',
+    },
+    {
+        keyword: 'WITHOUT',
+    },
+]
