d32d3577b0959f35d018a26e8a4e33c669d3cca7
Markus Mäkelä <markus.makela@mariadb.com>
2021-04-13 09:49:58 +0300

    MXS-2588: Add kafkaconsumer router
    The router simply creates a Consumer in the post_configure callback and
    restarts it whenever a configuration change is made.

---
+++ server/modules/routing/kafkaconsumer/kafkaconsumer.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2025-03-24
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include "kafkaconsumer.hh"
+
+namespace kafkaconsumer
+{
+
+bool KafkaConsumer::post_configure()
+{
+    // Reset the consumer before we start a new one to make sure there's only one of them running
+    m_consumer.reset();
+    m_consumer = std::make_unique<Consumer>(m_config, Producer(m_config, m_service));
+    return true;
+}
+}
+
+extern "C" MXS_MODULE* MXS_CREATE_MODULE()
+{
+    static MXS_MODULE info =
+    {
+        mxs::MODULE_INFO_VERSION,
+        MXS_MODULE_NAME,
+        mxs::ModuleType::ROUTER,
+        mxs::ModuleStatus::GA,
+        MXS_ROUTER_VERSION,
+        "Stream Kafka messages into MariaDB",
+        "1.0.0",
+        kafkaconsumer::KafkaConsumer::CAPS,
+        &mxs::RouterApi<kafkaconsumer::KafkaConsumer>::s_api,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        {{nullptr}},
+        kafkaconsumer::Config::specification()
+    };
+
+    return &info;
+}
---
+++ server/modules/routing/kafkaconsumer/kafkaconsumer.hh
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2025-03-24
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#define MXS_MODULE_NAME "kafkaconsumer"
+
+#include <maxscale/ccdefs.hh>
+#include <maxscale/router.hh>
+#include <maxscale/service.hh>
+
+#include "config.hh"
+#include "consumer.hh"
+
+namespace kafkaconsumer
+{
+
+class KafkaConsumer : public mxs::Router, public PostConfigurable
+{
+public:
+    KafkaConsumer(const KafkaConsumer&) = delete;
+    KafkaConsumer& operator=(const KafkaConsumer&) = delete;
+
+    ~KafkaConsumer() = default;
+
+    // Router capabilities
+    static constexpr uint64_t CAPS = RCAP_TYPE_RUNTIME_CONFIG;
+
+    static KafkaConsumer* create(SERVICE* pService)
+    {
+        return new KafkaConsumer(pService);
+    }
+
+    mxs::RouterSession* newSession(MXS_SESSION* pSession, const mxs::Endpoints& endpoints)
+    {
+        return nullptr;
+    }
+
+    uint64_t getCapabilities() const
+    {
+        return CAPS;
+    }
+
+    json_t* diagnostics() const
+    {
+        return nullptr;
+    }
+
+    mxs::config::Configuration& getConfiguration()
+    {
+        return m_config;
+    }
+
+    bool post_configure() final;
+
+private:
+    KafkaConsumer(SERVICE* pService)
+        : m_service(pService)
+        , m_config(pService->name(), this)
+    {
+    }
+
+    SERVICE*                  m_service;
+    Config                    m_config;
+    std::unique_ptr<Consumer> m_consumer;
+};
+}
