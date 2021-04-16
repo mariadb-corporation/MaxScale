803505c77d08c550e9c200ff72a7a20001db507a
Niclas Antti <niclas.antti@mariadb.com>
2021-04-16 09:01:48 +0300

    MXS-3407 Use CpuInfo

--- maxutils/maxsimd/src/canonical.cc
+++ maxutils/maxsimd/src/canonical.cc
@@ -12,39 +12,22 @@
  */
 
 #include <maxsimd/canonical.hh>
+#include <maxbase/cpuinfo.hh>
 #include "impl/canonical_impl.hh"
 
 #include <string>
-#include <atomic>
-#include <mutex>    // for call_once
 
 namespace maxsimd
 {
 
 namespace
 {
-// TODO move this to maxbase init, add a CpuCapabilities class.
-std::atomic<bool> avx2_supported {false};
-std::once_flag flag;
-
-bool is_avx_supported()
-{
-    std::call_once(flag, []() {
-                       __builtin_cpu_init();
-                       bool avx2 = __builtin_cpu_supports("avx2");
-                       avx2_supported.store(avx2, std::memory_order_release);
-                   });
-
-    return avx2_supported.load(std::memory_order_relaxed);
+const maxbase::CpuInfo& cpu_info {maxbase::CpuInfo::instance()};
 }
-}
-
 
 std::string* get_canonical(std::string* pSql)
 {
-    // Simple version, for the future the capabilities etc need to be
-    // a bit more accessible.
-    if (is_avx_supported())
+    if (cpu_info.has_avx2)
     {
         return maxsimd::simd256::get_canonical_impl(pSql);
     }
