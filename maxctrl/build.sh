fe91d5f722e02c4534b137ee58a57230985a0652
Markus Mäkelä <markus.makela@mariadb.com>
2021-05-05 09:26:40 +0300

    Only build NodeJS from source if absolutely necessary
    Newer OS versions have a new enough GLIBC to support the pre-built
    binaries that are available. As the NodeJS build takes quite a while, we
    want to avoid it as much as possible.

--- maxctrl/build.sh
+++ maxctrl/build.sh
@@ -11,15 +11,27 @@ src=$1
 if [ "$PWD" != "$src" ]
 then
     # Copy sources to working directory
-    cp -r -t $PWD/maxctrl $src/maxctrl/*
-    cp -r -t $PWD/ $src/VERSION*.cmake
+    cp -r -t "$PWD/maxctrl" "$src"/maxctrl/*
+    cp -r -t "$PWD/" "$src"/VERSION*.cmake
 fi
 
-cd $PWD/maxctrl
+cd "$PWD/maxctrl" || exit 1
 
 if [ "$(arch)" == "aarch64" ]
 then
-    opts="--build"
+    # Check if we have to build the NodeJS binary from source
+
+    if grep -q -E -i 'stretch|xenial' /etc/os-release
+    then
+        # GLIBC is too old on Debian Stretch and Ubuntu Xenial
+        opts="--build"
+    fi
+
+    if grep -q '7' /etc/redhat-release
+    then
+        # CentOS 7 also has an older GLIBC
+        opts="--build"
+    fi
 fi
 
 npm install --production
