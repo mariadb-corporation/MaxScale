9b34c4ff0776069866c759f9caa85cd4e35a43c2
Markus Mäkelä <markus.makela@mariadb.com>
2021-05-27 10:42:30 +0300

    MXS-2808: Move config creation into ConfigManager
    The objects are now stored in a flat array instead of being partitioned by
    their type. This allows the objects to be stored in the order in which
    they must be constructed which is easier to resolve during the
    configuration creation than it is to resolve them from the JSON.
    
    Currently the objects are stored in a static order. This will cause
    service creation to fail if a service uses a parameter that refers to
    another service and the services aren't ordered correctly. Currently this
    isn't a problem as none of the services have such parameters and they only
    refer to each other via the relationships.
    
    Having an object that manages the creation and loading of the
    configuration from JSON makes it simpler to retain the latest JSON
    configuration. Currently it isn't stored but the possibility is there.

--- server/core/configmanager.cc
+++ server/core/configmanager.cc
@@ -15,6 +15,8 @@
 
 #include <maxscale/cn_strings.hh>
 #include <maxscale/paths.hh>
+#include <maxscale/json.hh>
+#include <maxbase/json.hh>
 
 #include "internal/configmanager.hh"
 #include "internal/config_runtime.hh"
@@ -25,62 +27,94 @@ namespace
 {
 using namespace std::string_literals;
 
-static const std::unordered_set<std::string> keys_to_keep {
-    CN_PARAMETERS, CN_RELATIONSHIPS, CN_MODULE, CN_ROUTER
-};
-
-json_t* create_config()
+class ConfigManager
 {
-    json_t* res = json_object();
+public:
 
-    auto remove_extra = [](json_t* data) {
-            json_t* attr = json_object_get(data, CN_ATTRIBUTES);
-            void* ptr;
-            const char* key;
-            json_t* ignored;
+    mxb::Json create_config()
+    {
+        json_t* arr = json_array();
 
-            json_object_foreach_safe(attr, ptr, key, ignored)
-            {
-                if (keys_to_keep.count(key) == 0)
-                {
-                    json_object_del(attr, key);
-                }
-            }
+        append_config(arr, ServerManager::server_list_to_json(""));
+        append_config(arr, MonitorManager::monitor_list_to_json(""));
+        append_config(arr, service_list_to_json(""));
+        append_config(arr, FilterDef::filter_list_to_json(""));
+        append_config(arr, Listener::to_json_collection(""));
+        append_config(arr, config_maxscale_to_json(""));
 
-            // Remove the links, we don't need them
-            json_object_del(data, CN_LINKS);
+        json_t* obj = json_object();
+        json_object_set_new(obj, "config", arr);
+        json_object_set_new(obj, "version", json_integer(1));
+
+        mxb::Json rval(mxb::Json::Type::NONE);
+        rval.reset(obj);
+        return rval;
+    }
+
+    void process_config(mxb::Json&& new_json)
+    {
+    }
+
+private:
+
+    auto remove_extra_data(json_t* data)
+    {
+        static const std::unordered_set<std::string> keys_to_keep {
+            CN_PARAMETERS, CN_MODULE, CN_ROUTER
         };
 
-    auto to_data = [&](json_t* json) {
-            json_t* data = json_incref(json_object_get(json, CN_DATA));
+        json_t* attr = json_object_get(data, CN_ATTRIBUTES);
+        void* ptr;
+        const char* key;
+        json_t* value;
 
-            if (json_is_array(data))
+        json_object_foreach_safe(attr, ptr, key, value)
+        {
+            if (keys_to_keep.count(key) == 0)
             {
-                json_t* value;
-                size_t i;
-
-                json_array_foreach(data, i, value)
-                {
-                    remove_extra(value);
-                }
+                json_object_del(attr, key);
             }
             else
             {
-                remove_extra(data);
+                mxs::json_remove_nulls(value);
+            }
+        }
+
+        // Remove the links, we don't need them
+        json_object_del(data, CN_LINKS);
+    }
+
+    void append_config(json_t* arr, json_t* json)
+    {
+        json_t* data = json_object_get(json, CN_DATA);
+
+        if (json_is_array(data))
+        {
+            json_t* value;
+            size_t i;
+
+            json_array_foreach(data, i, value)
+            {
+                remove_extra_data(value);
             }
 
-            json_decref(json);
-            return data;
-        };
+            json_array_extend(arr, data);
+        }
+        else
+        {
+            remove_extra_data(data);
+            json_array_append(arr, data);
+        }
 
-    json_object_set_new(res, CN_MAXSCALE, to_data(config_maxscale_to_json("")));
-    json_object_set_new(res, CN_SERVERS, to_data(ServerManager::server_list_to_json("")));
-    json_object_set_new(res, CN_MONITORS, to_data(MonitorManager::monitor_list_to_json("")));
-    json_object_set_new(res, CN_SERVICES, to_data(service_list_to_json("")));
-    json_object_set_new(res, CN_LISTENERS, to_data(Listener::to_json_collection("")));
-    json_object_set_new(res, CN_FILTERS, to_data(FilterDef::filter_list_to_json("")));
+        json_decref(json);
+    }
+};
 
-    return res;
+ConfigManager manager;
+
+std::string dynamic_config_filename()
+{
+    return std::string(mxs::datadir()) + "/maxscale-config.json";
 }
 }
 
@@ -89,20 +123,32 @@ namespace maxscale
 
 bool have_dynamic_config()
 {
+    // TODO: Enable this once the loading is implemented
     return false;
 }
 
 bool save_dynamic_config()
 {
-    json_t* res = create_config();
-    std::string filename = mxs::datadir() + "/maxscale-config.json"s;
-    bool ok = json_dump_file(res, filename.c_str(), JSON_COMPACT) == 0;
-    json_decref(res);
-    return ok;
+    return manager.create_config().save(dynamic_config_filename(), mxb::Json::COMPACT);
 }
 
 bool load_dynamic_config()
 {
-    return false;
+    bool ok = true;
+    std::string filename = dynamic_config_filename();
+    mxb::Json new_json(mxb::Json::Type::NONE);
+
+    if (!new_json.load(filename))
+    {
+        MXS_ERROR("Failed to load dynamic config file from '%s': %s",
+                  filename.c_str(), new_json.error_msg().c_str());
+        ok = false;
+    }
+    else
+    {
+        manager.process_config(std::move(new_json));
+    }
+
+    return ok;
 }
 }
