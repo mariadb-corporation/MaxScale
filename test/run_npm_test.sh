fd046f44cb84715031ed30f118a8e744357881ab
Markus Mäkelä <markus.makela@mariadb.com>
2021-06-03 20:14:57 +0300

    Replace BUILD_MONGO in run_npm_test.sh

--- test/run_npm_test.sh
+++ test/run_npm_test.sh
@@ -77,7 +77,7 @@ cmake $srcdir -DCMAKE_BUILD_TYPE=Debug \
       -DBUILD_BINLOG=N \
       -DBUILD_GSSAPI=N \
       -DBUILD_TOOLS=N \
-      -DBUILD_MONGO=N \
+      -DBUILD_NOSQL=N \
       -DBUILD_CDC=N || exit 1
 
 make -j $(grep -c processor /proc/cpuinfo) install || exit 1
