9b618799cae922426b707e0ea0f30ddd02ea0e9d
Thien Ly <thien.ly@mariadb.com>
2021-06-04 12:04:40 +0300

    MXS-3569 Add result sets dropdown
    This allows user to select result set to be visualized.

--- maxgui/src/pages/QueryPage/VisualizeSideBar.vue
+++ maxgui/src/pages/QueryPage/VisualizeSideBar.vue
@@ -16,7 +16,27 @@
             :height="36"
             hide-details="auto"
         />
-        <!-- TODO: add graph configuration -->
+        <div v-if="selectedChart !== 'No Visualization'" class="mt-2">
+            <label class="field__label color text-small-text"> Select result set</label>
+            <v-select
+                v-model="selectedResultSet"
+                :items="resultSets"
+                outlined
+                class="std mariadb-select-input error--text__bottom"
+                :menu-props="{
+                    contentClass: 'mariadb-select-v-menu',
+                    bottom: true,
+                    offsetY: true,
+                }"
+                dense
+                :height="36"
+                hide-details="auto"
+                item-text="id"
+                item-value="id"
+                return-object
+            />
+        </div>
+        <!-- TODO: add x, y inputs to select field -->
     </div>
 </template>
 
@@ -33,6 +53,7 @@
  * of this software will be governed by version 2 or later of the General
  * Public License.
  */
+import { mapGetters, mapState } from 'vuex'
 export default {
     name: 'visualize-sidebar',
 
@@ -40,7 +61,46 @@ export default {
         return {
             selectedChart: 'No Visualization',
             chartTypes: ['No Visualization', 'Line', 'Bar - Horizontal', 'Bar - Vertical'],
+            selectedResultSet: null,
         }
+    },
+    computed: {
+        ...mapState({
+            SQL_QUERY_MODES: state => state.app_config.SQL_QUERY_MODES,
+            query_result: state => state.query.query_result,
+        }),
+        ...mapGetters({
+            getPrvwDataRes: 'query/getPrvwDataRes',
+        }),
+        resultSets() {
+            let resSets = []
+
+            let resSetArr = this.$typy(this.query_result, 'attributes.results').safeArray
+            for (const [i, res] of resSetArr.entries()) {
+                if (res.data) {
+                    res.id = `RESULT SET ${i + 1}`
+                    resSets.push(res)
+                }
+            }
+
+            let prvwData = this.$typy(this.getPrvwDataRes(this.SQL_QUERY_MODES.PRVW_DATA))
+                .safeObject
+            if (!this.$typy(prvwData).isEmptyObject) {
+                prvwData.id = this.$t('previewData')
+                resSets.push(prvwData)
+            }
+
+            let prvwDataDetails = this.$typy(
+                this.getPrvwDataRes(this.SQL_QUERY_MODES.PRVW_DATA_DETAILS)
+            ).safeObject
+
+            if (!this.$typy(prvwDataDetails).isEmptyObject) {
+                prvwDataDetails.id = this.$t('viewDetails')
+                resSets.push(prvwDataDetails)
+            }
+
+            return resSets
+        },
     },
     watch: {
         selectedChart(v) {
