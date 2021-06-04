521296e5145da5230fc3f2263e11640ecc231dfe
Markus Mäkelä <markus.makela@mariadb.com>
2021-06-04 13:15:25 +0300

    MXS-2808: Extend basic test case
    Now covers the case where one MaxScale has to catch up.

--- system-test/config_sync/config_sync.cc
+++ system-test/config_sync/config_sync.cc
@@ -8,9 +8,6 @@ using namespace std::chrono;
 
 const auto NORMAL = mxb::Json::Format::NORMAL;
 
-// This is both clearer and simpler than a pointer to a member function
-enum WhichMaxScale { FIRST, SECOND };
-
 using RestApi = std::unique_ptr<MaxRest>;
 
 RestApi api1;
@@ -18,105 +15,97 @@ RestApi api2;
 
 struct TestCase
 {
-    std::string   desc;     // Test description
-    WhichMaxScale which;    // The MaxScale to run the command on
-    std::string   cmd;      // The MaxCtrl command to execute
-    std::string   endpoint; // REST API endpoint to check, optional
-    std::string   ptr;      // JSON Pointer to field to check, optional
+    std::string desc;       // Test description
+    std::string cmd;        // The MaxCtrl command to execute
+    std::string endpoint;   // REST API endpoint to check, optional
+    std::string ptr;        // JSON Pointer to field to check, optional
+
+    void execute(TestConnections& test, Maxscales* maxscale) const
+    {
+        test.tprintf("  %s", desc.c_str());
+        auto res = maxscale->maxctrl(cmd);
+        test.expect(res.rc == 0, "MaxCtrl command '%s' failed: %s", cmd.c_str(), res.output.c_str());
+    }
 };
 
 std::vector<TestCase> tests
 {
     {
         "Change router parameter",
-        FIRST,
         "alter service RW-Split-Router max_sescmd_history 5",
         "services/RW-Split-Router",
         "/data/attributes/parameters/max_sescmd_history"
     },
     {
         "Change router parameter on the second MaxScale",
-        SECOND,
         "alter service RW-Split-Router max_sescmd_history 15",
         "services/RW-Split-Router",
         "/data/attributes/parameters/max_sescmd_history"
     },
     {
         "Create server",
-        FIRST,
         "create server test-server 127.0.0.1 3306",
         "servers/test-server",
         "/data/attributes/parameters"
     },
     {
         "Alter server",
-        FIRST,
         "alter server test-server port 3333",
         "servers/test-server",
         "/data/attributes/parameters/port"
     },
     {
         "Link server to monitor",
-        FIRST,
         "link monitor MariaDB-Monitor test-server",
         "monitors/MariaDB-Monitor",
         "/data/relationships/servers/data"
     },
     {
         "Unlink server from monitor",
-        FIRST,
         "unlink monitor MariaDB-Monitor test-server",
         "monitors/MariaDB-Monitor",
         "/data/relationships/servers/data"
     },
     {
         "Link server to service",
-        FIRST,
         "link service RW-Split-Router test-server",
         "services/RW-Split-Router",
         "/data/relationships/servers/data"
     },
     {
         "Unlink server from service",
-        FIRST,
         "unlink service RW-Split-Router test-server",
         "services/RW-Split-Router",
         "/data/relationships/servers/data"
     },
     {
         "Destroy server",
-        FIRST,
         "destroy server test-server",
     },
     {
         "Create service",
-        FIRST,
         "create service test-service readconnroute user=maxskysql password=skysql router_options=master",
         "services/test-service",
         "/data/attributes/parameters"
     },
     {
         "Alter service",
-        FIRST,
         "alter service test-service router_options slave",
         "services/test-service",
         "/data/attributes/parameters"
     },
     {
         "Destroy service",
-        FIRST,
         "destroy service test-service"
     },
     {
         "Create filter",
-        FIRST,
         "create filter test-filter qlafilter filebase=/tmp/qla-log log_type=unified append=true",
         "filters/test-filter",
         "/data/attributes/parameters"
     },
     {
         "Destroy filter",
-        FIRST,
         "destroy filter test-filter"
     },
 };
@@ -198,6 +187,11 @@ void wait_for_sync(TestConnections& test, int expected_version, size_t num_maxsc
 
 void expect_equal(TestConnections& test, const std::string& resource, const std::string& path)
 {
+    if (resource.empty())
+    {
+        return;
+    }
+
     auto value1 = get(api1, resource, path);
     auto value2 = get(api2, resource, path);
 
@@ -224,25 +218,29 @@ void test_sync(TestConnections& test)
     // Each test case should increment the version by one
     int version = 1;
 
+    test.tprintf("Execute tests with both MaxScales running");
+
     for (const auto& t : tests)
     {
-        test.tprintf("%s", t.desc.c_str());
-
-        auto res = (t.which == FIRST ? test.maxscale : test.maxscale2)->maxctrl(t.cmd);
-        test.expect(res.rc == 0, "MaxCtrl command '%s' failed: %s", t.cmd.c_str(), res.output.c_str());
-
+        t.execute(test, test.maxscale);
         wait_for_sync(test, version++, 2);
-
-        if (!t.endpoint.empty())
-        {
-            expect_equal(test, t.endpoint, t.ptr);
-        }
-
-        if (!test.ok())
-        {
-            break;
-        }
+        expect_equal(test, t.endpoint, t.ptr);
     }
+
+    test.tprintf("Execute tests with only one MaxScale");
+    test.maxscale2->stop();
+
+    for (const auto& t : tests)
+    {
+        t.execute(test, test.maxscale);
+    }
+
+
+    test.tprintf("Start the second MaxScale and make sure it catches up");
+
+    version = get(api1, "maxscale", "/data/attributes/config_sync").get_int("version");
+    test.maxscale2->start();
+    wait_for_sync(test, version, 2);
 }
 
 int main(int argc, char** argv)
