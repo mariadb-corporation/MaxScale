5e93821f6ed8cb1d4b18a82c4f845cd7eb5ad660
Markus Mäkelä <markus.makela@mariadb.com>
2021-06-08 11:27:19 +0300

    Fix MaxCtrl interactive mode
    Parsing multiple commands with the same parser seems to have broken in
    Yargs 17. Instantiating a new one appears to be the correct way to do it.

--- maxctrl/lib/core.js
+++ maxctrl/lib/core.js
@@ -14,7 +14,7 @@
 var fs = require("fs");
 var ini = require("ini");
 var os = require("os");
-var program = require("yargs");
+var yargs = require("yargs");
 var inquirer = require("inquirer");
 var readlineSync = require("readline-sync");
 
@@ -77,208 +77,216 @@ function configParser(filename) {
   return config.maxctrl;
 }
 
-program
-  .version(maxctrl_version)
-  .strict()
-  .exitProcess(false)
-  .showHelpOnFail(false)
-  .group(["c", "u", "p", "h", "t", "q", "tsv", "skip-sync"], "Global Options:")
-  .option("c", {
-    alias: "config",
-    global: true,
-    default: default_filename,
-    describe: "MaxCtrl configuration file",
-    type: "string",
-    config: true,
-    configParser: configParser,
-  })
-  .option("u", {
-    alias: "user",
-    global: true,
-    default: "admin",
-    describe: "Username to use",
-    type: "string",
-    requiresArg: true,
-  })
-  .option("p", {
-    alias: "password",
-    describe: "Password for the user. To input the password manually, use -p '' or --password=''",
-    default: "mariadb",
-    type: "string",
-    requiresArg: true,
-  })
-  .option("h", {
-    alias: "hosts",
-    describe:
-      "List of MaxScale hosts. The hosts must be in " +
-      "HOST:PORT format and each value must be separated by a comma.",
-    default: "127.0.0.1:8989",
-    type: "string",
-    requiresArg: true,
-  })
-  .option("t", {
-    alias: "timeout",
-    describe:
-      "Request timeout in plain milliseconds, e.g '-t 1000', " +
-      "or as duration with suffix [h|m|s|ms], e.g. '-t 10s'",
-    default: "10000",
-    type: "string",
-  })
-  .coerce("t", (opt) => {
-    if (typeof opt == "string") {
-      var pos = opt.search(/[^\d]/);
-      var duration = parseInt(pos == -1 ? opt : opt.substring(0, pos));
-      if (isNaN(duration)) {
-        throw Error("'" + opt + "' is not a valid duration.");
-      }
-      if (pos != -1) {
-        var suffix = opt.substr(pos);
-        switch (suffix) {
-          case "h":
-            duration *= 24;
-          case "m":
-            duration *= 60;
-          case "s":
-            duration *= 1000;
-          case "ms":
-            break;
-
-          default:
-            throw Error("'" + suffix + "' in '" + opt + "' is not a valid duration suffix.");
+function program() {
+  return yargs()
+    .version(maxctrl_version)
+    .strict()
+    .exitProcess(false)
+    .fail(false)
+    .showHelpOnFail(false)
+    .group(["c", "u", "p", "h", "t", "q", "tsv", "skip-sync"], "Global Options:")
+    .option("c", {
+      alias: "config",
+      global: true,
+      default: default_filename,
+      describe: "MaxCtrl configuration file",
+      type: "string",
+      config: true,
+      configParser: configParser,
+    })
+    .option("u", {
+      alias: "user",
+      global: true,
+      default: "admin",
+      describe: "Username to use",
+      type: "string",
+      requiresArg: true,
+    })
+    .option("p", {
+      alias: "password",
+      describe: "Password for the user. To input the password manually, use -p '' or --password=''",
+      default: "mariadb",
+      type: "string",
+      requiresArg: true,
+    })
+    .option("h", {
+      alias: "hosts",
+      describe:
+        "List of MaxScale hosts. The hosts must be in " +
+        "HOST:PORT format and each value must be separated by a comma.",
+      default: "127.0.0.1:8989",
+      type: "string",
+      requiresArg: true,
+    })
+    .option("t", {
+      alias: "timeout",
+      describe:
+        "Request timeout in plain milliseconds, e.g '-t 1000', " +
+        "or as duration with suffix [h|m|s|ms], e.g. '-t 10s'",
+      default: "10000",
+      type: "string",
+    })
+    .coerce("t", (opt) => {
+      if (typeof opt == "string") {
+        var pos = opt.search(/[^\d]/);
+        var duration = parseInt(pos == -1 ? opt : opt.substring(0, pos));
+        if (isNaN(duration)) {
+          throw Error("'" + opt + "' is not a valid duration.");
         }
-      }
-      opt = duration;
-    }
-    return opt;
-  })
-  .option("q", {
-    alias: "quiet",
-    describe: "Silence all output. Ignored while in interactive mode.",
-    default: false,
-    type: "boolean",
-  })
-  .option("tsv", {
-    describe: "Print tab separated output",
-    default: false,
-    type: "boolean",
-  })
-  .group(["s", "tls-key", "tls-passphrase", "tls-cert", "tls-ca-cert", "n"], "HTTPS/TLS Options:")
-  .option("s", {
-    alias: "secure",
-    describe: "Enable HTTPS requests",
-    default: false,
-    type: "boolean",
-  })
-  .option("tls-key", {
-    describe: "Path to TLS private key",
-    type: "string",
-    implies: "tls-cert",
-  })
-  .option("tls-cert", {
-    describe: "Path to TLS public certificate",
-    type: "string",
-    implies: "tls-key",
-  })
-  .option("tls-passphrase", {
-    describe: "Password for the TLS private key",
-    type: "string",
-  })
-  .option("tls-ca-cert", {
-    describe: "Path to TLS CA certificate",
-    type: "string",
-  })
-  .option("n", {
-    alias: "tls-verify-server-cert",
-    describe: "Whether to verify server TLS certificates",
-    default: true,
-    type: "boolean",
-  })
-  .option("skip-sync", {
-    describe: "Disable configuration synchronization for this command",
-    default: false,
-    type: "boolean",
-  })
+        if (pos != -1) {
+          var suffix = opt.substr(pos);
+          switch (suffix) {
+            case "h":
+              duration *= 24;
+            case "m":
+              duration *= 60;
+            case "s":
+              duration *= 1000;
+            case "ms":
+              break;
 
-  .command(require("./list.js"))
-  .command(require("./show.js"))
-  .command(require("./set.js"))
-  .command(require("./clear.js"))
-  .command(require("./drain.js"))
-  .command(require("./enable.js"))
-  .command(require("./disable.js"))
-  .command(require("./create.js"))
-  .command(require("./destroy.js"))
-  .command(require("./link.js"))
-  .command(require("./unlink.js"))
-  .command(require("./start.js"))
-  .command(require("./stop.js"))
-  .command(require("./alter.js"))
-  .command(require("./rotate.js"))
-  .command(require("./reload.js"))
-  .command(require("./call.js"))
-  .command(require("./cluster.js"))
-  .command(require("./api.js"))
-  .command(require("./classify.js"))
-  .epilog(
-    "If no commands are given, maxctrl is started in interactive mode. " +
-      "Use `exit` to exit the interactive mode."
-  )
-  .help()
-  .scriptName("maxctrl")
-  .command("*", false, {}, function (argv) {
-    if (argv._.length == 0) {
-      // No password given, ask it from the command line
-      // TODO: Combine this into the one in common.js
-      if (argv.password == "") {
-        if (process.stdin.isTTY) {
-          argv.password = readlineSync.question("Enter password: ", {
-            hideEchoBack: true,
-          });
-        } else {
-          var line = fs.readFileSync(0);
-          argv.password = line.toString().trim();
+            default:
+              throw Error("'" + suffix + "' in '" + opt + "' is not a valid duration suffix.");
+          }
         }
+        opt = duration;
       }
+      return opt;
+    })
+    .option("q", {
+      alias: "quiet",
+      describe: "Silence all output. Ignored while in interactive mode.",
+      default: false,
+      type: "boolean",
+    })
+    .option("tsv", {
+      describe: "Print tab separated output",
+      default: false,
+      type: "boolean",
+    })
+    .group(["s", "tls-key", "tls-passphrase", "tls-cert", "tls-ca-cert", "n"], "HTTPS/TLS Options:")
+    .option("s", {
+      alias: "secure",
+      describe: "Enable HTTPS requests",
+      default: false,
+      type: "boolean",
+    })
+    .option("tls-key", {
+      describe: "Path to TLS private key",
+      type: "string",
+      implies: "tls-cert",
+    })
+    .option("tls-cert", {
+      describe: "Path to TLS public certificate",
+      type: "string",
+      implies: "tls-key",
+    })
+    .option("tls-passphrase", {
+      describe: "Password for the TLS private key",
+      type: "string",
+    })
+    .option("tls-ca-cert", {
+      describe: "Path to TLS CA certificate",
+      type: "string",
+    })
+    .option("n", {
+      alias: "tls-verify-server-cert",
+      describe: "Whether to verify server TLS certificates",
+      default: true,
+      type: "boolean",
+    })
+    .option("skip-sync", {
+      describe: "Disable configuration synchronization for this command",
+      default: false,
+      type: "boolean",
+    })
 
-      base_opts = [
-        "--user=" + argv.user,
-        "--password=" + argv.password,
-        "--hosts=" + argv.hosts,
-        "--timeout=" + argv.timeout,
-        "--tsv=" + argv.tsv,
-        "--secure=" + argv.secure,
-        "--tls-verify-server-cert=" + argv["tls-verify-server-cert"],
-      ];
-
-      // Only set the string options if they are defined, otherwise we'll end up with the value as
-      // the string 'undefined'
-      for (i of ["tls-key", "tls-cert", "tls-passphrase", "tls-ca-cert"]) {
-        if (argv[i]) {
-          base_opts.push("--" + i + "=" + argv[i]);
+    .command(require("./list.js"))
+    .fail(false)
+    .command(require("./show.js"))
+    .command(require("./set.js"))
+    .command(require("./clear.js"))
+    .command(require("./drain.js"))
+    .command(require("./enable.js"))
+    .command(require("./disable.js"))
+    .command(require("./create.js"))
+    .command(require("./destroy.js"))
+    .command(require("./link.js"))
+    .command(require("./unlink.js"))
+    .command(require("./start.js"))
+    .command(require("./stop.js"))
+    .command(require("./alter.js"))
+    .command(require("./rotate.js"))
+    .command(require("./reload.js"))
+    .command(require("./call.js"))
+    .command(require("./cluster.js"))
+    .command(require("./api.js"))
+    .command(require("./classify.js"))
+    .epilog(
+      "If no commands are given, maxctrl is started in interactive mode. " +
+        "Use `exit` to exit the interactive mode."
+    )
+    .help()
+    .scriptName("maxctrl")
+    .command("*", false, {}, function (argv) {
+      if (argv._.length == 0) {
+        // No password given, ask it from the command line
+        // TODO: Combine this into the one in common.js
+        if (argv.password == "") {
+          if (process.stdin.isTTY) {
+            argv.password = readlineSync.question("Enter password: ", {
+              hideEchoBack: true,
+            });
+          } else {
+            var line = fs.readFileSync(0);
+            argv.password = line.toString().trim();
+          }
         }
-      }
 
-      return askQuestion();
-    } else {
-      maxctrl(argv, function () {
-        msg = "Unknown command " + JSON.stringify(argv._);
-        return error(msg + ". See output of `--help` for a list of commands.");
-      });
-    }
-  });
+        base_opts = [
+          "--user=" + argv.user,
+          "--password=" + argv.password,
+          "--hosts=" + argv.hosts,
+          "--timeout=" + argv.timeout,
+          "--tsv=" + argv.tsv,
+          "--secure=" + argv.secure,
+          "--tls-verify-server-cert=" + argv["tls-verify-server-cert"],
+        ];
 
-function doCommand(argv) {
-  return new Promise(function (resolve, reject) {
-    program.parse(argv, { resolve: resolve, reject: reject }, function (err, argv, output) {
-      // This callback only receives input if no command was called and Yargs encountered some sort of
-      // an error or auto-generated the output. The promise will be resolved or rejected via the reject
-      // and resolve values stored in argv.
-      if (err) {
-        reject(err.message);
-      } else if (output) {
-        resolve(output);
+        // Only set the string options if they are defined, otherwise we'll end up with the value as
+        // the string 'undefined'
+        for (i of ["tls-key", "tls-cert", "tls-passphrase", "tls-ca-cert"]) {
+          if (argv[i]) {
+            base_opts.push("--" + i + "=" + argv[i]);
+          }
+        }
+
+        return askQuestion();
+      } else {
+        maxctrl(argv, function () {
+          msg = "Unknown command " + JSON.stringify(argv._);
+          return error(msg + ". See output of `--help` for a list of commands.");
+        });
       }
     });
+}
+
+function doCommand(argv) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      program().parse(argv, { resolve: resolve, reject: reject }, function (err, argv, output) {
+        // This callback only receives input if no command was called and Yargs encountered some sort of
+        // an error or auto-generated the output. The promise will be resolved or rejected via the reject
+        // and resolve values stored in argv.
+        if (err) {
+          reject(err.message);
+        } else if (output) {
+          resolve(output);
+        }
+      });
+    } catch (err) {
+      reject(err.message);
+    }
   });
 }
 
