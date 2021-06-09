e4a06536fe10f7e7055f87e9f575ae5fcda461ae
Markus Mäkelä <markus.makela@mariadb.com>
2021-06-09 09:58:29 +0300

    Make Maxrest error behavior configurable
    Not all errors should cause the test to fail.

--- system-test/maxtest/src/maxrest.cc
+++ system-test/maxtest/src/maxrest.cc
@@ -180,7 +180,11 @@ unique_ptr<json_t> MaxRest::curl(Command command, const string& path) const
 
 void MaxRest::raise(const std::string& message) const
 {
-    ++m_test.global_result;
+    if (m_fail_on_error)
+    {
+        ++m_test.global_result;
+    }
+
     throw runtime_error(message);
 }
 
