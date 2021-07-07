mariadb-corporation/MaxScale

commit efb6d2d1cae47f95498c52ef1e2a17ef690c52ef

Fix cluster usage in services
The servers weren't retrieved for the monitor if it wasn't changed at
runtime. This was most likely caused by the changes in the order in which
servers are added to services, previously the cluster was set before the
servers were assigned to the monitor.

server/core/service.cc
@@ -2016,6 +2016,11 @@ bool Service::check_update_user_account_manager(mxs::ProtocolModule* protocol_mo
 
 void Service::set_cluster(mxs::Monitor* monitor)
 {
+    for (auto a : monitor->servers())
+    {
+        m_data->targets.push_back(a->server);
+    }
+
     m_monitor = monitor;
 }
 
@@ -2025,13 +2030,8 @@ bool Service::change_cluster(mxs::Monitor* monitor)
 
     if (m_monitor == nullptr && m_data->targets.empty())
     {
-        for (auto a : monitor->servers())
-        {
-            m_data->targets.push_back(a->server);
-        }
-
+        set_cluster(monitor);
         targets_updated();
-        m_monitor = monitor;
         rval = true;
     }
 

commit 6cf7094d0729fd32689c4eeb2b37cd820b249217

Fix duplication of targets in a service
This caused twice the number of connections being
opened to all servers of ReadWriteSplit.

If a service defined 'cluster=...' in the config, the
list of targets (servers) was duplicated. This was caused by
commit efb6d2d1c. The commit eludes to the possibility that
Monitor::populate_services() can be called before servers have
been added to the Monitor (at startup). Either way this commit
is valid.

server/core/service.cc
@@ -1800,8 +1800,11 @@ void Service::remove_target(Service* target)
 
 void Service::add_target(SERVER* target)
 {
-    m_data->targets.push_back(target);
-    propagate_target_update();
+    if (std::find(begin(m_data->targets), end(m_data->targets), target) == end(m_data->targets))
+    {
+        m_data->targets.push_back(target);
+        propagate_target_update();
+    }
 }
 
 void Service::add_target(Service* target)