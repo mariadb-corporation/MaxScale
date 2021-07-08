d7bb9fcec394c1958097c3c18bbc9ca78e21ae89
Johan Wikman <johan.wikman@gmail.com>
2021-07-08 12:27:02 +0300

    6.1 Fix ..._runtime tests
    Without this change, mxs1929_filter_runtime and friends will
    crash, because the post_configure() function is not called at
    all, which means that 'backend_select_fct' of RWS will be NULL.

--- server/core/config2.cc
+++ server/core/config2.cc
@@ -749,7 +749,6 @@ bool Configuration::configure(json_t* json, std::set<std::string>* pUnrecognized
     mxb_assert(m_pSpecification->size() >= size());
 
     bool configured = true;
-    bool changed = false;
 
     map<string, mxs::ConfigParameters> nested_parameters;
 
@@ -777,7 +776,6 @@ bool Configuration::configure(json_t* json, std::set<std::string>* pUnrecognized
 
                 if (!json_equal(old_val, value))
                 {
-                    changed = true;
                     string message;
 
                     if (!pValue->set_from_json(value, &message))
@@ -805,7 +803,7 @@ bool Configuration::configure(json_t* json, std::set<std::string>* pUnrecognized
         }
     }
 
-    if (configured && (changed || !nested_parameters.empty()))
+    if (configured)
     {
         configured = post_configure(nested_parameters);
     }
