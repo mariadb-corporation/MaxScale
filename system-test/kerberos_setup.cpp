40463ae4d0b12bc269d16619097df68cb681f7fe
Markus Mäkelä <markus.makela@mariadb.com>
2021-07-12 14:15:27 +0300

    Reset timeouts in kerberos_setup
    The MDBCI operations can take longer than the timeout of 300
    seconds. Resetting the timeout between the commands should avoid the
    problem.

--- system-test/kerberos_setup.cpp
+++ system-test/kerberos_setup.cpp
@@ -9,19 +9,23 @@
 #include <iostream>
 #include <maxtest/testconnections.hh>
 
-int install_kerberos(std::string machine_name, std::string maria_version)
+int install_kerberos(TestConnections& test, std::string machine_name, std::string maria_version)
 {
-    int res = system((std::string("mdbci install_product --product kerberos_server ") + machine_name).c_str());
+    int res = system(("mdbci install_product --product kerberos_server " + machine_name).c_str());
+    test.reset_timeout();
     // Ignoring exit code becase in some versions of MariaDB gssapi is included into client/server
-    system((std::string("mdbci install_product --product plugin_gssapi_client --product-version ") + maria_version + " " + machine_name).c_str());
-    system((std::string("mdbci install_product --product plugin_gssapi_server --product-version ") + maria_version + " " + machine_name).c_str());
+    system(("mdbci install_product --product plugin_gssapi_client --product-version " + maria_version + " "
+            + machine_name).c_str());
+    test.reset_timeout();
+    system(("mdbci install_product --product plugin_gssapi_server --product-version " + maria_version + " "
+            + machine_name).c_str());
+    test.reset_timeout();
     return res;
 }
 
 int main(int argc, char* argv[])
 {
     TestConnections* Test = new TestConnections(argc, argv);
-    Test->reset_timeout();
     char str[1024];
     char str1[1024];
 
@@ -47,7 +51,7 @@ int main(int argc, char* argv[])
     {
         std::string machine_name = Test->get_mdbci_config_name() + "/" + Test->repl->mdbci_node_name(i);
         std::string maria_vrs = Test->repl->backend(i)->version_as_string();
-        install_kerberos(machine_name, maria_vrs);
+        install_kerberos(*Test, machine_name, maria_vrs);
         auto homedir = Test->repl->access_homedir(i);
         Test->repl->copy_to_node_legacy(str, homedir, i);
         sprintf(str1, "cp %s/krb5.conf /etc/", homedir);
@@ -72,7 +76,7 @@ int main(int argc, char* argv[])
     Test->tprintf("Instaling Kerberos server packages to Maxscale node\n");
     std::string machine_name = Test->get_mdbci_config_name() + "/" + Test->maxscale->node_name();
     std::string maria_vrs = Test->repl->backend(0)->version_as_string();
-    install_kerberos(machine_name, maria_vrs);
+    install_kerberos(*Test, machine_name, maria_vrs);
 
     Test->maxscale->ssh_node((char*)"rngd -r /dev/urandom -o /dev/random", true);
 
