a7a154cf39047d81478a21097f729d460b1107ac
Thien Ly <thien.ly@mariadb.com>
2021-08-13 14:55:34 +0300

    MXS-3636 Detach column list to a separated component
    The component uses to toggle the visibility of table
    columns can be detached into a component which can be later reused
    for filtering result by specific column.

--- maxgui/src/pages/QueryPage/ResultDataTable.vue
+++ maxgui/src/pages/QueryPage/ResultDataTable.vue
@@ -12,83 +12,12 @@
                 hide-details
             />
             <result-export :rows="filteredRows_wo_idx" :headers="visHeaders_wo_idx" />
-            <v-menu
-                allow-overflow
-                transition="slide-y-transition"
-                offset-y
-                left
-                content-class="mariadb-select-v-menu mariadb-select-v-menu--full-border"
-                :close-on-content-click="false"
-            >
-                <template v-slot:activator="{ on, attrs, value }">
-                    <v-btn
-                        x-small
-                        class="text-capitalize font-weight-medium"
-                        outlined
-                        depressed
-                        color="accent-dark"
-                        v-bind="attrs"
-                        v-on="on"
-                    >
-                        {{ $t('columns') }}
-                        <v-icon
-                            size="24"
-                            color="accent-dark"
-                            :class="{ 'column-list-toggle--active': value }"
-                        >
-                            arrow_drop_down
-                        </v-icon>
-                    </v-btn>
-                </template>
-                <v-list max-width="220px" :max-height="tableHeight - 20" class="column-list">
-                    <v-list-item class="px-0" dense>
-                        <v-text-field
-                            v-model="searchHeaderKeyword"
-                            dense
-                            outlined
-                            height="36"
-                            class="std column-list__search"
-                            :placeholder="$t('search')"
-                            hide-details
-                        />
-                    </v-list-item>
-                    <v-divider />
-
-                    <v-list-item class="px-2" dense link>
-                        <v-checkbox
-                            dense
-                            color="primary"
-                            class="pa-0 ma-0 checkbox d-flex align-center"
-                            hide-details
-                            :label="$t('selectAll')"
-                            :input-value="isAllHeaderChecked"
-                            @change="toggleAllHeaders"
-                        />
-                    </v-list-item>
-                    <v-divider />
-                    <v-list-item
-                        v-for="item in columnList"
-                        :key="`${item.name}_${item.index}`"
-                        class="px-2"
-                        dense
-                        link
-                    >
-                        <v-checkbox
-                            v-model="visHeaderIndexes"
-                            dense
-                            color="primary"
-                            class="pa-0 ma-0 checkbox d-flex align-center"
-                            :value="item.index"
-                            hide-details
-                        >
-                            <template v-slot:label>
-                                <truncate-string :text="item.name" />
-                            </template>
-                        </v-checkbox>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-
+            <column-list
+                v-model="visHeaderIdxs"
+                :label="$t('columns')"
+                :cols="tableHeaders"
+                :maxHeight="tableHeight - 20"
+            />
             <v-tooltip
                 top
                 transition="slide-y-transition"
@@ -146,10 +75,12 @@
  * Public License.
  */
 import ResultExport from './ResultExport'
+import ColumnList from './ColumnList.vue'
 export default {
     name: 'result-data-table',
     components: {
         'result-export': ResultExport,
+        'column-list': ColumnList,
     },
     props: {
         headers: { type: Array, require: true },
@@ -159,10 +90,9 @@ export default {
     },
     data() {
         return {
-            visHeaderIndexes: [],
+            visHeaderIdxs: [],
             searchCellKeyword: '',
             tableToolsHeight: 0,
-            searchHeaderKeyword: '',
             isVertTable: false,
         }
     },
@@ -178,14 +108,6 @@ export default {
         rowsWithIndex() {
             return this.rows.map((row, i) => [i + 1, ...row])
         },
-        columnList() {
-            let list = this.$help.lodash
-                .cloneDeep(this.tableHeaders)
-                .map((h, i) => ({ index: i, name: h }))
-            return list.filter(obj =>
-                this.$help.ciStrIncludes(`${obj.name}`, this.searchHeaderKeyword)
-            )
-        },
         filteredRows_wo_idx() {
             return this.filteredRows.map(row => row.filter((cell, i) => i !== 0))
         },
@@ -194,29 +116,18 @@ export default {
         },
         filteredRows() {
             const rows = this.rowsWithIndex.map(row =>
-                row.filter((cell, i) => this.visHeaderIndexes.includes(i))
+                row.filter((cell, i) => this.visHeaderIdxs.includes(i))
             )
             return rows.filter(row => this.$help.ciStrIncludes(`${row}`, this.searchCellKeyword))
         },
         visibleHeaders() {
-            return this.tableHeaders.filter((h, i) => this.visHeaderIndexes.includes(i))
-        },
-        isAllHeaderChecked() {
-            return this.visHeaderIndexes.length === this.tableHeaders.length
+            return this.tableHeaders.filter((h, i) => this.visHeaderIdxs.includes(i))
         },
     },
     activated() {
         this.setTableToolsHeight()
-        this.showAllHeaders()
     },
     methods: {
-        toggleAllHeaders(v) {
-            if (!v) this.visHeaderIndexes = []
-            else this.showAllHeaders()
-        },
-        showAllHeaders() {
-            this.visHeaderIndexes = this.tableHeaders.map((h, i) => i)
-        },
         setTableToolsHeight() {
             if (!this.$refs.tableTools) return
             this.tableToolsHeight = this.$refs.tableTools.clientHeight
@@ -231,34 +142,6 @@ export default {
 }
 .table-tools {
     width: 100%;
-}
-.column-list-toggle--active {
-    transform: rotate(180deg);
-    transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1), visibility 0s;
-}
-.column-list {
-    overflow-y: auto;
-    &__search {
-        ::v-deep .v-input__control {
-            fieldset {
-                border: none !important;
-            }
-        }
-    }
-}
-::v-deep.checkbox {
-    width: 100%;
-    height: 36px;
-    label {
-        height: 36px !important;
-        font-size: 0.875rem;
-        color: $navigation;
-        display: inline-block !important;
-        white-space: nowrap !important;
-        overflow: hidden !important;
-        text-overflow: ellipsis !important;
-        line-height: 36px;
-    }
 }
 .rotate-icon__vert {
     transform: rotate(90deg);
