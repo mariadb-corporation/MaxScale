5077ee55a8e54727273498e1c15417d75678dca1
Thien Ly <thien.ly@mariadb.com>
2021-08-23 14:20:43 +0300

    Fix `timeout` value type when creating a connection
    Value of `timeout` should be number otherwise it won't work.
    
    This add number modifier to typecast user input for timeout.

--- maxgui/src/pages/QueryPage/ConnectionDialog.vue
+++ maxgui/src/pages/QueryPage/ConnectionDialog.vue
@@ -153,7 +153,7 @@
                             {{ $t('timeout') }}
                         </label>
                         <v-text-field
-                            v-model="body.timeout"
+                            v-model.number="body.timeout"
                             type="number"
                             class="std error--text__bottom"
                             name="timeout"
