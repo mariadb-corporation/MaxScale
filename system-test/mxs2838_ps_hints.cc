25b583b41850ec87e65c3c69e86c23fe0ed0ebe0
Markus Mäkelä <markus.makela@mariadb.com>
2021-10-14 09:54:34 +0300

    MXS-3812: Reset current PS ID when reply is complete
    The value must also be reset when the prepared statement is
    successful. Otherwise unrelated errors can cause the hint to be removed.
    
    Added a test case that reproduces the problem and verifies that it is
    fixed.

--- system-test/mxs2838_ps_hints.cc
+++ system-test/mxs2838_ps_hints.cc
@@ -48,6 +48,46 @@ std::string test_one_hint(TestConnections& test, std::string hint)
     return buffer;
 }
 
+void test_unrelated_failure(TestConnections& test, const std::string& master_id)
+{
+    Connection conn = test.maxscale->rwsplit();
+    test.expect(conn.connect(), "Connection to MaxScale failed: %s", conn.error());
+
+    MYSQL_STMT* stmt = conn.stmt();
+    std::string query = "SELECT @@server_id -- maxscale route to master";
+
+    test.expect(mysql_stmt_prepare(stmt, query.c_str(), query.size()) == 0,
+                "PREPARE failed: %s", conn.error());
+
+    // Wait for a while to make sure the prepared statement has finished on all servers.
+    sleep(2);
+
+    // Execute SQL that will result in an error
+    conn.query("This will cause an error to be generated");
+
+    test.expect(mysql_stmt_execute(stmt) == 0,
+                "EXECUTE failed: %s", conn.error());
+
+    char buffer[100] = "";
+    my_bool err = false;
+    my_bool isnull = false;
+    MYSQL_BIND bind = {};
+
+    bind.buffer_length = sizeof(buffer);
+    bind.buffer = buffer;
+    bind.error = &err;
+    bind.is_null = &isnull;
+
+    test.expect(mysql_stmt_bind_result(stmt, &bind) == 0,
+                "Failed to bind result: %s", mysql_stmt_error(stmt));
+    test.expect(mysql_stmt_fetch(stmt) == 0,
+                "Failed to fetch result: %s", mysql_stmt_error(stmt));
+
+    test.expect(buffer == master_id, "Expected master's ID %s, got %s.", master_id.c_str(), buffer);
+
+    mysql_stmt_close(stmt);
+}
+
 int main(int argc, char* argv[])
 {
     TestConnections test(argc, argv);
@@ -65,6 +105,9 @@ int main(int argc, char* argv[])
     expect_eq(master_id, "route to server server1");
     expect_eq(slave_id, "route to slave");
     expect_eq(slave_id, "route to server server2");
+
+    // MXS-3812
+    test_unrelated_failure(test, master_id);
 
     return test.global_result;
 }
