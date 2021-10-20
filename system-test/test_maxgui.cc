703f6c94c7178b4490e089c9301962349321bf6b
Markus Mäkelä <markus.makela@mariadb.com>
2021-10-20 14:47:21 +0300

    Fix test_maxgui
    The argumets to src seem to now be quoted. Looking for just the /js/
    directory should be adequate.

--- system-test/test_maxgui.cc
+++ system-test/test_maxgui.cc
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
     res = test.maxscale->ssh_output("curl -s -f localhost:8989/index.html");
 
     test.expect(res.rc == 0, "Root resource should serve the GUI main page.");
-    test.expect(res.output.find("src=/js/") != std::string::npos,
+    test.expect(res.output.find("/js/") != std::string::npos,
                 "GUI main page should load javascript: %s", res.output.c_str());
 
     return test.global_result;
