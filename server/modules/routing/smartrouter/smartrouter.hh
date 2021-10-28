1bc5737f7bb436d15ce54a601a25d108db998c76
Markus Mäkelä <markus.makela@mariadb.com>
2021-10-28 16:01:12 +0300

    Fix smartrouter capabilities
    The capabilities it declared were different.

--- server/modules/routing/smartrouter/smartrouter.hh
+++ server/modules/routing/smartrouter/smartrouter.hh
@@ -39,6 +39,9 @@ class SmartRouter : public mxs::Router
 public:
     ~SmartRouter();
 
+    static constexpr uint64_t CAPABILITIES = RCAP_TYPE_TRANSACTION_TRACKING | RCAP_TYPE_STMT_INPUT
+        | RCAP_TYPE_STMT_OUTPUT | RCAP_TYPE_QUERY_CLASSIFICATION | RCAP_TYPE_OLD_PROTOCOL;
+
     class Config : public config::Configuration
     {
     public:
