e8f596e0a38aca061e8e8a7df7658dc0c1290bd4
Esa Korhonen <esa.korhonen@mariadb.com>
2021-10-29 17:51:27 +0300

    MXS-3843 Update inih-library

--- inih/tests/unittest.c
+++ inih/tests/unittest.c
@@ -5,10 +5,8 @@ redirected to an output file (baseline_*.txt) and checked into the Subversion
 repository. This baseline file is the test output, so the idea is to check it
 once, and if it changes -- look at the diff and see which tests failed.
 
-Here's how I produced the two baseline files (with Tiny C Compiler):
-
-tcc -DINI_ALLOW_MULTILINE=1 ../ini.c -run unittest.c > baseline_multi.txt
-tcc -DINI_ALLOW_MULTILINE=0 ../ini.c -run unittest.c > baseline_single.txt
+See unittest.bat and unittest.sh for how to run this (with tcc and gcc,
+respectively).
 
 */
 
@@ -20,16 +18,29 @@ tcc -DINI_ALLOW_MULTILINE=0 ../ini.c -run unittest.c > baseline_single.txt
 int User;
 char Prev_section[50];
 
+#if INI_HANDLER_LINENO
+int dumper(void* user, const char* section, const char* name,
+           const char* value, int lineno)
+#else
 int dumper(void* user, const char* section, const char* name,
            const char* value)
+#endif
 {
-    User = (int)user;
-    if (strcmp(section, Prev_section)) {
+    User = *((int*)user);
+    if (!name || strcmp(section, Prev_section)) {
         printf("... [%s]\n", section);
         strncpy(Prev_section, section, sizeof(Prev_section));
         Prev_section[sizeof(Prev_section) - 1] = '\0';
     }
-    printf("... %s=%s;\n", name, value);
+    if (!name) {
+        return 1;
+    }
+
+#if INI_HANDLER_LINENO
+    printf("... %s%s%s;  line %d\n", name, value ? "=" : "", value ? value : "", lineno);
+#else
+    printf("... %s%s%s;\n", name, value ? "=" : "", value ? value : "");
+#endif
 
     return strcmp(name, "user")==0 && strcmp(value, "parse_error")==0 ? 0 : 1;
 }
@@ -39,7 +50,7 @@ void parse(const char* fname) {
     int e;
 
     *Prev_section = '\0';
-    e = ini_parse(fname, dumper, (void*)u);
+    e = ini_parse(fname, dumper, &u);
     printf("%s: e=%d user=%d\n", fname, e, User);
     u++;
 }
@@ -54,5 +65,7 @@ int main(void)
     parse("multi_line.ini");
     parse("bad_multi.ini");
     parse("bom.ini");
+    parse("duplicate_sections.ini");
+    parse("no_value.ini");
     return 0;
 }
