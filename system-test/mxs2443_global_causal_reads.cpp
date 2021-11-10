6afbcbd645329a84dc9a480a426ccc5e4deac841
Markus Mäkelä <markus.makela@mariadb.com>
2021-11-10 07:58:25 +0200

    MXS-3856: Extend causal_reads=global test case
    The test reproduces the problem and verifies that the fix works.

--- system-test/mxs2443_global_causal_reads.cpp
+++ system-test/mxs2443_global_causal_reads.cpp
@@ -6,6 +6,43 @@
 
 #include <maxtest/testconnections.hh>
 
+void readonly_trx_test(TestConnections& test)
+{
+    // Create a table and insert some data into it
+    auto first = test.maxscale->rwsplit();
+    test.expect(first.connect(), "Connection should work");
+    first.query("CREATE OR REPLACE TABLE test.t1(id INT)");
+    first.query("INSERT INTO test.t1 VALUES (1)");
+
+    // Open a second connection and start a read-only transaction
+    auto second = test.maxscale->rwsplit();
+    test.expect(second.connect(), "Connection should work");
+    second.query("START TRANSACTION READ ONLY");
+
+    // Stop the monitor and stop replication on all the slave servers
+    test.maxctrl("stop monitor MySQL-Monitor");
+    test.repl->execute_query_all_nodes("STOP SLAVE");
+
+    // Insert a row to generate the next GTID
+    first.query("INSERT INTO test.t1 VALUES (2)");
+
+    bool ok = second.query("SELECT COUNT(*) FROM test.t1");
+    std::string err = second.error();
+    test.expect(!ok, "Causal read should fail");
+    test.expect(err.find("Causal read timed out") != std::string::npos,
+                "Wrong error message: %s", err.c_str());
+
+    // Resume replication, query should now work
+    test.repl->execute_query_all_nodes("START SLAVE");
+
+    ok = second.query("SELECT COUNT(*) FROM test.t1");
+    test.expect(ok, "Causal read should work: %s", second.error());
+    second.query("COMMIT");
+
+    // Cleanup
+    first.query("DROP TABLE test.t1");
+}
+
 int main(int argc, char** argv)
 {
     TestConnections::require_repl_version("10.3.8");
@@ -47,6 +84,9 @@ int main(int argc, char** argv)
     test.expect(conn.query("DROP TABLE test.t1"),
                 "Table creation should work: %s", conn.error());
     conn.disconnect();
+
+    // MXS-3856: Errors with causal_reads and read-only transactions
+    readonly_trx_test(test);
 
     return test.global_result;
 }
