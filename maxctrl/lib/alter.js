9ce4083bb0bdbeb9ca83e88dbd5329f5767a79c5
Markus Mäkelä <markus.makela@mariadb.com>
2021-11-17 14:19:57 +0200

    MXS-3849: Fix nested parameters sent via MaxCtrl
    Using lodash to assign the parsed value solves the problem.
    
    Technically this opens up some new problems as now the array notation is
    interpreted not as a name but as an array type parameter. In practice this
    isn't a problem as no parameter uses square brackets in its name.

--- maxctrl/lib/alter.js
+++ maxctrl/lib/alter.js
@@ -82,7 +82,7 @@ function updateParams(host, resource, key, value, extra) {
   var params = {};
 
   keys.forEach((k, i) => {
-    params[k] = parseValue(values[i]);
+    _.set(params, k, parseValue(values[i]));
   });
 
   return updateValue(host, resource, "data.attributes.parameters", params);
