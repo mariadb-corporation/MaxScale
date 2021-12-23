60d436d71c7b1569bb768df2b93d06d122e5104f
Johan Wikman <johan.wikman@gmail.com>
2021-12-23 13:39:41 +0200

    MXS-3925 Add grants to created users
    The user management is somewhat fragile. Depending on the grants
    of the currently logged in user, the creation of the user may fail,
    or it may succeed but setting the grants may fail, or the deletion
    of the created user, once it is noticed that setting the grants
    failed, may fail.

--- server/modules/protocol/NoSQL/commands/user_management.hh
+++ server/modules/protocol/NoSQL/commands/user_management.hh
@@ -95,79 +95,31 @@ public:
 
     using SingleCommand::SingleCommand;
 
+    ~CreateUser()
+    {
+        if (m_dcid)
+        {
+            worker().cancel_delayed_call(m_dcid);
+            m_dcid = 0;
+        }
+    }
+
     State translate(mxs::Buffer&& mariadb_response, GWBUF** ppNoSQL_response) override final
     {
-        ComResponse response(mariadb_response.data());
+        State state = State::READY;
 
-        DocumentBuilder doc;
-
-        switch (response.type())
+        switch (m_action)
         {
-        case ComResponse::OK_PACKET:
-            {
-                auto& um = m_database.context().um();
-
-                vector<uint8_t> salt = crypto::create_random_bytes(scram::SERVER_SALT_SIZE);
-                string salt_b64 = mxs::to_base64(salt);
-
-                if (um.add_user(m_scope, m_user, m_pwd, salt_b64, m_roles))
-                {
-                    doc.append(kvp("ok", 1));
-                }
-                else
-                {
-                    ostringstream ss;
-                    ss << "Could add user '" << m_user << "' to the MariaDB database, "
-                       << "but could not add the user to the local database " << um.path() << ".";
-
-                    string message = ss.str();
-
-                    MXS_ERROR("%s", message.c_str());
-
-                    throw SoftError(message, error::INTERNAL_ERROR);
-                }
-            }
+        case Action::CREATE:
+            state = translate_create(std::move(mariadb_response), ppNoSQL_response);
             break;
 
-        case ComResponse::ERR_PACKET:
-            {
-                ComERR err(response);
-
-                switch (err.code())
-                {
-                case ER_CANNOT_USER:
-                    {
-                        // We assume it's because the user exists.
-                        ostringstream ss;
-                        ss << "User \"" << m_user << "\" already exists";
-
-                        throw SoftError(ss.str(), error::LOCATION51003);
-                    }
-                    break;
-
-                case ER_SPECIFIC_ACCESS_DENIED_ERROR:
-                    {
-                        ostringstream ss;
-                        ss << "not authorized on " << m_database.name() << " to execute command "
-                           << bsoncxx::to_json(m_doc);
-
-                        throw SoftError(ss.str(), error::UNAUTHORIZED);
-                    }
-                    break;
-
-                default:
-                    throw MariaDBError(err);
-                }
-            }
+        case Action::DROP:
+            state = translate_drop(std::move(mariadb_response), ppNoSQL_response);
             break;
-
-        default:
-            mxb_assert(!true);
-            throw_unexpected_packet();
         }
 
-        *ppNoSQL_response = create_response(doc.extract());
-        return State::READY;
+        return state;
     }
 
 protected:
@@ -223,17 +175,255 @@ protected:
 
     string generate_sql() override
     {
-        ostringstream sql;
+        string user = "'" + m_scope + "." + m_user + "'@'%'";
+        string pwd(m_pwd.data(), m_pwd.length());
 
-        sql << "CREATE USER '" << m_scope << "." << m_user << "'@'%' IDENTIFIED BY '" << m_pwd << "'";
+        m_statements.push_back("CREATE USER " + user + " IDENTIFIED BY '" + pwd + "'");
 
-        return sql.str();
+        for (const auto& kv : m_role_by_dbs)
+        {
+            const string& db = kv.first;
+            const role::Id role_id = kv.second;
+
+            string scope = (db == "admin" ? "*" : db);
+
+            vector<string> privileges;
+
+            switch (role_id)
+            {
+            case role::Id::DB_ADMIN:
+                privileges.push_back("ALTER");
+                privileges.push_back("CREATE");
+                privileges.push_back("DROP");
+                break;
+
+            case role::Id::READ_WRITE:
+                privileges.push_back("DELETE");
+                privileges.push_back("INSERT");
+                privileges.push_back("UPDATE");
+            case role::Id::READ:
+                privileges.push_back("SELECT");
+                break;
+
+            default:
+                mxb_assert(!true);
+            }
+
+            string grant = "GRANT " + mxb::join(privileges) + " ON " + scope + ".* to " + user;
+
+            m_statements.push_back(grant);
+        }
+
+        return mxb::join(m_statements, ";");
     }
 
 private:
+    void check_create(const ComResponse& response)
+    {
+        switch (response.type())
+        {
+        case ComResponse::OK_PACKET:
+            break;
+
+        case ComResponse::ERR_PACKET:
+            {
+                ComERR err(response);
+
+                switch (err.code())
+                {
+                case ER_CANNOT_USER:
+                    {
+                        // We assume it's because the user exists.
+                        ostringstream ss;
+                        ss << "User \"" << m_user << "\" already exists";
+
+                        throw SoftError(ss.str(), error::LOCATION51003);
+                    }
+                    break;
+
+                case ER_SPECIFIC_ACCESS_DENIED_ERROR:
+                    {
+                        ostringstream ss;
+                        ss << "not authorized on " << m_database.name() << " to execute command "
+                           << bsoncxx::to_json(m_doc);
+
+                        throw SoftError(ss.str(), error::UNAUTHORIZED);
+                    }
+                    break;
+
+                default:
+                    throw MariaDBError(err);
+                }
+            }
+            break;
+
+        default:
+            mxb_assert(!true);
+            throw_unexpected_packet();
+        }
+    }
+
+    bool check_grant(const ComResponse& response, int i)
+    {
+        bool success = true;
+
+        switch (response.type())
+        {
+        case ComResponse::OK_PACKET:
+            break;
+
+        case ComResponse::ERR_PACKET:
+            {
+                ComERR err(response);
+
+                MXS_ERROR("Could create user '%s.%s'@'%%', but granting access with the "
+                          "statement \"%s\" failed with: (%d) \"%s\". Will now attempt to "
+                          "DROP the user.",
+                          m_scope.c_str(),
+                          m_user.c_str(),
+                          m_statements[i].c_str(),
+                          err.code(),
+                          err.message().c_str());
+
+                success = false;
+            }
+            break;
+
+        default:
+            mxb_assert(!true);
+            throw_unexpected_packet();
+        }
+
+        return success;
+    }
+
+    State translate_create(mxs::Buffer&& mariadb_response, GWBUF** ppNoSQL_response)
+    {
+        State state = State::READY;
+
+        uint8_t* pData = mariadb_response.data();
+        uint8_t* pEnd = pData + mariadb_response.length();
+
+        size_t i = 0;
+        DocumentBuilder doc;
+
+        bool success = true;
+        while ((pData < pEnd) && success)
+        {
+            ComResponse response(&pData);
+
+            if (i == 0)
+            {
+                check_create(response);
+            }
+            else
+            {
+                success = check_grant(response, i);
+            }
+
+            ++i;
+        }
+
+        if (success)
+        {
+            mxb_assert(i == m_statements.size());
+
+            auto& um = m_database.context().um();
+
+            vector<uint8_t> salt = crypto::create_random_bytes(scram::SERVER_SALT_SIZE);
+            string salt_b64 = mxs::to_base64(salt);
+
+            if (um.add_user(m_scope, m_user, m_pwd, salt_b64, m_roles))
+            {
+                doc.append(kvp("ok", 1));
+            }
+            else
+            {
+                ostringstream ss;
+                ss << "Could add user '" << m_user << "' to the MariaDB database, "
+                   << "but could not add the user to the local database " << um.path() << ".";
+
+                string message = ss.str();
+
+                MXS_ERROR("%s", message.c_str());
+
+                throw SoftError(message, error::INTERNAL_ERROR);
+            }
+
+            *ppNoSQL_response = create_response(doc.extract());
+            state = State::READY;
+        }
+        else
+        {
+            // Ok, so GRANTing access failed. To make everything simpler for everyone, will
+            // now attempt to DROP the user.
+
+            state = State::BUSY;
+
+            m_action = Action::DROP;
+            m_dcid = worker().delayed_call(0, [this](Worker::Call::action_t action) {
+                    m_dcid = 0;
+
+                    if (action == Worker::Call::EXECUTE)
+                    {
+                        string user = "'" + m_scope + "." + m_user + "'@'%'";
+
+                        ostringstream sql;
+                        sql << "DROP USER '" << m_scope << "." << m_user << "'@'%'";
+
+                        send_downstream(sql.str());
+                    }
+
+                    return false;
+                });
+        }
+
+        return state;
+    }
+
+    State translate_drop(mxs::Buffer&& mariadb_response, GWBUF** ppNoSQL_response)
+    {
+        ComResponse response(mariadb_response.data());
+
+        switch (response.type())
+        {
+        case ComResponse::OK_PACKET:
+            {
+                ostringstream ss;
+                ss << "Could create MariaDB user '" << m_scope << "." << m_user << "'@'%', but "
+                   << "could not give the required GRANTs. The current used does not have "
+                   << "the required privileges. See the MaxScale log for more details.";
+
+                throw SoftError(ss.str(), error::UNAUTHORIZED);
+            }
+            break;
+
+        case ComResponse::ERR_PACKET:
+            {
+                ComERR err(response);
+
+                ostringstream ss;
+                ss << "Could create MariaDB user '" << m_scope << "." << m_user << "'@'%', but "
+                   << "could not give the required GRANTs and the subsequent attempt to delete "
+                   << "the user failed: (" << err.code() << ") \"" << err.message() << "\". "
+                   << "You should now DROP the user manually.";
+
+                throw SoftError(ss.str(), error::INTERNAL_ERROR);
+            }
+            break;
+
+        default:
+            mxb_assert(!true);
+            throw_unexpected_packet();
+        }
+
+        mxb_assert(!true);
+        return State::READY;
+    }
+
     void add_role(const string& db, role::Id role_id)
     {
-        m_role_ids.push_back(make_pair(db, role_id));
+        m_role_by_dbs.push_back(make_pair(db, role_id));
     }
 
     void check_role(const string_view& role_name, const string& db)
@@ -316,11 +506,20 @@ private:
     }
 
 private:
+    enum class Action
+    {
+        CREATE,
+        DROP
+    };
+
+    Action                        m_action = Action::CREATE;
     string                        m_scope;
     string                        m_user;
     string_view                   m_pwd;
     bsoncxx::array::view          m_roles;
-    vector<pair<string,role::Id>> m_role_ids;
+    vector<pair<string,role::Id>> m_role_by_dbs;
+    vector<string>                m_statements;
+    uint32_t                      m_dcid = { 0 };
 };
 
 // https://docs.mongodb.com/v4.4/reference/command/dropAllUsersFromDatabase/
