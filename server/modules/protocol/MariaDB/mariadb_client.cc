de4653ffa111c225557ca63c01b145b30da9c2c4
Markus Mäkelä <markus.makela@mariadb.com>
2022-01-24 13:45:24 +0200

    MXS-3955: Use DEPRECATE_EOF with 5.7.5 or newer
    As the DEPRECATE_EOF and session tracking capabilities were added in MySQL
    5.7.5, they should not be advertised if the server in question doesn't
    support them.

--- server/modules/protocol/MariaDB/mariadb_client.cc
+++ server/modules/protocol/MariaDB/mariadb_client.cc
@@ -536,9 +536,11 @@ bool MariaDBClientConnection::send_server_handshake()
         mxb_assert((caps & GW_MYSQL_CAPABILITIES_DEPRECATE_EOF) == 0);
     }
 
-    if (cap_types == CapTypes::XPAND)
+    if (cap_types == CapTypes::XPAND || min_version < 50705)
     {
-        // XPand doesn't support SESSION_TRACK or DEPRECATE_EOF
+        // The DEPRECATE_EOF and session tracking were added in MySQL 5.7, anything older than that shouldn't
+        // advertise them. This includes XPand: it doesn't support SESSION_TRACK or DEPRECATE_EOF as it's
+        // MySQL 5.1 compatible on the protocol layer.
         caps &= ~(GW_MYSQL_CAPABILITIES_SESSION_TRACK | GW_MYSQL_CAPABILITIES_DEPRECATE_EOF);
     }
 
