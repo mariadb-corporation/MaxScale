fa95c62c5fc404b692e93eea1e21e7bd5a57609e
Thien Ly <thien.ly@mariadb.com>
2022-02-04 16:13:09 +0200

    MXS-3961 Show replication status when hovering on a slave node
    This uses `rep-tooltip` to show replication status for `cluster-node`

--- maxgui/src/pages/Visualization/ClusterNode.vue
+++ maxgui/src/pages/Visualization/ClusterNode.vue
@@ -1,70 +1,69 @@
 <template>
-    <v-card outlined class="server-node" width="273" height="88">
-        <div
-            class="d-flex align-center flex-row node-title-wrapper px-2 py-1"
-            :class="[droppableTargets.includes(node.id) ? 'server-node__droppable' : '']"
-        >
-            <icon-sprite-sheet
-                size="13"
-                class="mr-1 server-state-icon"
-                :frame="$help.serverStateIcon($typy(node, 'data.state').safeString)"
-            >
-                status
-            </icon-sprite-sheet>
-            <div class="text-truncate">
-                <router-link :to="`/dashboard/servers/${node.id}`" class="rsrc-link">
-                    {{ $typy(node, 'data.title').safeString }}
-                </router-link>
-            </div>
-            <v-spacer />
-            <div class="button-container">
-                <!--TODO: open a dialog to config the node -->
-                <v-btn small class="ml-2 gear-btn" icon>
-                    <v-icon
-                        size="16"
-                        :color="droppableTargets.includes(node.id) ? 'background' : 'primary'"
-                    >
-                        $vuetify.icons.settings
-                    </v-icon>
-                </v-btn>
-            </div>
-        </div>
-        <v-divider />
-        <div class="d-flex justify-center flex-column node-text-wrapper px-2 py-1">
-            <div class="d-flex flex-row flex-grow-1">
-                <span>{{ node.data.state }}</span>
-                <v-spacer />
-                <v-tooltip
-                    top
-                    transition="slide-y-transition"
-                    content-class="shadow-drop color text-navigation py-1 px-4"
+    <rep-tooltip
+        :serverInfo="$typy(node, 'data.server_info').safeArray"
+        :disabled="$typy(node, 'data.isMaster').safeBoolean"
+        :isMaster="false"
+        :nudge-top="275 / 2"
+        :offset-x="true"
+    >
+        <template v-slot:activator="{ on }">
+            <v-card outlined class="server-node" width="273" height="88" v-on="on">
+                <div
+                    class="d-flex align-center flex-row node-title-wrapper px-2 py-1"
+                    :class="[droppableTargets.includes(node.id) ? 'server-node__droppable' : '']"
                 >
-                    <template v-slot:activator="{ on }">
+                    <icon-sprite-sheet
+                        size="13"
+                        class="mr-1 server-state-icon"
+                        :frame="$help.serverStateIcon($typy(node, 'data.state').safeString)"
+                    >
+                        status
+                    </icon-sprite-sheet>
+                    <div class="text-truncate">
+                        <router-link :to="`/dashboard/servers/${node.id}`" class="rsrc-link">
+                            {{ $typy(node, 'data.title').safeString }}
+                        </router-link>
+                    </div>
+                    <v-spacer />
+                    <div class="button-container">
+                        <!--TODO: open a dialog to config the node -->
+                        <v-btn small class="ml-2 gear-btn" icon>
+                            <v-icon
+                                size="16"
+                                :color="
+                                    droppableTargets.includes(node.id) ? 'background' : 'primary'
+                                "
+                            >
+                                $vuetify.icons.settings
+                            </v-icon>
+                        </v-btn>
+                    </div>
+                </div>
+                <v-divider />
+                <div class="d-flex justify-center flex-column node-text-wrapper px-2 py-1">
+                    <div class="d-flex flex-row flex-grow-1">
+                        <span>{{ node.data.state }}</span>
+                        <v-spacer />
                         <span
                             v-if="
                                 !$typy(node.data).isEmptyObject &&
                                     !$typy(node, 'data.isMaster').safeBoolean
                             "
                             class="ml-1 color text-field-text"
-                            v-on="on"
                         >
                             (+{{ sbm }}s)
                         </span>
-                    </template>
-                    <span>
-                        <!-- TODO: Show Replication status by re-using rep-tooltip -->
-                        {{ $t('repLag') }}
-                    </span>
-                </v-tooltip>
-            </div>
-            <div class="d-flex flex-grow-1">
-                <span class="text-capitalize">{{ $tc('connections', 2) }}:</span>
-                <span class="ml-1">
-                    {{ node.data.connections }}
-                </span>
-            </div>
-        </div>
-    </v-card>
+                    </div>
+                    <div class="d-flex flex-grow-1">
+                        <span class="text-capitalize">{{ $tc('connections', 2) }}:</span>
+                        <span class="ml-1">
+                            {{ node.data.connections }}
+                        </span>
+                    </div>
+                </div>
+            </v-card>
+        </template>
+    </rep-tooltip>
 </template>
 
 <script>
