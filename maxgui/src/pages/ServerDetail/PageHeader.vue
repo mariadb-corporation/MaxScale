8ac883606388b3c2b4b4bafb40eb605a9859db48
Johan Wikman <johan.wikman@gmail.com>
2022-02-11 15:46:57 +0200

    Prepare for merge of 2.5

--- maxgui/src/pages/ServerDetail/PageHeader.vue
+++ maxgui/src/pages/ServerDetail/PageHeader.vue
@@ -140,7 +140,7 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
  *
- * Change Date: 2026-01-04
+ * Change Date: 2026-02-11
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2 or later of the General
