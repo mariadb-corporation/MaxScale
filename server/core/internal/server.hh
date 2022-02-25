2b5c33342b535e48093c47ad4bd5392dafedbffa
Markus Mäkelä <markus.makela@mariadb.com>
2022-02-25 09:47:40 +0200

    MXS-3808: Don't flush pool for REST API requests
    This is very bad for the REST API performance as it causes the workers
    to iterate over all the pooled connections. In addition to this, under
    heavy workloads the workers might be slow to respond which further slows
    down the requests. Since the only reason why it was done in the first
    place was to make tests pass, an alternative for it can be added later
    on.

--- server/core/internal/server.hh
+++ server/core/internal/server.hh
@@ -431,15 +431,6 @@ private:
 
     mxs::WorkerGlobal<std::unordered_map<uint32_t, uint64_t>> m_gtids;
 
-    /**
-     * @brief Clean up any stale persistent connections
-     *
-     * This function purges any stale persistent connections from @c server.
-     *
-     * @param server Server to clean up
-     */
-    void cleanup_persistent_connections() const;
-
     bool configure_ssl(const mxs::ConfigParameters& params);
 };
 
