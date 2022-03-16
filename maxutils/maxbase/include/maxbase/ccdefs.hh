2c618fd8fd94f75d3c56429d9905d43508d38ac6
Johan Wikman <johan.wikman@mariadb.com>
2022-03-16 10:14:54 +0200

    MXS-4049 Remove maxbase/cdefs.h

--- maxutils/maxbase/include/maxbase/ccdefs.hh
+++ maxutils/maxbase/include/maxbase/ccdefs.hh
@@ -17,13 +17,58 @@
  * @file ccdefs.hh
  *
  * This file is to be included first by all C++ headers.
+ *
+ * This file has several purposes.
+ *
+ * - Its purpose is the same as that of x86_64-linux-gnu/sys/cdefs.h, that is,
+ *   it defines things that are dependent upon the compilation environment.
+ * - Since this *must* be included as the very first header by all other MaxScale
+ *   headers, it allows you to redefine things globally, should that be necessary,
+ *   for instance, when debugging something.
+ * - Global constants applicable across the line can be defined here.
  */
 
 #if !defined (__cplusplus)
 #error This file is only to be included by C++ code.
 #endif
 
-#include <maxbase/cdefs.h>
+#undef _GNU_SOURCE
+#define _GNU_SOURCE 1
+
+#undef OPENSSL_THREAD_DEFINES
+#define OPENSSL_THREAD_DEFINES 1
+
+/**
+ * Fix compile errors for PRId64
+ * in Centos 6
+ */
+#ifndef __STDC_FORMAT_MACROS
+# define __STDC_FORMAT_MACROS
+#endif
+
+/**
+ * Needs to be defined for INT32_MAX and others
+ */
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+
+/**
+ * Define function attributes
+ *
+ * The function attributes are compiler specific.
+ */
+#ifdef __GNUC__
+#define mxb_attribute(a) __attribute__ (a)
+#else
+#define mxb_attribute(a)
+#endif
+
+/**
+ * COMMON INCLUDE FILES
+ */
+#include <cstddef>
+#include <cstdint>
 #include <exception>
 #include <new>
 #include <string>
