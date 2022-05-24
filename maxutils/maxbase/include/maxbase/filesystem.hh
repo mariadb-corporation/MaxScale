640f9cb9ecb57cac56454608367267c73bde73d1
Markus Mäkelä <markus.makela@mariadb.com>
2022-05-24 07:04:15 +0300

    MXS-2662: Move file read/write code into maxbase
    Both the REST API code as well as the KeyManager both need to read files
    from disk. Given that this is a relatively common pattern, moving them
    into maxbase makes sense.
    
    The mxb::load_file function takes the container type as one of its
    template arguments to allow it to be used with both binary files
    (usually `std::vector<uint8_t>`) and text files (almost always
    `std::string`).
    
    The `mxb::save_file` function is a modified version of the code found in
    config_runtime.cc.  The writing of the file does not need to be a
    template as a pointer to the data and its size can be easily derived
    from the data. An overload for strings is needed as writing strings into
    files seems to be a very common pattern in MaxScale.

---
+++ maxutils/maxbase/include/maxbase/filesystem.hh
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-05-03
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#pragma once
+
+#include <maxbase/ccdefs.hh>
+#include <maxbase/format.hh>
+#include <maxbase/string.hh>
+
+#include <fstream>
+
+namespace maxbase
+{
+
+/**
+ * Load a file from disk
+ *
+ * @param file The path to the file to load
+ *
+ * @return The file contents if the file was loaded successfully or an error message if it wasn't
+ */
+template<class Container>
+std::pair<Container, std::string> load_file(std::string file)
+{
+    static_assert(sizeof(typename Container::iterator::value_type) == sizeof(char));
+    std::string err;
+    Container data;
+    std::ifstream infile(file, std::ios_base::ate | std::ios_base::binary);
+
+    if (infile)
+    {
+        data.resize(infile.tellg());
+        infile.seekg(0, std::ios_base::beg);
+
+        if (!infile.read(reinterpret_cast<char*>(data.data()), data.size()))
+        {
+            err = mxb::string_printf("Failed to read from file '%s': %d, %s",
+                                     file.c_str(), errno, mxb_strerror(errno));
+            data.clear();
+        }
+    }
+    else if (errno != ENOENT)
+    {
+        err = mxb::string_printf("Failed to open file '%s': %d, %s",
+                                 file.c_str(), errno, mxb_strerror(errno));
+    }
+
+    return {data, err};
+}
+
+/**
+ * Atomically save a file ok disk
+ *
+ * The atomicity is achieved by writing out the data into a temporary file and then renaming it.
+ *
+ * @param file The path where the file is saved
+ * @param ptr  Pointer to data
+ * @param size Size of the data
+ *
+ * @return An empty string if the file was saved successfully or an error message if it wasn't
+ */
+std::string save_file(std::string file, const void* ptr, size_t size);
+
+inline std::string save_file(std::string file, const std::string& str)
+{
+    return save_file(file, str.data(), str.size());
+}
+}
---
+++ maxutils/maxbase/src/filesystem.cc
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-05-03
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <maxbase/filesystem.hh>
+
+#include <cstdio>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+namespace maxbase
+{
+
+std::string save_file(std::string file, const void* ptr, size_t size)
+{
+    std::string err;
+    std::string tmp = file + "XXXXXX";
+    int fd = mkstemp(tmp.data());
+
+    if (fd == -1)
+    {
+        err = mxb::string_printf("Failed to open temporary file '%s': %d, %s",
+                                 tmp.c_str(), errno, mxb_strerror(errno));
+    }
+    else
+    {
+        if (write(fd, ptr, size) == -1)
+        {
+            err = mxb::string_printf("Write to file '%s' failed: %d, %s",
+                                     tmp.c_str(), errno, mxb_strerror(errno));
+        }
+        else if (rename(tmp.c_str(), file.c_str()) == -1)
+        {
+            err = mxb::string_printf("Failed to rename '%s' to '%s': %d, %s",
+                                     tmp.c_str(), file.c_str(), errno, mxb_strerror(errno));
+        }
+
+        close(fd);
+
+        if (!err.empty())
+        {
+            // Remove the temporary file in case we failed to write to it.
+            remove(tmp.c_str());
+        }
+    }
+
+    return err;
+}
+}
