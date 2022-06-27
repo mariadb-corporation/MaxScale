52873d9a57b40cb32339ca11226e6e6cbe23a2fd
Niclas Antti <niclas.antti@mariadb.com>
2022-06-27 15:59:30 +0300

    MXS-3394 Add missing copyright notices

--- server/modules/filter/rewritefilter/rewritesql.cc
+++ server/modules/filter/rewritefilter/rewritesql.cc
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2022 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl.
+ *
+ * Change Date: 2026-06-06
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
 #include "rewritesql.hh"
 #include <iostream>
 #include <sstream>
