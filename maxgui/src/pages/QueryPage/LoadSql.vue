5afd75cc2489c902863e942e6803ea0c89e13a6f
Thien Ly <thien.ly@mariadb.com>
2022-06-29 12:06:13 +0300

    MXS-3723 Add `Save Script` icon button and its handler
    The handler supports the legacy download file approach and
    also supports writing the content directly to the user's file
    on the local disk if the browser supports it.

--- maxgui/src/pages/QueryPage/LoadSql.vue
+++ maxgui/src/pages/QueryPage/LoadSql.vue
@@ -81,6 +81,29 @@
             </template>
             <span>{{ $t('openScript') }}</span>
         </v-tooltip>
+        <v-tooltip
+            top
+            transition="slide-y-transition"
+            content-class="shadow-drop color text-navigation py-1 px-4"
+        >
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    text
+                    class="save-sql-btn session-toolbar-square-btn"
+                    type="file"
+                    :disabled="!getIsFileUnsaved"
+                    v-on="on"
+                    @click="supportFs ? saveFile() : saveFileLegacy()"
+                >
+                    <v-icon size="20" color="accent-dark">
+                        {{
+                            supportFs ? 'mdi-content-save-outline' : 'mdi-content-save-edit-outline'
+                        }}
+                    </v-icon>
+                </v-btn>
+            </template>
+            <span>{{ $t('saveScript') }}</span>
+        </v-tooltip>
     </div>
 </template>
 <script>
@@ -118,6 +141,8 @@ export default {
         ...mapState({
             query_sessions: state => state.querySession.query_sessions,
             active_wke_id: state => state.wke.active_wke_id,
+            blob_file: state => state.editor.blob_file,
+            query_txt: state => state.editor.query_txt,
         }),
         ...mapGetters({
             getActiveSessionId: 'querySession/getActiveSessionId',
@@ -168,22 +193,6 @@ export default {
             return await this.getFileTextLegacy(fileHandle)
         },
 
-        /**
-         * @param {Blob} blob - blob
-         */
-        async dontSave(blob) {
-            await this.loadScriptToActiveSession(blob)
-            this.confDlg.isOpened = false
-        },
-        /**
-         * @param {Blob} blob - blob
-         */
-        async onSave(blob) {
-            /* TODO: Handle saving file to user's local device if supportFs or downloading it
-             * before calling loadScriptToActiveSession
-             */
-            await this.loadScriptToActiveSession(blob)
-        },
         /**
          * @param {Blob} blob - blob
          */
@@ -252,6 +261,115 @@ export default {
                 },
                 id: this.getActiveSessionId,
             })
+        },
+
+        /**
+         * @param {Blob} blob - blob
+         */
+        async dontSave(blob) {
+            await this.loadScriptToActiveSession(blob)
+            this.confDlg.isOpened = false
+        },
+        /**
+         * @param {Blob} blob - blob
+         */
+        async onSave(blob) {
+            if (this.supportFs) await this.saveFile()
+            // load new blob
+            await this.loadScriptToActiveSession(blob)
+        },
+
+        /*
+         * Download the file to user's local device
+         */
+        async saveFileLegacy() {
+            let a = document.createElement('a')
+            /** legacy download file name
+             *  If there is no file_handle, use the current session tab name
+             */
+            const fileName =
+                `${this.$typy(this.blob_file, 'file_handle.name').safeString}` ||
+                `${this.getActiveSession.name}.sql`
+
+            a.href = `data:application/text;charset=utf-8;, ${encodeURIComponent(this.query_txt)}`
+            a.download = fileName
+            document.body.appendChild(a)
+            a.click()
+            document.body.removeChild(a)
+            // update blob_file
+            this.SET_BLOB_FILE({
+                payload: {
+                    ...this.blob_file,
+                    txt: this.query_txt,
+                },
+                id: this.getActiveSessionId,
+            })
+        },
+        /*
+         *Save new content for the opened file to user's local device
+         */
+        async saveFile() {
+            try {
+                const hasPriv = await this.verifyWritePriv(this.blob_file.file_handle)
+                if (hasPriv) {
+                    await this.writeFile({
+                        fileHandle: this.blob_file.file_handle,
+                        contents: this.query_txt,
+                    })
+                    // update blob_file
+                    this.SET_BLOB_FILE({
+                        payload: {
+                            file_handle: this.blob_file.file_handle,
+                            txt: this.query_txt,
+                        },
+                        id: this.getActiveSessionId,
+                    })
+                }
+            } catch (e) {
+                this.$logger('LoadSql-saveFile').error(e)
+            }
+        },
+
+        /**
+         * Writes the contents to disk.
+         * @param {FileSystemFileHandle} param.fileHandle File handle to write to.
+         * @param {string} param.contents Contents to write.
+         */
+        async writeFile({ fileHandle, contents }) {
+            // Support for Chrome 82 and earlier.
+            if (fileHandle.createWriter) {
+                // Create a writer (request permission if necessary).
+                const writer = await fileHandle.createWriter()
+                // Write the full length of the contents
+                await writer.write(0, contents)
+                // Close the file and write the contents to disk
+                await writer.close()
+                return
+            }
+            // For Chrome 83 and later.
+            // Create a FileSystemWritableFileStream to write to.
+            const writable = await fileHandle.createWritable()
+            // Write the contents of the file to the stream.
+            await writable.write(contents)
+            // Close the file and write the contents to disk.
+            await writable.close()
+        },
+        /**
+         * Verify the user has granted permission to read and write to the file, if
+         * permission hasn't been granted, request permission.
+         * @param {FileSystemFileHandle} fileHandle File handle to check.
+         * @return {boolean} True if the user has granted read/write permission.
+         */
+        async verifyWritePriv(fileHandle) {
+            const opts = {}
+            opts.writable = true
+            opts.mode = 'readwrite' // For Chrome 86 and later...
+            // Check if we already have permission, if so, return true.
+            if ((await fileHandle.queryPermission(opts)) === 'granted') return true
+            // Request permission to the file, if the user grants permission, return true.
+            if ((await fileHandle.requestPermission(opts)) === 'granted') return true
+            // The user didn't grant permission, return false.
+            return false
         },
     },
 }
