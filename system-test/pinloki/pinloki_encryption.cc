b8cfffac714c6ec6683c7c1b6491e8878cf455b0
Markus Mäkelä <markus.makela@mariadb.com>
2022-07-01 14:20:51 +0300

    Fix pinloki_encryption
    The `openssl` client isn't installed on the VM and it isn't really even
    required. Using a hard-coded encryption key is adequate to determine
    whether encryption works.

--- system-test/pinloki/pinloki_encryption.cc
+++ system-test/pinloki/pinloki_encryption.cc
@@ -95,8 +95,10 @@ int main(int argc, char** argv)
     TestConnections::skip_maxscale_start(true);
     TestConnections test(argc, argv);
 
-    // Create the encryption key before MaxScale is started
-    test.maxscale->ssh_node_f(true, "(echo -n '1;'; openssl rand -hex 32)|cat > /tmp/encryption.key");
+    // Create the encryption key before MaxScale is started. Use a hard-coded key as the OpenSSL client isn't
+    // installed on the test VM.
+    const char* key = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
+    test.maxscale->ssh_node_f(true, "echo -n '1;%s' > /tmp/encryption.key", key);
     test.maxscale->start();
 
     auto rv = EncryptionTest(test).result();
