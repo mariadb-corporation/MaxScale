f6f30574782ca7a380b0213584f204cec870b775
Markus Mäkelä <markus.makela@mariadb.com>
2022-07-22 15:59:26 +0300

    MXS-4217: Update jwt-cpp to 0.6.0
    The new version no longer requires a patch to work. It also adds some
    new signature algorithms.

--- cmake/BuildJwtCpp.cmake
+++ cmake/BuildJwtCpp.cmake
@@ -1,13 +1,12 @@
 # Downloads jwt-cpp: https://github.com/Thalhammer/jwt-cpp
 ExternalProject_Add(jwt-cpp
   GIT_REPOSITORY "https://github.com/Thalhammer/jwt-cpp.git"
-  GIT_TAG "v0.4.0"
+  GIT_TAG "v0.6.0"
   GIT_SHALLOW TRUE
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   UPDATE_COMMAND ""
   INSTALL_COMMAND ""
-  PATCH_COMMAND cd ${CMAKE_BINARY_DIR}/jwt-cpp-prefix/src/jwt-cpp/ && git checkout -f && git apply ${CMAKE_SOURCE_DIR}/cmake/jwt-cpp.patch
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
   LOG_CONFIGURE 1
