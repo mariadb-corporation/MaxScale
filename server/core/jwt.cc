315d0c14802544ed65bc67fba15a4852f9e804d3
Markus Mäkelä <markus.makela@mariadb.com>
2022-07-22 10:58:55 +0300

    Use 512-bit keys to sign JWTs
    The code already attempted it but it ended up using a 256-bit key. The
    code also generated 512 bytes of randomness, not 512 bits like it was
    supposed to.

--- server/core/jwt.cc
+++ server/core/jwt.cc
@@ -15,6 +15,7 @@
 
 #include <jwt-cpp/jwt.h>
 #include <random>
+#include <openssl/rand.h>
 
 #include <maxbase/assert.hh>
 
@@ -25,15 +26,9 @@ struct ThisUnit
     ThisUnit()
     {
         // Initialize JWT signing key
-        std::random_device gen;
         constexpr auto KEY_BITS = 512;
-        constexpr auto VALUE_SIZE = sizeof(decltype(gen()));
-        constexpr auto NUM_VALUES = KEY_BITS / VALUE_SIZE;
-        std::vector<decltype(gen())> key;
-        key.reserve(NUM_VALUES);
-        std::generate_n(std::back_inserter(key), NUM_VALUES, std::ref(gen));
-        sign_key.assign((const char*)key.data(), key.size() * VALUE_SIZE);
-        mxb_assert(sign_key.size() == KEY_BITS);
+        sign_key.resize(KEY_BITS / 8);
+        RAND_bytes((uint8_t*)sign_key.data(), sign_key.size());
     }
 
     std::string sign_key;
@@ -56,7 +51,7 @@ std::string create(const std::string& issuer, const std::string& audience, int m
            .set_audience(audience)
            .set_issued_at(now)
            .set_expires_at(now + std::chrono::seconds {max_age})
-           .sign(::jwt::algorithm::hs256 {this_unit.sign_key});
+           .sign(::jwt::algorithm::hs512 {this_unit.sign_key});
 }
 
 std::pair<bool, std::string> get_audience(const std::string& issuer, const std::string& token)
@@ -69,7 +64,7 @@ std::pair<bool, std::string> get_audience(const std::string& issuer, const std::
         auto d = ::jwt::decode(token);
 
         ::jwt::verify()
-        .allow_algorithm(::jwt::algorithm::hs256 {this_unit.sign_key})
+        .allow_algorithm(::jwt::algorithm::hs512 {this_unit.sign_key})
         .with_issuer(issuer)
         .verify(d);
 
