e468de3225aa1c4e5f5bcf08b9785eb85cce8765
Thien Ly <thien.ly@mariadb.com>
2022-07-27 10:42:51 +0300

    Use `--no-optional` flag
    The `omit` flag doesn't exist in npm v6.

--- maxgui/build.sh
+++ maxgui/build.sh
@@ -20,5 +20,5 @@ cd $PWD/maxgui
 # prints verbose output: https://github.com/npm/cli/issues/3314
 export buildPath=$PWD
 export VUE_APP_GIT_COMMIT=$(cd $src && git rev-list --max-count=1 HEAD)
-npm ci --production --omit=optional |& tee
+npm ci --production --no-optional |& tee
 npm run build |& tee
