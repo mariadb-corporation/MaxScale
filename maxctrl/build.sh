01ad31ccc0371d989bff097631287cf556004ca0
Markus Mäkelä <markus.makela@mariadb.com>
2022-07-28 10:26:17 +0300

    Fix MaxCtrl and MaxGUI build scripts
    Piping the output to `tee` made them always work even if the commands
    themselves failed. This is because by default bash returns the exit code
    of the last command in the pipeline. Using the pipefail option makes it
    return the exit code of the first failing command instead.

--- maxctrl/build.sh
+++ maxctrl/build.sh
@@ -34,8 +34,13 @@ then
     fi
 fi
 
+# The pipefail option will cause the command to return the exit code of the
+# first failing command in the pipeline rather than the default of returning the
+# exit code of the last command in the pipeline.
+set -o pipefail
+
 # Piping the output through `tee` works around a problem in npm where it always
 # prints verbose output: https://github.com/npm/cli/issues/3314
-npm install --production |& tee
-npm install --production pkg@5 |& tee
-node_modules/pkg/lib-es5/bin.js --options max_old_space_size=4096 $opts -t node10-linux . |& tee
+(npm install --production && \
+     npm install --production pkg@5 && \
+     node_modules/pkg/lib-es5/bin.js --options max_old_space_size=4096 $opts -t node10-linux .) |& tee
