481b685cd2f85a732aeaa2efb8cca4c49f55dfcc
Esa Korhonen <esa.korhonen@mariadb.com>
2022-09-02 09:51:15 +0300

    Cleanup mysqlmon_rejoin_good

--- system-test/mariadbmonitor/mysqlmon_rejoin_good.cpp
+++ system-test/mariadbmonitor/mysqlmon_rejoin_good.cpp
@@ -11,25 +11,29 @@
  * Public License.
  */
 
-#include "fail_switch_rejoin_common.cpp"
+#include <maxtest/testconnections.hh>
+#include "mariadbmon_utils.hh"
 
 using std::string;
 
+void test_main(TestConnections& test);
+
 int main(int argc, char** argv)
 {
-    TestConnections test(argc, argv);
-    // Set up test table
-    basic_test(test);
+    TestConnections test;
+    return test.run_test(argc, argv, test_main);
+}
 
+void test_main(TestConnections& test)
+{
     auto& mxs = *test.maxscale;
-    // Advance gtid:s a bit to so gtid variables are updated.
-    MYSQL* maxconn = test.maxscale->open_rwsplit_connection();
-    generate_traffic_and_check(test, maxconn, 10);
-    mysql_close(maxconn);
-    test.tprintf(LINE);
+
+    auto maxconn = mxs.open_rwsplit_connection2();
+    generate_traffic_and_check(test, maxconn.get(), 5);
+
     mxs.wait_for_monitor();
-    mxs.get_servers().print();
-    mxs.check_servers_status(mxt::ServersInfo::default_repl_states());
+    mxs.check_print_servers_status(mxt::ServersInfo::default_repl_states());
+
     auto old_master = test.get_repl_master();
     test.expect(old_master, "No master at start.");
     mxt::MariaDBServer* new_master = nullptr;
@@ -37,7 +41,6 @@ int main(int argc, char** argv)
     if (test.ok())
     {
         test.tprintf("Stopping master and waiting for failover. Check that another server is promoted.");
-        test.tprintf(LINE);
         old_master->stop_database();
         mxs.wait_for_monitor(2);
         new_master = test.get_repl_master();
@@ -46,20 +49,19 @@ int main(int argc, char** argv)
         string gtid_final_master;
         if (test.ok())
         {
-            test.tprintf("'%s' is new master.", new_master->cnf_name().c_str());
-            test.tprintf("Sending more inserts.");
-            maxconn = test.maxscale->open_rwsplit_connection();
-            generate_traffic_and_check(test, maxconn, 5);
+            test.tprintf("'%s' is new master. Sending more inserts.", new_master->cnf_name().c_str());
+            maxconn = mxs.open_rwsplit_connection2();
+            generate_traffic_and_check(test, maxconn.get(), 5);
             mxs.wait_for_monitor(1);
             auto status_before_rejoin = mxs.get_servers();
             status_before_rejoin.print();
             gtid_final_master = status_before_rejoin.get(new_master->cnf_name()).gtid;
             string gtid_old_master_before = status_before_rejoin.get(old_master->cnf_name()).gtid;
+            test.expect(!gtid_final_master.empty() && !gtid_old_master_before.empty(), "Gtid error");
             test.expect(gtid_final_master != gtid_old_master_before, "Old master is still replicating.");
         }
 
         test.tprintf("Bringing old master back online. It should rejoin the cluster and catch up in events.");
-        test.tprintf(LINE);
         old_master->start_database();
         mxs.wait_for_monitor(2);
 
@@ -72,11 +74,9 @@ int main(int argc, char** argv)
                         "Old master did not successfully rejoin the cluster.");
 
             test.tprintf("Switchover back to server1");
-            mxs.maxctrl("call command mysqlmon switchover MySQL-Monitor server1 server2");
+            mxs.maxctrl("call command mysqlmon switchover MariaDB-Monitor server1 server2");
             mxs.wait_for_monitor(2);
             mxs.check_servers_status(mxt::ServersInfo::default_repl_states());
         }
     }
-
-    return test.global_result;
 }
