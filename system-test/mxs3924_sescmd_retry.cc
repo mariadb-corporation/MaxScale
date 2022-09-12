5980f105c9cd5fb5960c8e5c42a2c2ca819c75ba
Markus Mäkelä <markus.makela@mariadb.com>
2022-09-12 16:55:31 +0300

    MXS-4289: Add test case
    The test case reproduces the problem and hits the debug assertion.

--- system-test/mxs3924_sescmd_retry.cc
+++ system-test/mxs3924_sescmd_retry.cc
@@ -28,6 +28,44 @@ void do_test(TestConnections& test, Connection& c)
     test.expect(c.query("SELECT 1"), "SELECT failed: %s", c.error());
 }
 
+void mxs4289(TestConnections& test, Connection& c)
+{
+    std::thread thr(
+        [&]() {
+        sleep(3);
+        test.log_printf("Blocking first three nodes");
+        for (int i = 0; i < 3; i++)
+        {
+            test.repl->block_node(i);
+        }
+
+        test.maxscale->wait_for_monitor(2);
+        sleep(2);
+
+        test.log_printf("Blocking final node");
+        test.repl->block_node(3);
+        test.maxscale->wait_for_monitor(2);
+        sleep(5);
+
+        test.log_printf("Unblocking all nodes");
+        test.repl->unblock_all_nodes();
+        test.maxscale->wait_for_monitor(2);
+        test.tprintf("thread done");
+    });
+    test.expect(c.connect(), "Failed to connect: %s", c.error());
+    test.expect(c.query("SET autocommit=0"), "SET autocommit=0 failed: %s", c.error());
+
+    test.log_printf("Executing SELECT SLEEP");
+    test.expect(c.query("SET @a=(SELECT SLEEP(10))"), "SET failed: %s", c.error());
+
+    thr.join();
+
+    test.log_printf("Executing SELECT 1");
+    test.expect(c.query("SELECT 1"), "SELECT failed: %s", c.error());
+
+    test.expect(c.query("COMMIT"), "COMMIT failed: %s", c.error());
+}
+
 int main(int argc, char** argv)
 {
     TestConnections test(argc, argv);
@@ -56,6 +94,9 @@ int main(int argc, char** argv)
     test.expect(c.query("SET autocommit=0"), "SET autocommit=0 failed: %s", c.error());
     do_test(test, c);
     test.expect(c.query("COMMIT"), "COMMIT failed: %s", c.error());
+
+    test.log_printf("MXS-4289: Retrying failing session command inside a transaction");
+    mxs4289(test, c);
 
     return test.global_result;
 }
