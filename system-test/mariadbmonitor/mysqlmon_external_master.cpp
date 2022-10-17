f99e78bb9c87c775dbcd9069ef07f444b1c8970d
Esa Korhonen <esa.korhonen@mariadb.com>
2022-10-17 16:21:18 +0300

    Fix mysqlmon_external_master
    In branch 6.4 the test runs with ignore_external_masters on.
    In 22.08 that option no longer exists so the expected results are
    different. Also, the ongoing writes complicate matters a bit.

--- system-test/mariadbmonitor/mysqlmon_external_master.cpp
+++ system-test/mariadbmonitor/mysqlmon_external_master.cpp
@@ -26,7 +26,7 @@ void test_main(TestConnections& test)
     auto master = mxt::ServerInfo::master_st;
     auto slave = mxt::ServerInfo::slave_st;
     auto down = mxt::ServerInfo::DOWN;
-    // ignore_external_masters is on so "Slave Of External Master"-status is not visible.
+    auto ext = mxt::ServerInfo::SERVER_SLAVE_OF_EXT_MASTER;
 
     // Create a table and a user and start a thread that does writes.
     auto node0 = repl.backend(0)->open_connection();
@@ -61,7 +61,7 @@ void test_main(TestConnections& test)
     ext_server->cmd("reset slave all;");
     repl.replicate_from(0, 3);
     mxs.wait_for_monitor(1);
-    mxs.check_print_servers_status({master, slave, slave});
+    mxs.check_print_servers_status({master | ext, slave, slave});
 
     if (test.ok())
     {
@@ -71,34 +71,46 @@ void test_main(TestConnections& test)
         test.tprintf("Stop server1, expect server2 to be promoted as the master");
         repl.stop_node(0);
         mxs.wait_for_monitor(3);
+        // Because the writer thread is doing writes to server1, and those updates are not yet replicated to
+        // server4, the server4->server1 slave connection will fail. Thus, server2 will not get
+        // "Slave of External Server".
         mxs.check_print_servers_status({down, master, slave});
 
         if (test.ok())
         {
+            auto restart_slave = [&mxs, &repl](int i) {
+                auto conn = repl.backend(i)->admin_connection();
+                conn->cmd("stop slave;");
+                conn->cmd("start slave;");
+                mxs.wait_for_monitor(1);
+            };
             sleep(1);
             test.tprintf("Configure master-master replication between server2 and the external server");
             repl.replicate_from(3, 1);
-            mxs.wait_for_monitor(1);
-            mxs.check_print_servers_status({down, master, slave});
+            sleep(1);
+            // Restart replication from external server to clear errors.
+            restart_slave(1);
+            mxs.check_print_servers_status({down, master | ext, slave});
 
             test.tprintf("Start server1, expect it to rejoin the cluster");
             // Rejoin should redirect the existing external master connection in server1.
             repl.start_node(0);
             mxs.wait_for_monitor(2);
-            mxs.check_print_servers_status({slave, master, slave});
+            mxs.check_print_servers_status({slave, master | ext, slave});
 
             test.tprintf("Stop server2, expect server1 to be promoted as the master. Manually redirect "
                          "external server to server1.");
             repl.stop_node(1);
             mxs.wait_for_monitor(2);
             repl.replicate_from(3, 0);
-
-            mxs.check_servers_status({master, down, slave});
+            sleep(1);
+            restart_slave(0);
+            mxs.check_servers_status({master | ext, down, slave});
 
             test.tprintf("Start server2, expect it to rejoin the cluster");
             repl.start_node(1);
             mxs.wait_for_monitor(2);
-            mxs.check_servers_status({master, slave, slave});
+            mxs.check_servers_status({master | ext, slave, slave});
 
             auto conn = repl.backend(0)->open_connection();
             conn->cmd("stop slave;");
