97394b295a190a5a63fc9bccf075b28c2b20c232
Markus Mäkelä <markus.makela@mariadb.com>
2022-11-25 11:11:14 +0200

    MXS-4376: Add mxb::latch
    The API for it is identical to the C++20 std::latch which makes it
    easier to switch over to C++20 when we eventually start using it.

---
+++ maxutils/maxbase/include/maxbase/latch.hh
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-10-04
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#pragma once
+#include <maxbase/ccdefs.hh>
+
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+#include <maxbase/assert.hh>
+
+// Identical to std::latch from C++20. Probably less efficient as it uses a mutex and a condition_variable
+// instead of std::atomic::wait.
+namespace maxbase
+{
+class latch
+{
+public:
+    latch(const latch&) = delete;
+    latch& operator=(const latch&) = delete;
+
+    latch(std::ptrdiff_t expected)
+        : m_value(expected)
+    {
+    }
+
+    void count_down(std::ptrdiff_t n = 1)
+    {
+        auto old = m_value.fetch_sub(n, std::memory_order_release);
+
+        if (old == n)
+        {
+            m_cv.notify_all();
+        }
+    }
+
+    bool try_wait() const noexcept
+    {
+        return m_value.load(std::memory_order_acquire) == 0;
+    }
+
+    void wait() const
+    {
+        std::unique_lock guard(m_lock);
+        m_cv.wait(guard, [this](){
+            return try_wait();
+        });
+    }
+
+    void arrive_and_wait(std::ptrdiff_t n = 1)
+    {
+        count_down(n);
+        wait();
+    }
+
+private:
+    std::atomic<std::ptrdiff_t>     m_value;
+    mutable std::mutex              m_lock;
+    mutable std::condition_variable m_cv;
+};
+}
---
+++ maxutils/maxbase/src/test/test_latch.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-11-16
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#ifndef SS_DEBUG
+#define SS_DEBUG
+#endif
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include <maxbase/ccdefs.hh>
+
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+#include <maxbase/log.hh>
+#include <maxbase/latch.hh>
+
+int main(int argc, char* argv[])
+{
+    mxb::Log logger(MXB_LOG_TARGET_STDOUT);
+
+    for (int loops = 0; loops < 100; loops++)
+    {
+        std::vector<std::thread> threads;
+        std::atomic<int> value {0};
+        mxb::latch sync_latch{105};
+
+        for (int i = 0; i < 100; i++)
+        {
+            threads.emplace_back([&, i](){
+                value.fetch_add(1, std::memory_order_relaxed);
+                sync_latch.arrive_and_wait();
+                int v = value.load(std::memory_order_relaxed);
+                mxb_assert_message(v == 100, "Value should be 100 after arrive_and_wait(): %d", v);
+            });
+        }
+
+        sync_latch.count_down(5);
+
+        while (!sync_latch.try_wait())
+        {
+            std::this_thread::yield();
+        }
+
+        int v = value.load(std::memory_order_relaxed);
+        mxb_assert_message(v == 100, "Value should be 100 after try_wait() returns true: %d", v);
+
+        std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+        v = value.load(std::memory_order_relaxed);
+        mxb_assert_message(v == 100, "Value should be 100 after joining threads: %d", v);
+    }
+
+    return 0;
+}
