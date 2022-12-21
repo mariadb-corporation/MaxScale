d0588261d886a64c80093263b516b8c30a19a3de
Johan Wikman <johan.wikman@mariadb.com>
2022-12-21 15:18:23 +0200

    MXS-4107 Add forgotten header

---
+++ server/modules/filter/cache/storage/storage_memcached/storage_memcached.hh
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-11-16
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#define MXB_MODULE_NAME "storage_memcached"
+#include <maxscale/ccdefs.hh>
+#include "../../cache_storage_api.hh"
