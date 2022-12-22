ae2e630a48e5ff341568e1ba31ce99a2af06ec5a
Johan Wikman <johan.wikman@mariadb.com>
2022-12-22 10:13:58 +0200

    MXS-4107 Add redis SSL system-test
    SSL support is not enabled by default in redis, so the
    server must be built in order to be able to test SSL
    usage of redis. Fortunately the building takes only 1.5
    minutes or so.

---
+++ system-test/cache_redis_ssl.cpp
@@ -0,0 +1,219 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-11-16
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <maxtest/testconnections.hh>
+
+using namespace std;
+
+namespace
+{
+
+const int PORT_RWS_REDIS = 4006;
+
+bool build_redis(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    int rv;
+
+    test.tprintf("Removing possible old redis installation.");
+    rv = maxscale->ssh_node_f(false, "cd %s; rm -rf redis",
+                              maxscale->access_homedir());
+    test.expect(rv == 0, "Could not remove old redis installation.");
+
+    if (rv == 0)
+    {
+        test.tprintf("Cloning redis.");
+        rv = maxscale->ssh_node_f(false, "cd %s; git clone https://github.com/redis/redis.git",
+                                  maxscale->access_homedir());
+        test.expect(rv == 0, "Could not clone redis.");
+
+        if (rv == 0)
+        {
+            test.tprintf("Checking out 6.2.8.");
+            rv = maxscale->ssh_node_f(false, "cd %s/redis; git checkout 6.2.8",
+                                      maxscale->access_homedir());
+            test.expect(rv == 0, "Could not checkout 6.2.8.");
+
+            if (rv == 0)
+            {
+                test.tprintf("Building redis.");
+                rv = maxscale->ssh_node_f(false, "cd %s/redis; make BUILD_TLS=yes",
+                                          maxscale->access_homedir());
+                test.expect(rv == 0, "Could not build redis.");
+            }
+        }
+    }
+
+    return rv == 0;
+}
+
+bool generate_certificates(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    int rv;
+
+    test.tprintf("Generating certificates.");
+    rv = maxscale->ssh_node_f(false, "cd %s/redis; ./utils/gen-test-certs.sh",
+                              maxscale->access_homedir());
+    test.expect(rv == 0, "Could not generate certificates.");
+
+    return rv == 0;
+}
+
+bool stop_system_redis(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    int rv;
+
+    test.tprintf("Stopping system redis.");
+    rv = maxscale->ssh_node_f(true, "systemctl stop redis");
+    test.expect(rv == 0, "Could not stop system redis.");
+
+    return rv == 0;
+}
+
+bool start_custom_redis(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    int rv;
+
+    test.tprintf("Starting custom redis.");
+    rv = maxscale->ssh_node_f(false,
+                              "cd %s/redis; "
+                              "./src/redis-server --daemonize yes --tls-port 6379 --port 0 "
+                              "--tls-cert-file ./tests/tls/redis.crt "
+                              "--tls-key-file ./tests/tls/redis.key "
+                              "--tls-ca-cert-file ./tests/tls/ca.crt",
+                              maxscale->access_homedir());
+    test.expect(rv == 0, "Could not start custom redis.");
+
+    return rv == 0;
+}
+
+bool stop_custom_redis(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    int rv;
+
+    test.tprintf("Stopping custom redis.");
+    rv = maxscale->ssh_node_f(true, "pkill redis-server");
+    test.expect(rv == 0, "Could not stop custom redis.");
+
+    return rv == 0;
+}
+
+void test_that_usage_fails(TestConnections& test)
+{
+    test.tprintf("Testing that usage fails.");
+
+    Connection c = test.maxscale->get_connection(PORT_RWS_REDIS);
+    test.expect(c.connect(), "Could not connect to MaxScale.");
+
+    c.query("SELECT 1"); // Trigger connecting to Redis
+    sleep(1);
+    c.query("SELECT 1");
+    sleep(1);
+
+    test.log_includes("I/O-error; will attempt to reconnect");
+}
+
+void test_that_usage_succeeds(TestConnections& test)
+{
+    test.tprintf("Testing that usage succeeds.");
+
+    Connection c = test.maxscale->get_connection(PORT_RWS_REDIS);
+    test.expect(c.connect(), "Could not connect to MaxScale.");
+
+    c.query("SELECT 1"); // Trigger connecting to Redis
+    sleep(1);
+    c.query("SELECT 1");
+    sleep(1);
+
+    test.log_excludes("I/O-error; will attempt to reconnect");
+}
+
+void run_test(TestConnections& test)
+{
+    auto maxscale = test.maxscale;
+
+    test.expect(maxscale->start_and_check_started(), "Could not start maxscale.");
+    test_that_usage_fails(test);
+
+    test.expect(maxscale->stop_and_check_stopped(), "Could not stop maxscale.");
+
+    test.tprintf("Configuring MaxScale for SSL.");
+    int rv = maxscale->ssh_node_f(
+        true,
+        "sed -i "
+        "-e \"s@storage_redis.ssl=false@storage_redis.ssl=true@\" "
+        "-e \"s@storage_redis.ssl_cert=/etc/maxscale.cnf@storage_redis.ssl_cert=%s/redis/tests/tls/redis.crt@\" "
+        "-e \"s@storage_redis.ssl_key=/etc/maxscale.cnf@storage_redis.ssl_key=%s/redis/tests/tls/redis.key@\" "
+        "-e \"s@storage_redis.ssl_ca=/etc/maxscale.cnf@storage_redis.ssl_ca=%s/redis/tests/tls/ca.crt@\" "
+        "/etc/maxscale.cnf",
+        maxscale->access_homedir(),
+        maxscale->access_homedir(),
+        maxscale->access_homedir());
+
+    test.expect(rv == 0, "Could not configure MaxScale for SSL.");
+
+    if (rv == 0)
+    {
+        rv = maxscale->ssh_node_f(true, "rm /var/log/maxscale/maxscale.log");
+        test.expect(rv == 0, "Could not remove /var/log/maxscale/maxscale.log");
+
+        test.expect(maxscale->start_and_check_started(), "Could not start maxscale.");
+        test_that_usage_succeeds(test);
+    }
+}
+
+}
+
+int main(int argc, char* argv[])
+{
+    TestConnections::skip_maxscale_start(true);
+    TestConnections test(argc, argv);
+
+    if (build_redis(test))
+    {
+        test.tprintf("Redis built.");
+
+        if (generate_certificates(test))
+        {
+            test.tprintf("Certificates generated.");
+
+            if (stop_system_redis(test))
+            {
+                test.tprintf("System redis stopped.");
+
+                if (start_custom_redis(test))
+                {
+                    test.tprintf("Custom redis started.");
+
+                    run_test(test);
+
+                    if (stop_custom_redis(test))
+                    {
+                        test.tprintf("Custom redis stopped.");
+                    }
+                }
+            }
+        }
+    }
+
+    return test.global_result;
+}
