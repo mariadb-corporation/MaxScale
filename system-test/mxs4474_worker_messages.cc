8434866cb51857c5ca343d9625e7413bf29db796
Markus Mäkelä <markus.makela@mariadb.com>
2023-01-10 13:57:01 +0200

    MXS-4474: Add test case
    The test reproduces the problem where session commands caused excessive
    messages to be generated as well as the case where KILL commands caused
    the whole MaxScale process to stall. It also verifies that the fixes to
    the problem work and that they complete in a reasonable time (around 4
    seconds on my laptop).

---
+++ system-test/mxs4474_worker_messages.cc
@@ -0,0 +1,55 @@
+#include <maxtest/testconnections.hh>
+
+int main(int argc, char** argv)
+{
+    TestConnections test(argc, argv);
+    test.repl->execute_query_all_nodes("SET GLOBAL max_connections = 10000");
+
+    auto idle = test.maxscale->rwsplit();
+    test.expect(idle.connect(), "Failed to create first connection: %s", idle.error());
+    uint32_t id = idle.thread_id();
+
+    std::vector<Connection> connections;
+
+    // We'll probably run into some file descriptor limits before we create the connections. If we don't, try
+    // to keep it at a reasonable level.
+    for (int i = 0; i < 9000; i++)
+    {
+        auto c = test.maxscale->rwsplit();
+
+        if (c.connect() && c.query("SELECT 1"))
+        {
+            connections.push_back(std::move(c));
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    test.tprintf("Managed to create %lu connections through MaxScale", connections.size());
+
+    for (auto& c : connections)
+    {
+        test.expect(c.send_query("USE test"), "Sending USE should work: %s", c.error());
+    }
+
+    for (auto& c : connections)
+    {
+        test.expect(c.read_query_result(), "Reading USE result should work: %s", c.error());
+    }
+
+    for (auto& c : connections)
+    {
+        test.expect(c.send_query("KILL " + std::to_string(id)), "Sending KILL should work: %s", c.error());
+    }
+
+    for (auto& c : connections)
+    {
+        // The KILL might fail due to the MainWorker being overloaded but
+        // the request itself should not time out.
+        c.read_query_result();
+    }
+
+    return test.global_result;
+}
