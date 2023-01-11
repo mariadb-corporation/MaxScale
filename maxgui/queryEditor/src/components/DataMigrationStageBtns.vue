6de86d60b3a08f92f978ce7fec2ac7108239ba4b
Thien Ly <thien.ly@mariadb.com>
2023-01-11 11:23:10 +0200

    MXS-4430 Use `v-stepper` for navigating between etl stages
    Each stage will be wrapped inside `v-form` component, so user inputs
    can be validated.
    
    This also adds `data-migration-stage-btns` component to handle
    render `Previous` and `Next` buttons.

---
+++ maxgui/queryEditor/src/components/DataMigrationStageBtns.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="d-flex justify-end">
+        <v-btn
+            v-if="step > ETL_STAGE_INDEX.CONN"
+            small
+            height="36"
+            color="primary"
+            class="font-weight-medium px-7 text-capitalize"
+            rounded
+            outlined
+            depressed
+            @click="$emit('prev', step)"
+        >
+            {{ $mxs_t('prev') }}
+        </v-btn>
+        <v-btn
+            v-if="step < ETL_STAGE_INDEX.DATA_MIGR"
+            small
+            height="36"
+            color="primary"
+            class="ml-2 font-weight-medium px-7 text-capitalize"
+            rounded
+            depressed
+            :disabled="isNextDisabled"
+            @click="$emit('next', step)"
+        >
+            {{ $mxs_t('next') }}
+        </v-btn>
+        <!-- TODO: Add cancel button? -->
+    </div>
+</template>
+
+<script>
+/*
+ * Copyright (c) 2020 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-11-16
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+import { mapState } from 'vuex'
+
+export default {
+    name: 'data-migration-stage-btns',
+    props: {
+        step: { type: Number, required: true },
+        isNextDisabled: { type: Boolean, required: true },
+    },
+    computed: {
+        ...mapState({
+            ETL_STAGE_INDEX: state => state.queryEditorConfig.config.ETL_STAGE_INDEX,
+        }),
+    },
+}
+</script>
--- maxgui/queryEditor/src/components/DataMigrationStageCtr.vue
+++ maxgui/queryEditor/src/components/DataMigrationStageCtr.vue
@@ -1,10 +1,45 @@
 <template>
-    <v-sheet class="d-flex flex-column fill-height mt-12">
-        <!-- TODO: Utilize v-stepper and its related components to navigate between etl stages -->
-        <!-- TODO: Add the "Prerequisites" stage -->
-        <etl-conns-ctr />
-        <etl-src-tree />
-    </v-sheet>
+    <v-stepper
+        v-model="activeStageIdx"
+        class="data-migration-stepper d-flex flex-column mt-4"
+        outlined
+    >
+        <div
+            class="d-flex flex-wrap align-stretch justify-space-between mxs-color-helper border-bottom-table-border"
+        >
+            <v-stepper-step
+                v-for="(stage, stageIdx) in stages"
+                :key="stageIdx"
+                :step="stageIdx"
+                :complete="stage.isComplete"
+            >
+                {{ stage.name }}
+            </v-stepper-step>
+        </div>
+        <v-stepper-items class="fill-height">
+            <v-stepper-content
+                v-for="(stage, stageIdx) in stages"
+                :key="stageIdx"
+                :step="stageIdx"
+                class="fill-height"
+            >
+                <div
+                    class="stage-container pa-6 fill-height d-flex flex-column justify-space-between"
+                >
+                    <v-form ref="form" v-model="isFormValid" lazy-validation>
+                        <component :is="stage.component" ref="stageComponent" />
+                    </v-form>
+                    <data-migration-stage-btns
+                        class="mt-4"
+                        :step="stageIdx"
+                        :isNextDisabled="isNextDisabled"
+                        @prev="prev"
+                        @next="next"
+                    />
+                </div>
+            </v-stepper-content>
+        </v-stepper-items>
+    </v-stepper>
 </template>
 
 <script>
@@ -20,14 +55,107 @@
  * of this software will be governed by version 2 or later of the General
  * Public License.
  */
+import EtlTask from '@queryEditorSrc/store/orm/models/EtlTask'
 import EtlSrcTree from '@queryEditorSrc/components/EtlSrcTree.vue'
 import EtlConnsCtr from '@queryEditorSrc/components/EtlConnsCtr.vue'
+import DataMigrationStageBtns from '@queryEditorSrc/components/DataMigrationStageBtns.vue'
+import { mapState } from 'vuex'
 
 export default {
     name: 'data-migration-stage-ctr',
     components: {
         EtlSrcTree,
         EtlConnsCtr,
+        DataMigrationStageBtns,
+    },
+    data() {
+        return {
+            isFormValid: true,
+        }
+    },
+    computed: {
+        ...mapState({
+            ETL_STAGE_INDEX: state => state.queryEditorConfig.config.ETL_STAGE_INDEX,
+        }),
+        activeEtlTask() {
+            return EtlTask.getters('getActiveEtlTaskWithRelation')
+        },
+        stages() {
+            const { CONN, SRC_OBJ, OBJ_MIGR, DATA_MIGR } = this.ETL_STAGE_INDEX
+            // TODO: Handle isComplete value
+            return [
+                {
+                    name: this.$mxs_tc('connections', 1),
+                    component: 'etl-conns-ctr',
+                    isComplete: this.activeStageIdx > CONN,
+                },
+                {
+                    name: this.$mxs_t('objSelection'),
+                    component: 'etl-src-tree',
+                    isComplete: this.activeStageIdx > SRC_OBJ,
+                },
+                {
+                    name: this.$mxs_t('objMigration'),
+                    component: 'div', //  TODO: Replace with the objects migration component
+                    isComplete: this.activeStageIdx > OBJ_MIGR,
+                },
+                {
+                    name: this.$mxs_t('dataMigration'),
+                    component: 'div', //  TODO: Replace with the data migration report component
+                    isComplete: this.activeStageIdx > DATA_MIGR,
+                },
+            ]
+        },
+        activeStageIdx: {
+            get() {
+                return this.activeEtlTask.active_stage_index
+            },
+            set(v) {
+                EtlTask.update({
+                    where: this.activeEtlTask.id,
+                    data: { active_stage_index: v },
+                })
+            },
+        },
+        isNextDisabled() {
+            return !this.isFormValid
+        },
+    },
+    watch: {
+        async activeStageIdx(v) {
+            // Reset validation after changing the stage
+            await this.$refs.form[v].resetValidation()
+        },
+    },
+    methods: {
+        prev() {
+            this.activeStageIdx--
+        },
+        async next(currentStage) {
+            await this.$refs.form[currentStage].validate()
+            if (this.isFormValid) {
+                const { CONN } = this.ETL_STAGE_INDEX
+                switch (currentStage) {
+                    case CONN:
+                    // TODO: open etl connections by getting form data via this.$refs.stageComponent[currentStage].$data
+                }
+                this.activeStageIdx++
+            }
+        },
     },
 }
 </script>
+
+<style lang="scss">
+.data-migration-stepper {
+    .v-stepper__content {
+        padding: 0px;
+    }
+    .v-stepper__wrapper {
+        height: 100%;
+        .stage-container {
+            overflow-y: auto;
+        }
+    }
+}
+</style>
