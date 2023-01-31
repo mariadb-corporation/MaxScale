55fb3929a17a41ce26a46db9f365c36fe8846f5e
Johan Wikman <johan.wikman@mariadb.com>
2023-01-31 11:21:09 +0200

    MXS-4493 Minor tuning of copyright string
    In preparation for batch-update of all copyrights.

--- connectors/cdc-connector/cdc_connector.cpp
+++ connectors/cdc-connector/cdc_connector.cpp
@@ -1,4 +1,5 @@
-/* Copyright (c) 2017, MariaDB Corporation. All rights reserved.
+/*
+ * Copyright (c) 2017, MariaDB Corporation. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
