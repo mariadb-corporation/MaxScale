e9031b3cb007d1fe7a575b472706a4a45c9ac02c
Markus Mäkelä <markus.makela@mariadb.com>
2023-02-01 12:54:55 +0200

    Fix ETL test copyrights

--- system-test/sql_api/etl_postgresql.cc
+++ system-test/sql_api/etl_postgresql.cc
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2023 MariaDB Corporation Ab
- * Copyright (c) 2023 MariaDB plc, Finnish Branch
+ * Copyright (c) 2023 MariaDB plc
  *
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
