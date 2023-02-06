3766902c9ae4bc284a291bcf2b1d0c2a66e795e4
Johan Wikman <johan.wikman@mariadb.com>
2023-02-06 09:49:22 +0200

    23.02 Add VERSION2302.cmake and update VERSION.cmake

--- VERSION2302.cmake
+++ VERSION2302.cmake
@@ -3,12 +3,12 @@
 # This file contains cache values for CMake which control MaxScale's version
 # number.
 
-set(MAXSCALE_VERSION_MAJOR "22" CACHE STRING "Major version")
-set(MAXSCALE_VERSION_MINOR "08" CACHE STRING "Minor version")
-set(MAXSCALE_VERSION_PATCH "5" CACHE STRING "Patch version")
+set(MAXSCALE_VERSION_MAJOR "23" CACHE STRING "Major version")
+set(MAXSCALE_VERSION_MINOR "02" CACHE STRING "Minor version")
+set(MAXSCALE_VERSION_PATCH "0" CACHE STRING "Patch version")
 
 # Used in version.hh.in, no leading 0.
-set(MAXSCALE_VERSION_MINOR_NUM "8" CACHE STRING "Minor version")
+set(MAXSCALE_VERSION_MINOR_NUM "2" CACHE STRING "Minor version")
 
 # This should only be incremented if a package is rebuilt
 set(MAXSCALE_BUILD_NUMBER 1 CACHE STRING "Release number")
