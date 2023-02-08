6e74f016fe09b9b1f3cb1cff0d6fa7b952a537f1
Esa Korhonen <esa.korhonen@mariadb.com>
2023-02-08 17:27:22 +0200

    Remove Galera dependency from mxs1110_16mb
    The blob data was copied to Galera via tee-filter but was not
    checked. Tee-filter is tested elsewhere so the dependency can
    be removed. Removed the other filters as well since they
    were unused.

--- system-test/mxs1110_16mb.cpp
+++ system-test/mxs1110_16mb.cpp
@@ -6,60 +6,40 @@
 
 #include <maxtest/testconnections.hh>
 #include <maxtest/blob_test.hh>
-#include <maxtest/galera_cluster.hh>
+
+using std::string;
+
+namespace
+{
+void test_main(TestConnections& test)
+{
+    auto& mxs = *test.maxscale;
+    auto& repl = *test.repl;
+    int chunk_size = 2500000;
+    int chunk_num = 5;
+    int rows = 2;
+
+    repl.execute_query_all_nodes("set global max_allowed_packet=200000000");
+    mxs.connect_maxscale();
+    repl.connect();
+    test.tprintf("LONGBLOB: Trying send data via RWSplit");
+    test_longblob(test, mxs.conn_rwsplit, "LONGBLOB", chunk_size, chunk_num, rows);
+    repl.close_connections();
+    mxs.close_maxscale_connections();
+
+    repl.sync_slaves();
+    mxs.connect_maxscale();
+    test.tprintf("Checking data via RWSplit");
+    check_longblob_data(test, mxs.conn_rwsplit, chunk_size, chunk_num, rows);
+    test.tprintf("Checking data via ReadConn master");
+    check_longblob_data(test, mxs.conn_master, chunk_size, chunk_num, rows);
+    test.tprintf("Checking data via ReadConn slave");
+    check_longblob_data(test, mxs.conn_slave, chunk_size, chunk_num, rows);
+    mxs.close_maxscale_connections();
+}
+}
 
 int main(int argc, char* argv[])
 {
-    TestConnections::skip_maxscale_start(true);
-    TestConnections* Test = new TestConnections(argc, argv);
-    Test->maxscale->stop();
-    Test->reset_timeout();
-    int chunk_size = 2500000;
-    int chunk_num = 5;
-    std::string src_dir = mxt::SOURCE_DIR;
-    std::string masking_rules = src_dir + "/masking/masking_user/masking_rules.json";
-    std::string cache_rules = src_dir + "/cache/cache_basic/cache_rules.json";
-    std::string fw_rules = src_dir + "/fw";
-    std::string home = Test->maxscale->access_homedir();
-
-    Test->maxscale->copy_to_node(masking_rules.c_str(), home.c_str());
-
-    Test->maxscale->copy_to_node(cache_rules.c_str(), home.c_str());
-
-    Test->maxscale->ssh_node("chmod a+rw *.json", true);
-
-    Test->maxscale->copy_fw_rules("rules2", fw_rules);
-
-    Test->maxscale->start_maxscale();
-
-    Test->repl->execute_query_all_nodes((char*) "set global max_allowed_packet=200000000");
-    Test->galera->execute_query_all_nodes((char*) "set global max_allowed_packet=200000000");
-
-    Test->maxscale->connect_maxscale();
-    Test->repl->connect();
-    Test->tprintf("LONGBLOB: Trying send data via RWSplit\n");
-    test_longblob(*Test, Test->maxscale->conn_rwsplit, "LONGBLOB", chunk_size, chunk_num, 2);
-    Test->repl->close_connections();
-    Test->maxscale->close_maxscale_connections();
-
-    Test->repl->sync_slaves();
-    Test->maxscale->connect_maxscale();
-    Test->tprintf("Checking data via RWSplit\n");
-    check_longblob_data(*Test, Test->maxscale->conn_rwsplit, chunk_size, chunk_num, 2);
-    Test->tprintf("Checking data via ReadConn master\n");
-    check_longblob_data(*Test, Test->maxscale->conn_master, chunk_size, chunk_num, 2);
-    Test->tprintf("Checking data via ReadConn slave\n");
-    check_longblob_data(*Test, Test->maxscale->conn_slave, chunk_size, chunk_num, 2);
-    Test->maxscale->close_maxscale_connections();
-
-    MYSQL* conn_galera = open_conn(4016,
-                                   Test->maxscale->ip4(),
-                                   Test->maxscale->user_name(),
-                                   Test->maxscale->password(),
-                                   Test->maxscale_ssl);
-    mysql_close(conn_galera);
-
-    int rval = Test->global_result;
-    delete Test;
-    return rval;
+    return TestConnections().run_test(argc, argv, test_main);
 }
