7145f075256c6a3a30a45dfd5127c135ce24c3ea
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-20 17:55:12 -0500

    spelling: repeated
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- system-test/mxs822_maxpasswd.cpp
+++ system-test/mxs822_maxpasswd.cpp
@@ -6,7 +6,7 @@
  * - replace passwords in maxscale.cnf with generated encrypted password
  * - try to connect to RWSplit
  * - restore passwords in maxscale.cnf
- * - repeate for several other password with special characters
+ * - repeated for several other password with special characters
  */
 
 #include <iostream>
