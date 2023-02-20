e21e9eb3826eb98c27aa5bc01d9b77384f174c6b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-20 16:49:09 -0500

    spelling: autocommit
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- server/modules/filter/cache/cachefiltersession.cc
+++ server/modules/filter/cache/cachefiltersession.cc
@@ -544,7 +544,7 @@ bool CacheFilterSession::clientReply(GWBUF&& data, const mxs::ReplyRoute& down, 
     if (m_invalidate_now)
     {
         // The response to either a COMMIT, or to UPDATE/DELETE/INSERT with
-        // autcommit being true.
+        // autocommit being true.
 
         if (reply.is_complete())
         {
