343571f209e10069a0d8682cb165a3b11eb02146
Thien Ly <thien.ly@mariadb.com>
2023-02-27 10:29:58 +0200

    MXS-4477 Pause graphs when `Next refresh` value is `No refresh`
    The is no reason to keep drawing the graphs when the users choose
    `No refresh`.
    
    This uses the `pause` option from `chartjs-plugin-streaming` to stop
    drawing.
    
    This also uses the `frameRate` option with value as 24 (24 frames per
    second, default was 30) to decrease CPU usage.

--- maxgui/share/components/common/MxsCharts/MxsLineChartStream.vue
+++ maxgui/share/components/common/MxsCharts/MxsLineChartStream.vue
@@ -40,13 +40,16 @@ export default {
     data() {
         return {
             uniqueTooltipId: this.$helpers.lodash.uniqueId('tooltip_'),
+            delay: 2000,
         }
     },
     computed: {
+        isPaused() {
+            return this.refreshRate === -1
+        },
         lineChartStreamOptions() {
             const scope = this
             return {
-                showLine: true,
                 elements: { point: { radius: 0 } },
                 scales: {
                     x: {
@@ -61,8 +64,8 @@ export default {
                 },
                 plugins: {
                     streaming: {
-                        duration: this.refreshRate * 2000,
-                        delay: (this.refreshRate + 2) * 1000,
+                        delay: this.delay,
+                        frameRate: 24,
                     },
                     tooltip: {
                         external: context =>
@@ -76,13 +79,28 @@ export default {
         },
     },
     watch: {
-        refreshRate(v, oV) {
-            if (!this.$helpers.lodash.isEqual(v, oV)) this.chartInstance.update('none')
+        refreshRate: {
+            immediate: true,
+            handler(v, oV) {
+                /**
+                 * Handle pause/resume stream in the next tick for the initial render,
+                 * because chartInstance is only available in the nextTick
+                 */
+                if (this.$typy(oV).isUndefined) this.$nextTick(() => this.handleStream())
+                else this.handleStream()
+            },
         },
     },
     beforeDestroy() {
         let tooltipEl = document.getElementById(this.uniqueTooltipId)
         if (tooltipEl) tooltipEl.remove()
+    },
+    methods: {
+        handleStream() {
+            let realtimeOpts = this.chartInstance.config.options.plugins.streaming
+            realtimeOpts.pause = this.isPaused
+            realtimeOpts.delay = this.isPaused ? this.delay : (this.refreshRate + 1) * 1000
+        },
     },
 }
 </script>
