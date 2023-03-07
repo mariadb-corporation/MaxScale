62f1e6905d2bb7103457970f07b3c91cc8b2ed6a
Niclas Antti <jnantti@gmail.com>
2023-03-07 14:05:48 +0200

    MXS-4528 Minimal working client statemachine
    This assumes authentication trust and existing
    user "maxuser" and database "maxuser" (hardoded in
    pgbackendconnection.hh). With those setup psql
    command line will work.

--- server/modules/protocol/Postgres/pgclientconnection.cc
+++ server/modules/protocol/Postgres/pgclientconnection.cc
@@ -14,6 +14,31 @@
 #include "pgclientconnection.hh"
 #include <maxscale/dcb.hh>
 
+namespace
+{
+GWBUF create_startup_reply(const GWBUF& startup_message)
+{
+    // Minimal reply
+    const auto auth_len = 1 + 4 + 4;    // Byte1('R'), Int32(8) len, Int32(0) ok
+    const auto rdy_len = 1 + 4 + 1;     // Byte1('Z'), Int32(5) len, Byte1('I') for idle
+    GWBUF resp{auth_len + rdy_len};
+
+    uint8_t* ptr = resp.data();
+
+    // Authentication OK
+    *ptr++ = pg::AUTHENTICATION;
+    ptr += pg::set_uint32(ptr, 8);
+    ptr += pg::set_uint32(ptr, 0);
+
+    // Ready for query
+    *ptr++ = pg::READY_FOR_QUERY;
+    ptr += pg::set_uint32(ptr, 5);
+    *ptr++ = 'I';   // Idle
+
+    return resp;
+}
+}
+
 PgClientConnection::PgClientConnection(MXS_SESSION* pSession, mxs::Component* pComponent)
     : m_session(pSession)
     , m_down(pComponent)
@@ -26,12 +51,85 @@ void PgClientConnection::ready_for_reading(DCB* dcb)
 
     if (m_session->state() == MXS_SESSION::State::CREATED && !m_session->start())
     {
+        // TODO Deal with this occasional timing issue
+        return;
+    }
+
+    pg::ExpectCmdByte expect = m_state == State::INIT ? pg::ExpectCmdByte::NO : pg::ExpectCmdByte::YES;
+
+    if (auto [ok, gwbuf] = pg::read_packet(m_dcb, expect); ok && gwbuf)
+    {
+        switch (m_state)
+        {
+        case State::INIT:
+            m_state = state_init(gwbuf);
+            break;
+
+        case State::SSL_HANDSHAKE:
+            m_state = state_ssl_handshake(gwbuf);
+            break;
+
+        case State::AUTH:
+            m_state = state_auth(gwbuf);
+            break;
+
+        case State::ROUTE:
+            m_state = state_route(std::move(gwbuf));
+            break;
+
+        case State::ERROR:
+            // pass, handled below
+            break;
+        }
+    }
+
+    // TODO check more states and such, kill session on error
+    if (m_state == State::ERROR)
+    {
         m_session->kill();
     }
-    else if (auto [ok, buf] = m_dcb->read(0, 0); ok && buf)
+}
+
+PgClientConnection::State PgClientConnection::state_init(const GWBUF& gwbuf)
+{
+    State next_state = State::ERROR;
+
+    uint32_t first_word = pg::get_uint32(gwbuf.data() + 4);
+
+    if (gwbuf.length() == 8 && first_word == pg::SSLREQ_MAGIC)
     {
-        m_down->routeQuery(std::move(buf));
+        uint8_t authok[] = {pg::SSLREQ_NO};     // TODO no SSL yet
+        write(GWBUF {authok, sizeof(authok)});
+        next_state = State::INIT;               // Waiting for Startup message
     }
+    else
+    {
+        m_dcb->writeq_append(create_startup_reply(gwbuf));
+        // TODO: are there more packets that should be read from the dcb
+        //       before going to ROUTE or "normal" state.
+        next_state = State::ROUTE;
+    }
+
+    return next_state;
+}
+
+PgClientConnection::State PgClientConnection::state_ssl_handshake(const GWBUF& gwbuf)
+{
+    MXB_ALERT("Not implemented yet: %s", __func__);
+    return State::ERROR;
+}
+
+PgClientConnection::State PgClientConnection::state_auth(const GWBUF& gwbuf)
+{
+    MXB_ALERT("Not implemented yet: %s", __func__);
+    return State::ERROR;
+}
+
+PgClientConnection::State PgClientConnection::state_route(GWBUF&& gwbuf)
+{
+    m_down->routeQuery(std::move(gwbuf));
+
+    return State::ROUTE;
 }
 
 void PgClientConnection::write_ready(DCB* dcb)
@@ -63,7 +161,7 @@ bool PgClientConnection::write(GWBUF&& buffer)
 
 bool PgClientConnection::init_connection()
 {
-    MXB_ALERT("Not implemented yet: %s", __func__);
+    // The client will send the first message
     return true;
 }
 
--- server/modules/protocol/Postgres/pgclientconnection.hh
+++ server/modules/protocol/Postgres/pgclientconnection.hh
@@ -41,6 +41,21 @@ public:
     size_t sizeof_buffers() const override;
 
 private:
+    enum class State
+    {
+        INIT,          // Expecting either SSL request or Startup msg
+        SSL_HANDSHAKE, // State if SSL was requested
+        AUTH,          // Authentication (not entered if method is trust)
+        ROUTE,         // Entered after Startup msg reply has been sent
+        ERROR
+    };
+
+    State state_init(const GWBUF& gwbuf);
+    State state_ssl_handshake(const GWBUF& gwbuf);
+    State state_auth(const GWBUF& gwbuf);
+    State state_route(GWBUF&& gwbuf);
+
+    State           m_state = State::INIT;
     MXS_SESSION*    m_session;
     mxs::Component* m_down;
 };
