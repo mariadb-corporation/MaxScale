8a7004c7a230ba65998268b2732f9c6136f98f01
Johan Wikman <johan.wikman@mariadb.com>
2023-03-07 10:25:36 +0200

    MXS-4523 Add mxs::TestParser
    mxs::TestParser is a parser class intended to be used only in
    a test-setup. It saves you from manually setting up things that
    in the normal MaxScale setting are set up.

---
+++ include/maxscale/testparser.hh
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2026-12-27
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#include <maxscale/ccdefs.hh>
+#include <maxscale/cachingparser.hh>
+
+namespace maxscale
+{
+
+/**
+ * @class TestParser
+ *
+ * A instantiable parser convenience class, intended to be used in testing.
+ * It loads the plugin, initializes it and the CachingParser as well. Any
+ * errors during the setup are reported using std::runtime_error exceptions.
+ */
+class TestParser : public CachingParser
+{
+public:
+    static constexpr const char* DEFAULT_PLUGIN = "qc_sqlite";
+
+    TestParser(const TestParser&) = delete;
+    TestParser& operator=(const TestParser&) = delete;
+
+    TestParser()
+        : TestParser(DEFAULT_PLUGIN, QC_SQL_MODE_DEFAULT, std::string {})
+    {
+    }
+
+    TestParser(const std::string& plugin)
+        : TestParser(plugin, QC_SQL_MODE_DEFAULT, std::string {})
+    {
+    }
+
+    TestParser(const std::string& plugin, qc_sql_mode_t sql_mode)
+        : TestParser(plugin, sql_mode, std::string {})
+    {
+    }
+
+    TestParser(const std::string& plugin, const std::string& plugin_args)
+        : TestParser(plugin, QC_SQL_MODE_DEFAULT, plugin_args)
+    {
+    }
+
+    TestParser(const std::string& plugin, qc_sql_mode_t sql_mode, const std::string& plugin_args);
+    ~TestParser();
+};
+
+}
