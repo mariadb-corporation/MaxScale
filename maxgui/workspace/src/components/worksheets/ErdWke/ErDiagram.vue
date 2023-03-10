70143a5ff8b924d7bbb68a31e941cb238f3c85e1
Thien Ly <thien.ly@mariadb.com>
2023-03-10 10:15:30 +0200

    MXS-4544 Preliminary implementation of `er-diagram` component
    The graph is drawn using `d3-force` which is Force-directed graph
    layout library using velocity Verlet integration. The library is used
    to setup layout of nodes to prevent it from being collided.

---
+++ maxgui/workspace/src/components/worksheets/ErdWke/ErDiagram.vue
@@ -0,0 +1,193 @@
+<template>
+    <graph-board
+        class="erd-graph fill-height"
+        :dim="ctrDim"
+        :graphDim="ctrDim"
+        @get-graph-ctr="svgGroup = $event"
+    >
+        <template v-slot:append="{ data: { transform, zoom } }">
+            <graph-nodes
+                ref="graphNodes"
+                :nodes="graphNodes"
+                :coordMap.sync="graphNodeCoordMap"
+                :style="{ transform }"
+                :defNodeSize="defNodeSize"
+                draggable
+                :boardZoom="zoom"
+                dynHeight
+                dynWidth
+                @node-size-map="dynNodeSizeMap = $event"
+                @drag="onNodeDrag"
+                @drag-end="onNodeDragEnd"
+            >
+                <template v-slot:default="{ data: { node } }">
+                    <div class="px-2" :style="{ backgroundColor: node.data.bgColor }">
+                        <div class="text-center font-weight-bold">{{ node.id }}</div>
+                        <div v-for="col in node.data.cols" :key="`key_${node.id}_${col}`">
+                            {{ col }}
+                        </div>
+                    </div>
+                </template>
+            </graph-nodes>
+        </template>
+    </graph-board>
+</template>
+
+<script>
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-02-21
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+import { line, curveLinear } from 'd3-shape'
+import { forceSimulation, forceLink, forceManyBody, forceCenter, forceCollide } from 'd3-force'
+import GraphBoard from '@share/components/common/MxsCharts/GraphBoard.vue'
+import GraphNodes from '@share/components/common/MxsCharts/GraphNodes.vue'
+import { drawLinks, changeLinkGroupStyle } from '@share/components/common/MxsCharts/utils'
+
+export default {
+    name: 'er-diagram',
+    components: {
+        'graph-board': GraphBoard,
+        'graph-nodes': GraphNodes,
+    },
+    props: {
+        ctrDim: { type: Object, required: true },
+        data: { type: Object, required: true },
+    },
+    data() {
+        return {
+            graphDim: this.ctrDim,
+            svgGroup: null,
+            graphNodeCoordMap: {},
+            graphData: { nodes: [], links: [] },
+            graphNodes: [], // staging data
+            svg: null,
+            simulation: null,
+            defNodeSize: { width: 200, height: 100 },
+            dynNodeSizeMap: {},
+            sizeChangesCount: 0,
+            isDraggingNode: false,
+            highlightedLinks: [],
+            fieldHeight: 22,
+        }
+    },
+    watch: {
+        /**
+         * The dynNodeSizeMap is calculated after the nodes are rendered in graph-nodes,
+         * so the handleCollision method can only get the correct node sizes on the second render.
+         * Also, the handleCollision method should only be automatically executed once because
+         * if there is a new col added to the table, it shouldn't be triggered to keep the
+         * layout steady.
+         */
+        dynNodeSizeMap: {
+            deep: true,
+            handler(v) {
+                if (!this.$typy(v).isEmptyObject && this.sizeChangesCount < 1) {
+                    this.sizeChangesCount++
+                    this.handleCollision()
+                }
+            },
+        },
+        isDraggingNode(v) {
+            for (const link of this.highlightedLinks) changeLinkGroupStyle({ link, isDragging: v })
+        },
+        data: {
+            deep: true,
+            immediate: true,
+            handler(v, oV) {
+                if (!this.$helpers.lodash.isEqual(v, oV)) this.assignData()
+            },
+        },
+    },
+    mounted() {
+        this.drawChart(this.graphData)
+    },
+    methods: {
+        assignData() {
+            this.graphData = this.data
+            this.graphNodes = this.$helpers.lodash.cloneDeep(this.graphData.nodes)
+        },
+        drawChart() {
+            if (this.graphData.nodes.length) {
+                const { nodes, links } = this.graphData
+                this.simulation = forceSimulation(nodes)
+                    .force(
+                        'link',
+                        forceLink(links).id(d => d.id)
+                    )
+                    .force('charge', forceManyBody().theta(0.5))
+                    .force('center', forceCenter(this.ctrDim.width / 2, this.ctrDim.height / 2))
+                    .on('tick', () => {
+                        this.setGraphNodeCoordMap()
+                        this.handleDrawLinks()
+                    })
+            }
+        },
+        setGraphNodeCoordMap() {
+            this.graphNodeCoordMap = this.graphData.nodes.reduce((map, n) => {
+                const { x, y, id } = n
+                if (id) map[id] = { x, y }
+                return map
+            }, {})
+        },
+        //TODO: Draw the link from the row of the source table to the row of the target table
+        linkPathGenerator(d) {
+            return line()
+                .x(d => d.x)
+                .y(d => d.y)
+                .curve(curveLinear)([
+                { x: d.source.x, y: d.source.y },
+                { x: d.target.x, y: d.target.y },
+            ])
+        },
+        /**
+         * @param {Object} node -  node
+         * @returns {Object} - { width: Number, height: Number}
+         */
+        getNodeSize(node) {
+            return this.$refs.graphNodes.getNodeSize(this.$typy(node, 'id').safeString)
+        },
+        linkStrokeGenerator: d => d.source.data.linkColor,
+        handleDrawLinks() {
+            drawLinks({
+                containerEle: this.svgGroup,
+                data: this.simulation.force('link').links(),
+                linkPathGenerator: this.linkPathGenerator,
+                linkStrokeGenerator: this.linkStrokeGenerator,
+            })
+        },
+        handleCollision() {
+            this.simulation.force(
+                'collide',
+                forceCollide().radius(d => {
+                    const { width, height } = this.getNodeSize(d)
+                    // Because nodes are densely packed,  this adds an extra radius of 25 pixels to the nodes
+                    return Math.sqrt(width * width + height * height) / 2 + 25
+                })
+            )
+        },
+        onNodeDrag({ node, diffX, diffY }) {
+            this.isDraggingNode = true
+            const nodeData = this.graphData.nodes.find(n => n.id === node.id)
+            nodeData.x = nodeData.x + diffX
+            nodeData.y = nodeData.y + diffY
+            const links = this.simulation.force('link').links()
+            this.highlightedLinks = links.filter(
+                d => d.source.id === node.id || d.target.id === node.id
+            )
+            this.handleDrawLinks()
+        },
+        onNodeDragEnd() {
+            this.isDraggingNode = false
+        },
+    },
+}
+</script>
