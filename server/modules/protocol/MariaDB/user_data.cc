ebb915f8d85e127f006432387df95422a086dc80
Markus Mäkelä <markus.makela@mariadb.com>
2023-03-21 14:56:31 +0200

    MXS-4556: Fix lower_case_table_names
    When set to 1 in MaxScale, the comparison was not case-insensitive like
    it is in MariaDB.

--- server/modules/protocol/MariaDB/user_data.cc
+++ server/modules/protocol/MariaDB/user_data.cc
@@ -1635,6 +1635,7 @@ MariaDBUserCache::find_user(const string& user, const string& host, const string
 
     case UserDatabase::DBNameCmpMode::LOWER_CASE:
         eff_requested_db = mxb::tolower(requested_db);
+        case_sensitive_db = false;
         break;
 
     case UserDatabase::DBNameCmpMode::CASE_INSENSITIVE:
