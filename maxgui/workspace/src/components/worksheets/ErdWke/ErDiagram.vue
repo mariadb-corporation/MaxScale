1e45e8dac0540b7bbc654035c993bb59fbf01947
Thien Ly <thien.ly@mariadb.com>
2023-03-31 08:54:18 +0300

    MXS-4544 Add function to draw entity relation link
    This gets inspiration from `https://app.diagrams.net/` for the
    "entity relation" link in terms of the way the shape of the link
    being changed depends on the position of the nodes.
    
    This also adjusts `handleCollision` to add 100px as an extra radius
    to prevent nodes and its link from being overlapped.

--- maxgui/workspace/src/components/worksheets/ErdWke/ErDiagram.vue
+++ maxgui/workspace/src/components/worksheets/ErdWke/ErDiagram.vue
@@ -24,6 +24,7 @@
                                 :style="{
                                     backgroundColor: node.data.highlightColor,
                                     height: `${entityHeaderHeight}px`,
+                                    color: node.data.headerTxtColor,
                                 }"
                             >
                                 {{ node.id }}
@@ -61,7 +62,6 @@
  * of this software will be governed by version 2 or later of the General
  * Public License.
  */
-import { line, curveLinear } from 'd3-shape'
 import {
     forceSimulation,
     forceLink,
@@ -101,7 +101,12 @@ export default {
             entityHeaderHeight: 32,
         }
     },
-
+    computed: {
+        // Ensure that the marker remains visible while dragging a node by allocating a specific width.
+        relMarkFixedWidth() {
+            return 30
+        },
+    },
     watch: {
         isDraggingNode(v) {
             for (const link of this.highlightedLinks) changeLinkGroupStyle({ link, isDragging: v })
@@ -170,54 +175,142 @@ export default {
             }, {})
         },
         /**
-         * Reposition the y value to the center of the relational column
-         * @param {Object} param.node - entity node
-         * @param {String} param.col - name of the relational column
+         * Return the y position of a node based on its dynamic height and
+         * the provided column name
+         * @param {Object} param.node - The node to reposition.
+         * @param {string} param.col - The name of the relational column
+         * @returns {number} The new y position of the node.
          */
-        repositionY({ node, col }) {
-            const size = this.dynNodeSizeMap[node.id]
+        getColYPos({ node, col }) {
+            const nodeHeight = this.dynNodeSizeMap[node.id].height
             const colIdx = node.data.cols.findIndex(c => c.name === col)
             return (
                 node.y +
-                size.height / 2 -
-                (size.height - this.entityHeaderHeight) +
+                nodeHeight / 2 -
+                (nodeHeight - this.entityHeaderHeight) +
                 colIdx * this.trHeight +
                 this.trHeight / 2
             )
         },
         /**
-         * Reposition the x value to either right or left edge of the relational column
-         * @param {Object} param.source - source node
-         * @param {Object} param.target - target node
+         * Get the y position of source and target nodes
+         * @param {Object} param.source - The source node of the relationship.
+         * @param {Object} param.target - The target node of the relationship.
+         * @param {string} param.relationshipData.source_col - The name of the relational column of the source
+         * @param {string} param.relationshipData.target_col - The name of the relational column of the target
+         * @returns {Object} An object containing the new y positions of the source and target nodes.
          */
-        handleRepositionX({ source, target }) {
-            const sourceSize = this.dynNodeSizeMap[source.id]
-            const targetSize = this.dynNodeSizeMap[target.id]
-            const isSrcRightWard = source.x - target.x > 0
-            const srcOffset = sourceSize.width / 2,
-                targetOffset = targetSize.width / 2
+        getYPositions({ source, target, relationshipData }) {
+            const { source_col, target_col } = relationshipData
             return {
-                sourceX: source.x + (isSrcRightWard ? -srcOffset : srcOffset),
-                targetX: target.x + (isSrcRightWard ? targetOffset : -targetOffset),
+                y0: this.getColYPos({ node: source, col: source_col }),
+                y1: this.getColYPos({ node: target, col: target_col }),
             }
         },
-        //TODO: Replace curveLinear with a custom function
+        /**
+         * Checks the horizontal position of the target node.
+         * @param {number} params.srcX - The horizontal position of the source node relative to the center.
+         * @param {number} params.targetX - The horizontal position of the target node relative to the center.
+         * @param {number} params.halfSrcWidth - Half the width of the source node.
+         * @param {number} params.halfTargetWidth - Half the width of the target node.
+         * @returns {string} Returns the relative position of the target node to the source node.
+         * "target-right" if the target node is to the right of the source node.
+         * "target-left" if the target node is to the left of the source node.
+         * "intersect" if the target node intersects with the source node.
+         */
+        checkTargetPosX({ srcX, targetX, halfSrcWidth, halfTargetWidth }) {
+            const srcZone = [srcX - halfSrcWidth, srcX + halfSrcWidth],
+                targetZone = [targetX - halfTargetWidth, targetX + halfTargetWidth]
+            if (targetZone[0] - srcZone[1] >= 0) return 'target-right'
+            else if (srcZone[0] - targetZone[1] >= 0) return 'target-left'
+            return 'intersect'
+        },
+        /**
+         * Repositions the x position of a node and its target based on the
+         * dynamic node sizes and the distance between them.
+         * @param {Object} param.source - The source node.
+         * @param {Object} param.target - The target node.
+         * @returns {Object} An object containing the x positions of the source and target
+         * nodes, as well as the midpoint positions of the link.
+         */
+        getXPositions({ source, target }) {
+            const srcWidth = this.dynNodeSizeMap[source.id].width,
+                targetWidth = this.dynNodeSizeMap[target.id].width,
+                halfSrcWidth = srcWidth / 2,
+                halfTargetWidth = targetWidth / 2
+
+            // D3 returns the mid point of the entities for source.x, target.x
+            const srcX = source.x,
+                targetX = target.x
+
+            let x0 = srcX,
+                x1 = targetX,
+                dx1 = x0,
+                dx4 = x1,
+                dx2,
+                dx3
+
+            switch (this.checkTargetPosX({ srcX, targetX, halfSrcWidth, halfTargetWidth })) {
+                case 'target-right': {
+                    x0 = srcX + halfSrcWidth
+                    x1 = targetX - halfTargetWidth
+                    dx1 = x0 + this.relMarkFixedWidth
+                    dx4 = x1 - this.relMarkFixedWidth
+                    if (dx4 - dx1 <= 0) {
+                        dx2 = dx1
+                        dx3 = dx4
+                    }
+                    break
+                }
+                case 'target-left': {
+                    x0 = srcX - halfSrcWidth
+                    x1 = targetX + halfTargetWidth
+                    dx1 = x0 - this.relMarkFixedWidth
+                    dx4 = x1 + this.relMarkFixedWidth
+                    if (dx1 - dx4 <= 0) {
+                        dx2 = dx1
+                        dx3 = dx4
+                    }
+                    break
+                }
+                case 'intersect': {
+                    // move x0 & x1 to the right edge of the nodes
+                    x0 = srcX + halfSrcWidth
+                    x1 = targetX + halfTargetWidth
+                    dx1 = x1 + this.relMarkFixedWidth
+                    dx4 = dx1
+                    if (dx1 - this.relMarkFixedWidth <= x0) {
+                        dx1 = x0 + this.relMarkFixedWidth
+                        if (dx4 - dx1 <= 0) {
+                            dx2 = dx1
+                            dx3 = dx1
+                        }
+                    }
+                    if (dx4 <= dx1) dx4 = dx1
+                    break
+                }
+            }
+
+            return { x0, x1, dx1, dx2, dx3, dx4 }
+        },
+        //TODO: handle overlapped y pos as a field can be associated with multiple tables
         linkPathGenerator(d) {
-            const {
+            const { source, target, relationshipData } = d
+            const { y0, y1 } = this.getYPositions({ source, target, relationshipData })
+            const { x0, x1, dx1, dx2, dx3, dx4 } = this.getXPositions({
                 source,
                 target,
-                relationshipData: { source_col, target_col },
-            } = d
-            const { sourceX, targetX } = this.handleRepositionX({ source, target })
-            let sourceY = this.repositionY({ node: source, col: source_col }),
-                targetY = this.repositionY({ node: target, col: target_col })
-            return line()
-                .x(d => d.x)
-                .y(d => d.y)
-                .curve(curveLinear)([
-                { x: sourceX, y: sourceY },
-                { x: targetX, y: targetY },
-            ])
+            })
+            const point0 = [x0, y0]
+            const point1 = [dx1, y0]
+            const point4 = [dx4, y1]
+            const point5 = [x1, y1]
+            if (dx2 && dx3) {
+                const point2 = [dx2, (y0 + y1) / 2],
+                    point3 = [dx3, (y0 + y1) / 2]
+                return `M${point0} L${point1} L${point2} L${point3} L${point4} L${point5}`
+            }
+            return `M${point0} L${point1} L${point4} L${point5}`
         },
         /**
          * @param {Object} node -  node
@@ -240,8 +333,8 @@ export default {
                 'collide',
                 forceCollide().radius(d => {
                     const { width, height } = this.getNodeSize(d)
-                    // Because nodes are densely packed,  this adds an extra radius of 25 pixels to the nodes
-                    return Math.sqrt(width * width + height * height) / 2 + 25
+                    // Because nodes are densely packed,  this adds an extra radius of 100 pixels to the nodes
+                    return Math.sqrt(width * width + height * height) / 2 + 100
                 })
             )
         },
@@ -265,7 +358,7 @@ export default {
 
 <style lang="scss" scoped>
 .entity {
-    background: rgba(255, 255, 255, 0);
+    background: white;
     .entity-fields {
         width: 100%;
         border-spacing: 0px;
