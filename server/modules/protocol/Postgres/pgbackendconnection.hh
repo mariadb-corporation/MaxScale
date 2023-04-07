8d9c7cef744f0f28e85f4421d28a8c390163f6f3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-06 21:46:29 -0400

    spelling: duration
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- server/modules/protocol/Postgres/pgbackendconnection.hh
+++ server/modules/protocol/Postgres/pgbackendconnection.hh
@@ -105,7 +105,7 @@ private:
     uint32_t m_process_id {0};      // The process ID on the backend server
     uint32_t m_secret_key {0};      // Secret key for canceling requests
 
-    // Backlog of packets that need to be written again. These are only buffered for the duratio of the
+    // Backlog of packets that need to be written again. These are only buffered for the duration of the
     // connection creation and authentication after which they are re-sent to the write() function.
     std::vector<GWBUF> m_backlog;
 
