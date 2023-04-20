d2b5fc3eb3dc7b2bba5b8a2ea93ba77602891876
Thien Ly <thien.ly@mariadb.com>
2023-04-20 16:41:29 +0300

    MXS-4545 Add regexes to parse column definitions
    This commit parses some column definitions. The rest of column
    definitions will be handled in a later commit.

--- maxgui/workspace/src/utils/TableParser.js
+++ maxgui/workspace/src/utils/TableParser.js
@@ -11,7 +11,7 @@
  * Public License.
  */
 import { t } from 'typy'
-import tokenizer, { ESCAPE } from '@wsSrc/utils/createTableTokenizer'
+import tokenizer from '@wsSrc/utils/createTableTokenizer'
 
 /**
  * This parser works when sql_quote_show_create on
@@ -27,9 +27,47 @@ export default class TableParser {
         while ((match = tokenizer.tableOptions.exec(optsStr)) !== null) {
             const key = match[1]
             const value = match[2]
-            opts[key.toLocaleLowerCase()] = value.replace(new RegExp(ESCAPE, 'g'), '')
+            opts[key.toLocaleLowerCase()] = value
         }
         return opts
+    }
+    parseColDef(def) {
+        const [
+            isMatched,
+            name,
+            dataType,
+            dataTypeValue,
+            un,
+            zf,
+            nn,
+            charset,
+            collate,
+            generated_exp,
+            generated,
+            ai,
+        ] = def.match(tokenizer.colDef) || []
+        if (isMatched) {
+            return {
+                name,
+                dataType,
+                dataTypeValue,
+                isUN: Boolean(un),
+                isZF: Boolean(zf),
+                isNN: Boolean(nn),
+                charset,
+                collate,
+                generated_exp,
+                generated,
+                isAI: Boolean(ai),
+                //TODO: Parse PK, UQ, DEFAULT exp, and COMMENT
+            }
+        }
+        //TODO: Parse reference_definition
+        return def
+    }
+    parseTableDefs(defsStr) {
+        const defs = defsStr.split('\n')
+        return defs.map(def => this.parseColDef(def.trim().replace(/,\s*$/, '')))
     }
     // Parse the result of SHOW CREATE TABLE
     parse(sql) {
@@ -37,8 +75,7 @@ export default class TableParser {
         let tbl_name, table_options, table_definitions
         if (match) {
             tbl_name = t(match, '[1]').safeString.trim()
-            //TODO: Parse table definitions
-            table_definitions = t(match, '[2]').safeString.trim()
+            table_definitions = this.parseTableDefs(t(match, '[2]').safeString.trim())
             table_options = this.parseTableOpts(t(match, '[3]').safeString)
         }
         return {
--- maxgui/workspace/src/utils/createTableTokenizer.js
+++ maxgui/workspace/src/utils/createTableTokenizer.js
@@ -10,25 +10,53 @@
  * of this software will be governed by version 2 or later of the General
  * Public License.
  */
-export const WHITESPACE_ZERO_OR_MORE = '\\s*'
-export const PAREN_OPEN = '\\('
-export const PAREN_CLOSE = '\\)'
-export const ALL_CHARS_OPTIONAL = '[\\s\\S]*'
-export const ESCAPE = '[\'"`]'
 
-const TBL_NAME = `${ESCAPE}(.*)${ESCAPE}`
+const WHITESPACE_ZERO_OR_MORE = '\\s*'
+const WHITESPACE_ONE_OR_MORE = '\\s+'
+const PAREN_OPEN = '\\('
+const PAREN_CLOSE = '\\)'
+const ANY_CHARS_ZERO_OR_MORE = '[\\s\\S]*'
+const ESCAPE = '[\'"`]'
 
-const TBL_DEFS = [PAREN_OPEN, `(${ALL_CHARS_OPTIONAL})`, PAREN_CLOSE].join(WHITESPACE_ZERO_OR_MORE)
+const ESCAPE_NAME = `${ESCAPE}.*?${ESCAPE}`
 
-const TBL_OPTS = `(${ALL_CHARS_OPTIONAL})`
+const TBL_DEFS = [PAREN_OPEN, `(${ANY_CHARS_ZERO_OR_MORE})`, PAREN_CLOSE].join(
+    WHITESPACE_ZERO_OR_MORE
+)
+const TBL_OPTS = `(${ANY_CHARS_ZERO_OR_MORE})`
+
+const DATA_TYPE_DEF = '(\\w+)' + '(?:\\((.*?)\\))?' // name + size
+
+// matching by groups
+const UN = '(UNSIGNED)?'
+const ZF = '(ZEROFILL)?'
+const NN = '(NOT NULL)?'
+const CHARSET = '(?:CHARACTER SET\\s+(\\w*))?'
+const COLLATE = '(?:COLLATE\\s(\\w*))?'
+
+// group 1: generated expression value, group 2: type of generated
+const GENERATED =
+    '(?:GENERATED ALWAYS AS' +
+    WHITESPACE_ONE_OR_MORE +
+    '(?:\\((.*)\\))' +
+    WHITESPACE_ONE_OR_MORE +
+    '(VIRTUAL|PERSISTENT|STORED))?'
+
+const AI = '(AUTO_INCREMENT)?'
 
 export default {
     createTable: new RegExp(
-        ['CREATE', 'TABLE', TBL_NAME, TBL_DEFS, TBL_OPTS].join(WHITESPACE_ZERO_OR_MORE),
+        ['CREATE', 'TABLE', `(${ESCAPE_NAME})`, TBL_DEFS, TBL_OPTS].join(WHITESPACE_ZERO_OR_MORE),
         'i'
     ),
     tableOptions: new RegExp(
-        ['(\\w+)', '=', `(${ESCAPE}(.*)${ESCAPE}|(\\w+))`].join(WHITESPACE_ZERO_OR_MORE),
+        ['(\\w+)', '=', `(${ESCAPE}.*${ESCAPE}|\\w+)`].join(WHITESPACE_ZERO_OR_MORE),
         'gi'
+    ),
+    colDef: new RegExp(
+        [`(${ESCAPE_NAME})`, DATA_TYPE_DEF].join(WHITESPACE_ONE_OR_MORE) +
+            WHITESPACE_ZERO_OR_MORE +
+            [UN, ZF, NN, CHARSET, COLLATE, GENERATED, AI].join(WHITESPACE_ZERO_OR_MORE),
+        'i'
     ),
 }
