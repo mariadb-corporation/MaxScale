dea00cfe60f521522b768d428793d7091f0e4b32
Johan Wikman <johan.wikman@mariadb.com>
2023-05-10 14:24:53 +0300

    MXS-4610 Move content of paths.(hh|cc) to config.(hh|cc)
    And remove paths.cc. Paths.hh will be removed later.
    
    The paths are part of the MaxScale configuration.

--- include/maxscale/config.hh
+++ include/maxscale/config.hh
@@ -24,6 +24,137 @@
 namespace maxscale
 {
 
+/**
+ * Set the library directory. Modules will be loaded from here.
+ */
+void set_libdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the share directory
+ */
+void set_sharedir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the data directory
+ */
+void set_datadir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the process data directory
+ */
+void set_process_datadir(std::string_view path);
+
+/**
+ * Set the cache directory
+ */
+void set_cachedir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the configuration file directory
+ */
+void set_configdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the configuration parts file directory
+ */
+void set_config_persistdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the module configuration file directory
+ */
+void set_module_configdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the log file directory
+ */
+void set_logdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the language file directory
+ */
+void set_langdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the PID file directory
+ */
+void set_piddir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the executable directory. Internal processes will look for executables
+ * from here.
+ */
+void set_execdir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Set the connector plugin directory.
+ */
+void set_connector_plugindir(std::string_view path, config::Origin origin = config::Origin::USER);
+
+/**
+ * Get the directory with all the modules.
+ */
+const char* libdir();
+
+/**
+ * Get the share directory
+ */
+const char* sharedir();
+
+/**
+ * Get the MaxScale data directory
+ */
+const char* datadir();
+
+/**
+ * Get the process specific data directory
+ */
+const char* process_datadir();
+
+/**
+ * Get the service cache directory
+ */
+const char* cachedir();
+
+/**
+ * Get the configuration file directory
+ */
+const char* configdir();
+
+/**
+ * Get the configuration file directory
+ */
+const char* config_persistdir();
+
+/**
+ * Get the module configuration file directory
+ */
+const char* module_configdir();
+
+/**
+ * Get the PID file directory which contains maxscale.pid
+ */
+const char* piddir();
+
+/**
+ * Return the log file directory
+ */
+const char* logdir();
+
+/**
+ * Path to the directory which contains the errmsg.sys language file
+ */
+const char* langdir();
+
+/**
+ * Get the directory with the executables.
+ */
+const char* execdir();
+
+/**
+ * Get connector plugin directory
+ */
+const char* connector_plugindir();
+
 // JSON Web Token signature algorithms
 enum class JwtAlgo
 {
--- server/core/config.cc
+++ server/core/config.cc
@@ -60,6 +60,7 @@
 #include "internal/adminusers.hh"
 #include "internal/config.hh"
 #include "internal/configmanager.hh"
+#include "internal/defaults.hh"
 #include "internal/event.hh"
 #include "internal/filter.hh"
 #include "internal/modules.hh"
@@ -120,6 +121,28 @@ constexpr char CN_SERVER[] = "server";
 static int64_t DEFAULT_QC_CACHE_SIZE = get_total_memory() * 0.15;
 static int64_t DEFAULT_MAX_READ_AMOUNT = 0;
 
+struct Directory
+{
+    Directory(const char* zPath)
+        : path(zPath)
+    {
+    }
+
+    void set(std::string_view path, mxs::config::Origin origin)
+    {
+        mxb_assert(origin != mxs::config::Origin::DEFAULT);
+
+        if (static_cast<int>(origin) >= static_cast<int>(this->origin))
+        {
+            this->path = clean_up_pathname(std::string{path});
+            this->origin = origin;
+        }
+    }
+
+    std::string path;
+    mxs::config::Origin origin { mxs::config::Origin::DEFAULT };
+};
+
 struct ThisUnit
 {
     bool mask_passwords = true;
@@ -132,12 +155,156 @@ struct ThisUnit
 
     // The names of all objects mapped to the source file they were read from.
     std::map<std::string, std::string> source_files;
+
+    Directory   configdir           { cmake_defaults::DEFAULT_CONFIGDIR };
+    Directory   config_persistdir   { cmake_defaults::DEFAULT_CONFIG_PERSISTDIR };
+    Directory   module_configdir    { cmake_defaults::DEFAULT_MODULE_CONFIGDIR };
+    Directory   logdir              { cmake_defaults::DEFAULT_LOGDIR };
+    Directory   libdir              { cmake_defaults::DEFAULT_LIBDIR };
+    Directory   sharedir            { cmake_defaults::DEFAULT_SHAREDIR };
+    Directory   cachedir            { cmake_defaults::DEFAULT_CACHEDIR };
+    Directory   datadir             { cmake_defaults::DEFAULT_DATADIR };
+    std::string processdatadir      { cmake_defaults::DEFAULT_DATADIR };
+    Directory   langdir             { cmake_defaults::DEFAULT_LANGDIR };
+    Directory   piddir              { cmake_defaults::DEFAULT_PIDDIR };
+    Directory   execdir             { cmake_defaults::DEFAULT_EXECDIR };
+    Directory   connector_plugindir { cmake_defaults::DEFAULT_CONNECTOR_PLUGINDIR };
 } this_unit;
 
 }
 
 namespace maxscale
 {
+
+void set_configdir(std::string_view path, config::Origin origin)
+{
+    this_unit.configdir.set(path, origin);
+}
+
+void set_module_configdir(std::string_view path, config::Origin origin)
+{
+    this_unit.module_configdir.set(path, origin);
+}
+
+void set_config_persistdir(std::string_view path, config::Origin origin)
+{
+    this_unit.config_persistdir.set(path, origin);
+}
+
+void set_logdir(std::string_view path, config::Origin origin)
+{
+    this_unit.logdir.set(path, origin);
+}
+
+void set_langdir(std::string_view path, config::Origin origin)
+{
+    this_unit.langdir.set(path, origin);
+}
+
+void set_piddir(std::string_view path, config::Origin origin)
+{
+    this_unit.piddir.set(path, origin);
+}
+
+void set_cachedir(std::string_view path, config::Origin origin)
+{
+    this_unit.cachedir.set(path, origin);
+}
+
+void set_datadir(std::string_view path, config::Origin origin)
+{
+    this_unit.datadir.set(path, origin);
+}
+
+void set_process_datadir(std::string_view path)
+{
+    this_unit.processdatadir = clean_up_pathname(std::string{path});
+}
+
+void set_libdir(std::string_view path, config::Origin origin)
+{
+    this_unit.libdir.set(path, origin);
+}
+
+void set_sharedir(std::string_view path, config::Origin origin)
+{
+    this_unit.sharedir.set(path, origin);
+}
+
+void set_execdir(std::string_view path, config::Origin origin)
+{
+    this_unit.execdir.set(path, origin);
+}
+
+void set_connector_plugindir(std::string_view path, config::Origin origin)
+{
+    this_unit.connector_plugindir.set(path, origin);
+}
+
+const char* libdir()
+{
+    return this_unit.libdir.path.c_str();
+}
+
+const char* sharedir()
+{
+    return this_unit.sharedir.path.c_str();
+}
+
+const char* cachedir()
+{
+    return this_unit.cachedir.path.c_str();
+}
+
+const char* datadir()
+{
+    return this_unit.datadir.path.c_str();
+}
+
+const char* process_datadir()
+{
+    return this_unit.processdatadir.c_str();
+}
+
+const char* configdir()
+{
+    return this_unit.configdir.path.c_str();
+}
+
+const char* module_configdir()
+{
+    return this_unit.module_configdir.path.c_str();
+}
+
+const char* config_persistdir()
+{
+    return this_unit.config_persistdir.path.c_str();
+}
+
+const char* piddir()
+{
+    return this_unit.piddir.path.c_str();
+}
+
+const char* logdir()
+{
+    return this_unit.logdir.path.c_str();
+}
+
+const char* langdir()
+{
+    return this_unit.langdir.path.c_str();
+}
+
+const char* execdir()
+{
+    return this_unit.execdir.path.c_str();
+}
+
+const char* connector_plugindir()
+{
+    return this_unit.connector_plugindir.path.c_str();
+}
 
 bool Config::Specification::validate(const Configuration* pConfig,
                                      const ConfigParameters& params,
