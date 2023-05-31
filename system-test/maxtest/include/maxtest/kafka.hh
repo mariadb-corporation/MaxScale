c033d7794fefe1f8a7677f31fda9a4311fa4bcff
Markus Mäkelä <markus.makela@mariadb.com>
2023-05-31 18:25:12 +0300

    Reset test timeouts after each Kafka installation step
    This should help avoid the test from timing out if the installation step
    takes a long time.

--- system-test/maxtest/include/maxtest/kafka.hh
+++ system-test/maxtest/include/maxtest/kafka.hh
@@ -111,6 +111,7 @@ wget -q "https://www.apache.org/dyn/closer.cgi?filename=/kafka/2.7.0/kafka_2.13-
             return false;
         }
 
+        m_test.reset_timeout();
         std::string file = m_test.maxscale->access_homedir();
         file += "/kafka_2.13-2.7.0.tgz";
 
@@ -120,6 +121,8 @@ wget -q "https://www.apache.org/dyn/closer.cgi?filename=/kafka/2.7.0/kafka_2.13-
             return false;
         }
 
+        m_test.reset_timeout();
+
         // TODO: Install java with mdbci, this is a dumb workaround that will fail at some point.
         if (m_test.maxscale->ssh_node_f(false, "sudo yum -y install java-latest-openjdk;") != 0
             && m_test.maxscale->ssh_node_f(false, "sudo yum -y install java-11-openjdk;") != 0)
@@ -127,6 +130,8 @@ wget -q "https://www.apache.org/dyn/closer.cgi?filename=/kafka/2.7.0/kafka_2.13-
             m_test.add_failure("Failed to install java-latest-openjdk");
             return false;
         }
+
+        m_test.reset_timeout();
 
         // The link can be updated by getting the closest mirror link from the Kafka download page and
         // changing `file` to `filename` and adding `action=download` (these are options to closer.cgi).
@@ -143,6 +148,7 @@ mv kafka_2.13-2.7.0 kafka;
             return false;
         }
 
+        m_test.reset_timeout();
         return true;
     }
 
