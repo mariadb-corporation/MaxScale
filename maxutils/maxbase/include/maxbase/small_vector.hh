2a79f106c5105222eee48720a150ffa22df4a0e1
Markus Mäkelä <markus.makela@mariadb.com>
2023-06-16 13:34:14 +0300

    Add mxb::small_vector
    The small_vector is a generalization of the std::string optimization for
    small strings. The most known implementations of these types of
    containers are in LLVM and Boost. The container is optimized for use as
    a small sort buffer that does not allocate heap storage if the number of
    elements is below a compile-time constant.
    
    For MaxScale, this is beneficial for readwritesplit that used a
    std::vector to sort a handful of pointers. By swapping out the
    std::vector for a mxb::small_vector, the number of heap allocations
    drops down to zero if the number of backend servers remains below 4. The
    choice of how many values to store internally was mostly arbitrary but
    with 4 values the size of the Candidates type ends up being exactly 64
    bytes.
    
    Added a unit test that makes sure the behavior of the container is
    identical to a std::vector for the supported operations.

---
+++ maxutils/maxbase/include/maxbase/small_vector.hh
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-05-22
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#include <maxbase/ccdefs.hh>
+#include <maxbase/assert.hh>
+
+#include <array>
+#include <algorithm>
+#include <vector>
+#include <variant>
+
+namespace maxbase
+{
+
+// Calculates the number of values that can be stored "for free" in a mxb::small_vector. This is the number of
+// elements that fit into the same space that the heap portion uses. Usually this is around 23 bytes of
+// storage. Values larger than the "free space" return 1.
+template<class T>
+constexpr uint8_t ideal_small_vector_size()
+{
+    size_t ideal_size = (sizeof(std::vector<T>) - sizeof(uint8_t)) / sizeof(T);
+    return std::clamp(ideal_size, 1UL, (size_t)std::numeric_limits<uint8_t>::max());
+}
+
+static_assert(ideal_small_vector_size<uint8_t>() == 23);
+static_assert(ideal_small_vector_size<uint16_t>() == 11);
+static_assert(ideal_small_vector_size<uint32_t>() == 5);
+static_assert(ideal_small_vector_size<uint64_t>() == 2);
+static_assert(ideal_small_vector_size<std::array<uint64_t, 4>>() == 1);
+
+// A very simple implementation of a small vector, similar to the ones found in LLVM and Boost (InnoDB
+// supposedly has one too). It allocates some internal storage but moves onto heap storage if the internal one
+// runs out. The container is primarily designed for use as a temporary sort buffer and it doesn't switch back
+// to the internal storage unless clear() is called or the container goes empty as a result of a erase() call.
+//
+// The implementation uses a std::variant to share the storage used by the heap and the internal storage
+// implementations. This makes it possible to store some values "for free" without having to allocate separate
+// space for them. The container might store more internal values than is requested if it does not cause
+// the size of the object to grow.
+template<class T, uint8_t Capacity = 1>
+class small_vector
+{
+public:
+    static_assert(Capacity > 0, "Capacity must be positive");
+
+    using size_type = size_t;
+    using difference_type = std::ptrdiff_t;
+    using const_reference = const T&;
+    using const_iterator = const T*;
+    using Big = std::vector<T>;
+
+    static constexpr uint8_t REAL_CAPACITY = std::max(Capacity, ideal_small_vector_size<T>());
+
+    struct Small
+    {
+        std::array<T, REAL_CAPACITY> data;
+        uint8_t                      size {0};
+    };
+
+    void push_back(T t)
+    {
+        if (is_small())
+        {
+            Small& stack = std::get<Small>(m_storage);
+
+            if (stack.size == stack.data.size())
+            {
+                Big heap;
+                heap.reserve(stack.size + 1);
+                heap.assign(stack.data.begin(), stack.data.end());
+                heap.push_back(std::move(t));
+                m_storage = std::move(heap);
+            }
+            else
+            {
+                mxb_assert(stack.size < stack.data.size());
+                stack.data[stack.size++] = std::move(t);
+            }
+        }
+        else
+        {
+            std::get<Big>(m_storage).push_back(std::move(t));
+        }
+    }
+
+    void erase(const_iterator it)
+    {
+        mxb_assert(it != end());
+        auto offset = std::distance(begin(), it);
+
+        if (is_small())
+        {
+            Small& stack = std::get<Small>(m_storage);
+            std::copy(stack.data.begin() + offset + 1,
+                      stack.data.begin() + stack.size,
+                      stack.data.begin() + offset);
+            --stack.size;
+        }
+        else
+        {
+            Big& heap = std::get<Big>(m_storage);
+            heap.erase(heap.begin() + offset);
+
+            if (heap.empty())
+            {
+                m_storage = Small{};
+            }
+        }
+    }
+
+    const_iterator begin() const
+    {
+        return is_small() ? std::get<Small>(m_storage).data.data() :  std::get<Big>(m_storage).data();
+    }
+
+    const_iterator end() const
+    {
+        if (is_small())
+        {
+            const Small& stack = std::get<Small>(m_storage);
+            return stack.data.data() + stack.size;
+        }
+        else
+        {
+            const Big& heap = std::get<Big>(m_storage);
+            return heap.data() + heap.size();
+        }
+    }
+
+    const_reference front() const
+    {
+        return is_small() ? std::get<Small>(m_storage).data.front() : std::get<Big>(m_storage).front();
+    }
+
+    const_reference back() const
+    {
+        if (is_small())
+        {
+            const Small& stack = std::get<Small>(m_storage);
+            return stack.data[stack.size - 1];
+        }
+        else
+        {
+            return std::get<Big>(m_storage).back();
+        }
+    }
+
+    const_reference operator[](size_t i) const
+    {
+        return *(begin() + i);
+    }
+
+    size_type size() const
+    {
+        return is_small() ? std::get<Small>(m_storage).size : std::get<Big>(m_storage).size();
+    }
+
+    bool empty() const
+    {
+        return is_small() ? std::get<Small>(m_storage).size == 0 : std::get<Big>(m_storage).empty();
+    }
+
+    void clear()
+    {
+        m_storage = Small{};
+    }
+
+private:
+    bool is_small() const
+    {
+        return std::holds_alternative<Small>(m_storage);
+    }
+
+    std::variant<Small, Big> m_storage{Small{}};
+};
+}
---
+++ maxutils/maxbase/src/test/test_small_vector.cc
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-05-22
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#ifndef SS_DEBUG
+#define SS_DEBUG
+#endif
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include <maxbase/ccdefs.hh>
+#include <maxbase/small_vector.hh>
+
+static int64_t ints = 0;
+
+int identity(int i)
+{
+    return i;
+}
+
+void* int_to_ptr(int i)
+{
+    std::intptr_t p = i;
+    return (void*)p;
+}
+
+std::string int_to_string(int i)
+{
+    return std::to_string(i);
+}
+
+template<size_t Size, class Type, auto Generator>
+constexpr void run_test(size_t num_values)
+{
+    mxb::small_vector<Type, Size> small_vec;
+    std::vector<Type> vec;
+
+    const auto do_check = [&](){
+        for (size_t i = 0; i < vec.size(); i++)
+        {
+            mxb_assert(vec[i] == small_vec[i]);
+        }
+        mxb_assert(vec.size() == small_vec.size());
+        mxb_assert(vec.empty() == small_vec.empty());
+
+        if (!vec.empty())
+        {
+            mxb_assert(vec.front() == small_vec.front());
+            mxb_assert(vec.back() == small_vec.back());
+        }
+    };
+
+    for (size_t i = 0; i < num_values; i++)
+    {
+        Type t = Generator(ints++);
+        small_vec.push_back(t);
+        vec.push_back(t);
+        do_check();
+    }
+
+    if (Size / 2 < vec.size())
+    {
+        vec.erase(vec.begin() + Size / 2);
+        small_vec.erase(small_vec.begin() + Size / 2);
+        do_check();
+    }
+
+    while (!vec.empty())
+    {
+        vec.erase(vec.begin());
+        small_vec.erase(small_vec.begin());
+        do_check();
+    }
+}
+
+template<size_t ... Size>
+constexpr void run_all_tests(size_t num_values, std::index_sequence<Size...>)
+{
+    (run_test<Size, int, identity>(num_values), ...);
+    (run_test<Size, char, identity>(num_values), ...);
+    (run_test<Size, size_t, identity>(num_values), ...);
+    (run_test<Size, float, identity>(num_values), ...);
+    (run_test<Size, void*, int_to_ptr>(num_values), ...);
+    (run_test<Size, std::string, int_to_string>(num_values), ...);
+}
+
+int main(int argc, char* argv[])
+{
+    mxb::Log logger(MXB_LOG_TARGET_STDOUT);
+
+    for (int i = 1; i <= 31; i += 3)
+    {
+        run_all_tests(i, std::index_sequence<1, 2, 3, 4, 5, 11, 29> {});
+    }
+
+    return 0;
+}
