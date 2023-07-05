b773bdadbbb200ebcd9818e23e1e45a193c3dc2a
Markus Mäkelä <markus.makela@mariadb.com>
2023-07-05 17:16:31 +0300

    MXS-4618: Test LOAD DATA LOCAL INFILE
    The test is expected to just pass through the filter in the normal
    MariaDB case. For Xpand, the LOAD DATA LOCAL INFILE command is converted
    into an xpand_import call.

--- system-test/filters/ldi/ldi_basics.cc
+++ system-test/filters/ldi/ldi_basics.cc
@@ -14,9 +14,18 @@
 #include <maxtest/testconnections.hh>
 #include <maxtest/docker.hh>
 
-const size_t TARGET_BYETS = 1024 * 1024 * 50;   // We want a 50MiB file
-const size_t TARGET_ROWS = TARGET_BYETS / 6;    // Length of 'hello\n' is 6
+const size_t TARGET_BYTES = 1024 * 1024 * 50;   // We want a 50MiB file
+const size_t TARGET_ROWS = TARGET_BYTES / 6;    // Length of 'hello\n' is 6
 const std::string ROWS = std::to_string(TARGET_ROWS);
+
+// The S3 variables are required by normal MariaDB data imports. The others are required by Xpand.
+const char* SET_VARIABLES = "SET @maxscale.ldi.s3_key='my-access-key', "
+                            "@maxscale.ldi.s3_secret='my-secret-key', "
+                            "@maxscale.ldi.import_user='maxskysql', "
+                            "@maxscale.ldi.import_password='skysql'";
+
+// Puts InnoDB into a special mode that makes it faster
+const char* GO_FASTER = "SET autocommit=0, unique_checks=0, foreign_key_checks=0";
 
 void ldi_from_s3(TestConnections& test)
 {
@@ -25,9 +34,8 @@ void ldi_from_s3(TestConnections& test)
 
     auto c = test.maxscale->rwsplit();
     MXT_EXPECT_F(c.connect(), "Failed to connect: %s", c.error());
-    MXT_EXPECT_F(c.query("SET @maxscale.ldi.s3_key='my-access-key', @maxscale.ldi.s3_secret='my-secret-key'"),
-                 "SET failed: %s", c.error());
-    MXT_EXPECT(c.query("SET autocommit=0, unique_checks=0, foreign_key_checks=0"));
+    MXT_EXPECT_F(c.query(SET_VARIABLES), "SET failed: %s", c.error());
+    MXT_EXPECT(c.query(GO_FASTER));
     MXT_EXPECT_F(c.query("LOAD DATA INFILE 's3://my-bucket/test.csv' INTO TABLE t1"),
                  "LOAD DATA failed: %s", c.error());
     MXT_EXPECT(c.query("COMMIT"));
@@ -35,6 +43,42 @@ void ldi_from_s3(TestConnections& test)
     auto rows = c.field("SELECT COUNT(*) FROM t1");
     MXT_EXPECT(c.query("COMMIT"));
     MXT_EXPECT_F(rows == ROWS, "Expected %s rows, got %s", ROWS.c_str(), rows.c_str());
+}
+
+void normal_ldli(TestConnections& test)
+{
+    char filepath[256] = "/tmp/data.csv.XXXXXX";
+    int fd = mkstemp(filepath);
+
+    for (size_t i = 0; i < TARGET_ROWS; i++)
+    {
+        write(fd, "hello\n", 6);
+    }
+
+    close(fd);
+
+    try
+    {
+        auto conn = test.repl->backend(0)->open_connection();
+        auto table = conn->create_table("test.t1", "data CHAR(10)");
+
+        auto c = test.maxscale->rwsplit();
+        MXT_EXPECT_F(c.connect(), "Failed to connect: %s", c.error());
+        MXT_EXPECT_F(c.query(SET_VARIABLES), "SET failed: %s", c.error());
+        MXT_EXPECT(c.query(GO_FASTER));
+        MXT_EXPECT(c.query(mxb::cat("LOAD DATA LOCAL INFILE '", filepath, "' INTO TABLE t1")));
+        MXT_EXPECT(c.query("COMMIT"));
+        MXT_EXPECT(c.query("BEGIN"));
+        auto rows = c.field("SELECT COUNT(*) FROM t1");
+        MXT_EXPECT(c.query("COMMIT"));
+        MXT_EXPECT_F(rows == ROWS, "Expected %s rows, got %s", ROWS.c_str(), rows.c_str());
+    }
+    catch (const std::exception& e)
+    {
+        test.add_failure("Caught exception: %s", e.what());
+    }
+
+    remove(filepath);
 }
 
 void test_main(TestConnections& test)
@@ -55,7 +99,11 @@ void test_main(TestConnections& test)
         container.execute(cmd);
     }
 
+    test.log_printf("Testing LDI from S3");
     ldi_from_s3(test);
+
+    test.log_printf("Testing normal LDLI");
+    normal_ldli(test);
 }
 
 int main(int argc, char** argv)
