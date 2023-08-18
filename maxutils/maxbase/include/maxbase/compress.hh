198b3f9e2d7608e026a3fecc5774a58d5f5fb090
Niclas Antti <jnantti@gmail.com>
2023-08-18 17:26:36 +0300

    MXS-3986 Add compression classes

---
+++ maxutils/maxbase/include/maxbase/compress.hh
@@ -0,0 +1,175 @@
+﻿/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-08-18
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class ZSTD_CCtx_s;
+class ZSTD_DCtx_s;
+
+namespace maxbase
+{
+
+/*
+ *  The status INIT_ERROR is set if Compressor or Decompressor
+ *  constructors fail to initialize the compression library. When the status
+ *  is COMPRESSION_ERROR last_comp_error() and last_comp_error_str()
+ *  return the compression library specific error.
+ */
+enum class CompressionStatus : char
+{
+    OK,
+    COMPRESSION_ERROR,
+    EMPTY_INPUT_STREAM,
+    INIT_ERROR
+};
+
+/**
+ * @brief Compressor class. One instance per thread, decompress can be
+ *        called multiple times on the same instance (avoid re-creation of
+ *        stream context).
+ */
+class Compressor final
+{
+public:
+    /**
+     * @brief Compressor   Check the status for INIT_ERROR after creation.
+     * @param level        - Compress level 0-19, in practice 2-6 give reasonable
+     *                       compression ratio/speed.
+     * @param nthreads     - How many threads compression uses internally. The value -1
+     *                       causes all available hardware threads to be used.
+     * @param cpu_limit    - Limit CPU usage: 1.0 no limit, min 0.25. This does not
+     *                       affect the speeed at which the compression functions operate,
+     *                       but the CPU usage of the calling thread.
+     */
+    Compressor(int level, int nthreads = -1, float cpu_limit = 0);
+    Compressor(Compressor&&) = delete;
+    ~Compressor();
+
+    /**
+     * @brief  If the status() is COMPRESSION_ERROR, last_comp_error() and last_comp_error_str()
+     *         report the underlying compression library error.
+     * @return CompressionStatus
+     */
+    CompressionStatus status() const;
+
+    /**
+     * @brief compress
+     * @param in            - Stream in  is read until no more bytes are available,
+     *                        so is not suitable for a stream that is being written to
+     *                        while compression is in process.
+     * @param out           - Stream out
+     * @return CompressionStatus
+     */
+    CompressionStatus compress(std::istream& in, std::ostream& out);
+
+    /**
+     * @brief verify_integrity
+     * @param in    - Stream in
+     * @return bool - true if stream is a valid compressed file
+     */
+    static bool verify_integrity(const std::istream& in);
+
+    /**
+     * @brief  last_comp_error - Last compression library specific error. The error
+     *                           is reset when decompress() is called.
+     * @return compression library specfic error code, 0 is no error
+     */
+    size_t last_comp_error() const;
+
+    /**
+     * @brief last_error_str
+     * @return human readable error string
+     */
+    std::string last_comp_error_str() const;
+
+    // Getters mainly for loggin
+    int level() const;
+    int ntheads() const;
+    int cpu_limit() const;
+
+    // For tuning and testing.
+    void  set_level(int level);
+    void  set_nthread(int nthreads);
+    float set_cpu_limit(float limit);
+    void  set_buffer_sizes(size_t input_size, size_t output_size);
+
+private:
+    ZSTD_CCtx_s*      m_pContext;
+    std::vector<char> m_input_buffer;
+    std::vector<char> m_output_buffer;
+    CompressionStatus m_status = CompressionStatus::OK;
+    size_t            m_last_err = 0;
+    int               m_level;
+    int               m_nthreads;
+    float             m_cpu_limit = 0;
+};
+
+class Decompressor final
+{
+public:
+    /**
+     * @brief Decompressor    Check the status for INIT_ERROR after creation.
+     *        called multiple times on the same instance (avoid re-creation of
+     *        stream context).
+     *
+     * @param flush_nchars - flush stream every n chars, 0 means flush only at the end.
+     */
+    Decompressor(int flush_nchars = 0);
+    ~Decompressor();
+
+    /**
+     * @brief  status - If the status() is COMPRESSION_ERROR, last_comp_error()
+     *                  and last_comp_error_str() report the underlying
+     *                  compression library error.
+     * @return CompressionStatus
+     */
+    CompressionStatus status() const;
+
+    /**
+     * @brief decompress
+     * @param in            - Stream in is read until no more bytes are available,
+     *                        so is not suitable for a stream that is being written to
+     *                        while compression is in process.
+     * @param out           - Stream out. If out is read while decompression
+     * @return CompressionStatus
+     */
+    CompressionStatus decompress(std::istream& in, std::ostream& out);
+    /**
+     * @brief  last_comp_error - Last compression library specific error. The error
+     *                           is reset when decompress() is called.
+     * @return compression library specfic error code, 0 is no error
+     */
+    size_t last_comp_error() const;
+
+    /**
+     * @brief last_error_str
+     * @return human readable error string
+     */
+    std::string last_comp_error_str() const;
+
+    // For tuning and testing.
+    void set_buffer_sizes(size_t input_size, size_t output_size);
+
+private:
+    ZSTD_DCtx_s*      m_pContext;
+    std::vector<char> m_input_buffer;
+    std::vector<char> m_output_buffer;
+    size_t            m_flush_nchars;
+    size_t            m_last_err = 0;
+    CompressionStatus m_status = CompressionStatus::OK;
+};
+}
---
+++ maxutils/maxbase/src/compress.cc
@@ -0,0 +1,330 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-08-18
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include <maxbase/compress.hh>
+#include <maxbase/log.hh>
+#include <maxbase/assert.hh>
+#include <sstream>
+#include <zstd.h>
+#include <zstd_errors.h>
+#include <thread>
+#include <algorithm>
+
+// TODO verify_integrity
+// TODO cpu limit
+// TODO check input and output stream
+// TODO don't compile if zstd not available,
+//      but add libzstd to install_build_deps.sh
+
+
+namespace maxbase
+{
+Compressor::Compressor(int level, int nthreads, float cpu_limit)
+    : m_pContext(ZSTD_createCCtx())
+    , m_input_buffer(ZSTD_CStreamInSize())
+    , m_output_buffer(ZSTD_CStreamOutSize())
+    , m_level(level)
+    , m_nthreads(nthreads)
+{
+    set_cpu_limit(cpu_limit);
+
+    if (m_nthreads == -1)
+    {
+        m_nthreads = std::thread::hardware_concurrency();
+    }
+
+    if (m_pContext == nullptr)
+    {
+        m_status = CompressionStatus::INIT_ERROR;
+        MXB_SERROR("Failed to create Compressor context");
+    }
+}
+
+Compressor::~Compressor()
+{
+    ZSTD_freeCCtx(m_pContext);
+}
+
+size_t Compressor::last_comp_error() const
+{
+    return m_last_err;
+}
+
+std::string Compressor::last_comp_error_str() const
+{
+    return ZSTD_getErrorName(m_last_err);
+}
+
+void Compressor::set_level(int level)
+{
+    m_level = level;
+}
+
+void Compressor::set_nthread(int nthreads)
+{
+    m_nthreads = nthreads;
+}
+
+float Compressor::set_cpu_limit(float percent)
+{
+    return m_cpu_limit = std::clamp(percent, 0.25f, 1.0f);
+}
+
+void Compressor::set_buffer_sizes(size_t input_size, size_t output_size)
+{
+    m_input_buffer.resize(input_size);
+    m_output_buffer.resize(output_size);
+}
+
+CompressionStatus Compressor::compress(std::istream& in,
+                                       std::ostream& out)
+{
+    mxb_assert(m_pContext != nullptr);
+
+    if (m_last_err != ZSTD_error_no_error)
+    {
+        m_last_err = ZSTD_CCtx_reset(m_pContext, ZSTD_reset_session_only);
+
+        if (ZSTD_isError(m_last_err))
+        {
+            MXB_SERROR("Failed to reset compressor context: " << last_comp_error_str());
+            m_status = CompressionStatus::COMPRESSION_ERROR;
+            return m_status;
+        }
+    }
+
+    m_last_err = ZSTD_CCtx_setParameter(m_pContext, ZSTD_c_checksumFlag, 1);
+
+    if (!ZSTD_isError(m_last_err))
+    {
+        m_last_err = ZSTD_CCtx_setParameter(m_pContext, ZSTD_c_compressionLevel, m_level);
+    }
+
+    if (ZSTD_isError(m_last_err))
+    {
+        MXB_SERROR("Failed to set compressor parameter: " << last_comp_error_str());
+        m_status = CompressionStatus::COMPRESSION_ERROR;
+        return m_status;
+    }
+
+    if (m_nthreads > 1)
+    {
+        size_t const ret = ZSTD_CCtx_setParameter(m_pContext, ZSTD_c_nbWorkers, m_nthreads);
+
+        if (ZSTD_isError(ret))
+        {
+            MXB_SINFO("Installed libzstd does not support multithreading."
+                      " Continue single threaded execution.");
+        }
+    }
+
+    long to_read = m_input_buffer.size();
+    auto no_input = true;
+
+    for (;;)
+    {
+        in.read(m_input_buffer.data(), to_read);
+        if (in.gcount() == 0)
+        {
+            break;
+        }
+        no_input = false;
+
+        bool last_chunk = in.gcount() < to_read;
+        ZSTD_EndDirective mode = last_chunk ? ZSTD_e_end : ZSTD_e_continue;
+        ZSTD_inBuffer input{m_input_buffer.data(), size_t(in.gcount()), 0};
+
+        auto done = false;
+        do
+        {
+            ZSTD_outBuffer output = {m_output_buffer.data(), m_output_buffer.size(), 0};
+            // The return value is either remaining bytes to be flushed, or
+            // an error code.
+            m_last_err = ZSTD_compressStream2(m_pContext, &output, &input, mode);
+
+            if (ZSTD_isError(m_last_err))
+            {
+                MXB_SERROR("Compression error: " << last_comp_error_str());
+                m_status = CompressionStatus::COMPRESSION_ERROR;
+                return m_status;
+            }
+
+            out.write(m_output_buffer.data(), output.pos);
+
+            done = last_chunk ? m_last_err == 0 : input.pos == input.size;
+        }
+        while (!done);
+    }
+
+    out.flush();
+
+    if (no_input)
+    {
+        mxb_assert(!true);
+        MXB_SERROR("Empty input stream");
+        m_status = CompressionStatus::EMPTY_INPUT_STREAM;
+    }
+    else
+    {
+        m_status = CompressionStatus::OK;
+    }
+
+    return m_status;
+}
+
+CompressionStatus Compressor::status() const
+{
+    return m_status;
+}
+
+bool Compressor::verify_integrity(const std::istream& in)
+{
+    // TODO, what is the fastest method.
+    return false;
+}
+
+int Compressor::level() const
+{
+    return m_level;
+}
+
+int Compressor::ntheads() const
+{
+    return m_nthreads;
+}
+
+int Compressor::cpu_limit() const
+{
+    return m_cpu_limit;
+}
+
+Decompressor::Decompressor(int flush_nchars)
+    : m_pContext(ZSTD_createDCtx())
+    , m_input_buffer(ZSTD_CStreamInSize())
+    , m_output_buffer(ZSTD_CStreamOutSize())
+    , m_flush_nchars(flush_nchars)
+{
+    if (m_pContext == nullptr)
+    {
+        m_status = CompressionStatus::INIT_ERROR;
+        MXB_SERROR("Could not create decompressor context.");
+    }
+}
+
+maxbase::Decompressor::~Decompressor()
+{
+    ZSTD_freeDCtx(m_pContext);
+}
+
+CompressionStatus Decompressor::status() const
+{
+    return m_status;
+}
+
+CompressionStatus Decompressor::decompress(std::istream& in,
+                                           std::ostream& out)
+{
+    mxb_assert(m_pContext != nullptr);
+
+    if (m_last_err != ZSTD_error_no_error)
+    {
+        m_last_err = ZSTD_DCtx_reset(m_pContext, ZSTD_reset_session_only);
+
+        if (ZSTD_isError(m_last_err))
+        {
+            MXB_SERROR("Failed to reset compressor context: " << last_comp_error_str());
+            m_status = CompressionStatus::COMPRESSION_ERROR;
+            return m_status;
+        }
+    }
+
+    auto to_read = m_input_buffer.size();
+    auto no_input = true;
+    size_t bytes_out = 0;
+    size_t last_ret;
+
+    for (;;)
+    {
+        in.read(m_input_buffer.data(), to_read);
+        if (in.gcount() == 0)
+        {
+            break;
+        }
+
+        no_input = false;
+
+        ZSTD_inBuffer input{m_input_buffer.data(), size_t(in.gcount()), 0};
+        while (input.pos < input.size)
+        {
+            ZSTD_outBuffer output = {m_output_buffer.data(), m_output_buffer.size(), 0};
+
+            // The return value is either remaining bytes to be flushed, or
+            // an error code.
+            last_ret = ZSTD_decompressStream(m_pContext, &output, &input);
+            m_last_err = last_ret;
+
+            if (ZSTD_isError(m_last_err))
+            {
+                MXB_SERROR("Decompression error = " << ZSTD_getErrorName(m_last_err));
+                m_status = CompressionStatus::COMPRESSION_ERROR;
+                return m_status;
+            }
+
+            out.write(m_output_buffer.data(), output.pos);
+
+            bytes_out += output.pos;
+            if (m_flush_nchars && bytes_out > m_flush_nchars)
+            {
+                out.flush();
+                bytes_out = 0;
+            }
+        }
+    }
+
+    out.flush();
+
+    if (no_input)
+    {
+        mxb_assert(!true);
+        MXB_SERROR("Empty input stream");
+        m_status = CompressionStatus::EMPTY_INPUT_STREAM;
+    }
+    else if (last_ret)
+    {
+        MXB_SERROR("Decompression error, possible truncated stream.");
+        m_status = CompressionStatus::COMPRESSION_ERROR;
+    }
+    else
+    {
+        m_status = CompressionStatus::OK;
+    }
+
+    return m_status;
+}
+
+size_t Decompressor::last_comp_error() const
+{
+    return m_last_err;
+}
+
+std::string Decompressor::last_comp_error_str() const
+{
+    return ZSTD_getErrorName(m_last_err);
+}
+
+void Decompressor::set_buffer_sizes(size_t input_size, size_t output_size)
+{
+    m_input_buffer.resize(input_size);
+    m_output_buffer.resize(output_size);
+}
+}
