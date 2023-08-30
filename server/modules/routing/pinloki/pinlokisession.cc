1e3d255b13391387b834ab9e79f03d4093cc53a9
Markus Mäkelä <markus.makela@mariadb.com>
2023-08-31 01:04:29 +0300

    Report completely unknown commands
    If some input that does not match the grammar at all is given,
    x3::phrase_parse returns false but it does not produce an error or an
    exception. In these situations a custom message is needed to make sure
    something is always returned to the client.
    
    This caused a debug assertion in 23.08 where the error generation
    functions expect the error message to not be empty.

--- server/modules/routing/pinloki/pinlokisession.cc
+++ server/modules/routing/pinloki/pinlokisession.cc
@@ -624,6 +624,7 @@ void PinlokiSession::purge_logs(const std::string& up_to)
 
 void PinlokiSession::error(const std::string& err)
 {
+    mxb_assert(!err.empty());
     set_response(mariadb::create_error_packet(1, 1064, "42000", err.c_str()));
 }
 }
