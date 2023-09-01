628c2d8507c390a382298c76136615c2eb51528d
Thien Ly <thien.ly@mariadb.com>
2023-09-01 16:48:53 +0300

    MXS-3760 DRY `query-cnf-dlg-ctr`
    This merges `query-cnf-dlg` into `query-cnf-dlg-ctr` as there is no
    need to have a presentational component as it won't be reused anywhere
    else. It was decomposed to prepare for packaging the query editor in
    22.08 but it turns out that there is no need to do so.

--- maxgui/workspace/src/components/QueryCnfDlgCtr.vue
+++ maxgui/workspace/src/components/QueryCnfDlgCtr.vue
@@ -1,17 +1,111 @@
 <template>
-    <query-cnf-dlg
+    <mxs-dlg
+        v-model="isOpened"
+        :onSave="onSave"
+        :title="$mxs_t('queryConfig')"
+        :lazyValidation="false"
+        minBodyWidth="512px"
+        :hasChanged="hasChanged"
         v-bind="{ ...$attrs }"
-        :cnf="{
-            query_row_limit,
-            query_confirm_flag,
-            query_history_expired_time,
-            query_show_sys_schemas_flag,
-            tab_moves_focus,
-            max_statements,
-        }"
         v-on="$listeners"
-        @confirm-save="save"
-    />
+    >
+        <template v-slot:form-body>
+            <div v-for="field in numericFields" :key="field.name" class="px-1 py-2">
+                <label class="field__label mxs-color-helper text-small-text label-required">
+                    {{ $mxs_t(field.name) }}
+                </label>
+                <!-- Add key to trigger rerender when dialog is opened, otherwise the input will be empty -->
+                <row-limit-ctr
+                    v-if="field.name === 'rowLimit'"
+                    :key="isOpened"
+                    :height="36"
+                    hide-details="auto"
+                    class="vuetify-input--override error--text__bottom mb-2 rowLimit"
+                    @change="config[field.name] = $event"
+                />
+                <template v-else>
+                    <v-text-field
+                        v-model.number="config[field.name]"
+                        type="number"
+                        :rules="[
+                            v =>
+                                validatePositiveNumber({
+                                    v,
+                                    inputName: $mxs_t(field.name),
+                                }),
+                        ]"
+                        class="vuetify-input--override error--text__bottom mb-2"
+                        :class="field.name"
+                        dense
+                        :height="36"
+                        hide-details="auto"
+                        outlined
+                        required
+                        :suffix="field.suffix"
+                        @keypress="$helpers.preventNonNumericalVal($event)"
+                    />
+                </template>
+                <template v-if="field.hasWarningInfo">
+                    <v-icon size="16" color="warning" class="mr-2">
+                        $vuetify.icons.mxs_alertWarning
+                    </v-icon>
+                    <small v-html="$mxs_t(`info.${field.name}`)" />
+                </template>
+            </div>
+            <div
+                v-for="(value, key) in $helpers.lodash.pick(config, [
+                    'showQueryConfirm',
+                    'showSysSchemas',
+                    'tabMovesFocus',
+                ])"
+                :key="key"
+                class="pa-1"
+            >
+                <v-checkbox
+                    v-model="config[key]"
+                    class="v-checkbox--mariadb pa-0 ma-0"
+                    dense
+                    :class="[key]"
+                    color="primary"
+                    hide-details="auto"
+                >
+                    <template v-slot:label>
+                        <label class="v-label pointer">{{ $mxs_t(key) }}</label>
+                        <v-tooltip
+                            v-if="key === 'tabMovesFocus'"
+                            top
+                            transition="slide-y-transition"
+                            max-width="400"
+                        >
+                            <template v-slot:activator="{ on }">
+                                <v-icon
+                                    class="ml-1 material-icons-outlined pointer"
+                                    size="16"
+                                    color="info"
+                                    v-on="on"
+                                >
+                                    mdi-information-outline
+                                </v-icon>
+                            </template>
+                            <i18n
+                                :path="
+                                    config[key] ? 'mxs.info.tabMovesFocus' : 'mxs.info.tabInsetChar'
+                                "
+                                tag="span"
+                            >
+                                <template v-slot:shortcut>
+                                    <b>
+                                        {{ `${OS_KEY} ${$helpers.isMAC() ? '+ SHIFT' : ''}` }}
+                                        + M
+                                    </b>
+                                </template>
+                            </i18n>
+                        </v-tooltip>
+                    </template>
+                </v-checkbox>
+            </div>
+        </template>
+    </mxs-dlg>
 </template>
 
 <script>
@@ -29,15 +123,19 @@
  * Public License.
  */
 import { mapMutations, mapState } from 'vuex'
-import QueryCnfDlg from './QueryCnfDlg.vue'
+import RowLimitCtr from '@wkeComps/QueryEditor/RowLimitCtr.vue'
 export default {
     name: 'query-cnf-dlg-ctr',
-    components: {
-        QueryCnfDlg,
-    },
+    components: { RowLimitCtr },
     inheritAttrs: false,
+    data() {
+        return {
+            config: {},
+        }
+    },
     computed: {
         ...mapState({
+            OS_KEY: state => state.mxsWorkspace.config.OS_KEY,
             query_row_limit: state => state.prefAndStorage.query_row_limit,
             query_confirm_flag: state => state.prefAndStorage.query_confirm_flag,
             query_history_expired_time: state => state.prefAndStorage.query_history_expired_time,
@@ -45,6 +143,44 @@ export default {
             tab_moves_focus: state => state.prefAndStorage.tab_moves_focus,
             max_statements: state => state.prefAndStorage.max_statements,
         }),
+        isOpened: {
+            get() {
+                return this.$attrs.value
+            },
+            set(value) {
+                this.$emit('input', value)
+            },
+        },
+        persistedCnf() {
+            return {
+                rowLimit: this.query_row_limit,
+                showQueryConfirm: Boolean(this.query_confirm_flag),
+                queryHistoryRetentionPeriod: this.$helpers.daysDiff(
+                    this.query_history_expired_time
+                ),
+                showSysSchemas: Boolean(this.query_show_sys_schemas_flag),
+                tabMovesFocus: this.tab_moves_focus,
+                maxStatements: this.max_statements,
+            }
+        },
+        numericFields() {
+            return [
+                { name: 'rowLimit', hasWarningInfo: true },
+                { name: 'maxStatements', hasWarningInfo: true },
+                { name: 'queryHistoryRetentionPeriod', suffix: this.$mxs_t('days') },
+            ]
+        },
+        hasChanged() {
+            return !this.$helpers.lodash.isEqual(this.persistedCnf, this.config)
+        },
+    },
+    watch: {
+        isOpened: {
+            immediate: true,
+            handler(v) {
+                if (v) this.config = this.$helpers.lodash.cloneDeep(this.persistedCnf)
+            },
+        },
     },
     methods: {
         ...mapMutations({
@@ -55,13 +191,23 @@ export default {
             SET_TAB_MOVES_FOCUS: 'prefAndStorage/SET_TAB_MOVES_FOCUS',
             SET_MAX_STATEMENTS: 'prefAndStorage/SET_MAX_STATEMENTS',
         }),
-        save(cnf) {
-            this.SET_QUERY_ROW_LIMIT(cnf.query_row_limit)
-            this.SET_QUERY_CONFIRM_FLAG(cnf.query_confirm_flag)
-            this.SET_QUERY_HISTORY_EXPIRED_TIME(cnf.query_history_expired_time)
-            this.SET_QUERY_SHOW_SYS_SCHEMAS_FLAG(cnf.query_show_sys_schemas_flag)
-            this.SET_TAB_MOVES_FOCUS(cnf.tab_moves_focus)
-            this.SET_MAX_STATEMENTS(cnf.max_statements)
+        validatePositiveNumber({ v, inputName }) {
+            if (this.$typy(v).isEmptyString)
+                return this.$mxs_t('errors.requiredInput', { inputName })
+            if (v <= 0) return this.$mxs_t('errors.largerThanZero', { inputName })
+            if (v > 0) return true
+            return false
+        },
+
+        onSave() {
+            this.SET_QUERY_ROW_LIMIT(this.config.rowLimit)
+            this.SET_QUERY_CONFIRM_FLAG(Number(this.config.showQueryConfirm))
+            this.SET_QUERY_HISTORY_EXPIRED_TIME(
+                this.$helpers.addDaysToNow(this.config.queryHistoryRetentionPeriod)
+            )
+            this.SET_QUERY_SHOW_SYS_SCHEMAS_FLAG(Number(this.config.showSysSchemas))
+            this.SET_TAB_MOVES_FOCUS(this.config.tabMovesFocus)
+            this.SET_MAX_STATEMENTS(this.config.maxStatements)
         },
     },
 }
--- maxgui/workspace/src/components/__tests__/QueryCnfDlgCtr.spec.js
+++ maxgui/workspace/src/components/__tests__/QueryCnfDlgCtr.spec.js
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2020 MariaDB Corporation Ab
- * Copyright (c) 2023 MariaDB plc, Finnish Branch
+ * Copyright (c) 2023 MariaDB plc
  *
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
@@ -13,23 +12,23 @@
  */
 
 import mount from '@tests/unit/setup'
-import QueryCnfDlg from '../QueryCnfDlg'
+import QueryCnfDlgCtr from '@wsComps/QueryCnfDlgCtr.vue'
 import { getErrMsgEle, inputChangeMock } from '@tests/unit/utils'
 import { addDaysToNow } from '@wsSrc/utils/helpers'
 
-const defCnf = {
-    query_row_limit: 10000,
-    query_confirm_flag: 1,
-    query_history_expired_time: addDaysToNow(30),
-    query_show_sys_schemas_flag: 1,
-}
 const mountFactory = opts =>
     mount({
         shallow: true,
-        component: QueryCnfDlg,
-        propsData: {
+        component: QueryCnfDlgCtr,
+        attrs: {
             value: true, // open dialog
-            cnf: defCnf,
+        },
+        computed: {
+            query_row_limit: () => 10000,
+            query_confirm_flag: () => 1,
+            query_history_expired_time: () => addDaysToNow(30),
+            query_show_sys_schemas_flag: () => 1,
+            max_statements: () => 1000,
         },
         ...opts,
     })
@@ -43,7 +42,7 @@ async function mockChangingConfig({ wrapper, key, value }) {
     })
 }
 
-describe(`QueryCnfDlg - child component's data communication tests `, () => {
+describe(`QueryCnfDlgCtr - child component's data communication tests `, () => {
     it(`Should pass accurate data to mxs-dlg via props`, () => {
         let wrapper = mountFactory()
         const { value, title, onSave, lazyValidation, hasChanged } = wrapper.findComponent({
@@ -65,19 +64,19 @@ describe(`QueryCnfDlg - child component's data communication tests `, () => {
     })
     it(`Should handle @change event emitted from row-limit-ctr`, () => {
         let wrapper = mountFactory()
-        const newVal = wrapper.vm.$data.config.rowLimit + 123
+        const newVal = 123
         wrapper.findComponent({ name: 'row-limit-ctr' }).vm.$emit('change', newVal)
         expect(wrapper.vm.$data.config.rowLimit).to.be.equals(newVal)
     })
 })
 
-describe(`QueryCnfDlg - tests after dialog is opened `, () => {
+describe(`QueryCnfDlgCtr - tests after dialog is opened `, () => {
     let wrapper
     beforeEach(() => {
         wrapper = mountFactory()
     })
-    it(`Should deep copy defCnf to config after dialog is opened `, async () => {
-        expect(wrapper.vm.config).to.be.deep.equals(wrapper.vm.defCnf)
+    it(`Should deep copy persistedCnf to config after dialog is opened `, async () => {
+        expect(wrapper.vm.config).to.be.deep.equals(wrapper.vm.persistedCnf)
     })
     it(`Should return accurate value for hasChanged`, async () => {
         await wrapper.setProps({ value: true }) // open dialog
@@ -87,37 +86,30 @@ describe(`QueryCnfDlg - tests after dialog is opened `, () => {
     })
 })
 
-describe(`QueryCnfDlg - form input tests`, () => {
+describe(`QueryCnfDlgCtr - form input tests`, () => {
     let wrapper
     beforeEach(() => {
-        //open dialog when component is mounted by assigning true to isOpened
         wrapper = mountFactory({ shallow: false })
     })
-    const intFields = ['queryHistoryRetentionPeriod']
+    const intFields = ['maxStatements', 'queryHistoryRetentionPeriod']
     intFields.forEach(field => {
         it(`Should parse value as number for ${field} field`, async () => {
-            const inputComponent = wrapper.findComponent({ name: 'mxs-dlg' }).find(`.${field}`)
+            const inputComponent = wrapper.find(`.${field}`)
             await inputChangeMock(inputComponent, '10')
             expect(wrapper.vm.config[field]).to.be.equals(10)
         })
     })
 
-    const boolFields = ['showQueryConfirm', 'showSysSchemas']
-    boolFields.forEach(field => {
-        it(`Should parse value as boolean for ${field} checkbox`, async () => {
-            const checkboxComponent = wrapper
-                .findComponent({ name: 'mxs-dlg' })
-                .find(`.${field}`)
-                .find('input')
-            const curVal = wrapper.vm.defCnf[field]
-            await checkboxComponent.trigger('click')
-            expect(wrapper.vm.config[field]).to.be.equals(!curVal)
+    const flagFields = ['showQueryConfirm', 'showSysSchemas']
+    flagFields.forEach(field => {
+        it(`Should parse flag value to boolean for ${field}`, () => {
+            expect(wrapper.vm.persistedCnf[field]).to.be.a('boolean')
         })
     })
 
     intFields.forEach(field => {
         it(`Should show accurate error message when ${field} value is 0`, async () => {
-            const inputComponent = wrapper.findComponent({ name: 'mxs-dlg' }).find(`.${field}`)
+            const inputComponent = wrapper.find(`.${field}`)
             await inputChangeMock(inputComponent, 0)
             expect(getErrMsgEle(inputComponent).text()).to.be.equals(
                 wrapper.vm.$mxs_t('errors.largerThanZero', {
@@ -126,28 +118,11 @@ describe(`QueryCnfDlg - form input tests`, () => {
             )
         })
         it(`Should show accurate error message when ${field} value is empty`, async () => {
-            const inputComponent = wrapper.findComponent({ name: 'mxs-dlg' }).find(`.${field}`)
+            const inputComponent = wrapper.find(`.${field}`)
             await inputChangeMock(inputComponent, '')
             expect(getErrMsgEle(inputComponent).text()).to.be.equals(
                 wrapper.vm.$mxs_t('errors.requiredInput', { inputName: wrapper.vm.$mxs_t(field) })
             )
-        })
-    })
-
-    it(`Should emit 'confirm-save' event with accurate args`, async () => {
-        wrapper = mountFactory()
-        await wrapper.vm.onSave()
-        expect(wrapper.emitted()).to.have.property('confirm-save')
-        const arg = wrapper.emitted()['confirm-save'][0][0]
-        const keys = [
-            'query_row_limit',
-            'query_confirm_flag',
-            'query_history_expired_time',
-            'query_show_sys_schemas_flag',
-        ]
-        keys.forEach(key => {
-            expect(arg).to.have.property(key)
-            expect(arg[key]).to.be.a('number')
         })
     })
 })
