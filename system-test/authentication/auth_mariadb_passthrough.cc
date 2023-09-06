bb93a7c80b4d041da058128fde69e143df40cb36
Esa Korhonen <esa.korhonen@mariadb.com>
2023-09-06 15:01:26 +0300

    MXS-4723 Test COM_CHANGE_USER with passthrough authentication

--- system-test/authentication/auth_mariadb_passthrough.cc
+++ system-test/authentication/auth_mariadb_passthrough.cc
@@ -19,9 +19,28 @@ using std::string;
 
 namespace
 {
+using Ssl = auth_utils::Ssl;
+
+void test_change_user(TestConnections& test, Ssl ssl, int port, const char* user1, const char* pw1,
+                      const char* user2, const char* pw2)
+{
+    const char change_failed[] = "COM_CHANGE_USER from %s to %s failed.";
+    auto mxs_ssl = ssl == Ssl::ON ? mxt::MaxScale::SslMode::ON : mxt::MaxScale::SslMode::OFF;
+    auto conn1 = test.maxscale->try_open_connection(mxs_ssl, port, user1, pw1);
+    bool ok1 = conn1->change_user(user2, pw2, "");
+    test.expect(ok1, change_failed, user1, user2);
+    // Try the other way around
+    auto conn2 = test.maxscale->try_open_connection(mxs_ssl, port, user2, pw2);
+    bool ok2 = conn2->change_user(user1, pw1, "");
+    test.expect(ok2, change_failed, user2, user1);
+    if (ok1 && ok2)
+    {
+        test.tprintf("COM_CHANGE_USER %s<-->%s succeeded.", user1, user2);
+    }
+}
+
 void test_main(TestConnections& test)
 {
-    using Ssl = auth_utils::Ssl;
     auto& repl = *test.repl;
     auto& mxs = *test.maxscale;
     auto* master_srv = repl.backend(0);
@@ -35,6 +54,11 @@ void test_main(TestConnections& test)
         const char user1[] = "native_user";
         const char pw1[] = "password1";
         auto pass_user = admin_conn->create_user(user1, "", pw1);
+
+        const char user2[] = "native_user2";
+        const char pw2[] = "password2";
+        auto pass_user2 = admin_conn->create_user(user2, "", pw2);
+
         const char no_pass[] = "native_nopass";
         auto no_pass_user = admin_conn->create_user(no_pass, "", "");
 
@@ -44,8 +68,16 @@ void test_main(TestConnections& test)
         {
             auto test_login = [&](int port, Ssl ssl) {
                 try_conn(test, port, ssl, user1, pw1, true);
+                try_conn(test, port, ssl, user2, pw2, true);
                 try_conn(test, port, ssl, user1, "wrong", false);
                 try_conn(test, port, ssl, no_pass, "", true);
+
+                // Test change user (MXS-4723)
+                if (test.ok())
+                {
+                    test_change_user(test, ssl, port, user1, pw1, user2, pw2);
+                    test_change_user(test, ssl, port, user1, pw1, no_pass, "");
+                }
             };
             test.tprintf("Testing mysql_native_password, ssl OFF.");
             test_login(4006, Ssl::OFF);
@@ -73,6 +105,11 @@ void test_main(TestConnections& test)
         const char pam_user[] = "pam_user";
         const char pam_pw[] = "pam_password";
         auth_utils::create_basic_pam_user(master_srv, pam_user, pam_pw);
+
+        const char pam_user2[] = "pam_user2";
+        const char pam_pw2[] = "pam_password2";
+        auth_utils::create_basic_pam_user(master_srv, pam_user2, pam_pw2);
+
         const char pam_no_pass[] = "pam_nopass";
         auth_utils::create_basic_pam_user(master_srv, pam_no_pass, "");
 
@@ -83,8 +120,16 @@ void test_main(TestConnections& test)
         {
             auto test_login = [&](int port, Ssl ssl) {
                 try_conn(test, port, ssl, pam_user, pam_pw, true);
+                try_conn(test, port, ssl, pam_user2, pam_pw2, true);
                 try_conn(test, port, ssl, pam_user, "wrong", false);
                 try_conn(test, port, ssl, pam_no_pass, "", true);
+
+                // Test change user (MXS-4723)
+                if (test.ok())
+                {
+                    test_change_user(test, ssl, port, pam_user, pam_pw, pam_user2, pam_pw2);
+                    test_change_user(test, ssl, port, pam_user, pam_pw, pam_no_pass, "");
+                }
             };
 
             test_login(4008, Ssl::OFF);
@@ -93,6 +138,7 @@ void test_main(TestConnections& test)
 
         // Cleanup users
         auth_utils::delete_basic_pam_user(master_srv, pam_user);
+        auth_utils::delete_basic_pam_user(master_srv, pam_user2);
         auth_utils::delete_basic_pam_user(master_srv, pam_no_pass);
         // Cleanup pam settings.
         master_srv->stop_database();
