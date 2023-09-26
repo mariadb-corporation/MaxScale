92d39e93d157e2b1c5a084a6e50712f668c9d3d4
Niclas Antti <jnantti@gmail.com>
2023-09-26 11:46:09 +0300

    MXS-3986 Add class BinlogFile

---
+++ server/modules/routing/pinloki/binlog_file.cc
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-08-18
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+
+#include "binlog_file.hh"
+#include "config.hh"
+
+namespace pinloki
+{
+
+BinlogFile::BinlogFile(std::string_view file_name)
+    : m_temp_file{Config::pinloki_temp_dir().temp_file()}
+{
+    auto res = open_file(file_name);
+    m_local_stat = res.local_stat;
+    m_input_file = res.file_name;
+
+    if (m_local_stat == FAILED)
+    {
+        MXB_THROW(BinlogReadError,
+                  "Could not open '" << file_name << "' for reading: "
+                                     << errno << ", " << mxb_strerror(errno));
+    }
+
+    if (m_local_stat == DECOMPRESSING)
+    {
+        m_compressed_in = std::move(res.in_stream);
+        m_decompressed_out = m_temp_file.make_stream<std::ofstream>(std::ios_base::out);
+        if (!m_decompressed_out)
+        {
+            MXB_THROW(BinlogReadError,
+                      "Could not open temp file '" << m_temp_file.name()
+                                                   << "': " << errno << ", "
+                                                   << mxb_strerror(errno));
+        }
+        else
+        {
+            MXB_SINFO("Start decompressing " << m_input_file << " to " << m_temp_file.name());
+            m_output_file = m_temp_file.name();
+            m_sDecompressor = std::make_unique<maxbase::Decompressor>();
+            m_future = std::async(std::launch::async,
+                                  &maxbase::Decompressor::decompress,
+                                  &*m_sDecompressor,
+                                  std::ref(m_compressed_in),
+                                  std::ref(m_decompressed_out));
+        }
+    }
+    else
+    {
+        m_output_file = m_input_file;
+    }
+}
+
+BinlogFile::~BinlogFile()
+{
+    if (m_local_stat == DECOMPRESSING)
+    {
+        // TODO Decompressor needs a stop()
+        try
+        {
+            m_future.get();
+        }
+        catch (std::exception& ex)
+        {
+            MXB_SERROR("Decompressor threw in ~BinlogFile, input file '"
+                       << m_input_file << "' ex: " << ex.what());
+        }
+    }
+}
+
+BinlogFile::OpenRes BinlogFile::open_file(std::string_view file_name_)
+{
+    auto file_name = std::string(file_name_);
+    strip_extension(file_name, COMPRESSION_EXTENSION);
+    std::ifstream file(file_name);
+    if (file.good())
+    {
+        return {REGULAR_FILE, file_name, std::move(file)};
+    }
+
+    file_name += '.' + COMPRESSION_EXTENSION;
+    file.open(file_name);
+
+    if (file.good())
+    {
+        return {DECOMPRESSING, std::move(file_name), std::move(file)};
+    }
+    else
+    {
+        return {FAILED, std::move(file_name), std::move(file)};
+    }
+}
+
+bool BinlogFile::check_status()
+{
+    if (m_local_stat == DECOMPRESSING && m_future.wait_for(0s) == std::future_status::ready)
+    {
+        if (auto stat = m_future.get(); stat != maxbase::CompressionStatus::OK)
+        {
+            MXB_THROW(BinlogReadError, "Decompression error: "
+                      << to_string(stat)
+                      << " from file '" << m_input_file << '\'');
+        }
+        m_local_stat = DECOMPRESS_DONE;
+        MXB_SINFO("Decompression done " << m_input_file << " to " << m_temp_file.name());
+    }
+
+    return m_local_stat == DECOMPRESSING;
+}
+}
---
+++ server/modules/routing/pinloki/binlog_file.hh
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2027-08-18
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+#include <maxbase/ccdefs.hh>
+#include <maxbase/temp_file.hh>
+#include <maxbase/compress.hh>
+#include <maxbase/exception.hh>
+
+#include <fstream>
+#include <future>
+#include <utility>
+
+namespace pinloki
+{
+
+class BinlogFile final
+{
+    enum LocalStat
+    {
+        REGULAR_FILE,
+        DECOMPRESSING,
+        FAILED,
+        DECOMPRESS_DONE
+    };
+
+public:
+    // Throws BinlogReadError.
+    // First remove the compression extension from file_name,
+    // if any, and try to open the file. If that fails,
+    // try to open file name + compression extension(s).
+    BinlogFile(std::string_view file_name);
+    ~BinlogFile();
+    BinlogFile(BinlogFile&&) = delete;
+
+    // Throws BinlogReadError.
+    // As long as check_status returns true, continue to call in a
+    // polling manner as decompression is ongoing and an error
+    // can happen while files are being read/written.
+    bool check_status();
+
+    // Throws BinlogReadError.
+    // Create an ifstream to read from.
+    std::ifstream make_ifstream();
+
+    // The actual name of the file that is opened for reading by
+    // make_ifstream(). This is a temp file if decompression
+    // is ongoing.
+    const std::string& file_name() const;
+
+private:
+    LocalStat     m_local_stat = FAILED;
+    bool          m_is_compressed = false;
+    std::string   m_input_file;
+    std::string   m_output_file;
+    mxb::TempFile m_temp_file;
+    std::ifstream m_compressed_in;
+    std::ofstream m_decompressed_out;
+
+    std::unique_ptr<maxbase::Decompressor> m_sDecompressor;
+    std::future<mxb::CompressionStatus>    m_future;
+
+    struct OpenRes
+    {
+        LocalStat     local_stat;
+        std::string   file_name;
+        std::ifstream in_stream;
+    };
+
+    OpenRes open_file(std::string_view file_name);
+};
+
+inline const std::string& BinlogFile::file_name() const
+{
+    return m_output_file;
+}
+
+inline std::ifstream BinlogFile::make_ifstream()
+{
+    check_status();
+    return std::ifstream{m_output_file};
+}
+}
