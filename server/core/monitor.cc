c4b2fd1078b7cc0f3ed187fe0b13f00e3062aeee
Esa Korhonen <esa.korhonen@mariadb.com>
2023-10-02 15:42:05 +0300

    Move more MonitorServer functions

--- server/core/monitor.cc
+++ server/core/monitor.cc
@@ -36,13 +36,11 @@
 #include <maxbase/externcmd.hh>
 #include <maxbase/format.hh>
 #include <maxbase/json.hh>
-#include <maxscale/diskspace.hh>
 #include <maxscale/http.hh>
 #include <maxscale/json_api.hh>
 #include <maxscale/listener.hh>
 #include <maxscale/mainworker.hh>
 #include <maxscale/paths.hh>
-#include <maxscale/protocol/mariadb/diskspace.hh>
 #include <maxscale/protocol/mariadb/maxscale.hh>
 #include <maxscale/secrets.hh>
 
@@ -338,39 +336,8 @@ bool MonitorSpec::do_post_validate(const cfg::Configuration* config, Params& par
     return ok;
 }
 
-/** Server type specific bits */
-const uint64_t server_type_bits = SERVER_MASTER | SERVER_SLAVE | SERVER_JOINED | SERVER_RELAY | SERVER_BLR;
-
-/** All server bits */
-const uint64_t all_server_bits = SERVER_RUNNING | SERVER_MAINT | SERVER_MASTER | SERVER_SLAVE
-    | SERVER_JOINED | SERVER_RELAY | SERVER_BLR;
-
 const char journal_name[] = "monitor.dat";
 const char journal_template[] = "%s/%s/%s";
-
-bool check_disk_space_exhausted(MonitorServer* pMs,
-                                const std::string& path,
-                                const maxscale::disk::SizesAndName& san,
-                                int32_t max_percentage)
-{
-    bool disk_space_exhausted = false;
-
-    int32_t used_percentage = ((san.total - san.available) / (double)san.total) * 100;
-
-    if (used_percentage >= max_percentage)
-    {
-        MXB_ERROR("Disk space on %s at %s is exhausted; %d%% of the the disk "
-                  "mounted on the path %s has been used, and the limit it %d%%.",
-                  pMs->server->name(),
-                  pMs->server->address(),
-                  used_percentage,
-                  path.c_str(),
-                  max_percentage);
-        disk_space_exhausted = true;
-    }
-
-    return disk_space_exhausted;
-}
 
 const char ERR_CANNOT_MODIFY[] =
     "The server is monitored, so only the maintenance status can be "
@@ -820,141 +787,6 @@ void Monitor::wait_for_status_change()
     }
 }
 
-/*
- * Determine a monitor event, defined by the difference between the old
- * status of a server and the new status.
- *
- * @return  monitor_event_t     A monitor event (enum)
- *
- * @note This function must only be called from mon_process_state_changes
- */
-mxs_monitor_event_t MonitorServer::get_event_type() const
-{
-    auto rval = event_type(m_prev_status, server->status());
-
-    mxb_assert_message(rval != UNDEFINED_EVENT,
-                       "No event for state transition: [%s] -> [%s]",
-                       Target::status_to_string(m_prev_status, server->stats().n_current_conns()).c_str(),
-                       server->status_string().c_str());
-
-    return rval;
-}
-
-// static
-mxs_monitor_event_t MonitorServer::event_type(uint64_t before, uint64_t after)
-{
-    typedef enum
-    {
-        DOWN_EVENT,
-        UP_EVENT,
-        LOSS_EVENT,
-        NEW_EVENT,
-        UNSUPPORTED_EVENT
-    } general_event_type;
-
-    general_event_type event_type = UNSUPPORTED_EVENT;
-
-    uint64_t prev = before & all_server_bits;
-    uint64_t present = after & all_server_bits;
-
-    if (prev == present)
-    {
-        /* This should never happen */
-        mxb_assert(false);
-        return UNDEFINED_EVENT;
-    }
-
-    if ((prev & SERVER_RUNNING) == 0)
-    {
-        /* The server was not running previously */
-        if ((present & SERVER_RUNNING) != 0)
-        {
-            event_type = UP_EVENT;
-        }
-        else
-        {
-            /* Otherwise, was not running and still is not running. This should never happen. */
-            mxb_assert(false);
-        }
-    }
-    else
-    {
-        /* Previous state must have been running */
-        if ((present & SERVER_RUNNING) == 0)
-        {
-            event_type = DOWN_EVENT;
-        }
-        else
-        {
-            /** These are used to detect whether we actually lost something or
-             * just transitioned from one state to another */
-            uint64_t prev_bits = prev & (SERVER_MASTER | SERVER_SLAVE);
-            uint64_t present_bits = present & (SERVER_MASTER | SERVER_SLAVE);
-
-            /* Was running and still is */
-            if ((!prev_bits || !present_bits || prev_bits == present_bits)
-                && (prev & server_type_bits))
-            {
-                /* We used to know what kind of server it was */
-                event_type = LOSS_EVENT;
-            }
-            else
-            {
-                /* We didn't know what kind of server it was, now we do*/
-                event_type = NEW_EVENT;
-            }
-        }
-    }
-
-    mxs_monitor_event_t rval = UNDEFINED_EVENT;
-
-    switch (event_type)
-    {
-    case UP_EVENT:
-        rval = (present & SERVER_MASTER) ? MASTER_UP_EVENT :
-            (present & SERVER_SLAVE) ? SLAVE_UP_EVENT :
-            (present & SERVER_JOINED) ? SYNCED_UP_EVENT :
-            (present & SERVER_RELAY) ? RELAY_UP_EVENT :
-            (present & SERVER_BLR) ? BLR_UP_EVENT :
-            SERVER_UP_EVENT;
-        break;
-
-    case DOWN_EVENT:
-        rval = (prev & SERVER_MASTER) ? MASTER_DOWN_EVENT :
-            (prev & SERVER_SLAVE) ? SLAVE_DOWN_EVENT :
-            (prev & SERVER_JOINED) ? SYNCED_DOWN_EVENT :
-            (prev & SERVER_RELAY) ? RELAY_DOWN_EVENT :
-            (prev & SERVER_BLR) ? BLR_DOWN_EVENT :
-            SERVER_DOWN_EVENT;
-        break;
-
-    case LOSS_EVENT:
-        rval = (prev & SERVER_MASTER) ? LOST_MASTER_EVENT :
-            (prev & SERVER_SLAVE) ? LOST_SLAVE_EVENT :
-            (prev & SERVER_JOINED) ? LOST_SYNCED_EVENT :
-            (prev & SERVER_RELAY) ? LOST_RELAY_EVENT :
-            (prev & SERVER_BLR) ? LOST_BLR_EVENT :
-            UNDEFINED_EVENT;
-        break;
-
-    case NEW_EVENT:
-        rval = (present & SERVER_MASTER) ? NEW_MASTER_EVENT :
-            (present & SERVER_SLAVE) ? NEW_SLAVE_EVENT :
-            (present & SERVER_JOINED) ? NEW_SYNCED_EVENT :
-            (present & SERVER_RELAY) ? NEW_RELAY_EVENT :
-            (present & SERVER_BLR) ? NEW_BLR_EVENT :
-            UNDEFINED_EVENT;
-        break;
-
-    default:
-        /* This should never happen */
-        mxb_assert(false);
-        break;
-    }
-
-    return rval;
-}
-
 const char* Monitor::get_event_name(mxs_monitor_event_t event)
 {
     static std::map<mxs_monitor_event_t, const char*> values =
@@ -1029,44 +861,6 @@ string Monitor::gen_serverlist(int status, CredentialsApproach approach)
             separator = ",";
         }
     }
-    return rval;
-}
-
-/**
- * Check if current monitored server status has changed.
- *
- * @return              true if status has changed
- */
-bool MonitorServer::status_changed()
-{
-    return status_changed(m_prev_status, server->status());
-}
-
-// static
-bool MonitorServer::status_changed(uint64_t before, uint64_t after)
-{
-    bool rval = false;
-
-    /* Previous status is -1 if not yet set */
-    if (before != static_cast<uint64_t>(-1))
-    {
-
-        uint64_t old_status = before & all_server_bits;
-        uint64_t new_status = after & all_server_bits;
-
-        /**
-         * The state has changed if the relevant state bits are not the same,
-         * the server is either running, stopping or starting and the server is
-         * not going into maintenance or coming out of it
-         */
-        if (old_status != new_status
-            && ((old_status | new_status) & SERVER_MAINT) == 0
-            && ((old_status | new_status) & SERVER_RUNNING) == SERVER_RUNNING)
-        {
-            rval = true;
-        }
-    }
-
     return rval;
 }
 
@@ -1982,104 +1776,6 @@ int64_t Monitor::get_time_ms()
     mxb_assert(rv == 0);
 
     return t.tv_sec * 1000 + (t.tv_nsec / 1000000);
-}
-
-bool MonitorServer::can_update_disk_space_status() const
-{
-    return m_ok_to_check_disk_space
-           && (!m_shared.monitor_disk_limits.empty() || server->have_disk_space_limits());
-}
-
-void MariaServer::update_disk_space_status()
-{
-    if (const auto info = disk::get_info_by_path(con); info.has_value())
-    {
-        // Server-specific setting takes precedence.
-        auto dst = server->get_disk_space_limits();
-        if (dst.empty())
-        {
-            dst = m_shared.monitor_disk_limits;
-        }
-
-        bool disk_space_exhausted = false;
-        int32_t star_max_percentage = -1;
-        std::set<std::string> checked_paths;
-
-        for (const auto& dst_item : dst)
-        {
-            const string& path = dst_item.first;
-            int32_t max_percentage = dst_item.second;
-
-            if (path == "*")
-            {
-                star_max_percentage = max_percentage;
-            }
-            else
-            {
-                auto j = info->find(path);
-
-                if (j != info->end())
-                {
-                    const disk::SizesAndName& san = j->second;
-
-                    disk_space_exhausted = check_disk_space_exhausted(this, path, san, max_percentage);
-                    checked_paths.insert(path);
-                }
-                else
-                {
-                    MXB_WARNING("Disk space threshold specified for %s even though server %s at %s"
-                                "does not have that.",
-                                path.c_str(), server->name(), server->address());
-                }
-            }
-        }
-
-        if (star_max_percentage != -1)
-        {
-            for (auto j = info->begin(); j != info->end(); ++j)
-            {
-                string path = j->first;
-
-                if (checked_paths.find(path) == checked_paths.end())
-                {
-                    const disk::SizesAndName& san = j->second;
-
-                    disk_space_exhausted = check_disk_space_exhausted(this, path, san, star_max_percentage);
-                }
-            }
-        }
-
-        if (disk_space_exhausted)
-        {
-            m_pending_status |= SERVER_DISK_SPACE_EXHAUSTED;
-        }
-        else
-        {
-            m_pending_status &= ~SERVER_DISK_SPACE_EXHAUSTED;
-        }
-    }
-    else
-    {
-        SERVER* pServer = server;
-
-        if (mysql_errno(con) == ER_UNKNOWN_TABLE)
-        {
-            // Disable disk space checking for this server.
-            m_ok_to_check_disk_space = false;
-
-            MXB_ERROR("Disk space cannot be checked for %s at %s, because either the "
-                      "version (%s) is too old, or the DISKS information schema plugin "
-                      "has not been installed. Disk space checking has been disabled.",
-                      pServer->name(),
-                      pServer->address(),
-                      pServer->info().version_string());
-        }
-        else
-        {
-            MXB_ERROR("Checking the disk space for %s at %s failed due to: (%d) %s",
-                      pServer->name(), pServer->address(), mysql_errno(con), mysql_error(con));
-        }
-    }
 }
 
 void Monitor::flush_server_status()
