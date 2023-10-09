e12ca39a8bb8de770bb2af9ccb858d14e142b084
Markus Mäkelä <markus.makela@mariadb.com>
2023-10-09 15:22:19 +0300

    MXS-4787: Optimize mxb::pretty_size()
    The use of printf in the function was the most costly part. Using a less
    accurate conversion method combined with std::to_chars results in the
    conversion code being roughly 25 times faster than the original version.

--- maxutils/maxbase/src/pretty_print.cc
+++ maxutils/maxbase/src/pretty_print.cc
@@ -13,16 +13,20 @@
  */
 
 #include <maxbase/pretty_print.hh>
+#include <maxbase/assert.hh>
 #include <array>
 #include <tuple>
 #include <cmath>
 #include <cstring>
+#include <charconv>
 
 namespace maxbase
 {
-static std::array<const char*, 9> byte_prefix {"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "Zib", "YiB"};
+static std::array<std::string_view, 9> byte_prefix {
+    "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "Zib", "YiB"
+};
 
-static std::pair<double, const char*> pretty_number_split_binary(double dsize)
+static std::pair<double, std::string_view> pretty_number_split_binary(double dsize)
 {
     size_t index {0};
     constexpr int divisor = 1024;
@@ -38,26 +42,45 @@ static std::pair<double, const char*> pretty_number_split_binary(double dsize)
 
 std::string pretty_size(size_t sz)
 {
-    char buf[64];
+    // The maximum length of a string that is generated is 10 characters: 4 for the whole part, 1 for the
+    // decimal dot, 2 for the fractional part and 3 for the suffix. This also happens to be small enough that
+    // it fits into the SSO space of all std::string implementations.
+    char buf[16];
 
     auto [dsize, suffix] = pretty_number_split_binary(sz);
 
-    // format with two decimals
-    auto len = std::sprintf(buf, "%.2f", dsize);
+    // This rounds the numbers incorrectly but it is about an order of magnitude faster than using printf.
+    // Since we're pretty-printing the values for human consumption, this is an acceptable amount of
+    // inaccuracy.
+    int digits = dsize;
+    int decimals = (dsize - digits) * 100.0;
 
-    // remove trailing 0-decimals
-    char* ptr = buf + len - 1;
-    while (*ptr == '0')
+    auto rc = std::to_chars(buf, buf + sizeof(buf) - 4, digits);
+    mxb_assert(rc.ec == std::errc {});
+    char* ptr = rc.ptr;
+
+    if (decimals)
     {
-        --ptr;
-    }
-    if (*ptr != '.')
-    {
-        ++ptr;
+        *ptr++ = '.';
+
+        if (decimals < 10)
+        {
+            *ptr++ = '0';
+        }
+
+        rc = std::to_chars(ptr, ptr + 3, decimals);
+        mxb_assert(rc.ec == std::errc {});
+        ptr = rc.ptr;
+
+        if (ptr[-1] == '0')
+        {
+            --ptr;      // Remove the trailing zero
+        }
     }
 
-    strcpy(ptr, suffix);
+    memcpy(ptr, suffix.data(), suffix.size());
+    ptr += suffix.size();
 
-    return buf;
+    return std::string(buf, ptr);
 }
 }
