mariadb-corporation/MaxScale

commit 295bbb36a277f68d6d9ddf3cc714f6e147f44dcc

Always set relationships before configuring routers
This way the behavior of the service creation is identical for both the
INI file versions and the JSON versions.

server/core/config_runtime.cc
@@ -1908,6 +1908,53 @@ bool update_service_relationships(Service* service, json_t* json)
     return rval;
 }
 
+void add_relationship_to_array(json_t* arr, const std::vector<mxb::Json>& relationships)
+{
+    for (const auto& rel : relationships)
+    {
+        json_array_append_new(arr, json_string(rel.get_string(CN_ID).c_str()));
+    }
+}
+
+void convert_relationships_to_parameters(json_t* params, json_t* json)
+{
+    json_object_del(params, CN_SERVERS);
+    json_object_del(params, CN_TARGETS);
+    json_object_del(params, CN_CLUSTER);
+    json_object_del(params, CN_FILTER);
+
+    mxb::Json js(json, mxb::Json::RefType::COPY);
+    auto cluster = js.at(MXS_JSON_PTR_RELATIONSHIPS_MONITORS).get_array_elems();
+    auto services = js.at(MXS_JSON_PTR_RELATIONSHIPS_SERVICES).get_array_elems();
+    auto servers = js.at(MXS_JSON_PTR_RELATIONSHIPS_SERVERS).get_array_elems();
+    auto filters = js.at(MXS_JSON_PTR_RELATIONSHIPS_FILTERS).get_array_elems();
+
+    if (!cluster.empty())
+    {
+        json_object_set_new(params, CN_CLUSTER, json_string(cluster[0].get_string(CN_ID).c_str()));
+    }
+    else if (!services.empty())
+    {
+        json_t* arr = json_array();
+        add_relationship_to_array(arr, services);
+        add_relationship_to_array(arr, servers);
+        json_object_set_new(params, CN_TARGETS, arr);
+    }
+    else
+    {
+        json_t* arr = json_array();
+        add_relationship_to_array(arr, servers);
+        json_object_set_new(params, CN_SERVERS, arr);
+    }
+
+    if (!filters.empty())
+    {
+        json_t* arr = json_array();
+        add_relationship_to_array(arr, filters);
+        json_object_set_new(params, CN_FILTERS, arr);
+    }
+}
+
 bool runtime_create_service_from_json(json_t* json)
 {
     UnmaskPasswords unmask;
@@ -1927,20 +1974,19 @@ bool runtime_create_service_from_json(json_t* json)
             json_object_set(params, CN_ROUTER, router);
             mxb::json_remove_nulls(params);
 
+            convert_relationships_to_parameters(params, json);
+
             if (auto service = Service::create(name, params))
             {
-                if (update_service_relationships(service, json))
+                if (save_config(service))
                 {
-                    if (save_config(service))
-                    {
-                        MXB_NOTICE("Created service '%s'", name);
-                        service->start();
-                        rval = true;
-                    }
-                    else
-                    {
-                        MXB_ERROR("Failed to serialize service '%s'", name);
-                    }
+                    MXB_NOTICE("Created service '%s'", name);
+                    service->start();
+                    rval = true;
+                }
+                else
+                {
+                    MXB_ERROR("Failed to serialize service '%s'", name);
                 }
             }
             else
@@ -2056,6 +2102,7 @@ bool runtime_alter_service_from_json(Service* service, json_t* new_json)
 
         if (rval)
         {
+            // TODO: This should be done before configuration and reverted if the configuration change fails.
             rval = update_service_relationships(service, new_json);
         }
 

commit d992c22c6a52836587ff5ac2f107c6dce36b009b

Fix JSON after creating service
The injection of the relationships as parameters modified the original
JSON into a form that would be rejected if given to
config_runtime_alter_service_from_json(). The configuration
synchronization needs the JSON to remain valid and thus the
modifications must be reverted.

This fixes the config_sync test that was broken by commit
295bbb36a277f68d6d9ddf3cc714f6e147f44dcc.

server/core/config_runtime.cc
@@ -1916,12 +1916,17 @@ void add_relationship_to_array(json_t* arr, const std::vector<mxb::Json>& relati
     }
 }
 
-void convert_relationships_to_parameters(json_t* params, json_t* json)
+void remove_relationship_parameters(json_t* params)
 {
     json_object_del(params, CN_SERVERS);
     json_object_del(params, CN_TARGETS);
     json_object_del(params, CN_CLUSTER);
     json_object_del(params, CN_FILTER);
+}
+
+void convert_relationships_to_parameters(json_t* params, json_t* json)
+{
+    remove_relationship_parameters(params);
 
     mxb::Json js(json, mxb::Json::RefType::COPY);
     auto cluster = js.at(MXS_JSON_PTR_RELATIONSHIPS_MONITORS).get_array_elems();
@@ -1993,6 +1998,10 @@ bool runtime_create_service_from_json(json_t* json)
             {
                 MXB_ERROR("Could not create service '%s' with module '%s'", name, json_string_value(router));
             }
+
+            // Remove the parameters that were added. This keeps the original JSON object valid so that it can
+            // be reused for altering the service.
+            remove_relationship_parameters(params);
         }
     }
 