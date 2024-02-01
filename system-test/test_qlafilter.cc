ebfb12782a1ec94621fff2183281dd31f1557b07
Markus Mäkelä <markus.makela@mariadb.com>
2024-02-01 08:56:57 +0200

    MXS-4959: Add PS test case to test_qlafilter
    The new test makes sure that all of the types are converted correctly.

--- system-test/test_qlafilter.cc
+++ system-test/test_qlafilter.cc
@@ -15,6 +15,130 @@
 #include <maxtest/testconnections.hh>
 #include <fstream>
 
+static const std::string PS_QUERY = "SELECT ? FROM test.t1 WHERE id = ? OR 1 = 1";
+static const bool NEGATIVE = true;
+
+struct Buffers
+{
+    size_t                   length {};
+    std::array<uint8_t, 256> buffer {};
+    char                     err = 0;
+    char                     is_null = 0;
+};
+
+MYSQL_BIND* param(enum_field_types type, bool is_unsigned, const void* ptr, size_t len)
+{
+    static std::array<MYSQL_BIND, 2> s_binds{};
+    static std::array<Buffers, 2> s_buffers{};
+
+    for (size_t i = 0; i < s_binds.size(); i++)
+    {
+        auto& buffer = s_buffers[i];
+        memcpy(buffer.buffer.data(), ptr, len);
+        buffer.length = len;
+
+        auto& bind = s_binds[i];
+        bind.buffer = buffer.buffer.data();
+        bind.buffer_length = buffer.buffer.size();
+        bind.buffer_type = type;
+        bind.is_unsigned = is_unsigned;
+        bind.length = &buffer.length;
+        bind.error = &buffer.err;
+        bind.is_null = &buffer.is_null;
+    }
+
+    return s_binds.data();
+}
+
+MYSQL_BIND* str_param(std::string_view str)
+{
+    return param(MYSQL_TYPE_STRING, false, str.data(), str.size());
+}
+
+MYSQL_BIND* time_param(enum_field_types type, int year, int month, int day,
+                       int hour, int minute, int second, int micros, bool is_negative = false)
+{
+    MYSQL_TIME mt{};
+
+    if (type != MYSQL_TYPE_TIME)
+    {
+        mt.year = year;
+        mt.month = month;
+        mt.day = day;
+    }
+    else if (hour > 24)
+    {
+        mt.day = hour / 24;
+        hour %= 24;
+    }
+
+    if (type != MYSQL_TYPE_DATE)
+    {
+        mt.hour = hour;
+        mt.minute = minute;
+        mt.second = second;
+        mt.neg = is_negative;
+        mt.second_part = micros;
+    }
+
+    return param(type, false, &mt, sizeof(mt));
+}
+
+template<class T>
+MYSQL_BIND* int_param(enum_field_types type, bool is_unsigned, T t)
+{
+    return param(type, is_unsigned, &t, sizeof(T));
+}
+
+void execute_with_param(TestConnections& test, MYSQL_BIND* bind)
+{
+    auto c = test.maxscale->rwsplit();
+
+    if (test.expect(c.connect(), "Failed to connect: %s", c.error()))
+    {
+        MYSQL_STMT* stmt = c.stmt();
+        test.expect(mysql_stmt_prepare(stmt, PS_QUERY.c_str(), PS_QUERY.size()) == 0,
+                    "Failed to prepare: %s", mysql_stmt_error(stmt));
+
+        test.expect(mysql_stmt_bind_param(stmt, bind) == 0,
+                    "Failed to bind: %s", mysql_stmt_error(stmt));
+
+        // Executing the prepared statement twice without re-binding the parameters makes it so that the
+        // connector does not send the types. This means that the version that's cached in MaxScale is used to
+        // decode the binary data.
+        for (int i = 0; i < 2; i++)
+        {
+            test.expect(mysql_stmt_execute(stmt) == 0,
+                        "Failed to execute: %s", mysql_stmt_error(stmt));
+
+            MYSQL_BIND res{};
+            test.expect(mysql_stmt_bind_result(stmt, &res) == 0,
+                        "Failed to bind result: %s", mysql_stmt_error(stmt));
+
+            while (mysql_stmt_fetch(stmt) == 0)
+            {
+            }
+        }
+
+        mysql_stmt_close(stmt);
+    }
+}
+
+std::string to_sql(std::string_view str)
+{
+    std::string rval = PS_QUERY;
+
+    auto pos = rval.find('?');
+
+    while (pos != std::string::npos)
+    {
+        rval.replace(pos, 1, str);
+        pos = rval.find('?');
+    }
+
+    return rval;
+}
+
 void query(TestConnections& test, const std::vector<std::string>& queries)
 {
     auto c = test.maxscale->rwsplit();
@@ -146,6 +270,103 @@ void test_source_matching(TestConnections& test)
     });
 }
 
+void test_prepared_statements(TestConnections& test)
+{
+    test.check_maxctrl("alter filter QLA log_type=unified filebase=/tmp/qla.log.ps log_data=query");
+    test.maxscale->restart();
+
+    auto c = test.maxscale->rwsplit();
+    c.connect();
+    c.query("CREATE OR REPLACE TABLE test.t1(id INT) AS SELECT 1 id");
+
+    std::vector<std::string> queries;
+
+    execute_with_param(test, int_param<uint8_t>(MYSQL_TYPE_TINY, true, 1));
+    queries.push_back(to_sql("1"));
+
+    execute_with_param(test, int_param<int8_t>(MYSQL_TYPE_TINY, false, 2));
+    queries.push_back(to_sql("2"));
+
+    execute_with_param(test, int_param<uint16_t>(MYSQL_TYPE_SHORT, true, 3));
+    queries.push_back(to_sql("3"));
+
+    execute_with_param(test, int_param<int16_t>(MYSQL_TYPE_SHORT, false, 4));
+    queries.push_back(to_sql("4"));
+
+    execute_with_param(test, int_param<uint32_t>(MYSQL_TYPE_LONG, true, 5));
+    queries.push_back(to_sql("5"));
+
+    execute_with_param(test, int_param<int32_t>(MYSQL_TYPE_LONG, false, 6));
+    queries.push_back(to_sql("6"));
+
+    execute_with_param(test, int_param<uint64_t>(MYSQL_TYPE_LONGLONG, true, 7));
+    queries.push_back(to_sql("7"));
+
+    execute_with_param(test, int_param<int64_t>(MYSQL_TYPE_LONGLONG, false, 8));
+    queries.push_back(to_sql("8"));
+
+    execute_with_param(test, str_param("hello world!"));
+    queries.push_back(to_sql("'hello world!'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_DATETIME, 2023, 12, 24, 13, 14, 15, 0));
+    queries.push_back(to_sql("'2023-12-24 13:14:15'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_DATETIME, 0, 0, 0, 0, 0, 0, 0));
+    queries.push_back(to_sql("'0000-00-00 00:00:00'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_DATETIME, 2023, 12, 24, 13, 14, 15, 1617));
+    queries.push_back(to_sql("'2023-12-24 13:14:15.001617'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_DATE, 2023, 12, 24, 13, 14, 15, 0));
+    queries.push_back(to_sql("'2023-12-24'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_DATE, 0, 0, 0, 0, 0, 0, 0));
+    queries.push_back(to_sql("'0000-00-00 00:00:00'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 13, 14, 15, 0));
+    queries.push_back(to_sql("'13:14:15'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 0, 0, 0, 0));
+    queries.push_back(to_sql("'00:00:00'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 13, 14, 15, 1617));
+    queries.push_back(to_sql("'13:14:15.001617'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 13, 14, 15, 0, NEGATIVE));
+    queries.push_back(to_sql("'-13:14:15'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 0, 0, 0, 0, NEGATIVE));
+    queries.push_back(to_sql("'00:00:00'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 123, 14, 15, 0));
+    queries.push_back(to_sql("'123:14:15'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 123, 14, 15, 0, NEGATIVE));
+    queries.push_back(to_sql("'-123:14:15'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 123, 14, 15, 1617));
+    queries.push_back(to_sql("'123:14:15.001617'"));
+
+    execute_with_param(test, time_param(MYSQL_TYPE_TIME, 0, 0, 0, 123, 14, 15, 1617, NEGATIVE));
+    queries.push_back(to_sql("'-123:14:15.001617'"));
+
+    std::vector<std::tuple<int, int, std::string>> expected_content;
+    expected_content.emplace_back(1, 0, "CREATE OR REPLACE TABLE test.t1(id INT) AS SELECT 1 id");
+    int row = 2;
+
+    for (std::string exec_query : queries)
+    {
+        // The log will contain the COM_STMT_PREPARE and two executions of COM_STMT_EXECUTE
+        expected_content.emplace_back(row++, 0, PS_QUERY);
+        expected_content.emplace_back(row++, 0, exec_query);
+        expected_content.emplace_back(row++, 0, exec_query);
+    }
+
+    check_contents(test, "/tmp/qla.log.ps.unified", expected_content);
+
+    c.query("DROP TABLE test.t1");
+}
+
 int main(int argc, char** argv)
 {
     TestConnections test(argc, argv);
@@ -243,6 +464,9 @@ int main(int argc, char** argv)
     {
         test.add_failure("Failed to parse reply time: %s", e.what());
     }
+
+    test.log_printf("Test prepared statements");
+    test_prepared_statements(test);
 
     test.log_printf("Test user_match and user_exclude");
     test_user_matching(test);
