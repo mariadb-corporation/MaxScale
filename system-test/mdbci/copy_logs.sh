5d0aa2b03471d0e99be847aab2385436a9ed4e3e
Timofey Turenko <timofey.turenko@mariadb.com>
2024-02-09 17:41:34 +0200

    copy MDBCI json to logs server

--- system-test/mdbci/copy_logs.sh
+++ system-test/mdbci/copy_logs.sh
@@ -17,4 +17,4 @@ rsync -a --no-o --no-g LOGS ${logs_publish_dir}
 chmod a+r ${logs_publish_dir}/*
 cp -r ${MDBCI_VM_PATH}/${mdbci_config_name} ${logs_publish_dir}
 cp  ${MDBCI_VM_PATH}/${mdbci_config_name}.json ${logs_publish_dir}
-cp $HOME/$mdbci_config_name}_vms/*.json ${logs_publish_dir}
+cp $HOME/${mdbci_config_name}_vms/*.json ${logs_publish_dir}
