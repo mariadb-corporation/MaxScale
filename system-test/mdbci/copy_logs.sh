0a03f5d5f7c70a55c2f552196af45acc5995245f
Timofey Turenko <timofey.turenko@mariadb.com>
2024-02-10 00:55:56 +0200

    copy MDBCI config to LOGS

--- system-test/mdbci/copy_logs.sh
+++ system-test/mdbci/copy_logs.sh
@@ -13,7 +13,8 @@
 #
 
 set -x
+cp  ${MDBCI_VM_PATH}/${mdbci_config_name}.json LOGS/
+cp -r ${MDBCI_VM_PATH}/${mdbci_config_name} LOGS/
+rm -rf LOGS/${mdbci_config_name}/*.pem
 rsync -a --no-o --no-g LOGS ${logs_publish_dir}
 chmod a+r ${logs_publish_dir}/*
-cp -r ${MDBCI_VM_PATH}/${mdbci_config_name} ${logs_publish_dir}
-cp  ${MDBCI_VM_PATH}/${mdbci_config_name}.json ${logs_publish_dir}
