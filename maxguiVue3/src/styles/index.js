e27435ba451395308af68657275afad48306d26b
Thien Ly <thien.ly@mariadb.com>
2024-03-28 09:24:41 +0200

    Delete `maxguiVue3`
    The migration is aiming for 24.08 not 24.02 so the entire
    dir should be deleted.

--- maxguiVue3/src/styles/index.js
+++
@@ -1,1 +0,0 @@
-import.meta.glob('@/styles/[A-Za-z0-9,\\s]+.scss', { eager: true })
--- maxguiVue3/src/views/NotFoundView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="fill-height mt-n10 d-flex flex-column justify-center align-center">
-    <VIcon color="#E5E1E5" icon="mxs:notFound" size="96px" />
-    <h1 class="text-h1 font-weight-medium">{{ $t('notFoundErr') }}</h1>
-    <p style="font-size: 2.125rem" class="font-weight-light">{{ $t('notFoundErrMsg') }}</p>
-  </div>
-</template>
