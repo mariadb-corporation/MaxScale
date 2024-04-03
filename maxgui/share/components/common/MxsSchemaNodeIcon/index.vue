b8b87f735bdf7bafa863f1f1f912ef0d3ece30e6
Johan Wikman <johan.wikman@mariadb.com>
2024-04-03 09:29:29 +0300

    24.02.1 Update change date

--- maxgui/share/components/common/MxsSchemaNodeIcon/index.vue
+++ maxgui/share/components/common/MxsSchemaNodeIcon/index.vue
@@ -11,7 +11,7 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
  *
- * Change Date: 2028-02-27
+ * Change Date: 2028-04-03
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2 or later of the General
