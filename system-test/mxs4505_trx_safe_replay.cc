93a5d1bd713deee3df1d03fd5b7775eb7406523f
Markus Mäkelä <markus.makela@mariadb.com>
2024-05-03 16:40:04 +0300

    Fix mxs4505_trx_safe_replay
    Now that the sessions are marked dead when a KILL arrives, the test
    needs to be updated to bypass the KILL handling in MaxScale.

--- system-test/mxs4505_trx_safe_replay.cc
+++ system-test/mxs4505_trx_safe_replay.cc
@@ -17,7 +17,15 @@ bool do_test(TestConnections& test)
 {
     auto c1 = test.maxscale->rwsplit();
     auto c2 = test.maxscale->rwsplit();
-    test.expect(c1.connect() && c2.connect(), "Connections failed: %s%s", c1.error(), c2.error());
+    auto r1 = test.repl->get_connection(0);
+    test.expect(c1.connect() && c2.connect() && r1.connect(),
+                "Connections failed: %s%s%s", c1.error(), c2.error(), r1.error());
+
+    // Get the real connection ID on the master. We'll need to bypass the KILL handling in MaxScale to make
+    // sure the transaction replay takes place. Normally, a KILL will disable transaction replay to prevent
+    // the killed query from being attempted again.
+    auto c2_id = c2.field("SELECT CONNECTION_ID(), @@last_insert_id");
+    test.expect(!c2_id.empty(), "CONNECTION_ID() returned an empty result");
 
     test.log_printf("Create a table on one connection");
     c1.query("CREATE TABLE test.t1(id INT)");
@@ -33,7 +41,7 @@ bool do_test(TestConnections& test)
     c2.send_query("COMMIT");
 
     test.log_printf("KILL the second connection and unlock tables");
-    c1.query("KILL " + std::to_string(c2.thread_id()));
+    test.expect(r1.query("KILL " + c2_id), "KILL should succeed: %s", r1.error());
     c1.query("UNLOCK TABLES");
 
     test.log_printf("Read the result of the COMMIT");
