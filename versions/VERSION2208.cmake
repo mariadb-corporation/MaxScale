60174092a709a2e6518f3ee58e8c9e8aba6b798b
Johan Wikman <johan.wikman@mariadb.com>
2024-05-23 11:56:17 +0300

    22.08 Bump maintenance version

--- versions/VERSION2208.cmake
+++ versions/VERSION2208.cmake
@@ -5,7 +5,7 @@
 
 set(MAXSCALE_VERSION_MAJOR "22" CACHE STRING "Major version")
 set(MAXSCALE_VERSION_MINOR "08" CACHE STRING "Minor version")
-set(MAXSCALE_VERSION_PATCH "13" CACHE STRING "Patch version")
+set(MAXSCALE_VERSION_PATCH "14" CACHE STRING "Patch version")
 
 # Used in version.hh.in, no leading 0.
 set(MAXSCALE_VERSION_MINOR_NUM "8" CACHE STRING "Minor version")
