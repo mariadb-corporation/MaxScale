cc77b33a68708cf18996a6f6b81818473635a226
Thien Ly <thien.ly@mariadb.com>
2024-07-08 10:49:30 +0300

    MXS-5166 Add `QueryResultTabWrapper` component
    This wrapper component renders `DataTable` and `ResInfoBar`
    components, reducing repeated code.

---
+++ maxgui/src/components/workspace/worksheets/QueryEditor/QueryResultTabWrapper.vue
@@ -0,0 +1,62 @@
+<script setup>
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2029-02-28
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+import ResInfoBar from '@wkeComps/QueryEditor/ResInfoBar.vue'
+
+const props = defineProps({
+  dim: { type: Object, required: true },
+  isLoading: { type: Boolean, default: false },
+  showFooter: { type: Boolean, required: true },
+  resInfoBarProps: { type: Object, default: () => ({}) },
+})
+
+const typy = useTypy()
+
+const INFO_BAR_HEIGHT = 24
+
+const tblDim = computed(() => ({
+  width: props.dim.width - 40, //  Minus 40px which is the horizontal padding px-5
+  height: typy(props, 'dim.height').safeNumber - 8 - (props.showFooter ? INFO_BAR_HEIGHT : 0), // minus 8px to have a vertical divider space
+}))
+</script>
+
+<template>
+  <div class="query-result-tab-wrapper pos--relative">
+    <div class="pos--relative px-5">
+      <VProgressLinear
+        v-if="isLoading"
+        indeterminate
+        color="primary"
+        absolute
+        class="progress-loading-indicator"
+      />
+      <slot v-else :tblDim="tblDim" />
+    </div>
+    <ResInfoBar
+      v-if="showFooter"
+      :isLoading="isLoading"
+      :height="INFO_BAR_HEIGHT"
+      v-bind="resInfoBarProps"
+      class="res-info-bar px-5 pos--absolute"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.progress-loading-indicator {
+  top: 20px !important;
+}
+.res-info-bar {
+  bottom: 0;
+}
+</style>
---
+++ maxgui/src/components/workspace/worksheets/QueryEditor/__tests__/QueryResultTabWrapper.spec.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023 MariaDB plc
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2029-02-28
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ *  Public License.
+ */
+import mount from '@/tests/mount'
+import { find } from '@/tests/utils'
+import QueryResultTabWrapper from '@wkeComps/QueryEditor/QueryResultTabWrapper.vue'
+import { lodash } from '@/utils/helpers'
+
+const mountFactory = (opts) =>
+  mount(
+    QueryResultTabWrapper,
+    lodash.merge(
+      { shallow: false, props: { dim: { height: 800, width: 600 }, showFooter: false } },
+      opts
+    )
+  )
+
+describe('QueryResultTabWrapper', () => {
+  let wrapper
+
+  const isLoadingTestCases = [true, false]
+
+  isLoadingTestCases.forEach((v) => {
+    describe(`When isLoading is ${v}`, () => {
+      beforeEach(
+        () =>
+          (wrapper = mountFactory({
+            props: { isLoading: v },
+            slots: {
+              default: `<div data-test="def-slot-content"></div>`,
+            },
+          }))
+      )
+      it(`Should ${v ? '' : 'not '}render loading indicator`, () => {
+        expect(wrapper.findComponent({ name: 'VProgressLinear' }).exists()).toBe(v)
+      })
+      it(`Should ${v ? 'not ' : ''}render default slot`, () => {
+        expect(find(wrapper, 'def-slot-content').exists()).toBe(!v)
+      })
+    })
+  })
+
+  const showFooterTestCases = [true, false]
+  showFooterTestCases.forEach((v) =>
+    describe(`When showFooter is ${v}`, () => {
+      it(`Should ${v ? '' : 'not '}render DurationTimer`, () => {
+        wrapper = mountFactory({ props: { showFooter: v, resInfoBarProps: { result: {} } } })
+        expect(wrapper.findComponent({ name: 'ResInfoBar' }).exists()).toBe(v)
+      })
+    })
+  )
+})
