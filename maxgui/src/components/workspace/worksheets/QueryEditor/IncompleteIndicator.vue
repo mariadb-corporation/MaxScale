dea1a85b80174e0bf6710a45207db417caf63725
Thien Ly <thien.ly@mariadb.com>
2024-07-16 12:46:03 +0300

    MXS-5166 Adjust `IncompleteIndicator` message
    This tooltip message of this indicator is no longer accurate because
    `SELECT` query is now automatically injected with a `LIMIT` clause.
    Therefore, the message type should be changed from a warning type
    to an info type.

--- maxgui/src/components/workspace/worksheets/QueryEditor/IncompleteIndicator.vue
+++ maxgui/src/components/workspace/worksheets/QueryEditor/IncompleteIndicator.vue
@@ -13,19 +13,31 @@
  */
 defineOptions({ inheritAttrs: false })
 defineProps({ result: { type: Object, required: true } })
+
+const store = useStore()
+const query_row_limit = computed(() => store.state.prefAndStorage.query_row_limit)
 </script>
 
 <template>
   <VTooltip v-if="$typy(result, 'fields').isDefined && !result.complete" location="top">
     <template #activator="{ props }">
       <div
-        class="cursor--pointer d-flex align-center text-warning"
+        class="cursor--pointer d-flex align-center text-primary"
         v-bind="{ ...props, ...$attrs }"
       >
-        <VIcon size="16" color="warning" class="mr-1" icon="mxs:alertWarning" />
+        <VIcon size="16" color="primary" class="mr-1" icon="$mdiInformationOutline" />
         {{ $t('incomplete') }}
       </div>
     </template>
-    {{ $t('info.queryIncomplete') }}
+    {{
+      $t('info.incompleteResultSet', [
+        /**
+         * statement has limit field only when it is a select statement.
+         * However, sql_select_limit actually applies to any statement that
+         * returns result sets. In that case, query_row_limit will be used.
+         */
+        $typy(result, 'statement.limit').safeNumber || query_row_limit,
+      ])
+    }}
   </VTooltip>
 </template>
