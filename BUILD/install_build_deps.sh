42aa35e485f1e912662fe744364404b147019ec7
Markus Mäkelä <markus.makela@mariadb.com>
2024-08-12 11:06:15 +0300

    Use system pcre2
    PCRE2 is available on all supported operating systems and thus bundling
    it is no longer necessary.

--- BUILD/install_build_deps.sh
+++ BUILD/install_build_deps.sh
@@ -77,7 +77,7 @@ then
        perl libtool tcl tcl-dev uuid \
        uuid-dev libsqlite3-dev liblzma-dev libpam0g-dev pkg-config \
        libedit-dev libcurl4-openssl-dev libatomic1 \
-       libsasl2-dev libxml2-dev libkrb5-dev libicu-dev gnutls-dev libgcrypt-dev
+       libsasl2-dev libxml2-dev libkrb5-dev libicu-dev gnutls-dev libgcrypt-dev libpcre2-dev
 
   # One of these will work, older systems use libsystemd-daemon-dev
   ${apt_cmd} install libsystemd-dev || \
@@ -119,7 +119,7 @@ then
          gnupg flex rpmdevtools git wget tcl tcl-devel openssl libuuid-devel xz-devel \
          sqlite sqlite-devel pkgconfig rpm-build createrepo yum-utils \
          gnutls-devel libgcrypt-devel pam-devel libcurl-devel libatomic \
-         cyrus-sasl-devel libxml2-devel krb5-devel libicu-devel systemd-devel
+         cyrus-sasl-devel libxml2-devel krb5-devel libicu-devel systemd-devel pcre2-devel
 
     sudo yum install -d1 -y --nogpgcheck ${enable_power_tools} lua lua-devel libedit-devel
 
@@ -175,7 +175,7 @@ then
          git wget tcl tcl-devel libuuid-devel \
          xz-devel sqlite3 sqlite3-devel pkg-config lua lua-devel \
          gnutls-devel libgcrypt-devel pam-devel systemd-devel libcurl-devel libatomic1 \
-         cyrus-sasl-devel libxml2-devel krb5-devel libicu-devel
+         cyrus-sasl-devel libxml2-devel krb5-devel libicu-devel pcre2-devel
     sudo zypper -n install rpm-build
 
     if is_arm
