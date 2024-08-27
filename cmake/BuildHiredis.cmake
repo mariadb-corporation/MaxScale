d20a08e108e844ae5b25a12f22f3956894ceb845
Markus Mäkelä <markus.makela@mariadb.com>
2024-08-27 10:14:50 +0300

    Add extra output for storages redis and memcached
    This way it'll be clear when they're built from source and when the
    system libraries are used.

--- cmake/BuildHiredis.cmake
+++ cmake/BuildHiredis.cmake
@@ -11,7 +11,7 @@
 set(HIREDIS_REPO "https://github.com/redis/hiredis.git" CACHE STRING "Hiredis Git repository")
 set(HIREDIS_TAG "v1.0.2" CACHE STRING "Hiredis Git tag")
 
-message(STATUS "Using hiredis version ${HIREDIS_VERSION}")
+message(STATUS "Building hiredis version ${HIREDIS_VERSION}")
 
 ExternalProject_add(hiredis
   GIT_REPOSITORY ${HIREDIS_REPO}
