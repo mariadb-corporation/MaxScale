3e1a96ca0646a09f42f22ff3fbdddbce36989080
Markus Mäkelä <markus.makela@mariadb.com>
2024-08-28 14:43:59 +0300

    MXS-5227: Drop supplementary groups when changing UID
    Rpmlint warns that MaxScale does not drop all of the groups that it
    belongs to when it sets the UID and GID. Calling initgroups() should
    take care of this and make sure that the set of supplementary groups is
    correct for the user in question.

--- server/core/gateway.cc
+++ server/core/gateway.cc
@@ -39,6 +39,7 @@
 #include <ini.h>
 #include <openssl/opensslconf.h>
 #include <pwd.h>
+#include <grp.h>
 #include <sys/file.h>
 #include <sys/prctl.h>
 #include <sys/stat.h>
@@ -2886,6 +2887,14 @@ static int set_user(const char* user)
                errno,
                errno == 0 ? "User not found" : mxb_strerror(errno));
         return -1;
+    }
+
+    rval = initgroups(user, pwname->pw_gid);
+    if (rval != 0)
+    {
+        printf("Error: Failed to initialize groups for '%s: %d %s\n",
+               user,  errno, mxb_strerror(errno));
+        return rval;
     }
 
     rval = setgid(pwname->pw_gid);
