ea3d793bb9009b0b798f18e4e23439ada82c504a
Markus Mäkelä <markus.makela@mariadb.com>
2024-09-05 10:31:59 +0300

    MXS-5229: Use m_master for Master Stickiness
    The check for the master stickiness should use the currently selected
    master as the stickiness label indicator. This makes
    disable_master_failback work the same way for the normal and the
    use_priority use cases.

--- server/modules/monitor/galeramon/galeramon.cc
+++ server/modules/monitor/galeramon/galeramon.cc
@@ -136,7 +136,7 @@ json_t* GaleraMonitor::diagnostics(MonitorServer* server) const
             states.push_back(comment);
         }
 
-        if (m_disableMasterFailback && server->server->is_master() && it->second.local_index != 0)
+        if (m_disableMasterFailback && server->server->is_master() && server == m_master)
         {
             states.push_back("Master Stickiness");
         }
