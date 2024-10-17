4b9dc64fde5ba4f6c9fccc24e151d9b16ca5a3c9
Markus Mäkelä <markus.makela@mariadb.com>
2024-10-17 14:32:36 +0300

    MXS-5363: Write GDB output to a temporary file
    The problem with using popen() to communicate with GDB is that it uses a
    pipe and the pipe has a limited size before GDB's write gets
    blocked. The pipe is also never drained as MaxScale is stopped by GDB
    while it writes into the pipe. By redirecting GDB's output into a
    temporary file, this problem goes away.

--- maxutils/maxbase/src/stacktrace.cc
+++ maxutils/maxbase/src/stacktrace.cc
@@ -60,32 +60,6 @@ static void get_command_output(char* output, size_t size, const char* format, ..
     }
 }
 
-static void get_command_output_cb(void (* cb)(const char*), const char* format, ...)
-{
-    va_list valist;
-    va_start(valist, format);
-    int cmd_len = vsnprintf(NULL, 0, format, valist);
-    va_end(valist);
-
-    va_start(valist, format);
-    char cmd[cmd_len + 1];
-    vsnprintf(cmd, cmd_len + 1, format, valist);
-    va_end(valist);
-
-    if (FILE* file = popen(cmd, "r"))
-    {
-        char buf[512];
-
-        while (size_t n = fread(buf, 1, sizeof(buf) - 1, file))
-        {
-            buf[n] = '\0';
-            cb(buf);
-        }
-
-        pclose(file);
-    }
-}
-
 static void extract_file_and_line(char* symbols, char* cmd, size_t size)
 {
     const char* filename_end = strchr(symbols, '(');
@@ -238,12 +212,39 @@ void emergency_stacktrace(void (* handler)(const char*, const char*))
 
 void dump_gdb_stacktrace(void (* handler)(const char*))
 {
+    char path[] = "/tmp/maxscale-stacktrace-XXXXXX";
+    int fd = mkstemp(path);
+
+    if (fd == -1)
+    {
+        return;
+    }
+
+    char cmd[512];
+    sprintf(cmd,
+            "gdb --pid=%d -batch -nx "
+            "-iex 'set logging file %s' "
+            "-iex 'set logging redirect on' "
+            "-iex 'set logging enabled on' "
+            "-iex 'set auto-load off' "
+            "-iex 'set print thread-events off' "
+            "-ex 'info threads' -ex 'thr a a bt'",
+            getpid(), path);
+
     prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
-    get_command_output_cb(
-        handler,
-        "gdb --pid=%d -batch -nx -iex 'set auto-load off' -iex 'set print thread-events off' -ex 'info threads' -ex 'thr a a bt'",
-        getpid());
+    system(cmd);
     prctl(PR_SET_PTRACER, 0);
+
+    int rc;
+    // Reuse the command buffer for reading GDB output
+    while ((rc = read(fd, cmd, sizeof(cmd) - 1)) > 0)
+    {
+        cmd[rc] = 0;
+        handler(cmd);
+    }
+
+    close(fd);
+    unlink(path);
 }
 
 bool have_gdb()
