e1bf112f9292aaaa4be5a06be69407fccfd6b5a2
Markus Mäkelä <markus.makela@mariadb.com>
2025-01-21 12:11:02 +0200

    MXS-5450: Always set "command" field for queries
    Otherwise the binary protocol commands have no information in them.

--- server/core/session.cc
+++ server/core/session.cc
@@ -787,7 +787,7 @@ namespace
 
 void get_cmd_and_stmt(const GWBUF& buffer, const char** ppCmd, const char** ppStmt, int* pLen)
 {
-    *ppCmd = nullptr;
+    *ppCmd = STRPACKETTYPE(mxs_mysql_get_command(&buffer));
     *ppStmt = nullptr;
     *pLen = 0;
 
@@ -795,8 +795,6 @@ void get_cmd_and_stmt(const GWBUF& buffer, const char** ppCmd, const char** ppSt
 
     if (!sql.empty())
     {
-        auto cmd = mxs_mysql_get_command(&buffer);
-        *ppCmd = STRPACKETTYPE(cmd);
         *ppStmt = sql.c_str();
         *pLen = sql.length();
     }
