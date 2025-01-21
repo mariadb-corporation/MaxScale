cca3438418487958c0764631819e1f980333a705
Markus Mäkelä <markus.makela@mariadb.com>
2025-01-21 09:58:49 +0200

    MXS-5449: Add test case
    Added a test case that checks that the encrypted passwords are used
    correctly.

--- system-test/encrypted_passwords.cc
+++ system-test/encrypted_passwords.cc
@@ -64,10 +64,71 @@ void hash_password(TestConnections& test)
     test.expect(test.check_maxscale_alive() == 0, "MaxScale is not alive");
 }
 
+void encrypted_password_in_maxctrl(TestConnections& test)
+{
+    auto command = [&](std::string cmd, bool ok){
+        int rc = test.maxscale->ssh_node_f(false, "%s", cmd.c_str());
+        test.expect((rc == 0) == ok, "Command %s: %s", ok ? "failed" : "succeeded", cmd.c_str());
+    };
+
+    auto command_ok = [&](std::string cmd){
+        command(cmd, true);
+    };
+
+    auto command_err = [&](std::string cmd){
+        command(cmd, false);
+    };
+
+    test.tprintf("MXS-5449: Encrypted passwords in MaxCtrl");
+
+    const char* user = test.maxscale->access_user();
+    const char* secretsdir = test.maxscale->access_homedir();
+
+    test.maxscale->ssh_node_f(true,
+                              "cp /var/lib/maxscale/.secrets %s/.secrets;"
+                              "chown %s %s/.secrets",
+                              secretsdir, user, secretsdir);
+
+    test.maxctrl("create user foobar foobar");
+    auto enc = test.maxscale->ssh_output("maxpasswd "s + secretsdir + " foobar").output;
+
+    test.maxscale->ssh_node_f(true,
+                              "echo '[maxctrl]' > /tmp/maxctrl-plaintext.cnf;"
+                              "echo 'user=foobar' >> /tmp/maxctrl-plaintext.cnf;"
+                              "echo 'password=foobar' >> /tmp/maxctrl-plaintext.cnf;"
+                              "echo '[maxctrl]' > /tmp/maxctrl-encrypted.cnf;"
+                              "echo 'user=foobar' >> /tmp/maxctrl-encrypted.cnf;"
+                              "echo 'password=%s' >> /tmp/maxctrl-encrypted.cnf;"
+                              "echo 'secretsdir=%s' >> /tmp/maxctrl-encrypted.cnf;"
+                              "chmod 0600 /tmp/maxctrl-plaintext.cnf /tmp/maxctrl-encrypted.cnf;"
+                              "chown %s /tmp/maxctrl-plaintext.cnf /tmp/maxctrl-encrypted.cnf;",
+                              enc.c_str(), secretsdir, user);
+
+    command_ok("maxctrl --user=foobar --password=foobar list sessions");
+    command_ok("maxctrl -c /tmp/maxctrl-plaintext.cnf list sessions");
+    command_ok("maxctrl --user=foobar --password=" + enc + " --secretsdir=" + secretsdir + " list sessions");
+    command_ok(
+        "echo " + enc + "|maxctrl --user=foobar --password='' --secretsdir=" + secretsdir + " list sessions");
+    command_ok("maxctrl -c /tmp/maxctrl-encrypted.cnf list sessions");
+
+    test.maxscale->ssh_node_f(true, "rm %s/.secrets", secretsdir);
+
+    command_ok("maxctrl --user=foobar --password=foobar list sessions");
+    command_ok("maxctrl -c /tmp/maxctrl-plaintext.cnf list sessions");
+    command_err("maxctrl --user=foobar --password=" + enc + " --secretsdir=" + secretsdir + " list sessions");
+    command_err(
+        "echo " + enc + "|maxctrl --user=foobar --password='' --secretsdir=" + secretsdir + " list sessions");
+    command_err("maxctrl -c /tmp/maxctrl-encrypted.cnf list sessions");
+
+    test.maxctrl("destroy user foobar");
+    test.maxscale->ssh_node_f(true, "rm /tmp/maxctrl-plaintext.cnf /tmp/maxctrl-encrypted.cnf");
+}
+
 void test_main(TestConnections& test)
 {
     create_key(test);
     hash_password(test);
+    encrypted_password_in_maxctrl(test);
 }
 
 int main(int argc, char* argv[])
