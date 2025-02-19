02f0415f7dea2512d9956bb85b56fc2fbc725a59
Markus Mäkelä <markus.makela@mariadb.com>
2025-02-19 21:39:35 +0200

    Replace fallthrough comments with [[fallthrough]]
    A couple of places still used them when most others already had switched
    to C++17 attributes.

--- server/modules/filter/cache/cachefiltersession.cc
+++ server/modules/filter/cache/cachefiltersession.cc
@@ -1082,8 +1082,8 @@ CacheFilterSession::cache_action_t CacheFilterSession::get_cache_action(GWBUF* p
                             }
                         }
                     }
+                [[fallthrough]];
 
-                /* FALLTHROUGH */
                 case StatementType::UNKNOWN:
                     // A bit broad, as e.g. SHOW will cause the read only state to be turned
                     // off. However, during normal use this will always be an UPDATE, INSERT
