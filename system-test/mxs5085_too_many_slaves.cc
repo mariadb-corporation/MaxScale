2a899f76f4ce48317b3b85f17500e884c7ae136f
Markus Mäkelä <markus.makela@mariadb.com>
2025-02-26 12:26:58 +0200

    MXS-5529: Route as write with max_slave_connections=0
    Treating everying as a write when max_slave_connections=0 is used will
    cause the old connections to be discarded immediately. The session write
    handling is also skipped which further simplifies this mode of
    operation.
    
    Added a test case that reproduces the problem and verifies that the new
    behavior discards stale connections when session commands are executed.

--- system-test/mxs5085_too_many_slaves.cc
+++ system-test/mxs5085_too_many_slaves.cc
@@ -44,6 +44,15 @@ void test_mxs5209(TestConnections& test)
                 "Query should not be routed to the same server after master changes: %s",
                 mxb::join(first_id).c_str());
 
+    test.check_maxctrl("clear server server2 master");
+    test.check_maxctrl("set server server2 slave");
+    test.check_maxctrl("clear server server1 slave");
+    test.check_maxctrl("set server server1 master");
+    std::cout << test.maxctrl("list servers").output << std::endl;
+
+    c.query("SET @a=1");
+    test.log_includes("Replacing old master 'server2' with new master 'server1'");
+
     test.check_maxctrl("start monitor MariaDB-Monitor");
 }
 
