c04ad7a5bb231eed1a3367ad6111e10b2f43ca27
Markus Mäkelä <markus.makela@mariadb.com>
2025-03-02 09:04:19 +0200

    MXS-5533: Set log scope during configuration
    The scope in which any warnings or errors are logged by generic code is
    then much clearer. For example, without this log scope the use of the
    session_trace parameter in services would log a warning about
    session_trace being deprecated when in fact it's only deprecated when
    used in services.

--- server/core/config.cc
+++ server/core/config.cc
@@ -2681,6 +2681,7 @@ static bool process_config_context(ConfigSectionMap& context)
      */
     for (ConfigSection* obj : objects)
     {
+        mxb::LogScope scope(obj->name());
         std::string type = obj->m_parameters.get_string(CN_TYPE);
         mxb_assert(!type.empty());
 
