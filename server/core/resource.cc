ca7e250fc5b848fe7d4be905c64fbdfcdcd61b08
Markus Mäkelä <markus.makela@mariadb.com>
2025-06-24 09:28:15 +0300

    MXS-5673: Make config sync modify everything
    Whenever the version of the ConfigManager changes, clear out all the
    stored paths and reset the timestamps. This solves the problem without
    having to tightly couple the ConfigManager with the REST-API mechanisms.

--- server/core/resource.cc
+++ server/core/resource.cc
@@ -286,6 +286,12 @@ public:
         return res;
     }
 
+    void clear()
+    {
+        m_init = time(nullptr);
+        m_last_modified.clear();
+    }
+
 private:
     time_t                       m_init;
     map<string, WatchedResource> m_last_modified;
@@ -1657,6 +1663,7 @@ struct ThisUnit
 {
     RootResource    resources;          /**< Core resource set */
     ResourceWatcher watcher;            /**< Modification watcher */
+    int64_t         config_sync_version = 0;
 };
 
 ThisUnit this_unit;
@@ -1801,6 +1808,15 @@ static HttpResponse handle_request(const HttpRequest& request)
 
     HttpResponse rval;
 
+    auto manager = mxs::ConfigManager::get();
+    mxb_assert(manager);
+
+    if (manager->version() != this_unit.config_sync_version)
+    {
+        this_unit.watcher.clear();
+        this_unit.config_sync_version = manager->version();
+    }
+
     if (!request_precondition_met(request, rval))
     {
         return rval;
@@ -1826,9 +1842,6 @@ static HttpResponse handle_request(const HttpRequest& request)
             return HttpResponse(MHD_HTTP_FORBIDDEN, mxs_json_error("Missing request body"));
         }
     }
-
-    auto manager = mxs::ConfigManager::get();
-    mxb_assert(manager);
 
     if (requires_sync && !skip_sync && !manager->start())
     {
