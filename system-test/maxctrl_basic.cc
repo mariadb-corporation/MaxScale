2d71a8fe195eab5772c5fe2639a56ac557ac256d
Markus Mäkelä <markus.makela@mariadb.com>
2025-09-12 13:48:59 +0300

    MXS-5947: Add `create report` test case
    The `create report` command is now checked in the system tests as
    well. The test case also reproduces the problem where the command fails
    if the maxscale user is not in the systemd-journal group.

--- system-test/maxctrl_basic.cc
+++ system-test/maxctrl_basic.cc
@@ -145,6 +145,12 @@ int main(int argc, char** argv)
     res = test.maxctrl("alter listener RW-Split-Listener protocol=cdc");
     test.expect(res.rc != 0, "Changing listener protocol should fail.");
 
+    test.tprintf("MXS-5947: 'create report' broken with maxlog=false syslog=true");
+    test.check_maxctrl("create report");
+    test.check_maxctrl("create report /tmp/maxctrl-report.txt");
+    test.check_maxctrl("create report --archive /tmp/maxctrl-report.tar");
+    test.log_includes("Failed to read any data from the systemd journal");
+
     test.check_maxscale_alive();
     return test.global_result;
 }
